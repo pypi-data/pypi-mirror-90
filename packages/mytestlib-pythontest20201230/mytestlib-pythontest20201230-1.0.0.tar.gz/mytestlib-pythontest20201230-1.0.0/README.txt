26e1083729aec85b4017137461c00b0379265408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package mytestlib-pythontest20201230/1.0.0/mytestlib-pythontest20201230-1.0.0.tar.gz

---
+++ packages/mytestlib-pythontest20201230/mytestlib-pythontest20201230-1.0.0.tar.gz/mytestlib-pythontest20201230-1.0.0/README.txt
@@ -0,0 +1,1 @@
+This is My Test Library.20201230
---
+++ packages/mytestlib-pythontest20201230/mytestlib-pythontest20201230-1.0.0.tar.gz/mytestlib-pythontest20201230-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-pythontest20201230',
+    version='1.0.0',
+    # url='…',
+    author='ikegami',
+    author_email='takanori.ikegami@gmail.com',
+    description='My Test Library.',
+    packages=find_packages()
+)
