d434d52cf2411b33412b5a7f6ca59fae42a7e608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:09 +0000

    Add package smok/0.5b2/smok-0.5b2.tar.gz

---
+++ packages/smok/smok-0.5b2.tar.gz/smok-0.5b2/smok/basics.py
@@ -0,0 +1,93 @@
+import enum
+import typing as tp
+
+from satella.coding.structures import ReprableMixin
+from satella.json import JSONAble
+
+
+class Environment(enum.IntEnum):
+    """
+    An environment in which this device runs
+    """
+    PRODUCTION = 0  #: production
+    STAGING = 1  #: testing environment
+    LOCAL_DEVELOPMENT = 2  #: CI or local development
+
+
+class StorageLevel(enum.IntEnum):
+    """
+    A storage level defines how long is the pathpoint kept at SMOK server.
+    """
+    PERMANENT = 0  #: hold all values indefinitely
+    TREND = 1  #: values at most 2 weeks old will be kept
+
+
+class SlaveDeviceInfo(JSONAble, ReprableMixin):
+    """
+    Information about a slave device attached to primary device
+
+    :ivar device_id: slave device ID
+    :ivar master_controller: ID of master device
+    :ivar responsible_service: service responsible for this device, mostly "rapid"
+    :ivar configuration: a string containing configuration for this device
+    """
+    _REPR_FIELDS = ('device_id', 'master_controller', 'responsible_service',
+                    'configuration')
+    __slots__ = ('device_id', 'master_controller', 'responsible_service',
+                 'configuration')
+
+    def __init__(self, device_id: str, master_controller: str, responsible_service: str,
+                 configuration: str):
+        self.device_id = device_id
+        self.master_controller = master_controller
+        self.responsible_service = responsible_service
+        self.configuration = configuration
+
+    @classmethod
+    def from_json(cls, dct: dict) -> 'SlaveDeviceInfo':
+        return SlaveDeviceInfo(dct['device_id'], dct['master_controller'],
+                               dct['responsible_service'], dct['configuration'])
+
+    def to_json(self) -> dict:
+        return {
+            'device_id': self.device_id,
+            'configuration': self.configuration,
+            'master_controller': self.master_controller,
+            'responsible_service': self.responsible_service
+        }
+
+
+class DeviceInfo(ReprableMixin):
+    """
+    A class holding device information
+
+    :ivar device_id: device ID of the device
+    :ivar slaves: list of :class:`smokclient.basics.SlaveDeviceInfo` containing info
+        about the slave devices
+    :ivar facets: a set of strings, contains interfaces that access to this device is allowed for
+    :ivar timezone: local timezone of this device, in accordance with tzdata
+    :ivar language: language of this device, according to ISO639-1
+    :ivar units: either `metric` or `imperial`, units used on this device
+    :ivar verbose_name: human-readable name of this device
+    """
+    _REPR_FIELDS = ('device_id', 'facets', 'language', 'timezone', 'units',
+                    'slaves')
+    __slots__ = ('device_id', 'slaves', 'facets', 'language', 'timezone', 'units',
+                 'verbose_name')
+
+    def __init__(self, device_id: str, facets: tp.Set[str], language: str, timezone: str,
+                 units: str, verbose_name: str, slaves: tp.List[SlaveDeviceInfo]):
+        self.device_id = device_id
+        self.facets = facets
+        self.language = language
+        self.timezone = timezone
+        self.units = units
+        self.verbose_name = verbose_name
+        self.slaves = slaves
+
+    @classmethod
+    def from_json(cls, dct: dict) -> 'DeviceInfo':
+        ccon = dct['culture_context']
+        return DeviceInfo(dct['device_id'], set(dct['facets']), ccon['language'],
+                          ccon['timezone'], ccon['units'], dct['verbose_name'],
+                          [SlaveDeviceInfo.from_json(y) for y in dct['slave_devices']])
---
+++ packages/smok/smok-0.5b2.tar.gz/smok-0.5b2/smok/client/api.py
@@ -0,0 +1,62 @@
+import json
+
+import requests
+from satella.files import read_in_file
+
+from smok.basics import Environment
+from smok.exceptions import ResponseError
+
+
+class RequestsAPI:
+    __slots__ = ('environment', 'base_url', 'cert')
+
+    def __init__(self, device):
+        self.environment = device.environment
+        self.base_url = device.url
+        if self.environment == Environment.STAGING:
+            self.cert = read_in_file(device.cert[0], 'utf-8').replace('\n', '\t')
+        else:
+            self.cert = device.cert
+
+    def request(self, request_type, url, **kwargs):
+        """
+        :raises ResponseError: something went wrong
+        """
+        op = getattr(requests, request_type)
+        if self.environment == Environment.STAGING:
+            resp = op(self.base_url + url, headers={
+                'X-SSL-Client-Certificate': self.cert
+            }, **kwargs)
+        else:
+            try:
+                resp = op(self.base_url + url, cert=self.cert, **kwargs)
+            except requests.RequestException as e:
+                raise ResponseError(None, 'Requests error: %s' % (str(e), ))
+        if resp.status_code not in (200, 201):
+            try:
+                error_json = resp.json()['status']
+            except (json.decoder.JSONDecodeError, ValueError):
+                try:
+                    error_json = resp.text
+                except AttributeError:
+                    error_json = repr(resp.content)
+            raise ResponseError(resp.status_code, error_json)
+        try:
+            return resp.json()
+        except json.decoder.JSONDecodeError:
+            raise ResponseError(resp.status_code, resp.content)
+
+    def get(self, url):
+        return self.request('get', url)
+
+    def post(self, url, **kwargs):
+        return self.request('post', url, **kwargs)
+
+    def put(self, url, **kwargs):
+        return self.request('put', url, **kwargs)
+
+    def patch(self, url, **kwargs):
+        return self.request('patch', url, **kwargs)
+
+    def delete(self, url, **kwargs):
+        return self.request('delete', url, **kwargs)
