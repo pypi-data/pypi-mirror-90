7faf527f19abae57eaf2d4807a4978571ab592f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:22 +0000

    Add package pylocalc/0.1.1/pylocalc-0.1.1.tar.gz

---
+++ packages/pylocalc/pylocalc-0.1.1.tar.gz/pylocalc-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pylocalc']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pylocalc',
+    'version': '0.1.1',
+    'description': 'Python interface for manipulating LibreOffice Calc spreadsheets',
+    'long_description': '# PyLOcalc\n',
+    'author': 'Viliam Valent',
+    'author_email': 'pylocalc@viliamvalent.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ViliamV/pylocalc',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
