d1973af99629d67e3a1b414e0d676e729ed2ad96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package nunavut/0.7.0/nunavut-0.7.0-51-py3-none-any.whl

---
+++ packages/nunavut/nunavut-0.7.0-51-py3-none-any.whl/nunavut/version.py
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2018-2020  UAVCAN Development Team  <uavcan.org>
+# This software is distributed under the terms of the MIT License.
+#
+
+__version__ = "0.7.0"
+
+__license__ = 'MIT'
