a90cd9b67eb0076018a86e3668fb84a88104eabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package staircase/1.6.1/staircase-1.6.1-py3-none-any.whl

---
+++ packages/staircase/staircase-1.6.1-py3-none-any.whl/staircase/__init__.py
@@ -0,0 +1,18 @@
+try:
+    from ._version import get_versions
+    __version__ = get_versions()['version']
+    del get_versions
+except Exception:
+    __version__ = ''
+
+from .stairs import (
+    Stairs, sample, resample,
+    aggregate, hist_from_ecdf,
+    corr, cov
+)
+from .stairs import _mean as mean
+from .stairs import _median as median
+from .stairs import _min as min
+from .stairs import _max as max
+
+from .test_data import make_test_data
