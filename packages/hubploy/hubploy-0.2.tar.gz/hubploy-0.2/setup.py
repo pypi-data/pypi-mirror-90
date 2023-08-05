6c3d3c4aee3d9dbbd87f8ee9661b7544653417d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package hubploy/0.2/hubploy-0.2.tar.gz

---
+++ packages/hubploy/hubploy-0.2.tar.gz/hubploy-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='hubploy',
+    version='0.2',
+    url="https://github.com/yuvipanda/hubploy",
+    author="Yuvi Panda",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'docker',
+        'jupyter-repo2docker>=0.11',
+        'kubernetes',
+        'boto3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'hubploy = hubploy.__main__:main',
+        ],
+    },
+
+)
