cd10f3a0ce92c0dfac374633f1469984ca5701cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package pycarwings2/2.10/pycarwings2-2.10-py3-none-any.whl

---
+++ packages/pycarwings2/pycarwings2-2.10-py3-none-any.whl/tests/test_pycarwings2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+import pycarwings2
+import pytest
+
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+
+
+def test_null_test():
+    print("Test is good")
+    pass
+
+#def test_bad_password():
+#    with pytest.raises(pycarwings2.responses.CarwingsError) as excinfo:
+#        s = pycarwings2.Session("user@domain.com", "password", "NE")
+#        leaf = s.get_leaf()
+#        if leaf is not None:
+#            pass
+#        assert 'INVALID' in str(excinfo.value)
