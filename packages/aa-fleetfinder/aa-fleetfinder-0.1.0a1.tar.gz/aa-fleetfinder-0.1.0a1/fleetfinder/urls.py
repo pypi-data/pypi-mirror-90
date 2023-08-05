9bfc92c85a47dbfe2b159fc84931a194a1ba465d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package aa-fleetfinder/0.1.0a1/aa-fleetfinder-0.1.0a1.tar.gz

---
+++ packages/aa-fleetfinder/aa-fleetfinder-0.1.0a1.tar.gz/aa-fleetfinder-0.1.0a1/fleetfinder/urls.py
@@ -0,0 +1,19 @@
+"""
+url config
+"""
+
+from django.conf.urls import url
+
+from fleetfinder import views
+
+
+app_name: str = "fleetfinder"
+
+urlpatterns = [
+    url(r"^$", views.dashboard, name="dashboard"),
+    url(r"^create/$", views.create_fleet, name="create_fleet"),
+    url(r"^save/$", views.save_fleet, name="save_fleet"),
+    url(r"^join/(?P<fleet_id>[0-9]+)$", views.join_fleet, name="join_fleet"),
+    url(r"^details/(?P<fleet_id>[0-9]+)$", views.fleet_details, name="fleet_details"),
+    url(r"^edit/(?P<fleet_id>[0-9]+)$", views.edit_fleet, name="edit_fleet"),
+]
