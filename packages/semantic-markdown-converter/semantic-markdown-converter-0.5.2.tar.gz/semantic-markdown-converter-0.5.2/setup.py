e360c21dc2a96552ac3eebe57012eac9a5d302ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package semantic-markdown-converter/0.5.2/semantic-markdown-converter-0.5.2.tar.gz

---
+++ packages/semantic-markdown-converter/semantic-markdown-converter-0.5.2.tar.gz/semantic-markdown-converter-0.5.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(name='semantic-markdown-converter',
+      version='0.5.2',
+      description='Converts different typed link formats in Markdown into each other and to external formats. Supports Obsidian Neo4j plugin.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/HEmile/semantic-markdown-converter',
+      packages=find_packages(),
+      install_requires=['pyyaml', 'tqdm', 'py2neo', 'watchdog',
+                        'markdown', 'mdx-wikilink-plus'],
+      entry_points={
+          'console_scripts': ['smdc=smdc.convert:main', 'smds=smdc.server:main']
+      },
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      author='Emile van Krieken',
+      author_email='emilevankrieken@live.nl',
+      license='MIT',
+      zip_safe=False,
+      python_requires='>=3.6',)
