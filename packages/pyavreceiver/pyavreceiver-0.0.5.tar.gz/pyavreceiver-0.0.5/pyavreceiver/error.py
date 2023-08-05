85bf9a6af7a3ca58eb609851fde33137b905e61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package pyavreceiver/0.0.5/pyavreceiver-0.0.5.tar.gz

---
+++ packages/pyavreceiver/pyavreceiver-0.0.5.tar.gz/pyavreceiver-0.0.5/pyavreceiver/error.py
@@ -0,0 +1,9 @@
+"""pyavreceiver errors."""
+
+
+class AVReceiverInvalidArgumentError(Exception):
+    """Invalid argument error."""
+
+
+class AVReceiverIncompatibleDeviceError(Exception):
+    """Invalid argument error."""
