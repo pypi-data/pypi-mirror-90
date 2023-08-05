33fbaa88b7027297f06c54093b48ef22f14ee769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package wheezy.captcha/3.0.1/wheezy.captcha-3.0.1.tar.gz

---
+++ packages/wheezy.captcha/wheezy.captcha-3.0.1.tar.gz/wheezy.captcha-3.0.1/README.md
@@ -0,0 +1,44 @@
+# wheezy.captcha
+
+[![Build Status](https://travis-ci.org/akornatskyy/wheezy.captcha.svg?branch=master)](https://travis-ci.org/akornatskyy/wheezy.captcha)
+[![Coverage Status](https://coveralls.io/repos/github/akornatskyy/wheezy.captcha/badge.svg?branch=master)](https://coveralls.io/github/akornatskyy/wheezy.captcha?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/wheezycaptcha/badge/?version=latest)](https://wheezycaptcha.readthedocs.io/en/latest/?badge=latest)
+[![pypi version](https://badge.fury.io/py/wheezy.captcha.svg)](https://badge.fury.io/py/wheezy.captcha)
+
+[wheezy.captcha](https://pypi.org/project/wheezy.captcha/) is a
+[python](http://www.python.org) package written in pure Python code. It
+is a lightweight captcha library that provides integration with (one of
+below must be installed):
+
+- [PIL](http://www.pythonware.com/products/pil/) - Python Imaging
+  Library 1.1.7.
+- [Pillow](https://pypi.python.org/pypi/Pillow) - Python Imaging
+  Library (fork).
+
+It is optimized for performance, well tested and documented.
+
+Resources:
+
+- [source code](https://github.com/akornatskyy/wheezy.captcha),
+  [examples](https://github.com/akornatskyy/wheezy.captcha/tree/master/demos)
+  and [issues](https://github.com/akornatskyy/wheezy.captcha/issues)
+  tracker are available on
+  [github](https://github.com/akornatskyy/wheezy.captcha)
+- [documentation](https://wheezycaptcha.readthedocs.io/en/latest/)
+
+## Install
+
+[wheezy.captcha](https://pypi.org/project/wheezy.captcha/) requires
+[python](http://www.python.org) version 3.6+. It is independent of operating
+system. You can install it from
+[pypi](https://pypi.org/project/wheezy.captcha/) site (you need specify
+extra requirements per imaging library of your choice):
+
+```sh
+pip install wheezy.captcha
+pip install wheezy.captcha[PIL]
+pip install wheezy.captcha[Pillow]
+```
+
+If you run into any issue or have comments, go ahead and add on
+[github](https://github.com/akornatskyy/wheezy.captcha).
---
+++ packages/wheezy.captcha/wheezy.captcha-3.0.1.tar.gz/wheezy.captcha-3.0.1/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import multiprocessing
+import os
+import re
+
+from setuptools import setup
+
+extra = {}
+try:
+    from Cython.Build import cythonize
+
+    p = os.path.join("src", "wheezy", "captcha")
+    extra["ext_modules"] = cythonize(
+        [os.path.join(p, "*.py")],
+        exclude=os.path.join(p, "__init__.py"),
+        # https://github.com/cython/cython/issues/3262
+        nthreads=0 if multiprocessing.get_start_method() == "spawn" else 2,
+        compiler_directives={"language_level": 3},
+        quiet=True,
+    )
+except ImportError:
+    pass
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
+VERSION = (
+    re.search(
+        r'__version__ = "(.+)"',
+        open("src/wheezy/captcha/__init__.py").read(),
+    )
+    .group(1)
+    .strip()
+)
+
+install_requires = []
+
+try:
+    import uuid  # noqa
+except ImportError:
+    install_requires.append("uuid")
+
+setup(
+    name="wheezy.captcha",
+    version=VERSION,
+    python_requires=">=3.6",
+    description="A lightweight captcha library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akornatskyy/wheezy.captcha",
+    author="Andriy Kornatskyy",
+    author_email="andriy.kornatskyy@live.com",
+    license="MIT",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="wsgi http captcha",
+    packages=["wheezy", "wheezy.captcha"],
+    package_dir={"": "src"},
+    namespace_packages=["wheezy"],
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={"PIL": ["PIL"], "Pillow": ["Pillow"]},
+    platforms="any",
+    **extra
+)
