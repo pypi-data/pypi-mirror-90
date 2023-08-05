b30a04f5307a3f52c0e2deac6ab94095f9806e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package S3Lib/1.2.1/S3Lib-1.2.1.tar.gz

---
+++ packages/S3Lib/S3Lib-1.2.1.tar.gz/S3Lib-1.2.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+requires =  ['safeoutput>=2.0', 'future', 'docopt']
+test_requires = ['tox', 'pytest']
+
+setup(
+    name='S3Lib',
+    version='1.2.1',
+    author='Andrew Thomson',
+    author_email='athomsonguy@gmail.com',
+    packages=['s3lib'],
+    install_requires = requires,
+    tests_require = test_requires,
+    entry_points = {
+      'console_scripts': [
+        's3ls   = s3lib.ui:ls_main',
+        's3get  = s3lib.ui:get_main',
+        's3cp   = s3lib.ui:cp_main',
+        's3head = s3lib.ui:head_main',
+        's3put  = s3lib.ui:put_main',
+        's3rm   = s3lib.ui:rm_main',
+        's3sign = s3lib.ui:sign_main',
+        ],
+    },
+    url='http://pypi.python.org/pypi/S3Lib/',
+    license='LICENSE.txt',
+    description='Library and utilities for interfacing wtih S3',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
