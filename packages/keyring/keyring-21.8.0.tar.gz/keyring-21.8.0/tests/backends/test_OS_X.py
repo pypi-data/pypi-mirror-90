f2da64314d0af8055d84df97e935514ae2497d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package keyring/21.8.0/keyring-21.8.0.tar.gz

---
+++ packages/keyring/keyring-21.8.0.tar.gz/keyring-21.8.0/tests/backends/test_OS_X.py
@@ -0,0 +1,16 @@
+import sys
+
+import pytest
+
+from keyring.testing.backend import BackendBasicTests
+from keyring.backends import OS_X
+
+
+def is_osx_keychain_supported():
+    return sys.platform in ('mac', 'darwin')
+
+
+@pytest.mark.skipif(not is_osx_keychain_supported(), reason="Needs macOS")
+class TestOSXKeychain(BackendBasicTests):
+    def init_keyring(self):
+        return OS_X.Keyring()
