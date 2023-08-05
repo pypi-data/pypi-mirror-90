f3838343139ca6e80638c7f6664e12e2b02f4908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package easyfunc/0.2.4/easyfunc-0.2.4.tar.gz

---
+++ packages/easyfunc/easyfunc-0.2.4.tar.gz/easyfunc-0.2.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyfunc",
+    version="0.2.4",
+    author="K",
+    author_email="mastercoderk@gmail.com",
+    description="Easy and simple functional programming style helper for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiamondGo/easyfunc",
+    exclude_package_data={'': ['test_easyfunc.py']},
+    packages=setuptools.find_packages(),
+    keywords=['fp', 'python', 'functional'],
+    classifiers=(
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ),
+)
