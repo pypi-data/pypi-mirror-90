90e08af6ab29fb7b3995029661aa848f8c0b7e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package radicale-kalabash-auth-token/0.1.0/radicale_kalabash_auth_token-0.1.0-py3-none-any.whl

---
+++ packages/radicale-kalabash-auth-token/radicale_kalabash_auth_token-0.1.0-py3-none-any.whl/radicale_kalabash_auth_token/__init__.py
@@ -0,0 +1,56 @@
+"""Authentication plugin for Radicale."""
+
+from urllib.parse import parse_qs
+
+import requests
+
+from radicale_dovecot_auth import Auth as DovecotAuth
+
+
+class Auth(DovecotAuth):
+    """Allow read-only access using a token.
+
+    If a token is not provided or is invalid, it will fallback to
+    dovecot auth plugin.
+
+    Configuration:
+
+    [auth]
+    type = radicale_kalabash_auth_token
+
+    radicale_kalabash_auth_token_check_url = https://my.kalabash.com/api/v1/user-calendars/check_token/
+    radicale_kalabash_auth_token_token = MYTOKENHERE
+
+    """
+
+    def get_external_login(self, environ):
+        self._auth_is_ok = False
+        try:
+            check_token_url = self.configuration.get(
+                'auth', 'radicale_kalabash_auth_token_check_url')
+            api_token = self.configuration.get(
+                'auth', 'radicale_kalabash_auth_token_token')
+        except KeyError:
+            raise RuntimeError('check_token_url and token must be set')
+
+        if 'QUERY_STRING' not in environ:
+            return ()
+        qs = parse_qs(environ['QUERY_STRING'])
+        if 'token' not in qs:
+            return ()
+        body = {
+            'calendar': environ['PATH_INFO'].strip('/'),
+            'token': qs['token'][0]
+        }
+        headers = {'Authorization': 'Token {}'.format(api_token)}
+        response = requests.post(check_token_url, headers=headers, json=body)
+        if response.status_code != 200 or response.json()['status'] != 'ok':
+            return ()
+        self._auth_is_ok = True
+        return (qs['token'][0], '')
+
+    def is_authenticated2(self, login, user, password):
+        """Fallback to dovecot auth if needed."""
+        if self._auth_is_ok:
+            return True
+        return super().is_authenticated2(login, user, password)
