809c0b6ba2e8d475fbbc5ec06e98fe6201f29a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:06 +0000

    Add package flowrun/0.1.9/flowrun-0.1.9.tar.gz

---
+++ packages/flowrun/flowrun-0.1.9.tar.gz/flowrun-0.1.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flowrun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.25.0,<3.0.0', 'sseclient>=0.0.27,<0.0.28']
+
+setup_kwargs = {
+    'name': 'flowrun',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': None,
+    'author': 'pskishere',
+    'author_email': 'pskishere@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
