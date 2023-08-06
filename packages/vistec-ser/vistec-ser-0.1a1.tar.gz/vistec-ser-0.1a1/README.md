43ba1c2815447680175cf04de4573c2a51336283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:36 +0000

    Add package vistec-ser/0.1a1/vistec-ser-0.1a1.tar.gz

---
+++ packages/vistec-ser/vistec-ser-0.1a1.tar.gz/vistec-ser-0.1a1/README.md
@@ -0,0 +1,36 @@
+# Vistec-AIS Speech Emotion Recognition
+![python-badge](https://img.shields.io/badge/python-%3E%3D3.6-blue?logo=python)
+![tensorflow-badge](https://img.shields.io/badge/tensorflow-%3E%3D2.4.0-orange?logo=tensorflow)
+
+Speech Emotion Recognition Model and Inferencing using Tensorflow 2.x
+
+## Installation
+### From Pypi
+```shell
+pip install vistec-ser
+```
+
+### From source
+```shell
+git clone https://github.com/tann9949/vistec-ser.git
+cd vistec-ser
+python setup.py install
+```
+
+## Usage
+### Inferencing using pretrained weight
+*TODO*
+
+### Train with Your Own Data
+*TODO*
+
+## Model Description
+*TODO*
+
+## Reference
+This repository was structured based on [TensorflowASR](https://github.com/TensorSpeech/TensorFlowASR) repository by
+@usimarit. Please check it out!
+
+
+## Author
+Chompakorn Chaksangchaichot
---
+++ packages/vistec-ser/vistec-ser-0.1a1.tar.gz/vistec-ser-0.1a1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+requirements = [
+    "tensorflow>=2.4.0",
+    "tensorflow_io>=0.17",
+    "librosa>=0.8.0",
+    "nlpaug>=1.1.1",
+]
+
+setuptools.setup(
+    name="vistec-ser",
+    version="0.1a1",
+    author="Chompakorn Chaksangchaichot",
+    author_email="chompakorn.cc@gmail.com",
+    description="Speech Emotion Recognition models and training using Tensorflow 2.x",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tann9949/vistec-ser',
+    packages=setuptools.find_packages(include=['vistec_ser*']),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.8'
+)
