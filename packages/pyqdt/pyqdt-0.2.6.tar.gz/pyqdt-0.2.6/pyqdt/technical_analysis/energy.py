e35993b47b13a363889cad68412309793cbb2ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:35 +0000

    Add package pyqdt/0.2.6/pyqdt-0.2.6.tar.gz

---
+++ packages/pyqdt/pyqdt-0.2.6.tar.gz/pyqdt-0.2.6/pyqdt/technical_analysis/energy.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# 能量指标
+
+from .arithmetic import *
+
+@arithmetic_wrapper
+def VR(N=26, M=6):
+    """成交量变异率
+
+    公式：
+        TH:=SUM(IF(CLOSE>REF(CLOSE,1),VOL,0),N);
+        TL:=SUM(IF(CLOSE<REF(CLOSE,1),VOL,0),N);
+        TQ:=SUM(IF(CLOSE=REF(CLOSE,1),VOL,0),N);
+        VR:100*(TH*2+TQ)/(TL*2+TQ);
+        MAVR:MA(VR,M);
+    输入：
+        N：统计周期数 N
+        M：统计周期数 M
+    输出：
+        VR: 成交量变动率
+        MAVR：成交量变动率的M周期平均
+    定义：
+        RETURNS=(VR, MAVR)
+
+    """
+    TH = SUM(IF(CLOSE > REF(CLOSE, 1), VOL, 0), N)
+    TL = SUM(IF(CLOSE < REF(CLOSE, 1), VOL, 0), N)
+    TQ = SUM(IF(CLOSE == REF(CLOSE, 1), VOL, 0), N)
+    VR = 100 * (TH * 2 + TQ) / (TL * 2 + TQ)
+    MAVR = MA(VR, M)
+    return VR, MAVR
---
+++ packages/pyqdt/pyqdt-0.2.6.tar.gz/pyqdt-0.2.6/pyqdt/technical_analysis/path_lines.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# 路径型指标
+from .arithmetic import *
