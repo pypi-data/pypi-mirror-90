6b2d9bc5f8276604da79eb661f36d1f56e0de72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:06 +0000

    Add package odoo12-addon-ras2/12.0.1.0.11/odoo12-addon-ras2-12.0.1.0.11.tar.gz

---
+++ packages/odoo12-addon-ras2/odoo12-addon-ras2-12.0.1.0.11.tar.gz/odoo12-addon-ras2-12.0.1.0.11/README.md
@@ -0,0 +1,4 @@
+RFID Attendance System - RAS2 Terminal
+======================================
+[12.0.1.0.11]
+Odoo modules needed to operate the RAS2 Terminal of thingsintouch.com
---
+++ packages/odoo12-addon-ras2/odoo12-addon-ras2-12.0.1.0.11.tar.gz/odoo12-addon-ras2-12.0.1.0.11/odoo/addons/ras2/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 http://www.thingsintouch.com
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'RFID Attendance System RAS2 v12.0.1.0.10',
+    'version': '12.0.1.0.11',
+    'category': 'Human Resources',
+    'website': 'https://github.com/thingsintouch/attendance',
+    'author': 'thingsintouch.com, ',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'hr_attendance_rfid',
+    ],
+    'data': [
+    ],
+}
