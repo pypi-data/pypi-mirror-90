c29062b9ab15079922fe850a7ffd8252198aa067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:46 +0000

    Add package b4sh/0.0.0.1/b4sh-0.0.0.1.tar.gz

---
+++ packages/b4sh/b4sh-0.0.0.1.tar.gz/b4sh-0.0.0.1/b4sh/settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+B4SH_DIR = str(Path.home()) + "/" + ".b4sh"
+VERSION = "0.0.0.1"
+HOST = "https://b4sh.co/api"
---
+++ packages/b4sh/b4sh-0.0.0.1.tar.gz/b4sh-0.0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from b4sh.settings import VERSION
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="b4sh",
+    version=VERSION,
+    scripts=['./scripts/b4sh', './scripts/b'],
+    author="Sanix-darker",
+    author_email="s4nixd@gmail.com",
+    description="Create, use and share bashs commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sanix-darker/b4sh",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
