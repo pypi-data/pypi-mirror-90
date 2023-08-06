a316c6aa0d5b65eec2853dbe3a38e973ab896320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:59 +0000

    Add package udon2/0.1b1/udon2-0.1b1-cp37-cp37m-win_amd64.whl

---
+++ packages/udon2/udon2-0.1b1-cp37-cp37m-win_amd64.whl/udon2/visual/utils.py
@@ -0,0 +1,40 @@
+# Modified by Dmytro Kalpakchi, 2020
+# Adapted from spaCy's built in visualization suite for dependencies and named entities.
+# https://github.com/explosion/spaCy/tree/master/spacy/displacy
+
+def is_in_jupyter():
+    """Check if user is running spaCy from a Jupyter notebook by detecting the
+    IPython kernel. Mainly used for the displaCy visualizer.
+    RETURNS (bool): True if in Jupyter, False if not.
+    """
+    # https://stackoverflow.com/a/39662359/6400719
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+    except NameError:
+        return False  # Probably standard Python interpreter
+    return False
+
+
+def minify_html(html):
+    """Perform a template-specific, rudimentary HTML minification for displaCy.
+    Disclaimer: NOT a general-purpose solution, only removes indentation and
+    newlines.
+    html (unicode): Markup to minify.
+    RETURNS (unicode): "Minified" HTML.
+    """
+    return html.strip().replace("    ", "").replace("\n", "")
+
+
+def escape_html(text):
+    """Replace <, >, &, " with their HTML encoded representation. Intended to
+    prevent HTML errors in rendered displaCy markup.
+    text (unicode): The original text.
+    RETURNS (unicode): Equivalent text to be safely used within HTML.
+    """
+    text = text.replace("&", "&amp;")
+    text = text.replace("<", "&lt;")
+    text = text.replace(">", "&gt;")
+    text = text.replace('"', "&quot;")
+    return text
