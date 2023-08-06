9e5575f0493a3715408a3d4f35cce5df230034b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:02 +0000

    Add package invest/0.0.13/invest-0.0.13.tar.gz

---
+++ packages/invest/invest-0.0.13.tar.gz/invest-0.0.13/invest/dacc.py
@@ -0,0 +1,63 @@
+import os
+
+from py2store import LocalPickleStore
+from py2store.stores.local_store import AutoMkDirsOnSetitemMixin
+from py2store.persisters.local_files import ensure_slash_suffix
+from py2store.caching import mk_sourced_store
+from py2store.trans import add_ipython_key_completions, kv_wrap
+from py2store.key_mappers.paths import str_template_key_trans
+
+from invest import Ticker
+from invest.util import handle_missing_dir
+
+ROOTDIR_ENVVAR = 'INVEST_ROOTDIR'
+DFLT_ROOTDIR = os.environ.get(ROOTDIR_ENVVAR, os.path.expanduser('~/.invest'))
+DFLT_TICKER_DATA_DIR = os.path.join(DFLT_ROOTDIR, 'ticker_data')
+
+proj_root_dir = DFLT_ROOTDIR
+handle_missing_dir(proj_root_dir)
+
+
+def proj_file(*args):
+    return os.path.join(proj_root_dir, *args)
+
+
+path_sep = os.path.sep
+
+remote_field_trans = str_template_key_trans('{ticker}' + path_sep + '{field}', str_template_key_trans.key_types.str)
+
+
+@kv_wrap(remote_field_trans)
+class _RemoteYfDataReader:
+    def __getitem__(self, k):
+        ticker_symbol, field = k.split(path_sep)
+        return Ticker(ticker_symbol)[field]
+
+
+remote_data = _RemoteYfDataReader()
+
+ticker_field_trans = str_template_key_trans('{ticker}' + path_sep + '{field}.p', str_template_key_trans.key_types.str)
+
+
+@add_ipython_key_completions
+@kv_wrap(ticker_field_trans)
+class LocalTickerData(AutoMkDirsOnSetitemMixin, LocalPickleStore):
+    def __init__(self, ticker_data_dir=DFLT_TICKER_DATA_DIR):
+        handle_missing_dir(ticker_data_dir)
+        super().__init__(path_format=ensure_slash_suffix(ticker_data_dir), max_levels=1)
+
+
+_YahooData = mk_sourced_store(
+    store=LocalTickerData,
+    source=remote_data,
+    return_source_data=True,
+    __name__='_YahooData',
+    __module__=__name__
+)
+
+
+@add_ipython_key_completions
+class TickerData(_YahooData):
+    def __init__(self, ticker_data_dir=DFLT_TICKER_DATA_DIR):
+        handle_missing_dir(ticker_data_dir)
+        super().__init__(ticker_data_dir=ensure_slash_suffix(ticker_data_dir))
---
+++ packages/invest/invest-0.0.13.tar.gz/invest-0.0.13/invest/util.py
@@ -0,0 +1,130 @@
+import os
+from typing import Iterable
+from datetime import datetime
+
+DFLT_TICKER = "GOOG"
+DFLT_DATE_FORMAT = '%Y-%m-%d:%H:%M:%S'
+
+
+def hms_message(msg=''):
+    t = datetime.now()
+    return "({:02.0f}){:02.0f}:{:02.0f}:{:02.0f} - {}".format(t.day, t.hour, t.minute, t.second, msg)
+
+
+def print_progress(msg, refresh=None, display_time=True):
+    """
+    input: message, and possibly args (to be placed in the message string, sprintf-style
+    output: Displays the time (HH:MM:SS), and the message
+    use: To be able to track processes (and the time they take)
+    """
+    if display_time:
+        msg = hms_message(msg)
+    if refresh is not False:
+        print(msg, '\r')
+        # stdout.write('\r' + msg)
+        # stdout.write(refresh)
+        # stdout.flush()
+    else:
+        print(msg)
+
+
+def all_info(ticker):
+    for k, v in ticker.items():
+        if hasattr(v, '__len__'):
+            if len(v) > 0:
+                yield k, v
+        elif v is not None:
+            yield k, v
+
+
+def all_info_printable_string(ticker):
+    def gen():
+        for k, v in all_info(ticker):
+            yield f"\n----------{k}-------------"
+            yield f"{v}"
+
+    return '\n'.join(gen())
+
+
+def _assert_that_attrs_extraction_is_correct():
+    from invest.base import (
+        Ticker,
+        _ticker_attrs_that_are_properties,
+    )
+    from invest._prep import _getless_callable_attrs
+
+    assert _getless_callable_attrs.issubset(_ticker_attrs_that_are_properties), "Some callable attrs were lost"
+    ticker = Ticker(DFLT_TICKER)
+    for attr in _getless_callable_attrs:
+        prop_version = ticker[attr]
+        method_version = ticker[f'get_{attr}']
+        if isinstance(prop_version, pd.DataFrame):
+            comparison = prop_version.fillna(-9) == method_version.fillna(-9)
+            comparison = comparison.values.all()
+        else:
+            comparison = prop_version == method_version
+
+        assertion_error_msg = f"method and prop versions were not the same for {attr}"
+        if isinstance(comparison, Iterable):
+            assert all(comparison), assertion_error_msg
+        else:
+            assert comparison, assertion_error_msg
+
+
+def clog(condition, *args):
+    if condition:
+        print(*args)
+
+
+def handle_missing_dir(dirpath, prefix_msg='', ask_first=True, verbose=True):
+    if not os.path.isdir(dirpath):
+        if ask_first:
+            clog(verbose, prefix_msg)
+            clog(verbose, f"This directory doesn't exist: {dirpath}")
+            answer = input("Should I make that directory for you? ([Y]/n)?") or 'Y'
+            if next(iter(answer.strip().lower()), None) != 'y':
+                return
+        clog(verbose, f"Making {dirpath}...")
+        os.mkdir(dirpath)
+
+
+import json
+from datetime import datetime
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+
+def is_jsonizable(obj):
+    try:
+        json.loads(json.dumps(obj))
+        return True
+    except Exception:
+        return False
+
+
+from typing import Iterable
+
+
+def is_bsonizable(obj):
+    """
+
+    >>> is_bsonizable({"this": 'is normal'})  # also jsonizable
+    True
+    >>> is_bsonizable({"date": datetime.now()})  # NOT jsonizable
+    True
+    >>> is_bsonizable({datetime.now(): "datetime objects allowed as values, not keys"})  # also NOT jsonizable
+    False
+
+    """
+    from bson import json_util
+    try:
+        json_util.loads(json_util.dumps(obj))
+        return True
+    except Exception:
+        return False
