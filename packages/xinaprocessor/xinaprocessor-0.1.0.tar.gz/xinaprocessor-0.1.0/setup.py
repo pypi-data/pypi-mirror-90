afb2c90a4509c13edcfd0f8a210af3b784843c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:07 +0000

    Add package xinaprocessor/0.1.0/xinaprocessor-0.1.0.tar.gz

---
+++ packages/xinaprocessor/xinaprocessor-0.1.0.tar.gz/xinaprocessor-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='xinaprocessor',
+    version='0.1.0',
+    install_requires=required,
+    tests_require=['pytest'],
+    author="Xina AI",
+    download_url='https://github.com/xina-ai/xinaprocessor.git',
+    description="Arabic processing library",
+    packages=find_packages(include=['xinaprocessor'])
+)
