09e450d4f827480f3891dadb2c3f5667df2abb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:34 +0000

    Add package django-deprecate-fields/0.1.1/django-deprecate-fields-0.1.1.tar.gz

---
+++ packages/django-deprecate-fields/django-deprecate-fields-0.1.1.tar.gz/django-deprecate-fields-0.1.1/README.md
@@ -0,0 +1,50 @@
+# Django - Deprecate Field
+
+[![PyPi](https://img.shields.io/pypi/v/django-deprecate-fields.svg?branch=master)](https://pypi.python.org/pypi/django-deprecate-fields/)
+[![License](https://img.shields.io/github/license/3yourmind/django-deprecate-fields.svg)](./LICENSE)
+[![Contributing](https://img.shields.io/badge/PR-welcome-green.svg)](https://github.com/3YOURMIND/django-deprecate-fields/pulls)
+[![3yourminD-Careers](https://img.shields.io/badge/3YOURMIND-Hiring-brightgreen.svg)](https://www.3yourmind.com/career)
+[![Stars](https://img.shields.io/github/stars/3YOURMIND/django-deprecate-fields.svg?style=social&label=Stars)](https://github.com/3YOURMIND/django-deprecate-fields/stargazers)
+
+## Installation
+
+```
+pip install django-deprecate-fields
+```
+
+## Usage
+
+Assume the simple following model:
+```python
+from django.db import models
+
+class MyModel(models.Model):
+    field1 = models.CharField()
+    field2 = models.CharField()
+```
+
+In order to remove `field1`, it should first be marked as deprecated:
+```python
+from django.db import models
+from django_deprecate_fields import deprecate_field
+
+class MyModel(models.Model):
+    field1 = deprecate_field(models.CharField())
+    field2 = models.CharField()
+```
+
+Secondly, `makemigrations` should be called, which will change the field to be nullable. Any lingering references to it
+in your code will return `None` (or optionally any value or callable passed to `deprecate_field` as the
+`return_instead` argument)
+
+Lastly, after the changes above have been deployed, `field1` can then safely be removed in the model (plus another
+`makemigrations` run)
+
+## Contributing
+
+First of all, thank you very much for contributing to this project. Please base
+your work on the `master` branch and target `master` in your pull request.
+
+## License
+
+`django-deprecate-fields` is released under the [Apache 2.0 License](./LICENSE).
---
+++ packages/django-deprecate-fields/django-deprecate-fields-0.1.1.tar.gz/django-deprecate-fields-0.1.1/setup.py
@@ -0,0 +1,57 @@
+# Copyright 2018 3YOURMIND GmbH
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+PROJECT_DIR = path.abspath(path.dirname(__file__))
+
+long_description = open("README.md").read()
+
+install_requirements = ["Django>=2.1"]
+
+setup(
+    name="django-deprecate-fields",
+    version="0.1.1",
+    description="This package allows deprecating model fields and allows "
+    "removing them in a backwards compatible manner.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/3YOURMIND/django-deprecate-fields",
+    author="3YOURMIND GmbH",
+    license="Apache License 2.0",
+    packages=["django_deprecate_fields"],
+    install_requires=install_requirements,
+    keywords="django migration deprecation database backward compatibility",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
