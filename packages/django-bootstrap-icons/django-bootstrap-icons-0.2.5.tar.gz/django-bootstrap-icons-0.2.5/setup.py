d60547ad2803232efc1e6fa5d14b6900a7433cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:16 +0000

    Add package django-bootstrap-icons/0.2.5/django-bootstrap-icons-0.2.5.tar.gz

---
+++ packages/django-bootstrap-icons/django-bootstrap-icons-0.2.5.tar.gz/django-bootstrap-icons-0.2.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-bootstrap-icons',
+    version='0.2.5',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    description='A quick way to add Bootstrap Icons with Django template tags.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Christian Wiegand',
+    license='MIT',
+    url='https://github.com/christianwgd/django-bootstrap-icons',
+    keywords=['django', 'bootstrap', 'icons', 'templatetags'],
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
