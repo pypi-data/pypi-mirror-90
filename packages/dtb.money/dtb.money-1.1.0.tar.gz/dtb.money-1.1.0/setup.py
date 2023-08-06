d133f27a2c3353189e2f3da58177b83442ba7265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:50 +0000

    Add package dtb.money/1.1.0/dtb.money-1.1.0.tar.gz

---
+++ packages/dtb.money/dtb.money-1.1.0.tar.gz/dtb.money-1.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dtb', 'dtb.money']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dtb.currency>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dtb.money',
+    'version': '1.1.0',
+    'description': 'Simple money implementation',
+    'long_description': None,
+    'author': 'Dima Doroshev',
+    'author_email': 'dima@doroshev.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
