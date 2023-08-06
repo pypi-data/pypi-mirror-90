45cd2b828951e1bc84df4f7fc476cef5e69a9806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:36 +0000

    Add package awscdk-81-patch/1.1.1/awscdk-81-patch-1.1.1.tar.gz

---
+++ packages/awscdk-81-patch/awscdk-81-patch-1.1.1.tar.gz/awscdk-81-patch-1.1.1/setup.py
@@ -0,0 +1,63 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "awscdk-81-patch",
+    "version": "1.1.1",
+    "description": "awscdk-81-patch",
+    "license": "Apache-2.0",
+    "url": "https://github.com/eladb/awscdk-81-patch.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Elad Ben-Israel<benisrae@amazon.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/eladb/awscdk-81-patch.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "awscdk_81_patch",
+        "awscdk_81_patch._jsii"
+    ],
+    "package_data": {
+        "awscdk_81_patch._jsii": [
+            "awscdk-81-patch@1.1.1.jsii.tgz"
+        ],
+        "awscdk_81_patch": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.lambda-layer-awscli==1.81.0",
+        "aws-cdk.lambda-layer-kubectl==1.81.0",
+        "jsii>=1.16.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
---
+++ packages/awscdk-81-patch/awscdk-81-patch-1.1.1.tar.gz/awscdk-81-patch-1.1.1/src/awscdk_81_patch/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.lambda_layer_awscli._jsii
+import aws_cdk.lambda_layer_kubectl._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "awscdk-81-patch", "1.1.1", __name__[0:-6], "awscdk-81-patch@1.1.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
