bac649c855bbf27d3bb51b44140c80e97d117e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:20 +0000

    Add package seaqube/0.1.0/seaqube-0.1.0-py3-none-any.whl

---
+++ packages/seaqube/seaqube-0.1.0-py3-none-any.whl/seaqube/augmentation/corpus/__init__.py
@@ -0,0 +1,7 @@
+'''
+Copyright (c) 2020 by Benjamin Manns
+This file is part of the Semantic Quality Benchmark for Word Embeddings Tool in Python (SeaQuBe).
+:author: Benjamin Manns
+'''
+
+from .unigram import UnigramAugmentation
