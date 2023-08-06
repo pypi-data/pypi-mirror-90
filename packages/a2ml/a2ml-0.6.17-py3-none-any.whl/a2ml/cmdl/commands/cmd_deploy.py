e6903daa70586fe464d2520b08f2047ea0c48939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:24 +0000

    Add package a2ml/0.6.17/a2ml-0.6.17-py3-none-any.whl

---
+++ packages/a2ml/a2ml-0.6.17-py3-none-any.whl/a2ml/cmdl/commands/cmd_deploy.py
@@ -0,0 +1,20 @@
+import click
+from a2ml.api.a2ml import A2ML
+from a2ml.api.utils.context import pass_context
+
+
+@click.command('deploy', short_help='Deploy trained model.')
+@click.option('--provider', '-p', type=click.Choice(['auger','azure','external']), required=False,
+    help='Cloud AutoML Provider.')
+@click.argument('model-id', required=False, type=click.STRING)
+@click.option('--locally', is_flag=True, default=False,
+    help='Download and deploy trained model locally.')
+@click.option('--no-review', is_flag=True, default=False,
+    help='Do not support model review based on actual data.')
+@click.option('--name', '-n', required=False, type=click.STRING, 
+    help='Model friendly name.Used as name for Review Endpoint')
+@pass_context
+def cmdl(ctx, provider, model_id, locally, no_review, name):
+    """Deploy trained model."""
+    ctx.setup_logger(format='')
+    A2ML(ctx, provider).deploy(model_id, locally, not no_review, name=name)
