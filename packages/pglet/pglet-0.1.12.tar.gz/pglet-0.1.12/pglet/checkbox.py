9cae188ebee2e20c3445edd715426e5929eb8750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:26 +0000

    Add package pglet/0.1.12/pglet-0.1.12.tar.gz

---
+++ packages/pglet/pglet-0.1.12.tar.gz/pglet-0.1.12/pglet/checkbox.py
@@ -0,0 +1,34 @@
+from .utils import encode_attr
+from .control import Control
+
+class Checkbox(Control):
+    def __init__(self, id=None, label=None, value=None,
+            width=None, height=None, padding=None, margin=None,
+            visible=None, disabled=None):
+        Control.__init__(self, id=id,
+            width=width, height=height, padding=padding, margin=margin,
+            visible=visible, disabled=disabled)
+        self.value = value
+        self.label = label
+
+    def _getControlName(self):
+        return "checkbox"
+
+# value
+    @property
+    def value(self):
+        return self._get_attr("value")
+
+    @value.setter
+    def value(self, value):
+        assert value == None or isinstance(value, bool), "value must be a boolean"
+        self._set_attr("value", value)
+
+# label
+    @property
+    def label(self):
+        return self._get_attr("label")
+
+    @label.setter
+    def label(self, value):
+        self._set_attr("label", value)
---
+++ packages/pglet/pglet-0.1.12.tar.gz/pglet-0.1.12/pglet/text.py
@@ -0,0 +1,23 @@
+from .utils import encode_attr
+from .control import Control
+
+class Text(Control):
+    def __init__(self, id=None, value=None,
+            width=None, height=None, padding=None, margin=None,
+            visible=None, disabled=None):
+        Control.__init__(self, id=id,
+            width=width, height=height, padding=padding, margin=margin,
+            visible=visible, disabled=disabled)
+        self.value = value
+
+    def _getControlName(self):
+        return "text"
+
+# value
+    @property
+    def value(self):
+        return self._get_attr("value")
+
+    @value.setter
+    def value(self, value):
+        self._set_attr("value", value)
