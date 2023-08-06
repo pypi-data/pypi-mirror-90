765d83f71837eaa39b86341740bacbf84cb2716f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package clmutils/0.1.5/clmutils-0.1.5-py3-none-any.whl

---
+++ packages/clmutils/clmutils-0.1.5-py3-none-any.whl/clmutils/config.py
@@ -0,0 +1,38 @@
+"""Config."""
+# pylint: disable=too-few-public-methods
+
+import sys
+from pathlib import Path
+from pydantic import BaseSettings
+import dotenv
+
+
+class Settings(BaseSettings):
+    """Settings."""
+
+    remote_host: str = ""
+    remote_user: str = ""
+    remote_pubkey: str = ""
+    cl_key: str = ""
+    cl_key_pub: str = ""
+    gh_key: str = ""
+    user_name: str = ""
+    user_email: str = ""
+
+    class Config:
+        """Setting config."""
+
+        # in colab  and drive mounted
+        env_file = ""
+        _ = "/content/drive/MyDrive"
+        if "google.colab" in sys.modules and Path(_).is_dir():
+            _ = "/content/drive/MyDrive/dotenv"
+            if Path(_).is_file():
+                env_file = _
+            elif Path("/content/drive/MyDrive/.env").is_file():
+                env_file = "/content/drive/MyDrive/.env"
+
+        # neither exists, try .env in cwd and upper dirs
+        if not env_file:
+            # env_file = ".env"
+            env_file = dotenv.find_dotenv()
