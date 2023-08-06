0bd0df8ed7b3f29774e49ad31c7293e994370404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:18 +0000

    Add package colored-print/0.0.1/colored_print-0.0.1.tar.gz

---
+++ packages/colored-print/colored_print-0.0.1.tar.gz/colored_print-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='colored_print',
+    version='0.0.1',
+    description='Output color messages to the terminal',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Miker',
+    author_email='migruzdev@gmail.com',
+    url='https://github.com/TheSimonSays/c_print',
+    project_urls={
+        'Source': 'https://github.com/TheSimonSays/c_print/',
+    },
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Console',
+    ]
+)
