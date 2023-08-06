252487f12c42f2fc1933ec80a5f5efa201796e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:58 +0000

    Add package pydobiss/0.1.33/pydobiss-0.1.33.tar.gz

---
+++ packages/pydobiss/pydobiss-0.1.33.tar.gz/pydobiss-0.1.33/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='pydobiss',
+    version='0.1.33',
+    description='python interface to the dobiss developer api',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Kester Aernoudt',
+    author_email='kesteraernoudt@yahoo.com',
+    url='https://github.com/kesteraernoudt/pydobiss',
+    license='MIT',
+    packages=find_packages(),
+	install_requires=['pyjwt',
+                      'aiohttp',
+                      ],
+)
+
