21df538d5db4c94cd3a0005990ed43526ffcb243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:44 +0000

    Add package redorm/0.6.7/redorm-0.6.7.tar.gz

---
+++ packages/redorm/redorm-0.6.7.tar.gz/redorm-0.6.7/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['redorm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataclasses-jsonschema[fast-validation]>=2.13.0,<3.0.0',
+ 'environs>=9.3.0,<10.0.0',
+ 'fakeredis>=1.4.5,<2.0.0',
+ 'redis>=3.5.3,<4.0.0']
+
+setup_kwargs = {
+    'name': 'redorm',
+    'version': '0.6.7',
+    'description': 'A simple redis ORM',
+    'long_description': None,
+    'author': 'Jack Adamson',
+    'author_email': 'jack@mrfluffybunny.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
