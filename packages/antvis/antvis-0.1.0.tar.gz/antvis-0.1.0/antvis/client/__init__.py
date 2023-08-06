c2265b14188f4c44fd1b45a6ccc5af773c64b707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:19 +0000

    Add package antvis/0.1.0/antvis-0.1.0.tar.gz

---
+++ packages/antvis/antvis-0.1.0.tar.gz/antvis-0.1.0/antvis/client/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2020-06-01 09:00
+# @File    : __init__.py.py
+# @Author  : jian<jian@mltalker.com>
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
