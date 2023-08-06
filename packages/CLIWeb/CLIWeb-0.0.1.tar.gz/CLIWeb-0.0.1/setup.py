7eea75b3c4d4954a169f2beeb874ab422136eec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:52 +0000

    Add package CLIWeb/0.0.1/CLIWeb-0.0.1.tar.gz

---
+++ packages/CLIWeb/CLIWeb-0.0.1.tar.gz/CLIWeb-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CLIWeb',
+    version='0.0.1',
+    url='https://github.com/iiestIT/CLIWeb',
+    license='MIT',
+    author='iiestIT',
+    author_email='it.iiest.de@gmail.com',
+    description='A cli-tool to performe http requests and socket connections.',
+    python_requires='>=3.8',
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines()
+)
