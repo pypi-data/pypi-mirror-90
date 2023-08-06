a21057023164e7f7412aa30e83d869293dcd8045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:00 +0000

    Add package fabricauthenticator/0.0.2.7/fabricauthenticator-0.0.2.7-py3-none-any.whl

---
+++ packages/fabricauthenticator/fabricauthenticator-0.0.2.7-py3-none-any.whl/fabricauthenticator/__init__.py
@@ -0,0 +1,23 @@
+import os
+from datetime import timedelta, datetime
+
+from fss_utils.jwt_validate import JWTValidator
+
+from fabricauthenticator.token.vouch_helper import VouchHelper
+
+CILOGON_JWKS_URL = os.getenv('CILOGON_JWKS_URL')
+CILOGON_KEY_REFRESH = os.getenv('CILOGON_KEY_REFRESH')
+t = datetime.strptime(CILOGON_KEY_REFRESH, "%H:%M:%S")
+
+jwt_validator = JWTValidator(CILOGON_JWKS_URL,
+                             timedelta(hours=t.hour, minutes=t.minute, seconds=t.second))
+
+vouch_cookie_name = os.getenv('VOUCH_COOKIE_NAME')
+vouch_secret = os.getenv('VOUCH_COOKIE_SECRET')
+vouch_compression = os.getenv('VOUCH_COOKIE_COMPRESSION')
+vouch_compression_enable = False
+if vouch_compression.lower() == 'true':
+    vouch_compression_enable = True
+
+vouch_helper = VouchHelper(secret=vouch_secret, compression=vouch_compression_enable,
+                           cookie_name=vouch_cookie_name)
