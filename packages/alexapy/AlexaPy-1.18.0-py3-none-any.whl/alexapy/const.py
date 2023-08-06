220a0f563e15edfb9abfe21955f64f465f2bf416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:24 +0000

    Add package alexapy/1.18.0/AlexaPy-1.18.0-py3-none-any.whl

---
+++ packages/alexapy/AlexaPy-1.18.0-py3-none-any.whl/alexapy/const.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Python Package for controlling Alexa devices (echo dot, etc) programmatically.
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
+
+EXCEPTION_TEMPLATE = "An exception of type {0} occurred. Arguments:\n{1!r}"
+
+APP_NAME = "Amazon Media Player"
+USER_AGENT = f"AmazonWebView/{APP_NAME}/2.2.223830.0/iOS/11.4.1/iPhone"
