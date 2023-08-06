180e33063cb8d192668d4a374ad78f70eeb647b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:27 +0000

    Add package awscdk-81-patch/1.1.0/awscdk_81_patch-1.1.0-py3-none-any.whl

---
+++ packages/awscdk-81-patch/awscdk_81_patch-1.1.0-py3-none-any.whl/awscdk_81_patch/_jsii/__init__.py
@@ -0,0 +1,19 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "awscdk-81-patch", "1.1.0", __name__[0:-6], "awscdk-81-patch@1.1.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
