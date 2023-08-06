e85176fb58b1de92edc9ee832965ae71b2692fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:50 +0000

    Add package PyMusicPlayer/1.0.0/PyMusicPlayer-1.0.0.tar.gz

---
+++ packages/PyMusicPlayer/PyMusicPlayer-1.0.0.tar.gz/PyMusicPlayer-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    readme = file.read()
+
+setuptools.setup(
+    name='PyMusicPlayer',
+    version='1.0.0',
+    author='cmdvmd',
+    description='An MP3 music player interface',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cmdvmd/pymusicplayer',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'Topic :: Software Development :: Libraries :: pygame'
+    ]
+)
