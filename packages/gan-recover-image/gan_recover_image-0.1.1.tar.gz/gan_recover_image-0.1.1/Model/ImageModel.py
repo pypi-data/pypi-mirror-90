a88c32524f1a4b7b75814f5fdcce1101b8e23a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:17 +0000

    Add package gan-recover-image/0.1.1/gan_recover_image-0.1.1.tar.gz

---
+++ packages/gan-recover-image/gan_recover_image-0.1.1.tar.gz/gan_recover_image-0.1.1/Model/ImageModel.py
@@ -0,0 +1,9 @@
+class Image:
+    image = None
+    width = 0
+    height = 0
+
+    def __init__(self, image, widht, height):
+        self.image = image
+        self.width = widht
+        self.height = height
