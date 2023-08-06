d7244f9f5950a589d502992e552661e7783a4775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:52 +0000

    Add package karel-for-synthesis/0.1.0/karel-for-synthesis-0.1.0.tar.gz

---
+++ packages/karel-for-synthesis/karel-for-synthesis-0.1.0.tar.gz/karel-for-synthesis-0.1.0/karel_for_synthesis/errors.py
@@ -0,0 +1,17 @@
+class TimeoutError(Exception):
+    pass
+
+
+class Timeout:
+    def __init__(self, max_steps):
+        self.max_steps = max_steps
+        self.steps = 0
+
+    def inc(self):
+        self.steps += 1
+        if self.steps >= self.max_steps:
+            raise TimeoutError
+
+
+class KarelSyntaxError(Exception):
+    pass
