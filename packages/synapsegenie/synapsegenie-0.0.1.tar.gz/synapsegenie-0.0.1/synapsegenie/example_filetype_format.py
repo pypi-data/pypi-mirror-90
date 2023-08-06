0fcb6a4dc44125b47a7fb5fefcdd9ebe49a8614d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:56 +0000

    Add package synapsegenie/0.0.1/synapsegenie-0.0.1.tar.gz

---
+++ packages/synapsegenie/synapsegenie-0.0.1.tar.gz/synapsegenie-0.0.1/synapsegenie/example_filetype_format.py
@@ -0,0 +1,171 @@
+import logging
+import os
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+class FileTypeFormat:
+
+    _process_kwargs = ["newPath", "databaseSynId"]
+
+    _fileType = "fileType"
+
+    _validation_kwargs = []
+
+    def __init__(self, syn, center):
+        self.syn = syn
+        self.center = center
+
+    def _get_dataframe(self, filePathList):
+        '''
+        This function by defaults assumes the filePathList is length of 1
+        and is a tsv file.  Could change depending on file type.
+
+        Args:
+            filePathList:  A list of file paths (Max is 2 for the two
+                           clinical files)
+
+        Returns:
+            df: Pandas dataframe of file
+        '''
+        filePath = filePathList[0]
+        df = pd.read_csv(filePath, sep="\t", comment="#")
+        return df
+
+    def read_file(self, filePathList):
+        '''
+        Each file is to be read in for validation and processing.
+        This is not to be changed in any functions.
+
+        Args:
+            filePathList:  A list of file paths (Max is 2 for the two
+                           clinical files)
+
+        Returns:
+            df: Pandas dataframe of file
+        '''
+        df = self._get_dataframe(filePathList)
+        return df
+
+    def _validateFilename(self, filePath):
+        '''
+        Function that changes per file type for validating its filename
+        Expects an assertion error.
+
+        Args:
+            filePath: Path to file
+        '''
+        # assert True
+        raise NotImplementedError
+
+    def validateFilename(self, filePath):
+        '''
+        Validation of file name.  The filename is what maps the file
+        to its validation and processing.
+
+        Args:
+            filePath: Path to file
+
+        Returns:
+            str: file type defined by self._fileType
+        '''
+        self._validateFilename(filePath)
+        return self._fileType
+
+    def process_steps(self, df, **kwargs):
+        '''
+        This function is modified for every single file.
+        It reformats the file and stores the file into database and Synapse.
+        '''
+        return ''
+
+    def preprocess(self, newpath):
+        '''
+        This is for any preprocessing that has to occur to the entity name
+        to add to kwargs for processing.  entity name is included in
+        the new path
+
+        Args:
+            newpath: Path to file
+        '''
+        return dict()
+
+    def process(self, filePath, **kwargs):
+        '''
+        This is the main processing function.
+
+        Args:
+            filePath: Path to file
+            kwargs: The kwargs are determined by self._process_kwargs
+
+        Returns:
+            str: file path of processed file
+        '''
+        preprocess_args = self.preprocess(kwargs.get('newPath'))
+        kwargs.update(preprocess_args)
+        mykwargs = {}
+        for required_parameter in self._process_kwargs:
+            assert required_parameter in kwargs.keys(), \
+                "%s not in parameter list" % required_parameter
+            mykwargs[required_parameter] = kwargs[required_parameter]
+        logger.info('PROCESSING %s' % filePath)
+        path_or_df = self.read_file([filePath])
+        path = self.process_steps(path_or_df, **mykwargs)
+        return path
+
+    def _validate(self, df, **kwargs):
+        '''
+        This is the base validation function.
+        By default, no validation occurs.
+
+        Args:
+            df: A dataframe of the file
+            kwargs: The kwargs are determined by self._validation_kwargs
+
+        Returns:
+            tuple: The errors and warnings as a file from validation.
+                   Defaults to blank strings
+        '''
+        errors = ""
+        warnings = ""
+        logger.info(f"NO VALIDATION for {self._fileType} files")
+        return errors, warnings
+
+    def validate(self, filePathList, **kwargs):
+        '''
+        This is the main validation function.
+        Every file type calls self._validate, which is different.
+
+        Args:
+            filePathList: A list of file paths.
+            kwargs: The kwargs are determined by self._validation_kwargs
+
+        Returns:
+            tuple: The errors and warnings as a file from validation.
+        '''
+        mykwargs = {}
+        for required_parameter in self._validation_kwargs:
+            if required_parameter not in kwargs.keys():
+                raise ValueError(f"Missing '{required_parameter}' parameter.")
+            mykwargs[required_parameter] = kwargs[required_parameter]
+
+        errors = ""
+
+        try:
+            df = self.read_file(filePathList)
+        except Exception as e:
+            errors = (f"The file(s) ({filePathList}) cannot be read. "
+                      f"Original error: {str(e)}")
+            warnings = ""
+
+        if not errors:
+            logger.info("VALIDATING {}".format(
+                os.path.basename(",".join(filePathList))
+            ))
+            errors, warnings = self._validate(df, **mykwargs)
+        # File is valid if error string is blank
+        valid = (errors == '')
+        
+        return valid, errors, warnings
---
+++ packages/synapsegenie/synapsegenie-0.0.1.tar.gz/synapsegenie-0.0.1/tests/test_validate.py
@@ -0,0 +1,245 @@
+"""Tests validate.py"""
+from unittest import mock
+from unittest.mock import Mock, patch
+
+import pytest
+import synapseclient
+from synapseclient.core.exceptions import SynapseHTTPError
+
+from synapsegenie import (example_filetype_format, validate)
+
+CENTER = "SAGE"
+syn = mock.create_autospec(synapseclient.Synapse)
+
+CNA_ENT = synapseclient.File(name="data_CNA_SAGE.txt",
+                             path="data_CNA_SAGE.txt",
+                             parentId="syn12345")
+CLIN_ENT = synapseclient.File(name="data_clinical_supp_SAGE.txt",
+                              path="data_clinical_supp_SAGE.txt",
+                              parentId="syn12345")
+SAMPLE_ENT = synapseclient.File(name="data_clinical_supp_sample_SAGE.txt",
+                                path="data_clinical_supp_sample_SAGE.txt",
+                                parentId="syn12345")
+PATIENT_ENT = synapseclient.File(name="data_clinical_supp_patient_SAGE.txt",
+                                 path="data_clinical_supp_patient_SAGE.txt",
+                                 parentId="syn12345")
+WRONG_NAME_ENT = synapseclient.File(name="wrong.txt",
+                                    path="data_clinical_supp_SAGE.txt",
+                                    parentId="syn12345")
+
+
+class FileFormat(example_filetype_format.FileTypeFormat):
+    """Example file format"""
+    _fileType = "clinical"
+
+
+def test_perfect_determine_filetype():
+    """
+    Tests determining of file type through filenames
+    Parameters are passed in from filename_fileformat_map
+    """
+    filetype = "clincial"
+    ent_list = [SAMPLE_ENT]
+    with patch.object(FileFormat, "validateFilename",
+                      return_value=filetype):
+        validator = validate.ValidationHelper(
+            syn, None, CENTER, ent_list,
+            format_registry={filetype: FileFormat}
+        )
+        assert validator.determine_filetype() == filetype
+
+
+def test_wrongfilename_noerror_determine_filetype():
+    '''
+    Tests None is passed back when wrong filename is passed
+    when raise_error flag is False
+    '''
+    ent_list = [WRONG_NAME_ENT]
+    with patch.object(FileFormat, "validateFilename",
+                      side_effect=AssertionError):
+        validator = validate.ValidationHelper(
+            syn, project_id=None,
+            center=CENTER, entitylist=ent_list,
+            format_registry={"wrong": FileFormat})
+        assert validator.determine_filetype() is None
+
+
+def test_valid_collect_errors_and_warnings():
+    '''
+    Tests if no error and warning strings are passed that
+    returned valid and message is correct
+    '''
+    message = validate.collect_errors_and_warnings('', '')
+    assert message == "YOUR FILE IS VALIDATED!\n"
+
+
+def test_invalid_collect_errors_and_warnings():
+    """
+    Tests if error and warnings strings are passed that
+    returned valid and message is correct
+    """
+    message = validate.collect_errors_and_warnings("error\nnow",
+                                                   'warning\nnow')
+    assert message == (
+        "----------------ERRORS----------------\n"
+        "error\nnow"
+        "-------------WARNINGS-------------\n"
+        'warning\nnow')
+
+
+def test_warning_collect_errors_and_warnings():
+    """
+    Tests if no error but warnings strings are passed that
+    returned valid and message is correct
+    """
+    message = \
+        validate.collect_errors_and_warnings('', 'warning\nnow')
+    assert message == (
+        "YOUR FILE IS VALIDATED!\n"
+        "-------------WARNINGS-------------\n"
+        'warning\nnow')
+
+
+def test_valid_validate_single_file():
+    """
+    Tests that all the functions are run in validate single
+    file workflow and all the right things are returned
+    """
+    entitylist = [CLIN_ENT]
+    error_string = ''
+    warning_string = ''
+    expected_valid = True
+    expected_message = "valid message here!"
+    expected_filetype = "clinical"
+
+    with patch.object(validate.ValidationHelper,
+                      "determine_filetype",
+                      return_value=expected_filetype) as mock_determine_ftype,\
+         patch.object(FileFormat, "validate",
+                      return_value=(expected_valid, error_string,
+                                    warning_string)) as mock_genie_class,\
+         patch.object(validate, "collect_errors_and_warnings",
+                      return_value=expected_message) as mock_determine:
+        validator = validate.ValidationHelper(
+            syn, project_id="syn1234", center=CENTER,
+            entitylist=entitylist, format_registry={'clinical': FileFormat}
+        )
+        valid, message = validator.validate_single_file(oncotree_link=None,
+                                                        nosymbol_check=False)
+
+        assert valid == expected_valid
+        assert message == expected_message
+        assert validator.file_type == expected_filetype
+
+        mock_determine_ftype.assert_called_once_with()
+
+        mock_genie_class.assert_called_once_with(filePathList=[CLIN_ENT.path])
+
+        mock_determine.assert_called_once_with(error_string, warning_string)
+
+
+def test_filetype_validate_single_file():
+    """
+    Tests that if filetype is passed in that an error is thrown
+    if it is an incorrect filetype
+    """
+    entitylist = [WRONG_NAME_ENT]
+    expected_error = ("----------------ERRORS----------------\n"
+                      "Your filename is incorrect! Please change your "
+                      "filename before you run the validator or specify "
+                      "--filetype if you are running the validator locally")
+
+    with patch.object(FileFormat, "validateFilename",
+                      side_effect=AssertionError):
+        validator = validate.ValidationHelper(
+            syn=syn, project_id=None, center=CENTER, entitylist=entitylist,
+            format_registry={'wrong': FileFormat}
+        )
+
+        valid, message = validator.validate_single_file()
+        assert message == expected_error
+        assert not valid
+
+
+def test_wrongfiletype_validate_single_file():
+    """
+    Tests that if there is no filetype for the filename passed
+    in, an error is thrown
+    """
+    entitylist = [WRONG_NAME_ENT]
+    expected_error = ('----------------ERRORS----------------\n'
+                      'Your filename is incorrect! Please change your '
+                      'filename before you run the validator or specify '
+                      '--filetype if you are running the validator locally')
+
+    with patch.object(validate.ValidationHelper,
+                      "determine_filetype",
+                      return_value=None) as mock_determine_filetype:
+        validator = validate.ValidationHelper(
+            syn=syn, project_id=None, center=CENTER, entitylist=entitylist,
+            format_registry={'wrong': Mock()}
+        )
+        valid, message = validator.validate_single_file()
+
+        assert message == expected_error
+        assert not valid
+        mock_determine_filetype.assert_called_once()
+
+
+def test_nopermission__check_parentid_permission_container():
+    """Throws error if no permissions to access"""
+    parentid = "syn123"
+    with patch.object(syn, "get", side_effect=SynapseHTTPError),\
+         pytest.raises(ValueError,
+                       match="Provided Synapse id must be your input folder "
+                             "Synapse id or a Synapse Id of a folder inside "
+                             "your input directory"):
+        validate._check_parentid_permission_container(syn, parentid)
+
+
+def test_notcontainer__check_parentid_permission_container():
+    """Throws error if input if synid of file"""
+    parentid = "syn123"
+    file_ent = synapseclient.File("foo", parentId=parentid)
+    with patch.object(syn, "get", return_value=file_ent),\
+         pytest.raises(ValueError,
+                       match="Provided Synapse id must be your input folder "
+                             "Synapse id or a Synapse Id of a folder inside "
+                             "your input directory"):
+        validate._check_parentid_permission_container(syn, parentid)
+
+
+def test_valid__check_parentid_permission_container():
+    """
+    Test that parentid specified is a container and have permissions to access
+    """
+    parentid = "syn123"
+    folder_ent = synapseclient.Folder("foo", parentId=parentid)
+    with patch.object(syn, "get", return_value=folder_ent):
+        validate._check_parentid_permission_container(syn, parentid)
+
+
+def test_valid__check_center_input():
+    """Check valid center input"""
+    center = "FOO"
+    center_list = ["FOO", "WOW"]
+    validate._check_center_input(center, center_list)
+
+
+def test_invalid__check_center_input():
+    """Check that center is invalid"""
+    center = "BARFOO"
+    center_list = ["FOO", "WOW"]
+    with pytest.raises(ValueError,
+                       match="Must specify one of these "
+                             "centers: {}".format(", ".join(center_list))):
+        validate._check_center_input(center, center_list)
+
+
+def test_valid__upload_to_synapse():
+    """Test upload of file to synapse under right conditions"""
+    ent = synapseclient.File(id="syn123", parentId="syn222")
+    with patch.object(syn, "store", return_value=ent) as patch_synstore:
+        validate._upload_to_synapse(syn, ['foo'], True, parentid="syn123")
+        patch_synstore.assert_called_once_with(
+            synapseclient.File('foo', parent="syn123"))
