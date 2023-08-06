e8d121bb16fccbfb2d0ff436f72e94d23315b703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:54 +0000

    Add package gibbscorrections/0.2.3/gibbscorrections-0.2.3.tar.gz

---
+++ packages/gibbscorrections/gibbscorrections-0.2.3.tar.gz/gibbscorrections-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+long_description = (
+    "A module that automatises the tedious process of retrieving a Gaussian 16 geometry, and setting up and running"
+    + "a single point Orca calculation at that geometry. Of course, for a bunch of files at the same time."
+)
+
+setup(
+    name="gibbscorrections",
+    version="0.2.3",
+    description=long_description,
+    url="http://github.com/chemistry-scripts/gibbscorrections",
+    author="Emmanuel Nicolas",
+    author_email="latruelle@users.noreply.github.com",
+    license="MIT",
+    packages=["gibbscorrections"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "gibbscorrections=corrections:main",
+        ]
+    },
+)
