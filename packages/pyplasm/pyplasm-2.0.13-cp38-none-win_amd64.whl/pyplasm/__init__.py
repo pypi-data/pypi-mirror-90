db4f0b9739568016b7111061da5d4a96aa51ac5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package pyplasm/2.0.13/pyplasm-2.0.13-cp38-none-win_amd64.whl

---
+++ packages/pyplasm/pyplasm-2.0.13-cp38-none-win_amd64.whl/pyplasm/__init__.py
@@ -0,0 +1,15 @@
+import sys,types,math
+
+import OpenGL.GL   ; Gl   = OpenGL.GL
+import OpenGL.GLU  ; Glu  = OpenGL.GLU
+import OpenGL.GLUT ; Glut = OpenGL.GLUT
+
+from .xgepy import *
+from .fenvs import *
+
+
+
+
+
+
+
