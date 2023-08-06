300fd82974f31b6bab1e005b2dcecaf8dc58cdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:14 +0000

    Add package excel-handling/0.0.1/excel-handling-0.0.1.tar.gz

---
+++ packages/excel-handling/excel-handling-0.0.1.tar.gz/excel-handling-0.0.1/excel-handling/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='excel-handling',
+    version='0.0.1',
+    author='William Ma',
+    author_email='3327821469@qq.com',
+    url='https://github.com/theCoder-WM',
+    description=u'Excel处理工具',
+    packages=['excel-handling'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
