d5bb304fa1880fbdaf82d182d5e1ffb051e9980c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package panda3d-blend2bam/0.18/panda3d_blend2bam-0.18-py3-none-any.whl

---
+++ packages/panda3d-blend2bam/panda3d_blend2bam-0.18-py3-none-any.whl/blend2bam/common.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+Settings = namedtuple('Settings', (
+    'material_mode',
+    'physics_engine',
+    'blender_dir',
+    'append_ext',
+    'pipeline',
+    'no_srgb',
+    'textures',
+    'animations',
+))
+Settings.__new__.__defaults__ = (
+    'pbr', # material_mode
+    'builtin', # physics engine
+    '', # blender_dir
+    False, # append_ext
+    'gltf', # pipeline
+    'False', # no_srg
+    'ref', # textures
+    'embed', # animations
+)
+
+class ConverterBase:
+    '''Implements common functionality for converters'''
+
+    def __init__(self, settings=None):
+        if settings is None:
+            settings = Settings()
+        self.settings = settings
+
+    def convert_single(self, src, dst):
+        '''Convert a single src file to dst'''
+        raise NotImplementedError()
+
+    def convert_batch(self, srcroot, dstdir, files):
+        '''Convert files from srcroot to dstdir'''
+        raise NotImplementedError()
