c2e3ce93d9c0e05e668f0362f3dc43fa198cf999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:34 +0000

    Add package majortom-gateway/0.0.9/majortom_gateway-0.0.9.tar.gz

---
+++ packages/majortom-gateway/majortom_gateway-0.0.9.tar.gz/majortom_gateway-0.0.9/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+VERSION = "0.0.9"
+
+with open("README.md", "r") as readme:
+    readme_content = readme.read()
+
+
+setuptools.setup(
+    name="majortom_gateway",
+    version=VERSION,
+    author="Kubos",
+    author_email="open-source@kubos.com",
+    description="A package for interacting with Major Tom's Gateway API.",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kubos/majortom_gateway_package",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    keywords='majortom major_tom gateway kubos major tom satellite',
+    install_requires=[
+        "websockets",
+        "requests"
+    ]
+)
