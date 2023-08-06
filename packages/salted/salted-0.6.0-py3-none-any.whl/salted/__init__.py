b70f0a8e185a3ea13e616f244d14457c35c11517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:31 +0000

    Add package salted/0.6.0/salted-0.6.0-py3-none-any.whl

---
+++ packages/salted/salted-0.6.0-py3-none-any.whl/salted/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Smart, Asynchronous Link Tester with Database backend (SALTED)
+~~~~~~~~~~~~~~~~~~~~~
+Source: https://github.com/RuedigerVoigt/salted
+(c) 2020-2021 by Rüdiger Voigt
+Released under the Apache License 2.0
+"""
+import sys
+
+if sys.version_info.major != 3 or sys.version_info.minor < 8:
+    raise RuntimeError('You need at least Python 3.8 to run salted. ' +
+                       'You are running: ' + str(sys.version_info.major) +
+                       '.' + str(sys.version_info.minor))
+
+from salted.__main__ import Salted
+
+NAME = "salted"
+__version__ = "0.6.0"
+__author__ = "Rüdiger Voigt"
