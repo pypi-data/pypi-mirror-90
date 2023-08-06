f52d7aa101e903007d753fcbcc34ffa6aa214029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package pragma/0.2.4/pragma-0.2.4.tar.gz

---
+++ packages/pragma/pragma-0.2.4.tar.gz/pragma-0.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pragma',
+    version='0.2.4',
+    packages=['pragma', 'pragma.core', 'pragma.core.resolve'],
+    url='https://github.com/scnerd/pypragma',
+    license='MIT',
+    author='scnerd',
+    author_email='scnerd@gmail.com',
+    description='Python code transformers that mimic pragma compiler directives',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'miniutils',
+        'astor',
+    ]
+)
