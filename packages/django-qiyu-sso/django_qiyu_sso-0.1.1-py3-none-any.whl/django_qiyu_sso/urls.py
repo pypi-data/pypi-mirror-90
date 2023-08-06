055bd16338451f66d895a9f5d79bafd098200714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:13 +0000

    Add package django-qiyu-sso/0.1.1/django_qiyu_sso-0.1.1-py3-none-any.whl

---
+++ packages/django-qiyu-sso/django_qiyu_sso-0.1.1-py3-none-any.whl/django_qiyu_sso/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import OAuth2LoginView, LoginSuccessView
+
+__all__ = ["sso_urls"]
+
+sso_urls = (
+    [
+        path("account/login", OAuth2LoginView.as_view(), name="login"),
+        path("account/login/success", LoginSuccessView.as_view(), name="login_success"),
+    ],
+    "sso",
+    "sso",
+)
