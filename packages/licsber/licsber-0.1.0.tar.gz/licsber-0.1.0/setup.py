2ae7b6ecb578d76f9e5ec690acef6a0c69673848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package licsber/0.1.0/licsber-0.1.0.tar.gz

---
+++ packages/licsber/licsber-0.1.0.tar.gz/licsber-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='licsber',
+    version='0.1.0',
+    author='Licsber',
+    author_email='licsber@gmail.com',
+    url='https://www.cnblogs.com/licsber/',
+    description=u'个人娱乐工具箱.',
+    long_description=long_description,
+    packages=[
+        'licsber',
+        'licsber.mail',
+        'licsber.utils',
+        'licsber.auth'
+    ],
+    install_requires=[
+        'pymongo',
+        'requests',
+        'beautifulsoup4',
+        'pycrypto'
+    ],
+    entry_points={
+        'console_scripts': [
+            'licsber=licsber:licsber',
+            'count-dir=licsber:count_dir',
+            'flatten_dir=licsber:flatten_dir'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent'
+    ],
+    license='GPLv3'
+)
