9d7594405a404962a13229394ebddd97574243dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:28 +0000

    Add package flatland-model-diagram-editor/0.4.0/flatland-model-diagram-editor-0.4.0.tar.gz

---
+++ packages/flatland-model-diagram-editor/flatland-model-diagram-editor-0.4.0.tar.gz/flatland-model-diagram-editor-0.4.0/flatland/database/population/drawing/drawing_type_instances.py
@@ -0,0 +1,16 @@
+"""
+drawing_type_instances.py
+"""
+population = [
+    {'Name': 'Starr class diagram'},
+    {'Name': 'xuml class diagram'},
+    {'Name': 'Shlaer-Mellor class diagram'},
+
+    {'Name': 'xuml state machine diagram'},
+
+    {'Name': 'Starr class collaboration diagram'},
+    {'Name': 'xuml class collaboration diagram'},
+
+    {'Name': 'Starr domain diagram'},
+    {'Name': 'xuml domain diagram'},
+]
