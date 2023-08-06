31f90c320e0932111d8d9e189ae1acd6dc806f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:59 +0000

    Add package easytorch/1.7.1/easytorch-1.7.1.tar.gz

---
+++ packages/easytorch/easytorch-1.7.1.tar.gz/easytorch-1.7.1/easytorch/__init__.py
@@ -0,0 +1,7 @@
+from .easytorch import EasyTorch
+from .trainer import ETTrainer
+from .data import ETDataset, ETDataLoader
+from .metrics import ETMetrics, ETAverages, Prf1a
+from .etargs import default_args
+
+
