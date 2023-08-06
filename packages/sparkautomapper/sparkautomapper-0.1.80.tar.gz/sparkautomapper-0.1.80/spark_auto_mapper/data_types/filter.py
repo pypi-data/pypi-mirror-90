627636c59341c60ad4f10dffdef775496d7bb28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:05 +0000

    Add package sparkautomapper/0.1.80/sparkautomapper-0.1.80.tar.gz

---
+++ packages/sparkautomapper/sparkautomapper-0.1.80.tar.gz/sparkautomapper-0.1.80/spark_auto_mapper/data_types/filter.py
@@ -0,0 +1,27 @@
+from typing import Callable, Any, Dict, Optional
+
+from pyspark.sql import DataFrame, Column
+
+from spark_auto_mapper.data_types.array_base import AutoMapperArrayBase
+from spark_auto_mapper.helpers.spark_higher_order_functions import filter
+from spark_auto_mapper.type_definitions.wrapper_types import AutoMapperColumnOrColumnLikeType
+
+
+class AutoMapperFilterDataType(AutoMapperArrayBase):
+    def __init__(
+        self, column: AutoMapperColumnOrColumnLikeType,
+        func: Callable[[Dict[str, Any]], Any]
+    ) -> None:
+        super().__init__()
+
+        self.column: AutoMapperColumnOrColumnLikeType = column
+        self.func: Callable[[Dict[str, Any]], Any] = func
+
+    def get_column_spec(
+        self, source_df: DataFrame, current_column: Optional[Column]
+    ) -> Column:
+        return filter(
+            self.column.get_column_spec(
+                source_df=source_df, current_column=current_column
+            ), self.func
+        )
