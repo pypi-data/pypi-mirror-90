406394dd30a0a82cdf0557ffe858181ebaba52e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package rf-probability/0.1/rf_probability-0.1.tar.gz

---
+++ packages/rf-probability/rf_probability-0.1.tar.gz/rf_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'rf_probability',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['rf_probability'],
+      author = 'Ryan Fox',
+      author_email = 'ryanfox212@gmail.com',
+      zip_safe=False)
