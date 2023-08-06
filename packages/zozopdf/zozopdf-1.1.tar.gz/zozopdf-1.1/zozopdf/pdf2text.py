b3695da549c08dbb4c2df472bb3b536d130ee262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:11 +0000

    Add package zozopdf/1.1/zozopdf-1.1.tar.gz

---
+++ packages/zozopdf/zozopdf-1.1.tar.gz/zozopdf-1.1/zozopdf/pdf2text.py
@@ -0,0 +1,16 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+class Converter:
+    """ A simple converter for converting PDF to text. """
+
+    def convert(self, path):
+        """ Convert the given PDF to text.
+
+        Parameters:
+        path(str): The path to a PDF file.
+
+        Returns:
+        str: The content of the PDF file as text.
+        """
+        print("This is zozopdf pdf2text convert()")
