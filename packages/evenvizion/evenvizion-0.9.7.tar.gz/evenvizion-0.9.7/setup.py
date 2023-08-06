a5c551323668248a8ff15f642b9c73c365d6d79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:33 +0000

    Add package evenvizion/0.9.7/evenvizion-0.9.7.tar.gz

---
+++ packages/evenvizion/evenvizion-0.9.7.tar.gz/evenvizion-0.9.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="evenvizion",
+    version="0.9.7",
+    author="AIHunters",
+    author_email="oss@aihunters.com",
+    description="EvenVizion - is a video-based camera localization component",
+    url="https://github.com/AIHunters/EvenVizion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",	
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+	'imutils>=0.5.3',
+	'numpy>=1.19.2',
+'opencv-contrib-python==3.4.2.17'],
+    python_requires='>=3.6',
+)
+
