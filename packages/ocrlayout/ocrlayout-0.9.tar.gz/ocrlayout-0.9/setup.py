91a2eb5524f14b3c34d451ea1c503ffc27f07c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:13 +0000

    Add package ocrlayout/0.9/ocrlayout-0.9.tar.gz

---
+++ packages/ocrlayout/ocrlayout-0.9.tar.gz/ocrlayout-0.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='ocrlayout',  
+    version='0.9',
+    author="Nicolas Uthurriague",
+    author_email="puthurr@hotmail.com",
+    description="A Helper class to get more meaninful text out of common OCR outputs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://puthurr.github.io/",
+    project_urls={
+    'Documentation': 'https://puthurr.github.io/',
+    'Source': 'https://github.com/puthurr/ocrlayout/',
+    'Tracker': 'https://github.com/puthurr/ocrlayout/issues',
+    'Recipes':'https://github.com/puthurr/ocrlayout-recipes'
+    },
+    license='MIT',
+    packages=setuptools.find_packages(),
+    package_data={'ocrlayout': ['config/*.*']},
+    classifiers=[
+    "Programming Language :: Python :: 3.8",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    keywords='OCR, Computer Vision, Text Extraction, Knowledge Mining, BoundingBoxes',
+    python_requires='>=3.8',
+    install_requires=['numpy']
+ )
