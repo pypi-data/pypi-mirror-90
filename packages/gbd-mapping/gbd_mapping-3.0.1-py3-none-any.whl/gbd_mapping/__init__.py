dba8a4e36826f4540bb8769c4354af0a8b14dd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:17 +0000

    Add package gbd-mapping/3.0.1/gbd_mapping-3.0.1-py3-none-any.whl

---
+++ packages/gbd-mapping/gbd_mapping-3.0.1-py3-none-any.whl/gbd_mapping/__init__.py
@@ -0,0 +1,7 @@
+from .id import me_id, rei_id, c_id, s_id, cov_id, hs_id, scalar, UNKNOWN, UnknownEntityError
+from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Categories
+from .cause import Cause, causes
+from .sequela import Sequela, Healthstate, sequelae
+from .etiology import Etiology, etiologies
+from .risk_factor import RiskFactor, risk_factors
+from .covariate import Covariate, covariates
---
+++ packages/gbd-mapping/gbd_mapping-3.0.1-py3-none-any.whl/gbd_mapping_generator/etiology_builder.py
@@ -0,0 +1,65 @@
+from typing import List, Tuple
+
+from .data import get_etiology_data, get_etiology_list
+from .base_template_builder import modelable_entity_attrs, gbd_record_attrs
+from .util import make_import, make_module_docstring, make_record, SPACING, TAB
+from .globals import ID_TYPES
+
+IMPORTABLES_DEFINED = ('Etiology', 'etiologies')
+
+
+def get_base_types():
+    etiology_attrs = [('name', 'str'),
+                      ('kind', 'str'),
+                      ('gbd_id', f'Union[{ID_TYPES.REI_ID}, None]')]
+
+    return {
+        'Etiology': {
+            'attrs': tuple(etiology_attrs),
+            'superclass': ('ModelableEntity', modelable_entity_attrs),
+            'docstring': 'Container for etiology GBD ids and metadata.'
+        },
+        'Etiologies': {
+            'attrs': tuple([(name, 'Etiology') for name in get_etiology_list()]),
+            'superclass': ('GbdRecord', gbd_record_attrs),
+            'docstring': 'Container for GBD etiologies.',
+        },
+    }
+
+
+def make_etiology(name: str, rei_id: float) -> str:
+    out = ""
+    out += TAB + f"{name}=Etiology(\n"
+    out += TAB*2 + f"name='{name}',\n"
+    out += TAB * 2 + f"kind='etiology',\n"
+    out += TAB*2 + f"gbd_id={ID_TYPES.REI_ID}({rei_id}),\n"
+    out += TAB + "),\n"
+    return out
+
+
+def make_etiologies(etiology_list: List[Tuple[str, float]]) -> str:
+    out = "etiologies = Etiologies(\n"
+    for name, rei_id in etiology_list:
+        out += make_etiology(name, rei_id)
+    out += ")\n"
+    return out
+
+
+def build_mapping_template() -> str:
+    out = make_module_docstring('Mapping templates for GBD etiologies.', __file__)
+    out += make_import('typing', ('Union',)) + '\n'
+    out += make_import('.id', (ID_TYPES.REI_ID,))
+    out += make_import('.base_template', ('ModelableEntity', 'GbdRecord'))
+
+    for entity, info in get_base_types().items():
+        out += SPACING
+        out += make_record(entity, **info)
+    return out
+
+
+def build_mapping() -> str:
+    out = make_module_docstring('Mapping of GBD etiologies.', __file__)
+    out += make_import('.id', (ID_TYPES.REI_ID,))
+    out += make_import('.etiology_template', ('Etiology', 'Etiologies')) + SPACING
+    out += make_etiologies(get_etiology_data())
+    return out
