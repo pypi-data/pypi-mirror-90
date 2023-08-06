5648ca7aac210af3b9d06db960084605e4d37266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:29 +0000

    Add package flights-time-series-dataset/1.1.0/flights_time_series_dataset-1.1.0-py3-none-any.whl

---
+++ packages/flights-time-series-dataset/flights_time_series_dataset-1.1.0-py3-none-any.whl/flights_time_series_dataset/flight_series_dataset.py
@@ -0,0 +1,30 @@
+import calendar
+import seaborn as sns
+from time_series_dataset import TimeSeriesDataset
+from time_series_dataset_generator import make_time_series_dataset
+
+
+class FlightSeriesDataset(TimeSeriesDataset):
+    def __init__(self, pattern_length, n_to_predict, except_last_n, data_augmentation=None):
+        flights = sns.load_dataset("flights")
+        input_features_labels = ['month', 'year']
+        output_features_labels = ['passengers']
+
+        month = flights['month']
+        months_3l = [month_name[0:3]for month_name in list(calendar.month_name)]
+        month_number = [months_3l.index(_month)for _month in month]
+        flights['month'] = month_number
+
+        tsd = make_time_series_dataset(
+            flights,
+            pattern_length,
+            n_to_predict,
+            input_features_labels,
+            output_features_labels,
+            except_last_n,
+            data_augmentation
+        )
+        self.wrap(tsd)
+
+    def wrap(self, tsd):
+        self.__dict__ = tsd.__dict__
