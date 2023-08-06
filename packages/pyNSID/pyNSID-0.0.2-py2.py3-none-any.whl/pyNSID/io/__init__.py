770a16ebb05c46d3938356899f370151301389be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:02 +0000

    Add package pyNSID/0.0.2/pyNSID-0.0.2-py2.py3-none-any.whl

---
+++ packages/pyNSID/pyNSID-0.0.2-py2.py3-none-any.whl/pyNSID/io/__init__.py
@@ -0,0 +1,18 @@
+"""
+Tools to read, write data in h5NSID files
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: _autosummary
+
+    hdf_utils
+    hdf_io
+    nsi_reader
+"""
+from . import hdf_utils, hdf_io
+from .nsi_reader import NSIDReader
+from .hdf_io import *
+
+__all__ = ['hdf_utils', 'hdf_io', 'NSIDReader']
