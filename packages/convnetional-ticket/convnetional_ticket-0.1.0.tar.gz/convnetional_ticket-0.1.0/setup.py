91f698e8e00f8cb0eca60dcb7423d17e5457f603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package convnetional-ticket/0.1.0/convnetional_ticket-0.1.0.tar.gz

---
+++ packages/convnetional-ticket/convnetional_ticket-0.1.0.tar.gz/convnetional_ticket-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="convnetional_ticket",
+    version="0.1.0",
+    py_modules=["cz_convnetional_ticket"],
+    license="MIT",
+    long_description="Conventional commits with tickets in changelog",
+    install_requires=["commitizen"],
+)
