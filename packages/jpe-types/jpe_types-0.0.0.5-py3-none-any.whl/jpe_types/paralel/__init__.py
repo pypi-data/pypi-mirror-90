8d9dd48c5b5ca22dd555fa8ef50bd57c91891295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package jpe-types/0.0.0.5/jpe_types-0.0.0.5-py3-none-any.whl

---
+++ packages/jpe-types/jpe_types-0.0.0.5-py3-none-any.whl/jpe_types/paralel/__init__.py
@@ -0,0 +1,8 @@
+"""additons to paralel computation
+
+additional and slitly imporved thread
+Pricesses with retuns"""
+from jpe_types.paralel.thread import Thread, LockableThread, threadInharitanceFilter, LogThread
+from jpe_types.paralel.multiprocessing import Print as print, Process, classScharer
+
+#__all__ = ["Thread", "LockableThread", "threadInharitanceFilter"]
