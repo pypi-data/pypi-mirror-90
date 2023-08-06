0f58cc1f7e48904ef404c7bce5b2cdae485b1109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:11 +0000

    Add package mocket/3.9.35/mocket-3.9.35.tar.gz

---
+++ packages/mocket/mocket-3.9.35.tar.gz/mocket-3.9.35/mocket/__init__.py
@@ -0,0 +1,10 @@
+try:
+    # Py2
+    from mocket import Mocket, MocketEntry, Mocketizer, mocketize
+except ImportError:
+    # Py3
+    from mocket.mocket import Mocket, MocketEntry, Mocketizer, mocketize
+
+__all__ = ("mocketize", "Mocket", "MocketEntry", "Mocketizer")
+
+__version__ = "3.9.35"
