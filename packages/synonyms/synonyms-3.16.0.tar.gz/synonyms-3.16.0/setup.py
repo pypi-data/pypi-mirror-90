1c1ce1c62c09937c75d7d8405649641c6d282c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:15 +0000

    Add package synonyms/3.16.0/synonyms-3.16.0.tar.gz

---
+++ packages/synonyms/synonyms-3.16.0.tar.gz/synonyms-3.16.0/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+LONGDOC = """
+Synonyms
+=====================
+
+中文近义词
+
+https://github.com/chatopera/Synonyms
+
+"""
+
+setup(
+    name='synonyms',
+    version='3.16.0',
+    description='中文近义词：聊天机器人，智能问答工具包；Chinese Synonyms for Natural Language Processing and Understanding',
+    long_description=LONGDOC,
+    author='Hai Liang Wang, Hu Ying Xi',
+    author_email='hain@chatopera.com',
+    url='https://github.com/chatopera/Synonyms',
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic'],
+    keywords='corpus,machine-learning,NLU,NLP,Synonyms,Similarity,chatbot',
+    packages=find_packages(),
+    install_requires=[
+        'six>=1.11.0',
+        'numpy>=1.13.1',
+        'scipy>=1.0.0',
+        'scikit-learn>=0.19.1'
+    ],
+    package_data={
+        'synonyms': [
+            '**/**/idf.txt',
+            '**/**/*.p',
+            '**/*.gz',
+            '**/*.txt',
+            'LICENSE']})
