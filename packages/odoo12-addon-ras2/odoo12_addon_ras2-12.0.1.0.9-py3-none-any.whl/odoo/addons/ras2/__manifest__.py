a62ff3d78cbc07e66966c4d179d6a2867065fd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:20 +0000

    Add package odoo12-addon-ras2/12.0.1.0.9/odoo12_addon_ras2-12.0.1.0.9-py3-none-any.whl

---
+++ packages/odoo12-addon-ras2/odoo12_addon_ras2-12.0.1.0.9-py3-none-any.whl/odoo/addons/ras2/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 http://www.thingsintouch.com
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'thingsintouch RFID Attendance System RAS2',
+    'version': '12.0.1.0.9',
+    'category': 'Human Resources',
+    'website': 'https://github.com/thingsintouch/attendance',
+    'author': 'thingsintouch.com, ',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'hr_attendance_rfid',
+    ],
+    'data': [
+    ],
+}
