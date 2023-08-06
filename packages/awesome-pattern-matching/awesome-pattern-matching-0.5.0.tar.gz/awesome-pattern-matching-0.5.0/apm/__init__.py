05cb5d4937b3e5e0ecf757fd77e73c3bd930817e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package awesome-pattern-matching/0.5.0/awesome-pattern-matching-0.5.0.tar.gz

---
+++ packages/awesome-pattern-matching/awesome-pattern-matching-0.5.0.tar.gz/awesome-pattern-matching-0.5.0/apm/__init__.py
@@ -0,0 +1,30 @@
+from .core import \
+    AllOf, \
+    Capture, \
+    Either, \
+    MatchContext, \
+    MatchResult, \
+    Not, \
+    OneOf, \
+    Pattern, \
+    Remaining, \
+    Some, \
+    Strict, \
+    match
+
+from .patterns import \
+    Arguments, \
+    At, \
+    Between, \
+    Check, \
+    Contains, \
+    Each, \
+    EachItem, \
+    InstanceOf, \
+    Length, \
+    Regex, \
+    Returns, \
+    Transformed, \
+    Truish
+
+from .__pkginfo__ import __version__
