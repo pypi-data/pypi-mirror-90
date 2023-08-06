bbeb9dbb7057b70245e351c69712deef5c028006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:22 +0000

    Add package mopidy-async-client/0.6.2/mopidy_async_client-0.6.2-py3-none-any.whl

---
+++ packages/mopidy-async-client/mopidy_async_client-0.6.2-py3-none-any.whl/mopidy_async_client/__init__.py
@@ -0,0 +1,11 @@
+"""Async Mopidy Client via JSON/RPC Websocket interface"""
+
+# Fork of https://github.com/ismailof/mopidy-json-client by ismailof
+__author__ = 'svinerus (svinerus@gmail.com)'
+__version__ = '0.6.2'
+
+from .client import MopidyClient
+
+__all__ = [
+    'MopidyClient',
+]
