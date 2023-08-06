75fc0a672d12e3e26d7ec0bcfcf61fb625ab362b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:30 +0000

    Add package git-improved/0.1.3/git_improved-0.1.3-py3-none-any.whl

---
+++ packages/git-improved/git_improved-0.1.3-py3-none-any.whl/git_improved/constants.py
@@ -0,0 +1,36 @@
+
+BRANCHES_PREFIXES = {
+    'bugfix':           'Bugfix',
+    'fix':              'Bugfix', 
+    'documentation':    'Documentation', 
+    'doc':              'Documentation',
+    'feature':          'Feature', 
+    'structure':        'Structure',
+    'devops':           'CI/CD',
+    'ci':               'CI/CD',
+    'cd':               'CI/CD',
+    'enhancement':      'Enhancement',
+    'improve':          'Enhancement',
+    'improvement':      'Enhancement'
+}
+
+CATEGORIES_ICONS = {
+    'Bugfix':           ':fire:',
+    'Documentation':    ':books:',
+    'CI/CD':            ':rocket:',
+    'Enhancement':      ':sparkles:',
+    'Feature':          ':heavy_plus_sign:',
+    'Structure':        ':hammer:',
+    'Release':          ':package:'
+}
+
+GITHUB_ICONS_URLS = {
+    ':fire:': 'https://github.githubassets.com/images/icons/emoji/unicode/1f525.png',
+    ':books:': 'https://github.githubassets.com/images/icons/emoji/unicode/1f4da.png',
+    ':rocket:': 'https://github.githubassets.com/images/icons/emoji/unicode/1f680.png',
+    ':sparkles:': 'https://github.githubassets.com/images/icons/emoji/unicode/2728.png',
+    ':heavy_plus_sign:': 'https://github.githubassets.com/images/icons/emoji/unicode/2795.png',
+    ':hammer:': 'https://github.githubassets.com/images/icons/emoji/unicode/1f528.png',
+    ':package:': 'https://github.githubassets.com/images/icons/emoji/unicode/1f4e6.png',
+    ':star:': 'https://github.githubassets.com/images/icons/emoji/unicode/2b50.png'
+}
