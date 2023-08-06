c475698c0852decaabdddf468553ca19f7794634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:58 +0000

    Add package bryson/0.0.6/bryson-0.0.6.tar.gz

---
+++ packages/bryson/bryson-0.0.6.tar.gz/bryson-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bryson", # Replace with your own username
+    version="0.0.6",
+    author="Jonathan Zavialov",
+    author_email="jonzavialov@gmail.com",
+    description="A package using various blockchains to generate a random number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://bryson.studio/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
