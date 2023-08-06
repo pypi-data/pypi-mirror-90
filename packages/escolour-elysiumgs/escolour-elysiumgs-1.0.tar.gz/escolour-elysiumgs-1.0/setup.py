0c342f124f56a1e73e493c04d51a4e3934e41d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:21 +0000

    Add package escolour-elysiumgs/1.0/escolour-elysiumgs-1.0.tar.gz

---
+++ packages/escolour-elysiumgs/escolour-elysiumgs-1.0.tar.gz/escolour-elysiumgs-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='escolour-elysiumgs',
+    version='1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/lewisyouldon/escolour',
+    license='MIT Licence',
+    author='Lewis Youldon',
+    author_email='lewisyouldon@gmail.com',
+    description='A basic package for having vars for all term colours. Useful for use in formatted strings i.e: f"{RED}This is red{RST}"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
