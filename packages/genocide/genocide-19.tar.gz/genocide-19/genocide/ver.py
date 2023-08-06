c8bf49456461b758543bb1988fceb590e42bfef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:25 +0000

    Add package genocide/19/genocide-19.tar.gz

---
+++ packages/genocide/genocide-19.tar.gz/genocide-19/genocide/ver.py
@@ -0,0 +1,7 @@
+# GENOCIDE - One decision from a judge that it is after 2002
+#
+# this file is placed in the public domain
+
+def ver(event):
+    from genocide.req import __version__
+    event.reply("GENOCIDE %s | OTP-CR-117/19/001 | otp.informationdesk@icc-cpi.int | https://genocide.rtfd.io" % __version__)
