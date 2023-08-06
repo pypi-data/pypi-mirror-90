036530b9018b032e60408f9415005d9d16ea38e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package petit-interfacer/0.1.4/petit_interfacer-0.1.4-py2.py3-none-any.whl

---
+++ packages/petit-interfacer/petit_interfacer-0.1.4-py2.py3-none-any.whl/petit_interfacer/exceptions.py
@@ -0,0 +1,10 @@
+
+
+class MissingHints(Exception):
+    """The interfacer could'nt find a suitable match for the given prototype and function
+    """
+
+
+class TooManyBlindBinds(Exception):
+    """Too many BlindBinds in the given prototype
+    """
