75ebb7eb0827c58b39eee582891e707baff7cdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:14 +0000

    Add package crazyweirdpackage/0.0.1/crazyweirdpackage-0.0.1.tar.gz

---
+++ packages/crazyweirdpackage/crazyweirdpackage-0.0.1.tar.gz/crazyweirdpackage-0.0.1/crazyweirdpackage/__init__.py
@@ -0,0 +1,20 @@
+# class count_args:
+#   def count(args):
+#     args=args.split(" ")
+#     return len(args)-1
+# args_count=count_args
+# class split_args:
+#   def list(args):
+#     args=args.split(" ")
+#     return args
+# args_split=split_args
+
+
+def add(a,b):
+  return a+b
+def subtract(a,b):
+  return a-b
+def multiply(a,b):
+  return a*b
+def divide(a,b):
+  return a/b
