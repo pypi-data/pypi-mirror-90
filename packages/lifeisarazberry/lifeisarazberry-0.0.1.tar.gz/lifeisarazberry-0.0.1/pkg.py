a3550049c524592056d4866a21b1e904444267da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:11 +0000

    Add package lifeisarazberry/0.0.1/lifeisarazberry-0.0.1.tar.gz

---
+++ packages/lifeisarazberry/lifeisarazberry-0.0.1.tar.gz/lifeisarazberry-0.0.1/pkg.py
@@ -0,0 +1,3 @@
+import pkg_upload
+
+pkg_upload.package(pkg_name="lifeisarazberry",author="PkgDev",email="example@example.com",version="0.0.1",short_description="Test run",long_description="None",github_url="https://github.com",python_requirment=">=3.2")
