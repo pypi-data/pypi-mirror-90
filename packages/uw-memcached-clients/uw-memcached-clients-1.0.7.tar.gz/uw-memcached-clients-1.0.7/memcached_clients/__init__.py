1f68e6e8f309d55697b2c2a7776dd9702f69256c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package uw-memcached-clients/1.0.7/uw-memcached-clients-1.0.7.tar.gz

---
+++ packages/uw-memcached-clients/uw-memcached-clients-1.0.7.tar.gz/uw-memcached-clients-1.0.7/memcached_clients/__init__.py
@@ -0,0 +1,2 @@
+from memcached_clients.base import PymemcacheClient, MemcacheError
+from memcached_clients.restclient import RestclientPymemcacheClient
---
+++ packages/uw-memcached-clients/uw-memcached-clients-1.0.7.tar.gz/uw-memcached-clients-1.0.7/memcached_clients/base.py
@@ -0,0 +1,48 @@
+from pymemcache.exceptions import MemcacheError
+from pymemcache import HashClient
+from pymemcache import serde
+from commonconf import settings
+from threading import local
+from logging import getLogger
+import socket
+
+logger = getLogger(__name__)
+
+
+class PymemcacheClient():
+    """
+    A settings-based wrapper around pymemcache.
+    """
+    def __init__(self):
+        self._local = local()
+
+    def __getattr__(self, name, *args, **kwargs):
+        """
+        Pass unshimmed method calls through to the client, and add logging
+        for errors.
+        """
+        def handler(*args, **kwargs):
+            try:
+                return getattr(self.client, name)(*args, **kwargs)
+            except (MemcacheError, socket.gaierror) as ex:
+                logger.error("memcached {}: {}".format(name, ex))
+            except AttributeError:
+                raise
+        return handler
+
+    @property
+    def client(self):
+        if hasattr(self._local, "client"):
+            return self._local.client
+
+        client = HashClient(
+            getattr(settings, "MEMCACHED_SERVERS", []),
+            use_pooling=getattr(settings, "MEMCACHED_USE_POOLING", True),
+            max_pool_size=getattr(settings, "MEMCACHED_MAX_POOL_SIZE", 10),
+            connect_timeout=getattr(settings, "MEMCACHED_CONNECT_TIMEOUT", 2),
+            timeout=getattr(settings, "MEMCACHED_TIMEOUT", 2),
+            default_noreply=getattr(settings, "MEMCACHED_NOREPLY", True),
+            serde=serde.pickle_serde)
+
+        self._local.client = client
+        return client
