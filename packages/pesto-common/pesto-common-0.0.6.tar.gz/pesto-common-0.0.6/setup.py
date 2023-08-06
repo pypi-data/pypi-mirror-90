758df2aa70f367e72c4c09112be47919e4857062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:41 +0000

    Add package pesto-common/0.0.6/pesto-common-0.0.6.tar.gz

---
+++ packages/pesto-common/pesto-common-0.0.6.tar.gz/pesto-common-0.0.6/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+requirements = []
+
+setup(name='pesto-common',
+      version='0.0.6',
+      author='Dreampie',
+      author_email='Dreampie@outlook.com',
+      url='https://github.com/Dreampie/pesto',
+      description='Minimalist python utils',
+      long_description='Minimalist python utils',
+      long_description_content_type='text/markdown',
+      keywords=['minimalist', 'python', 'utils', 'pesto'],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      platforms=['all']
+      )
