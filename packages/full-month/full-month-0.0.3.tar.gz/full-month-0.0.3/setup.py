df535f8bcfd4905845a6479ce71464961b89dec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:39 +0000

    Add package full-month/0.0.3/full-month-0.0.3.tar.gz

---
+++ packages/full-month/full-month-0.0.3.tar.gz/full-month-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(name='full-month',
+      version='0.0.3',
+      description='show the full display of one month of 5 row * 7 day format,starting from Monday and ending on Sunday.',
+      author='neomaple',
+      author_email='xinkun7778@163.com',
+      license='None',
+      packages=find_packages())
