cf31c170cb78e238f13e913e8617c39b4565d85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:38 +0000

    Add package ez-qt/1.0.7/ez_qt-1.0.7.tar.gz

---
+++ packages/ez-qt/ez_qt-1.0.7.tar.gz/ez_qt-1.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ez_qt',
+    version='1.0.7',
+    packages=['ez_qt'],
+    url='https://www.github.com/nielsvaes/ez_qt',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='Niels Vaes',
+    author_email='nielsvaes@gmail.com',
+    description='Helper functions and classes to work with PySide2/PyQt5'
+)
