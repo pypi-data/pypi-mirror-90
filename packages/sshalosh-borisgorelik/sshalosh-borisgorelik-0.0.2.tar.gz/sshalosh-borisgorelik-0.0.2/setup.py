062d68a3480ba4a89813ef4e7cf5e102afabb3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:58 +0000

    Add package sshalosh-borisgorelik/0.0.2/sshalosh-borisgorelik-0.0.2.tar.gz

---
+++ packages/sshalosh-borisgorelik/sshalosh-borisgorelik-0.0.2.tar.gz/sshalosh-borisgorelik-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="sshalosh-borisgorelik",
+    version="0.0.2",
+    author="Boris Gorelik",
+    author_email="boris@gorelik.net",
+    description="convenience functions for serialization to s3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bgbg/shalosh",
+    packages=setuptools.find_packages('./'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
