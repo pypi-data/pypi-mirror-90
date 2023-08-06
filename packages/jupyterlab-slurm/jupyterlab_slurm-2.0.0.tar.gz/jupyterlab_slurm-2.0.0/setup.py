e35c6b6d83d4457bd7b58be51c6d4d8658026e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:08 +0000

    Add package jupyterlab-slurm/2.0.0/jupyterlab_slurm-2.0.0.tar.gz

---
+++ packages/jupyterlab-slurm/jupyterlab_slurm-2.0.0.tar.gz/jupyterlab_slurm-2.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from jupyterlab_slurm import __version__
+
+
+long_description = 'A Jupyter Notebook server extension that implements \
+                    common Slurm commands, which are executed in the server\'s shell; \
+                    to be used in conjuction with the jupyterlab-slurm JupyterLab extension.'
+
+# Allows automatic enabling of the server extension; see the docs:
+# https://jupyter-notebook.readthedocs.io/en/stable/examples/Notebook/Distributing%20Jupyter%20Extensions%20as%20Python%20Packages.html
+data_files = [
+    ("etc/jupyter/jupyter_notebook_config.d", [
+        "jupyter-config/jupyter_notebook_config.d/jupyterlab_slurm.json"
+    ])
+]
+
+setup(
+    name='jupyterlab_slurm',
+    version=__version__,
+    description='A Jupyter Notebook server extension that implements common Slurm commands.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("node_modules",)),
+    include_package_data=True,
+    data_files=data_files,
+    author='NERSC, Jon Hays, William Krinsman, Justin Ley',
+    license='BSD 3-Clause',
+    url='https://github.com/NERSC/jupyterlab-slurm',
+    keywords=['Jupyter', 'Jupyterlab', 'Slurm', 'NERSC'],
+    python_requires='>=3.6',
+    install_requires=['notebook']
+)
