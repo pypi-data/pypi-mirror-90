a372eb734c58f1572b65310c13cc36c10f4b7b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:29 +0000

    Add package pyuvm/1.0a7/pyuvm-1.0a7-py3-none-any.whl

---
+++ packages/pyuvm/pyuvm-1.0a7-py3-none-any.whl/pyuvm/error_classes.py
@@ -0,0 +1,44 @@
+class UVMNotImplemented(NotImplementedError):
+    """For methods that we haven't yet implemented."""
+
+
+class UVMError(Exception):
+    """
+    All UVM Errors
+    """
+
+
+class UsePythonMethod(NotImplementedError):
+    """
+    For cases where the user should use a Python
+    method rather than a UVM method.  For example
+    use __str__() instead of convert2string()
+    """
+
+
+class UVMFactoryError(UVMError):
+    """ For cases where a type is not registered with the factory"""
+
+
+class UVMTLMConnectionError(UVMError):
+    """For problems connecting TLM"""
+
+
+class UVMBadPhase(UVMError):
+    """Errors in phasing"""
+
+
+class UVMSequenceError(UVMError):
+    """Errors using sequences"""
+
+
+class UVMConfigError(UVMError):
+    """Errors using the config_db"""
+
+
+class UVMConfigItemNotFound(UVMError):
+    """Couldn't find something in config_db"""
+
+class UVMFatalError(UVMError):
+    """ Used to dump out of the testbench"""
+
---
+++ packages/pyuvm/pyuvm-1.0a7-py3-none-any.whl/pyuvm/s06_reporting_classes.py
@@ -0,0 +1,33 @@
+'''
+pyuvm uses the Python logging system to do reporting.  Still, we need this base class
+to be true to the hierarchy.  Every instance of a child class has its own logger.
+
+There may be a need to implement uvm_info, uvm_error, uvm_warning, and uvm_fatal, but
+it would be best to first see how the native Python logging system does the job.
+'''
+from pyuvm.s05_base_classes import uvm_object
+import logging
+
+
+# 6.2.1
+class uvm_report_object(uvm_object):
+    def __init__(self, name):
+        super().__init__(name)
+        full_name = self.get_full_name()
+        self.logger = logging.getLogger(full_name)
+        self.logger.setLevel(level=logging.DEBUG)
+        self.logger.propagate=False
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(levelname)s: %(filename)s(%(lineno)d)[%(name)s]: %(message)s")
+        handler.setFormatter(formatter)
+        self.add_logging_handler(handler)
+        pass
+
+    def set_logging_level(self, logging_level):
+        for handler in self.logger.handlers:
+            handler.setLevel(logging_level)
+
+    def add_logging_handler(self, handler):
+        assert isinstance(handler, logging.Handler), f"You must pass a logging.Handler not {type(handler)}"
+        self.logger.addHandler(handler)
