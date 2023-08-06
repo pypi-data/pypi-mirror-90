e8dc75213a14f0f649768340dc1b5210f23769b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:47 +0000

    Add package inventory-management/0.0.1/inventory_management-0.0.1-py3-none-any.whl

---
+++ packages/inventory-management/inventory_management-0.0.1-py3-none-any.whl/inventory_management/__meta__.py
@@ -0,0 +1,8 @@
+name = "inventory_management"  # See https://www.python.org/dev/peps/pep-0008/
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = "0.0.1"  # https://python.org/dev/peps/pep-0440 https://semver.org
+author = "Ian Eaves"
+author_email = "ian.k.eaves@gmail.com"
+description = ""  # One-liner
+url = ""  # your project homepage
+license = "MIT"  # See https://choosealicense.com
