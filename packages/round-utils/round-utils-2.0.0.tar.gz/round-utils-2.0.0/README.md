2ce30d6d84375a7f91516a7620e8934aed3e11fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package round-utils/2.0.0/round-utils-2.0.0.tar.gz

---
+++ packages/round-utils/round-utils-2.0.0.tar.gz/round-utils-2.0.0/README.md
@@ -0,0 +1,18 @@
+![Codecov](https://codecov.io/gh/IliyaZinoviev/round-utils/branch/master/graph/badge.svg)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+# Round utils
+This package provides functions to round numbers up to a multiplicity of a pointed step.
+Float value's rounded by literal representation.
+# Example of usage
+
+```python
+>>> import round_utils
+
+>>> round_utils.ceil(32.333, 0.02)
+32.34
+
+>>> round_utils.floor(32.333, 0.02)
+32.32
+```
+
+
---
+++ packages/round-utils/round-utils-2.0.0.tar.gz/round-utils-2.0.0/source/round_utils/round.py
@@ -0,0 +1,81 @@
+import re
+from itertools import repeat
+
+
+def _get_real_int_parts(num_str, num_del_pos):
+    if num_str[0] == '0':
+        int_num_part = None
+        re_obj = re.search(r'[1-9]\d*$', num_str[2:])
+        real_num_part = re_obj[0] if re_obj else ''
+    else:
+        int_num_part = num_str[:num_del_pos]
+        real_num_part = num_str[num_del_pos+1:]
+    return int_num_part, real_num_part
+
+
+def _exec_round_on_step(val, step, is_up):
+    remainder = val % step
+    if remainder == 0:
+        return val
+    res = val - remainder
+    return res if not is_up else res + step
+
+
+def _convert_int_str2float(int_str, int_str2, count):
+    if len(int_str) > len(int_str2):
+        count -= 1
+    elif len(int_str) < len(int_str2):
+        count += 1
+    return count
+
+
+def _get_del_pos_and_int_num(num_str):
+    num_del_pos = num_str.find('.')
+    int_num_part, real_num_part = _get_real_int_parts(num_str, num_del_pos)
+    int_num_str = int_num_part + real_num_part if int_num_part else real_num_part
+    return num_del_pos, int(int_num_str), int_num_str, real_num_part
+
+
+def _convert_norm_float_to_float_str(num: float) -> str:
+    if groups := re.match(r'^(?P<num>-?\d(.\d+)?)e-0?(?P<pow>\d+)$', str(num)):
+        return '0.' + ''.join(list(repeat('0', int(groups['pow']) - 1))) + ''.join(groups['num'].split('.'))
+    else:
+        return str(num)
+
+
+def _round_on_step(val, step, is_up=False):
+    is_neg = val < 0
+    if is_neg:
+        is_up = not is_up
+        val *= -1
+    if step > val:
+        if is_neg:
+            step *= -1
+        return step if is_up else 0
+    if step % 1 == 0:
+        return _exec_round_on_step(int(val), step, is_up)
+    step_str = _convert_norm_float_to_float_str(step)
+    val_str = _convert_norm_float_to_float_str(val)
+    if isinstance(val, int):
+        val_str += '.0'
+    val_str = val_str[:len(step_str.split('.')[1] + val_str.split('.')[0]) + 1]
+
+    step_del_pos,  int_step, _, _ = _get_del_pos_and_int_num(step_str)
+    val_del_pos, int_val, int_val_str, real_val_part = _get_del_pos_and_int_num(val_str)
+    int_res_str = str(_exec_round_on_step(int_val, int_step, is_up))
+
+    if val_zeros_count := len(val_str) - len(real_val_part) - 2 if val_str[0] == '0' else None:
+        val_zeros_count = _convert_int_str2float(int_res_str, int_val_str, val_zeros_count)
+        res = float('0.' + ''.join(list(repeat('0', val_zeros_count))) + int_res_str)
+    else:
+        val_del_pos = _convert_int_str2float(int_val_str, int_res_str, val_del_pos)
+        res = float(int_res_str[:val_del_pos] + '.' + int_res_str[val_del_pos:])
+    return -res if is_neg else res
+
+
+def ceil(val: float, step: float) -> float:
+    return _round_on_step(val, step, True)
+
+
+def floor(val: float, step: float) -> float:
+    return _round_on_step(val, step)
