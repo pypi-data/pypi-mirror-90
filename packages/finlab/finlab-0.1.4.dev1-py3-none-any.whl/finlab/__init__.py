5d5e153c93664860f57b56565e2f3c2edca45513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:54 +0000

    Add package finlab/0.1.4.dev1/finlab-0.1.4.dev1-py3-none-any.whl

---
+++ packages/finlab/finlab-0.1.4.dev1-py3-none-any.whl/finlab/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+__version__ = '0.1.4.dev1'
+
+class Token:
+    def __init__(self, interaction=True):
+
+        if interaction and 'finlab_id_token' not in os.environ:
+            id_token = input('Please login to finlab and copy id token here: \n')
+            os.environ['finlab_id_token'] = id_token
+        pass
+
+    def set(self, token):
+        os.environ['finlab_id_token'] = token
+
+    def get(self):
+        if 'finlab_id_token' in os.environ:
+            return os.environ['finlab_id_token']
+        else:
+            return None
