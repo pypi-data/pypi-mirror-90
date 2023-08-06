5a0191fbb3e93f11ac91584af46408623110220b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package croudtech-python-aws-lambda-helpers/1.0.19/croudtech-python-aws-lambda-helpers-1.0.19.tar.gz

---
+++ packages/croudtech-python-aws-lambda-helpers/croudtech-python-aws-lambda-helpers-1.0.19.tar.gz/croudtech-python-aws-lambda-helpers-1.0.19/tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for croudtech_python_aws_lambda_helpers.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
