516b5f0f5aa41b2d692f1856704d22d749155f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:45 +0000

    Add package pt-datasets/0.6.2/pt-datasets-0.6.2.tar.gz

---
+++ packages/pt-datasets/pt-datasets-0.6.2.tar.gz/pt-datasets-0.6.2/pt_datasets/__init__.py
@@ -0,0 +1,21 @@
+# PyTorch Datasets utility repository
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""PyTorch dataset loader repository"""
+from .create_dataloader import create_dataloader
+from .encode_features import encode_features
+from .load_dataset import load_dataset
+
+__author__ = "Abien Fred Agarap"
