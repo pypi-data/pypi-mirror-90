fb8b68d03a9c86cd4f3fcc506fccc01604a93547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:08 +0000

    Add package twitterbot2/0.0.2/twitterbot2-0.0.2-py3.7.egg

---
+++ packages/twitterbot2/twitterbot2-0.0.2-py3.7.egg/twitterbot/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Yoni Pacheco'
+__email__ = 'aniversarioperu1@gmail.com'
+__version__ = '0.0.2'
+
+from twitterbot.bot import TwitterBot
+
+__all__ = ['TwitterBot']
---
+++ packages/twitterbot2/twitterbot2-0.0.2-py3.7.egg/twitterbot/bot.py
@@ -0,0 +1,46 @@
+import base64
+
+import requests
+import tempfile
+from requests_oauthlib import OAuth1
+
+
+class TwitterBot(object):
+    def __init__(self, consumer_key, consumer_secret, oauth_token, oauth_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
+        self.oauth = self.get_oauth()
+
+    def get_oauth(self):
+        oauth = OAuth1(
+            self.consumer_key,
+            client_secret=self.consumer_secret,
+            resource_owner_key=self.oauth_token,
+            resource_owner_secret=self.oauth_token_secret,
+        )
+        return oauth
+
+    def send_tweet(self, message):
+        payload = {'status': message}
+        url = 'https://api.twitter.com/1.1/statuses/update.json'
+        res = requests.post(url=url, auth=self.oauth, params=payload)
+        return res.json()
+
+    def tweet_with_photo(self, message, photo_base64):
+        image_encoded = photo_base64.encode('utf-8')
+
+        image_bytes = base64.decodebytes(image_encoded)
+        payload = {'media': image_bytes}
+        url = "https://upload.twitter.com/1.1/media/upload.json"
+        res = requests.post(url, auth=self.oauth, files=payload)
+        print(1, res.status_code, res.text, res.content)
+
+        media_id = res.json()['media_id']
+
+        payload = {'status': message, 'media_ids': [media_id]}
+        url = 'https://api.twitter.com/1.1/statuses/update.json'
+        res = requests.post(url=url, auth=self.oauth, params=payload)
+        print(2, res)
+        return res.json()
