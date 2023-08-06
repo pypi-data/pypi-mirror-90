69ff6936e42492b8c9ccdd4eea2379c1b5487996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:08 +0000

    Add package bitsv/0.11.4/bitsv-0.11.4.tar.gz

---
+++ packages/bitsv/bitsv-0.11.4.tar.gz/bitsv-0.11.4/setup.py
@@ -0,0 +1,60 @@
+from setuptools import find_packages, setup
+
+with open('bitsv/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('= ')[1].strip("'")
+            break
+
+setup(
+    name='bitsv',
+    version=version,
+    description='Bitcoin SV made easier.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    author='AustEcon',
+    author_email='AustEcon0922@gmail.com',
+    maintainer='AustEcon',
+    maintainer_email='AustEcon0922@gmail.com',
+    url='https://github.com/AustEcon/bitsv',
+    download_url='https://github.com/AustEcon/bitsv/tarball/{}'.format(version),
+    license='MIT',
+
+    keywords=[
+        'bitcoinsv',
+        'bsv',
+        'bitcoin sv',
+        'cryptocurrency',
+        'payments',
+        'tools',
+        'wallet',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
+    install_requires=['coincurve>=4.3.0', 'requests', 'python-bitcoinrpc', 'whatsonchain'],
+    extras_require={
+        'cli': ('appdirs', 'click', 'privy', 'tinydb'),
+        'cache': ('lmdb', ),
+    },
+    tests_require=['pytest'],
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': (
+            'bitsv = bitsv.cli:bitsv',
+        ),
+    },
+)
