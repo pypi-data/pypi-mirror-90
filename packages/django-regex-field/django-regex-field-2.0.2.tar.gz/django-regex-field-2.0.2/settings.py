80db82bea0e05303cb083bb74a962a5712c6a040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:36 +0000

    Add package django-regex-field/2.0.2/django-regex-field-2.0.2.tar.gz

---
+++ packages/django-regex-field/django-regex-field-2.0.2.tar.gz/django-regex-field-2.0.2/settings.py
@@ -0,0 +1,54 @@
+import os
+
+from django.conf import settings
+
+
+def configure_settings():
+    """
+    Configures settings for manage.py and for run_tests.py.
+    """
+    if not settings.configured:
+        # Determine the database settings depending on if a test_db var is set in CI mode or not
+        test_db = os.environ.get('DB', None)
+        if test_db is None:
+            db_config = {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'ambition_test',
+                'USER': 'postgres',
+                'PASSWORD': '',
+                'HOST': 'db'
+            }
+        elif test_db == 'postgres':
+            db_config = {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'USER': 'postgres',
+                'NAME': 'regex_field',
+            }
+        elif test_db == 'sqlite':
+            db_config = {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'regex_field',
+            }
+        else:
+            raise RuntimeError('Unsupported test DB {0}'.format(test_db))
+
+        installed_apps = [
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.admin',
+            'regex_field',
+            'regex_field.tests',
+        ]
+
+        settings.configure(
+            DATABASES={
+                'default': db_config,
+            },
+            MIDDLEWARE_CLASSES={},
+            INSTALLED_APPS=installed_apps,
+            ROOT_URLCONF='regex_field.urls',
+            DEBUG=False,
+            NOSE_ARGS=['--nocapture', '--nologcapture', '--verbosity=1'],
+            TEST_RUNNER='django_nose.NoseTestSuiteRunner',
+        )
---
+++ packages/django-regex-field/django-regex-field-2.0.2.tar.gz/django-regex-field-2.0.2/setup.py
@@ -0,0 +1,57 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'regex_field/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+setup(
+    name='django-regex-field',
+    version=get_version(),
+    description='Django Regex Field',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ambitioninc/django-regex-field',
+    author='Wes Kendall',
+    author_email='opensource@ambition.com',
+    keywords='Django, Regex Field',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+    ],
+    license='MIT',
+    install_requires=[
+        'Django>=2.2',
+        'six',
+    ],
+    tests_require=[
+        'psycopg2',
+        'django-nose>=1.4',
+        'mock',
+    ],
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+    zip_safe=False,
+)
