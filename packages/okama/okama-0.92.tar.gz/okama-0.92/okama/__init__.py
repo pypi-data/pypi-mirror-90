aaee659e7115d2e426efc1484cb27aca7c6f7622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:31 +0000

    Add package okama/0.92/okama-0.92.tar.gz

---
+++ packages/okama/okama-0.92.tar.gz/okama-0.92/okama/__init__.py
@@ -0,0 +1,32 @@
+"""
+*okama* is a Python package developed for asset allocation and investments portfolio optimization tasks
+according to Modern Portfolio Theory (MPT).
+=====================================================================
+The package is supplied with **free** «end of day» historical stock markets data and macroeconomic indicators through API.
+
+Main features:
+
+- Investment portfolio constrained Markowitz Mean-Variance Analysis (MVA) and optimization
+- Rebalanced portfolio optimization
+- Monte Carlo Simulations for financial assets and investment portfolios
+- Popular risk metrics: VAR, CVaR, semi-deviation, variance and drawdowns
+- Forecasting models according to normal and lognormal distribution
+- Testing distribution on historical data
+- Dividend yield and other dividend indicators for stocks
+- Backtesting and comparing historical performance of broad range of assets and indexes in multiple currencies
+- Methods to track the performance of index funds (ETF) and compare them with benchmarks
+- Main macroeconomic indicators: inflation, central banks rates
+- Matplotlib visualization scripts for the Efficient Frontier, Transition map and assets risk / return performance
+
+"""
+
+from okama.assets import Asset, AssetList, Portfolio
+from okama.macro import Inflation, Rate
+from okama.frontier import EfficientFrontier
+from okama.frontier_reb import EfficientFrontierReb
+from okama.plots import Plots
+from okama.data import QueryData, API, search, namespaces, assets_namespaces, macro_namespaces
+from okama.helpers import Float, Frame, Rebalance, Date
+import okama.settings
+
+__version__ = '0.92'
