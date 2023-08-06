8d21649a52e42c8636ca72e13c6c09da4fee7686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:44 +0000

    Add package phonex/0.0.1/phonex-0.0.1.tar.gz

---
+++ packages/phonex/phonex-0.0.1.tar.gz/phonex-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='phonex',
+      version='0.0.1',
+      description='Phonex algorithm ported to python 3',
+      url='https://github.com/damiencorpataux/phonex',
+      author='Damien Corpataux',
+      author_email='d@mien.ch',
+      license='MIT',
+      packages=['phonex'],
+      zip_safe=False)
