9322073a94c91f6e8ec5545016e761d3be7a7dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package financeager/0.25.0.0/financeager-0.25.0.0-py2.py3-none-any.whl

---
+++ packages/financeager/financeager-0.25.0.0-py2.py3-none-any.whl/financeager/exceptions.py
@@ -0,0 +1,25 @@
+"""Module defining expections of the financeager application."""
+
+
+class FinanceagerException(Exception):
+    """Base class for exceptions raised by financeager."""
+
+
+class PreprocessingError(FinanceagerException):
+    """Error raised during preprocessing of user input."""
+
+
+class InvalidRequest(FinanceagerException):
+    """Server indicated invalid request."""
+
+
+class CommunicationError(FinanceagerException):
+    """Error in client-server communication."""
+
+
+class InvalidConfigError(FinanceagerException):
+    """Invalid configuration encountered."""
+
+
+class ConversionError(FinanceagerException):
+    """Invalid input for database conversion."""
