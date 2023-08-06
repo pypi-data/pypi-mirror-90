afd38c437690dc3fca73030b1dbf376ba86ce7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:40 +0000

    Add package pystokes/2.2.4/pystokes-2.2.4.tar.gz

---
+++ packages/pystokes/pystokes-2.2.4.tar.gz/pystokes-2.2.4/pystokes/__init__.py
@@ -0,0 +1,14 @@
+import pystokes.interface
+import pystokes.periodic
+import pystokes.twoWalls
+import pystokes.unbounded
+import pystokes.utils
+import pystokes.wallBounded
+import pystokes.forceFields
+import pystokes.phoretic.unbounded
+import pystokes.phoretic.wallBounded
+
+
+
+## latest version of PyStokes
+__version__="2.2.4"
