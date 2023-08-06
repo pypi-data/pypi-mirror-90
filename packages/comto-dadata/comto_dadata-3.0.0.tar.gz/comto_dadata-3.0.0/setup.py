5edaf04398797aae73b908abce8261fc53565e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:45 +0000

    Add package comto-dadata/3.0.0/comto_dadata-3.0.0.tar.gz

---
+++ packages/comto-dadata/comto_dadata-3.0.0.tar.gz/comto_dadata-3.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="comto_dadata",
+    version="3.0.0",
+    author="commercito",
+    author_email="commercito@gmail.com",
+    description="Работа с сервисом \"dadata.ru\": запросы, обработка, итд",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://commercito.ru",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Natural Language :: Russian",
+        "License :: Freely Distributable",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.6',
+)
