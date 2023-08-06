d85b629ca6252ecfa687672da347758aa595ea98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:28 +0000

    Add package WebXplore/1.0.3/WebXplore-1.0.3.tar.gz

---
+++ packages/WebXplore/WebXplore-1.0.3.tar.gz/WebXplore-1.0.3/webxplore/utils/Utilities.py
@@ -0,0 +1,20 @@
+import nltk
+
+
+class UtilFunctions:
+    """
+
+    Random Functions that are useful for all the other modules
+
+    """
+
+    @staticmethod
+    def separateSentences(input_Text):
+        # Ensure NLTK Downloads
+        nltk.download('vader_lexicon', quiet=True)
+        nltk.download('punkt', quiet=True)
+        nltk.download('stopwords', quiet=True)
+
+        # Separate the sentences
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        return tokenizer.tokenize(input_Text)
