a1d88f138bf8343929780355d3d0edfe92503131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:43 +0000

    Add package G4S/0.0.3/G4S-0.0.3.tar.gz

---
+++ packages/G4S/G4S-0.0.3.tar.gz/G4S-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="G4S",  # Replace with your own username
+    version="0.0.3",
+    author="PTST",
+    author_email="patrick@steffensen.io",
+    description="API interface for G4S Alarms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PTST/G4S_Alarm_Py",
+    packages=setuptools.find_packages(),
+    install_requires=["requests >= 2.25.0", "python-dateutil >= 2.8.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
