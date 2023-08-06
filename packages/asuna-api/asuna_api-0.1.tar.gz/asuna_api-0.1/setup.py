868b5b791da4723e4ad61d46c25b4ff9f3c30915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:10 +0000

    Add package asuna-api/0.1/asuna_api-0.1.tar.gz

---
+++ packages/asuna-api/asuna_api-0.1.tar.gz/asuna_api-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'asuna_api',
+  packages = ['asuna_api'],
+  version = '0.1', 
+  license='MIT', 
+  description = 'A python wrapper for the asuna api',
+  author = 'JDJGIncOfficial',
+  author_email = 'jdjgbot@gmail.com',
+  url = 'https://github.com/JDJGInc/asuna_api',
+  download_url = 'https://github.com/JDJGInc/asuna_api/archive/0.01.tar.gz',
+  keywords = ['wrapper', 'api', 'random'],
+  install_requires=['aiohttp','yarl'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
