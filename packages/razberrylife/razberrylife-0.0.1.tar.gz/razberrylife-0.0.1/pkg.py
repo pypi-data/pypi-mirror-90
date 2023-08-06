3af2822f19bdd5a1fab3b14dd58ef04509c89e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:20 +0000

    Add package razberrylife/0.0.1/razberrylife-0.0.1.tar.gz

---
+++ packages/razberrylife/razberrylife-0.0.1.tar.gz/razberrylife-0.0.1/pkg.py
@@ -0,0 +1,3 @@
+import pkg_upload
+
+pkg_upload.package(pkg_name="razberrylife",author="PkgDev",email="example@example.com",version="0.0.1",short_description="Test run",long_description="None",github_url="https://github.com",python_requirment=">=3.2")
---
+++ packages/razberrylife/razberrylife-0.0.1.tar.gz/razberrylife-0.0.1/razberrylife/__init__.py
@@ -0,0 +1,5 @@
+class awesome(object):
+	def __init__(self,result):
+		self.result=result
+	def test(self):
+		return self.result
