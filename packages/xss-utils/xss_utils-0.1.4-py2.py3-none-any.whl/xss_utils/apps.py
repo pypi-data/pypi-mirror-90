1287a1e2e79208306d6c841087c19d5e25b1dc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:37 +0000

    Add package xss-utils/0.1.4/xss_utils-0.1.4-py2.py3-none-any.whl

---
+++ packages/xss-utils/xss_utils-0.1.4-py2.py3-none-any.whl/xss_utils/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+xss_utils Django application initialization.
+"""
+from django.apps import AppConfig
+
+
+class XssUtilsConfig(AppConfig):
+    """
+    Configuration for the xss_utils Django application.
+    """
+
+    name = 'xss_utils'
