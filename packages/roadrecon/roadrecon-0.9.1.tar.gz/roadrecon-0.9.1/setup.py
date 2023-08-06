5542509e53a01a5e79529d00a7e30cd55b96d007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:48 +0000

    Add package roadrecon/0.9.1/roadrecon-0.9.1.tar.gz

---
+++ packages/roadrecon/roadrecon-0.9.1.tar.gz/roadrecon-0.9.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(name='roadrecon',
+      version='0.9.1',
+      description='Azure AD recon for red/blue',
+      author='Dirk-jan Mollema',
+      author_email='dirkjan@dirkjanm.io',
+      url='https://github.com/dirkjanm/ROADtools/',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      packages=['roadtools.roadrecon', 'roadtools.roadrecon.plugins'],
+      install_requires=['roadlib', 'flask', 'sqlalchemy', 'marshmallow', 'flask-sqlalchemy', 'flask-marshmallow', 'flask-cors', 'marshmallow-sqlalchemy>=0.22', 'aiohttp'],
+      zip_safe=False,
+      include_package_data=True,
+      entry_points={
+          'console_scripts': ['roadrecon-gui=roadtools.roadrecon.server:main',
+                              'roadrecon=roadtools.roadrecon.main:main']
+      }
+      )
