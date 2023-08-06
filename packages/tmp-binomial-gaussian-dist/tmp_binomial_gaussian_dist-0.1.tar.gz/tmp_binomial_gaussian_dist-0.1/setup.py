fa6476402bc5ae5f763bd254c9ce946b894302ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:35 +0000

    Add package tmp-binomial-gaussian-dist/0.1/tmp_binomial_gaussian_dist-0.1.tar.gz

---
+++ packages/tmp-binomial-gaussian-dist/tmp_binomial_gaussian_dist-0.1.tar.gz/tmp_binomial_gaussian_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tmp_binomial_gaussian_dist',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['tmp_binomial_gaussian_dist'],
+      author= 'saberny',
+      email = 'saberny@pm.me',
+      zip_safe=False)
