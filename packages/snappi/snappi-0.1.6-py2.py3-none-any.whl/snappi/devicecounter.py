4418ce7659963834534978a0345a990a4395906e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:37 +0000

    Add package snappi/0.1.6/snappi-0.1.6-py2.py3-none-any.whl

---
+++ packages/snappi/snappi-0.1.6-py2.py3-none-any.whl/snappi/devicecounter.py
@@ -0,0 +1,48 @@
+from .snappicommon import SnappiObject
+
+
+class DeviceCounter(SnappiObject):
+    def __init__(self, start=None, step=None):
+        super(DeviceCounter, self).__init__()
+        self.start = start
+        self.step = step
+
+    @property
+    def start(self):
+        """start getter
+
+        TBD
+
+        Returns: str
+        """
+        return self._properties['start']
+
+    @start.setter
+    def start(self, value):
+        """start setter
+
+        TBD
+
+        value: str
+        """
+        self._properties['start'] = value
+
+    @property
+    def step(self):
+        """step getter
+
+        TBD
+
+        Returns: str
+        """
+        return self._properties['step']
+
+    @step.setter
+    def step(self, value):
+        """step setter
+
+        TBD
+
+        value: str
+        """
+        self._properties['step'] = value
