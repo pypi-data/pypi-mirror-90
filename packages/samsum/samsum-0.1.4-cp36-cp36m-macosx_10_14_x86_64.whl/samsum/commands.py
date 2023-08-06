1966b7afde481be6c3a930dd2ce7d96d39531f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:34 +0000

    Add package samsum/0.1.4/samsum-0.1.4-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/samsum/samsum-0.1.4-cp36-cp36m-macosx_10_14_x86_64.whl/samsum/commands.py
@@ -0,0 +1,128 @@
+import os
+import logging
+import numpy
+
+from samsum import _version as ss_version
+from samsum import args as ss_args
+from samsum import classy as ss_class
+from samsum import logger as ss_log
+from samsum import file_parsers as ss_fp
+from samsum import utilities as ss_utils
+from samsum import alignment_utils as ss_aln_utils
+
+__author__ = 'Connor Morgan-Lang'
+
+
+def info(sys_args):
+    """
+    Function for writing version information about samsum and python dependencies.
+    Other related info (citation, executable versions, etc.) should also be written through this sub-command.
+    Create a SAMSumBase object for the `info` sub-command
+
+    :param sys_args: List of arguments parsed from the command-line.
+    :return: None
+    """
+    parser = ss_args.SAMSumArgumentParser(description="Return package and executable information.")
+    args = parser.parse_args(sys_args)
+    ss_log.prep_logging()
+    info_ss = ss_class.SAMSumBase("info")
+
+    logging.info("samsum version " + ss_version.__version__ + ".\n")
+
+    # Write the version of all python deps
+    py_deps = {"numpy": numpy.__version__}
+
+    logging.info("Python package dependency versions:\n\t" +
+                 "\n\t".join([k + ": " + v for k, v in py_deps.items()]) + "\n")
+
+    # TODO: Write path to installation directory
+
+    # Write the version of executable deps
+    info_ss.furnish_with_arguments(args)
+    logging.info(ss_utils.executable_dependency_versions(info_ss.executables))
+
+    if args.verbose:
+        pass
+        # logging.info(summary_str)
+
+    return 0
+
+
+def ref_sequence_abundances(aln_file: str, seq_file: str, map_qual=0, p_cov=50, min_aln=10, multireads=False) -> dict:
+    """
+    An API function that will return a dictionary of RefSequence instances indexed by their sequence names/headers
+    The RefSequence instances contain the populated variables:
+
+
+    :param aln_file: Path to a SAM/BAM file containing the read alignments to the reference FASTA
+    :param seq_file: Path to the reference FASTA file used to generate the SAM/BAM file
+    :param map_qual: The minimum mapping quality threshold for an alignment to pass
+    :param min_aln: The minimum percentage of a read's length that must be aligned to be included
+    :param multireads: Flag indicating whether reads that mapped ambiguously to multiple positions (multireads)
+    should be used in the counts
+    :param p_cov: The minimum percentage a reference sequence must be covered for its coverage stats to be included;
+    they are set to zero otherwise
+    :return: Dictionary of RefSequence instances indexed by their sequence names/headers
+    """
+    refseq_lengths = ss_fp.fasta_seq_lengths(seq_file)
+    references = ss_aln_utils.load_references(refseq_lengths)
+    refseq_lengths.clear()
+
+    # Parse the alignments and return the strings of reads mapped to each reference sequence
+    mapped_dict = ss_fp.sam_parser_ext(aln_file, multireads, map_qual)
+
+    num_unmapped, _ = ss_aln_utils.load_reference_coverage(refseq_dict=references, mapped_dict=mapped_dict,
+                                                           min_aln=min_aln)
+    mapped_dict.clear()
+
+    # Filter out alignments that with either short alignments or are from low-coverage reference sequences
+    num_unmapped += ss_aln_utils.proportion_filter(references, p_cov)
+
+    # Calculate the RPKM, FPKM and TPM for each reference sequence with reads mapped to it
+    ss_aln_utils.calculate_normalization_metrics(references, num_unmapped)
+
+    return references
+
+
+def stats(sys_args):
+    """
+    A user-facing sub-command to write an abundance table from provided SAM and FASTA files.
+
+    :param sys_args: List of arguments parsed from the command-line.
+    :return: None
+    """
+    parser = ss_args.SAMSumArgumentParser(description="Calculate read coverage stats over reference sequences.")
+    parser.add_stats_args()
+    args = parser.parse_args(sys_args)
+
+    ss_log.prep_logging(os.path.dirname(args.output_table) + os.sep + "samsum_log.txt", args.verbose)
+    stats_ss = ss_class.SAMSumBase("stats")
+    stats_ss.aln_file = args.am_file
+    stats_ss.seq_file = args.fasta_file
+
+    # Parse the FASTA file, calculating the length of each reference sequence and return this as a dictionary
+    refseq_lengths = ss_fp.fasta_seq_lengths(stats_ss.seq_file)
+    references = ss_aln_utils.load_references(refseq_lengths)
+    refseq_lengths.clear()
+
+    # Parse the alignments and return the strings of reads mapped to each reference sequence
+    mapped_dict = ss_fp.sam_parser_ext(stats_ss.aln_file, args.multireads, args.map_qual)
+
+    logging.debug(stats_ss.get_info())
+    num_unmapped, mapped_weight_sum = ss_aln_utils.load_reference_coverage(refseq_dict=references,
+                                                                           mapped_dict=mapped_dict,
+                                                                           min_aln=args.min_aln)
+    mapped_dict.clear()
+    stats_ss.num_frags = num_unmapped + mapped_weight_sum
+
+    # Filter out alignments that with either short alignments or are from low-coverage reference sequences
+    num_unmapped += ss_aln_utils.proportion_filter(references, args.p_cov)
+
+    # Calculate the RPKM, FPKM and TPM for each reference sequence with reads mapped to it
+    ss_aln_utils.calculate_normalization_metrics(references, num_unmapped)
+
+    # Write the summary table with each of the above metrics as well as variance for each
+    ss_fp.write_summary_table(references, args.output_table,
+                              ss_utils.file_prefix(stats_ss.aln_file), num_unmapped, args.sep)
+
+    return 0
---
+++ packages/samsum/samsum-0.1.4-cp36-cp36m-macosx_10_14_x86_64.whl/samsum/file_parsers.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import logging
+import itertools
+
+from pyfastx import Fasta
+
+import _sam_module
+from samsum import classy as ss_class
+
+__author__ = 'Connor Morgan-Lang'
+
+
+def sam_parser_ext(sam_file: str, multireads=False, aln_percent=0, min_mq=0) -> dict:
+    """
+    Wrapper function for using the _sam_parser extension to rapidly parse SAM files.
+
+    :param sam_file: Path to the SAM file to be parsed
+    :param multireads: Boolean flag indicating whether reads that have multiple ambiguous mapping positions are used
+    :param aln_percent: The minimum percentage of a read's length that must be aligned to be included.
+    :param min_mq: The minimum mapping quality for a read to be included in the analysis (as mapped)
+    :return: A dictionary mapping query sequence (read) names to a list of alignment data strings
+    """
+    if not os.path.isfile(sam_file):
+        logging.error("SAM file '%s' doesn't exist.\n" % sam_file)
+        sys.exit(3)
+
+    reads_mapped = dict()
+    mapping_list = iter(_sam_module.get_mapped_reads(sam_file, multireads, aln_percent, min_mq, 'r'))
+    if not mapping_list:
+        logging.error("No alignments were read from SAM file '%s'\n" % sam_file)
+        sys.exit(5)
+
+    mapping_list_grouped = itertools.groupby(sorted(mapping_list, key=lambda x: x.subject), lambda x: x.subject)
+
+    logging.info("Grouping alignment data by reference sequence... ")
+
+    for key, group in mapping_list_grouped:
+        reads_mapped[key] = list(group)
+
+    logging.info("done.\n")
+
+    logging.debug("%d of unique read names returned by _sam_module.\n" % len(reads_mapped))
+
+    return reads_mapped
+
+
+def fasta_seq_lengths(fasta_file: str, min_seq_length=0) -> dict:
+    """
+    Function for calculating the lengths of all sequences in a FASTA file.
+
+    :param fasta_file: Path to a FASTA file to be parsed
+    :param min_seq_length: The minimum length for a reference sequence to be included
+    :return: A dictionary of sequence lengths indexed by their respective sequence names
+    """
+    if not os.path.isfile(fasta_file):
+        logging.error("FASTA file '%s' doesn't exist.\n" % fasta_file)
+        sys.exit(3)
+
+    seq_lengths_map = {}
+    logging.debug("Using Pyfastx to retrieve sequence lengths from FASTA... ")
+    try:
+        py_fa = Fasta(fasta_file, build_index=False, full_name=True)
+    except RuntimeError as error:
+        logging.debug(str(error)+"\n")
+        return seq_lengths_map
+
+    for name, seq in py_fa:  # type: (str, str)
+        if len(seq) > min_seq_length:
+            seq_lengths_map[name] = len(seq)
+
+    if not seq_lengths_map:
+        logging.error("No sequences were parsed from the FASTA file '%s'\n" % fasta_file)
+        sys.exit(5)
+    logging.debug("done.\n")
+
+    logging.info(str(len(seq_lengths_map)) + " sequences were read from " + fasta_file + "\n")
+
+    return seq_lengths_map
+
+
+def write_summary_table(references: dict, output_table: str, samsum_exp: str, unmapped_reads: float, sep=",") -> None:
+    """
+    Writes the output file most people care about - the table summarizing abundance metrics for each reference sequence.
+    Takes a dictionary of sequence names indexing their RefSequence instances and writes specific data for each.
+    Current header is:
+    [RefSequence.name, Query.name, ProportionCovered, Reads, RPKM, FPKM, TPM]
+    Included in this table as the first row are the unmapped reads (UNMAPPED) with relevant information where possible
+
+    :param references: A dictionary of RefSequence instances indexed by the reference sequence names (headers)
+    :param samsum_exp: String representing the origin of the query reads, or alignment experiment name
+    :param output_table: A string representing the path of the file to write to
+    :param unmapped_reads: The number of reads that were not mapped to the reference sequences
+    :param sep: Field separator to use. The default is a comma.
+    :return: None
+    """
+    header = ["QueryName", "RefSequence", "ProportionCovered", "Coverage", "Fragments", "FPKM", "TPM"]
+    buffer = sep.join(header) + "\n"
+    # Add the unmapped reads data
+    buffer += sep.join([samsum_exp, "UNMAPPED", "NA", "NA", str(unmapped_reads), "NA", "NA"]) + "\n"
+
+    try:
+        ot_handler = open(output_table, 'w')
+    except IOError:
+        logging.error("Unable to open output table '%s' for writing.\n" % output_table)
+        sys.exit(3)
+
+    for seq_name in sorted(references, key=lambda x: references[x].tpm, reverse=True):  # type: str
+        ref_seq = references[seq_name]  # type: ss_class.RefSequence
+        data_fields = [ref_seq.covered, ref_seq.depth, ref_seq.weight_total, ref_seq.fpkm, ref_seq.tpm]
+
+        buffer += sep.join([samsum_exp, ref_seq.name] +
+                           [str(round(x, 3)) for x in data_fields]) + "\n"
+        if len(buffer) > 1E6:
+            ot_handler.write(buffer)
+            buffer = ""
+    ot_handler.write(buffer)
+
+    return
