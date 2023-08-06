38e477bec83ade54131f3ab9e46530ad175ac677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:39 +0000

    Add package commitcli/1.0.0/commitcli-1.0.0-py3-none-any.whl

---
+++ packages/commitcli/commitcli-1.0.0-py3-none-any.whl/commitcli/commitcli.py
@@ -0,0 +1,38 @@
+"""
+@Author Marco A. Gallegos
+@Date   2020/12/31
+@Update 2020/12/31
+@Description
+    This file conatains a function to build a commit message using a
+    terminal menu
+"""
+import os
+from commitcli.commit_message import CommitMessage
+
+
+def main()->bool:
+    """Funcion que realiza un commit
+
+    Returns:
+        bool: estado de la ejecucion
+    """
+    commit_msg = CommitMessage()
+    are_there_changes = os.system("git status --short -uno >> /dev/null")
+    if are_there_changes == 32768:
+        print("no existe un repositorio git")
+        return False
+
+    are_there_changes_output = os.popen("git diff --name-only --cached").read() #str with the output
+    if len(are_there_changes_output) == 0:
+        print("no hay cambios por ser rastreados")
+        return False
+    
+    commit_msg.get_answers()
+    commit_string = commit_msg.get_commit_string()
+
+    if commit_string:
+        print("haciendo commit")
+        print("=="*20)
+        os.system(f"git commit -m '{commit_string}'")
+    
+    return True
