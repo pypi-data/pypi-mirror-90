7ab138bdcde01b141ea9bf3631f9ac7753106867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package pwork/0.0.4/pwork-0.0.4.tar.gz

---
+++ packages/pwork/pwork-0.0.4.tar.gz/pwork-0.0.4/pwork/app.py
@@ -0,0 +1,11 @@
+def main():
+    print("please input such script to start:")
+    print("1. cookiecutter https://github.com/gaianote/python-framework")
+    print("2. create and cd to your project dir")
+    print("3. pipenv shell")
+    print("4. pipenv install --dev")
+    print('5. pip install -e ".[dev]"')
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pwork/pwork-0.0.4.tar.gz/pwork-0.0.4/test/test_main.py
@@ -0,0 +1,5 @@
+from pwork.app import main
+
+
+def test_app():
+    assert main() is None
