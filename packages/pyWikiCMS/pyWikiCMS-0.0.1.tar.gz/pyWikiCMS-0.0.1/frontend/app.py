31e6af00b2022e47ac98b5fece68bed348eb94e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:44 +0000

    Add package pyWikiCMS/0.0.1/pyWikiCMS-0.0.1.tar.gz

---
+++ packages/pyWikiCMS/pyWikiCMS-0.0.1.tar.gz/pyWikiCMS-0.0.1/frontend/app.py
@@ -0,0 +1,120 @@
+'''
+Created on 2020-12-30
+
+@author: wf
+'''
+from flask import Flask
+from frontend.wikicms import Frontend, Frontends
+from flask import render_template
+import os
+from wikibot.wikiuser import WikiUser
+
+class AppWrap:
+    ''' 
+    Wrapper for Flask Web Application 
+    '''
+    
+    def __init__(self, host='0.0.0.0',port=8251,debug=False):
+        '''
+        constructor
+        
+        Args:
+            wikiId(str): id of the wiki to use as a CMS backend
+            host(str): flask host
+            port(int): the port to use for http connections
+            debug(bool): True if debugging should be switched on
+        '''
+        self.debug=debug
+        self.port=port
+        self.host=host    
+        scriptdir=os.path.dirname(os.path.abspath(__file__))
+        self.app = Flask(__name__,template_folder=scriptdir+'/../templates')
+        self.frontends=Frontends()
+        self.frontends.load()
+        self.enabledSites=['admin']
+        
+    @staticmethod
+    def splitPath(path):
+        '''
+        split the given path
+        Args:
+            path(str): the path to split
+        Returns:
+            str,str: the site of the path an the actual path
+        '''
+        # https://stackoverflow.com/questions/2136556/in-python-how-do-i-split-a-string-and-keep-the-separators
+        parts=path.split(r"/")
+        site=""
+        if len(parts)>0:
+            site=parts[0]
+        path=""
+        if len(parts)>1:
+            for part in parts[1:]:
+                path=path+"/%s" % (part)
+        return site,path    
+    
+    def enableSites(self,sites):
+        '''
+        enable the sites given in the sites list
+        Args:
+            sites(list): a list of strings with wikiIds to be enabled
+        '''
+        for site in sites:
+            frontend=Frontend(site)
+            self.frontends.enable(frontend)
+            self.enabledSites.append(site)
+            
+    def admin(self)->str:
+        '''
+        render the admin view
+        
+        Returns:
+            str: the html for the admin view
+        '''
+        html=render_template("tableview.html",title="Sites",dictList=self.frontends.frontendConfigs)
+        return html
+    
+    def wikis(self)->str:
+        '''
+        render the wikis table
+        
+        Returns:
+            str: the html code for the table of wikis
+        '''
+        wikiUsers=WikiUser.getWikiUsers()
+        dictList=[]
+        for wikiUser in wikiUsers.values():
+            dictList.append({
+                'wikiId': wikiUser.wikiId,
+                'url':wikiUser.url,
+                'scriptPath':wikiUser.scriptPath,
+                'version':wikiUser.version
+            })
+        html=render_template("tableview.html",title="Wikis",dictList=dictList)
+        return html    
+        
+    def wrap(self,siteName,path):
+        '''
+        wrap the given path for the given site
+        Args:
+            siteName(str): the name of the site to wrap
+            path(path): the path to wrap
+        '''
+        content=None
+        template="index.html"
+        title="Error"
+        if not siteName in self.enabledSites:
+            error="access to site '%s' is not enabled you might want to add it via the --sites command line option" % siteName
+        else:
+            frontend=self.frontends.get(siteName) 
+            title,content,error=frontend.getContent(path);
+            template=frontend.site.template
+        return render_template(template,title=title,content=content,error=error)
+       
+    def run(self):
+        '''
+        start the flask webserver
+        '''
+        self.app.run(debug=self.debug,port=self.port,host=self.host)   
+        pass
+        
---
+++ packages/pyWikiCMS/pyWikiCMS-0.0.1.tar.gz/pyWikiCMS-0.0.1/frontend/wikicms.py
@@ -0,0 +1,182 @@
+'''
+Created on 27.07.2020
+
+@author: wf
+'''
+from wikibot.wikiclient import WikiClient
+from frontend.site import Site
+from lodstorage.jsonable import JSONAble
+import traceback
+from pathlib import Path
+from os.path import isdir
+from os import makedirs
+
+class Frontends(JSONAble):
+    homePath=None
+    '''
+    manager for the available frontends
+    '''
+    def __init__(self):
+        self.frontendConfigs=None
+        self.reinit()
+        
+    def reinit(self):
+        self.frontends={}
+        self.siteLookup={}
+        if Frontends.homePath is None:
+            self.homePath = str(Path.home())
+        else:
+            self.homePath=Frontends.homePath
+            
+    def enable(self,frontend):
+        '''
+        enable the given frontend
+        
+        Args:
+            frontend(Frontend): the frontend to enable
+        '''
+        if self.frontendConfigs is None:
+            raise Exception('Not frontend configurations loaded yet')
+        site=frontend.site
+        if site.name not in self.siteLookup:
+            raise Exception('frontend for site %s not configured yet' % site)
+        self.frontends[site.name]=frontend
+        config=self.siteLookup[site.name]
+        site.configure(config)
+        frontend.open()
+        pass
+        
+    def get(self,wikiId):
+        '''
+        get the frontend for the given wikiid
+        
+        Args:
+            wikiId(str): the wikiId to get the frontend for
+        
+        Returns:
+            Frontend: the frontend for this wikiId
+        '''
+        return self.frontends[wikiId]
+            
+    def load(self):
+        '''
+        load my front end configurations
+        '''
+        storePath=self.getStorePath()
+        self.restoreFromJsonFile(storePath)
+        self.reinit()
+        for config in self.frontendConfigs:
+            site=config["site"]
+            self.siteLookup[site]=config
+        pass
+        
+    def getStorePath(self,prefix="frontendConfigs"):
+        iniPath=self.homePath+"/.wikicms"
+        if not isdir(iniPath):
+            makedirs(iniPath)
+        storePath="%s/%s" % (iniPath,prefix)
+        return storePath
+         
+    def store(self):
+        if self.frontends is not None:
+            storePath=self.getStorePath()
+            self.storeToJsonFile(storePath,"frontendConfigs")
+
+class Frontend(object):
+    '''
+    Wiki Content Management System Frontend
+    '''
+    def __init__(self, siteName,debug=False):
+        '''
+        Constructor
+        Args:
+            siteName(str): the name of the site this frontend is for
+            defaultPage(str): the default page of this frontend
+        '''
+        self.site=Site(siteName)
+        self.debug=debug
+        self.wikiclient=None
+        
+    def log(self,msg):
+        '''
+        log the given message if debugging is true
+        
+        Args:
+            msg(str): the message to log
+        '''
+        if self.debug:
+            print(msg,flush=True)
+        
+    def open(self):
+        '''
+        open the frontend
+        '''
+        if self.wikiclient is None:
+            self.wikiclient=WikiClient.ofWikiId(self.site.wikiId)
+            #self.wikiclient.login()
+        
+    def errMsg(self,ex):
+        if self.debug:
+            msg="%s\n%s" % (repr(ex),traceback.format_exc())
+        else:
+            msg=repr(ex)
+        return msg
+    
+    def wikiPage(self,pagePath):
+        '''
+        get the wikiPage for the given pagePath
+        
+        Args:
+            pagePath(str): the page path
+        Returns:
+            str: the pageTitle
+        '''
+        if "/index.php/" in pagePath:
+            wikipage=pagePath.replace("/index.php/","")
+        elif pagePath.startswith("/"):
+            wikipage=pagePath[1:]    
+        else:
+            wikipage=pagePath
+        return wikipage
+        
+    
+    def checkPath(self,pagePath):
+        '''
+        check the given pathPath
+        
+        Args:
+            pagePath(str): the page Path to check
+            
+        Returns:
+            str: None or an error message with the illegal chars being used
+        '''
+        error=None
+        self.log(pagePath)
+        illegalChars=['{','}','<','>','[',']','|']
+        for illegalChar in illegalChars:
+            if illegalChar in pagePath:
+                error="invalid char %s in given pagePath " % (illegalChar)
+        return error;
+            
+    def getContent(self,pagePath):
+        ''' get the content for the given pagePath 
+        Args:
+            pagePath(str): the page Pageh
+        Returns:
+            str: the HTML content for the given path
+        '''
+        content=None
+        error=None
+        pageTitle="?"
+        try:
+            if pagePath=="/":
+                pageTitle=self.defaultPage
+            else:
+                error=self.checkPath(pagePath)
+                pageTitle=self.wikiPage(pagePath)
+            if error is None:
+                content=self.wikiclient.getHtml(pageTitle)
+        except Exception as e:
+            error=self.errMsg(e)
+        return pageTitle,content,error
+        
