eddeed56bf80e9e54e8589817004039f9ee080f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:49 +0000

    Add package todoist-service/1.3/todoist_service-1.3.tar.gz

---
+++ packages/todoist-service/todoist_service-1.3.tar.gz/todoist_service-1.3/todoist_service/consts.py
@@ -0,0 +1,19 @@
+class TaskFields:
+    ID = "id"
+    Title = "content"
+    Item = "item"
+    Labels = "labels"
+    ProjectID = "project_id"
+    Due = "due"
+
+class Due:
+    Date = "date"
+
+class ProjectFields:
+    Project = "project"
+    Name = "name"
+
+
+class LabelFields:
+    Label = "label"
+    Name = "name"
