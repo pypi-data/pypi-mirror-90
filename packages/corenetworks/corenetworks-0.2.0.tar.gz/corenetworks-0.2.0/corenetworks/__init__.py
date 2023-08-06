f00a1f2340507a9bb3201d5d149cd9e98e7e746c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:09 +0000

    Add package corenetworks/0.2.0/corenetworks-0.2.0.tar.gz

---
+++ packages/corenetworks/corenetworks-0.2.0.tar.gz/corenetworks-0.2.0/corenetworks/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Default package."""
+
+from corenetworks.client import CoreNetworks  # noqa
+
+__author__ = "Robert Kaussow"
+__project__ = "corenetworks"
+__license__ = "MIT"
+__maintainer__ = "Robert Kaussow"
+__email__ = "mail@thegeeklab.de"
+__url__ = "https://github.com/thegeeklab/corenetworks"
+__version__ = "0.2.0"
