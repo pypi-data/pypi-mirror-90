2d727cfedb2b058cfcc2143cd50029a828a161c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package jaxlie/0.0.0/jaxlie-0.0.0.tar.gz

---
+++ packages/jaxlie/jaxlie-0.0.0.tar.gz/jaxlie-0.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jaxlie",
+    version="0.0.0",
+    description="Matrix Lie groups in Jax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/brentyi/jaxlie",
+    author="brentyi",
+    author_email="brentyi@berkeley.edu",
+    license="MIT",
+    packages=find_packages(exclude=["examples", "tests"]),
+    package_data={"liejax": ["py.typed"]},
+    python_requires=">=3.8",
+    install_requires=[
+        "jax",
+        "jaxlib",
+        "numpy",
+        "overrides",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
