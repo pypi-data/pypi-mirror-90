40313168404af85c72a179ce3792c9688957f562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package bSecure/0.0.0.5/bSecure-0.0.0.5-py3-none-any.whl

---
+++ packages/bSecure/bSecure-0.0.0.5-py3-none-any.whl/bSecure/__init__.py
@@ -0,0 +1,9 @@
+# __author__ = 'sadaqatullah'
+
+# __version__ = '0.1.6.9'
+__version__ = '0.0.0.5'
+
+from bSecure.base import (
+    authenticate, set_order, create_order, update_order, single_sign_on_set_values, single_sign_on,
+    checkout_button_image, sso_login_button_image, sso_get_customer_profile, status_order
+)
