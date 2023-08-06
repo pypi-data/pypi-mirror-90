c47bdc317c2b85dc1e7d6c9d7e143f9d207de553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:00 +0000

    Add package ivy/4.1.1/ivy-4.1.1-py3-none-any.whl

---
+++ packages/ivy/ivy-4.1.1-py3-none-any.whl/ivy/cli/__init__.py
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# This package processes the application's command-line arguments.
+# ------------------------------------------------------------------------------
+
+import ivy
+import argslib
+
+from . import build
+from . import init
+from . import clear
+from . import serve
+from . import watch
+from . import make
+from . import tree
+from . import open
+
+
+helptext = """
+Usage: ivy [command]
+
+  Ivy is a static website generator. It transforms a directory of text files
+  into a self-contained website.
+
+Flags:
+  -h, --help          Print the application's help text and exit.
+  -v, --version       Print the application's version number and exit.
+
+Commands:
+  build               Build the site.
+  clear               Clear the output directory.
+  init                Initialize a new site directory.
+  make                Create a new node file.
+  open                Open an @root/ url in the default web browser.
+  serve               Run a web server on the site's output directory.
+  tree                Print the node tree.
+  watch               Monitor the site directory and rebuild on changes.
+
+Command Help:
+  help <command>      Print the specified command's help text and exit.
+"""
+
+
+# We store the root ArgParser instance globally so it's available to plugins.
+argparser = argslib.ArgParser(helptext, ivy.__version__)
+
+
+# Parse the application's command-line arguments. Plugins can use the `cli`
+# event to register their own custom commands.
+def parse_args():
+    ivy.events.fire('cli', argparser)
+    argparser.parse()
