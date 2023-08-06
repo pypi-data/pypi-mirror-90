7524c3e17fd71192017ce601790e9db2be730585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:57 +0000

    Add package pytest-report-me/0.0.4/pytest-report-me-0.0.4.tar.gz

---
+++ packages/pytest-report-me/pytest-report-me-0.0.4.tar.gz/pytest-report-me-0.0.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytest_report_me']
+
+package_data = \
+{'': ['*'], 'pytest_report_me': ['templates/*']}
+
+install_requires = \
+['pytest']
+
+entry_points = \
+{'pytest11': ['report = pytest_report_me.plugin']}
+
+setup_kwargs = {
+    'name': 'pytest-report-me',
+    'version': '0.0.4',
+    'description': 'A pytest plugin to generate report.',
+    'long_description': None,
+    'author': 'yuze',
+    'author_email': 'looker53@sina.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
