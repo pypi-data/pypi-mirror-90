de499636dfd6a830eec476aebe836197091115a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:20 +0000

    Add package binom-probability/1.3/binom_probability-1.3.tar.gz

---
+++ packages/binom-probability/binom_probability-1.3.tar.gz/binom_probability-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binom_probability',
+      version='1.3',
+      description='Gaussian distributions and Binomial distribution',
+      packages=['binom_probability'],
+      author='bantuaparna',
+      author_email='bantuaparna@gmail.com',
+      zip_safe=False)
