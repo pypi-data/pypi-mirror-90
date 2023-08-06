8199da0708139b62caf4d5a5bcfe3ee738f93a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package python-bayeux/1.0.0/python-bayeux-1.0.0.tar.gz

---
+++ packages/python-bayeux/python-bayeux-1.0.0.tar.gz/python-bayeux-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '1.0.0'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'requests',
+    'gevent',
+    'simplejson'
+]
+
+
+setup(name='python-bayeux',
+    version=version,
+    description="A bayeux client for python.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='bayeux cometd',
+    author='Adam J. Lincoln',
+    author_email='alincoln@salesforce.com',
+    url='https://github.com/SalesforceFoundation/python-bayeux',
+    download_url='https://github.com/SalesforceFoundation/python-bayeux/tarball/1.0.0',
+    license='BSD 3-Clause',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+    }
+)
