9e9ee9a6f517b06220ed03d7f85efa5f1b7eddc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:21 +0000

    Add package galaxyxml/0.4.12/galaxyxml-0.4.12.tar.gz

---
+++ packages/galaxyxml/galaxyxml-0.4.12.tar.gz/galaxyxml-0.4.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.rst") as fh:
+    readme = fh.read()
+
+setup(
+    name="galaxyxml",
+    version="0.4.12",
+    description="Galaxy XML generation library",
+    author="Helena Rasche",
+    author_email="hxr@hx42.org",
+    install_requires=["lxml"],
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    packages=["galaxyxml", "galaxyxml.tool", "galaxyxml.tool.parameters"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    data_files = [("", ["LICENSE.TXT"])]
+)
