e1433730a58822aba56a7588db675d79bd122152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:35 +0000

    Add package nestedtext/1.3.0/nestedtext-1.3.0.tar.gz

---
+++ packages/nestedtext/nestedtext-1.3.0.tar.gz/nestedtext-1.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name = 'nestedtext',
+    version = '1.3.0',
+    description = 'human readable and writable data interchange format',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
+    author = "Ken Kundert and Kale Kundert",
+    author_email = 'admin@nestedtext.org',
+    url = 'https://nestedtext.org',
+    download_url = 'https://github.com/kenkundert/nestedtext/tarball/master',
+    license = 'GPLv3+',
+    zip_safe = False,
+    py_modules = 'nestedtext'.split(),
+    install_requires = 'inform>=1.23'.split(),
+    python_requires = '>=3.6',
+    keywords = 'data'.split(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Utilities',
+    ],
+)
