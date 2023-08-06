f4e3fe399426bb3ec9d18a13c81744352f1e9c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:38 +0000

    Add package mock-mycroft-backend/0.3.0/mock-mycroft-backend-0.3.0.tar.gz

---
+++ packages/mock-mycroft-backend/mock-mycroft-backend-0.3.0.tar.gz/mock-mycroft-backend-0.3.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('mock_mycroft_backend')
+
+setup(
+    name='mock-mycroft-backend',
+    version='0.3.0',
+    packages=['mock_mycroft_backend',
+              'mock_mycroft_backend.utils',
+              'mock_mycroft_backend.backend',
+              'mock_mycroft_backend.database'],
+    install_requires=['Flask>=0.12', 'requests>=2.2.1', 'Flask-Mail',
+                      'speech2text', 'pyOpenSSL', "geocoder", "timezonefinder",
+                      "json_database"],
+    package_data={'': extra_files},
+    include_package_data=True,
+    url='https://github.com/OpenJarbas/mock-backend',
+    license='Apache-2.0',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='mock mycroft backend',
+    entry_points={
+        'console_scripts': [
+            'mock-mycroft-backend=mock_mycroft_backend.__main__:main'
+        ]
+    }
+)
