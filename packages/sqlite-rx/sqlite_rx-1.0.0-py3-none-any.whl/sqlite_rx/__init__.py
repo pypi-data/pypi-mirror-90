c3302e23d280762bfc8e0ac80aed5a80bdad9d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:59 +0000

    Add package sqlite-rx/1.0.0/sqlite_rx-1.0.0-py3-none-any.whl

---
+++ packages/sqlite-rx/sqlite_rx-1.0.0-py3-none-any.whl/sqlite_rx/__init__.py
@@ -0,0 +1,46 @@
+__version__ = "1.0.0"
+__author__ = "Abhishek Singh"
+__authoremail__ = "aosingh@asu.edu"
+
+
+def get_default_logger_settings(level: str = "DEBUG"):
+
+    return {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                '()': 'logging.Formatter',
+                'format': '%(asctime)s - %(levelname)s - [%(name)s:%(funcName)s:%(lineno)d] %(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': level,
+                'formatter': 'standard',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',  # Default is stderr
+            },
+        },
+        'loggers': {
+            '': {  # root logger
+                'handlers': ['default'],
+                'level': level,
+                'propagate': False
+            },
+            'sqlite_rx': {
+                'handlers': ['default'],
+                'level': level,
+                'propagate': False
+            },
+            '__main__': {  # if __name__ == '__main__'
+                'handlers': ['default'],
+                'level': level,
+                'propagate': False
+            },
+        }
+    }
+
+
+def get_version():
+    return __version__
