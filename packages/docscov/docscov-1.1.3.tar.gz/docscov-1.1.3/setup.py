8b055c9222307b30c817b237ae3d972c0b52cc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:26 +0000

    Add package docscov/1.1.3/docscov-1.1.3.tar.gz

---
+++ packages/docscov/docscov-1.1.3.tar.gz/docscov-1.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="docscov",
+    version="1.1.3",
+    author="Akib Azmain",
+    author_email="akib8492@gmail.com",
+    description="Sphinx extension to make a dynamic documentation coverage badge",
+    long_description=open("README.rst", "r").read(),
+    long_description_content_type="text/x-rst",
+    url="https://gitlab.com/AkibAzmain/docscov",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "coverxygen",
+        "sphinx"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.8"
+)
