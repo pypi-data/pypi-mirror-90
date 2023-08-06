04de8f0ce352aef6db16fc88fc91685c30d46a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:12 +0000

    Add package oclock/1.2.0/oclock-1.2.0-py3-none-any.whl

---
+++ packages/oclock/oclock-1.2.0-py3-none-any.whl/oclock/__init__.py
@@ -0,0 +1,6 @@
+"""Timing tools, including cancellable timers for loops of constant duration."""
+
+from .timer import Timer
+from .countdown import Countdown
+from .general import parse_time, measure_time, measure_duration
+from .loop import loop, interactiveloop
