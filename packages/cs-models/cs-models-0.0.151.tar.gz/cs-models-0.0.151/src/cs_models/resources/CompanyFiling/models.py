722fc07d47b09da1199c336b89a02c1caa1d237e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:11 +0000

    Add package cs-models/0.0.151/cs-models-0.0.151.tar.gz

---
+++ packages/cs-models/cs-models-0.0.151.tar.gz/cs-models-0.0.151/src/cs_models/resources/CompanyFiling/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+)
+from datetime import datetime
+
+from ...database import Base
+
+
+class CompanyFilingModel(Base):
+    __tablename__ = 'company_filings'
+
+    id = Column(Integer, primary_key=True)
+    company_sec_id = Column(
+        Integer,
+        ForeignKey('companies_sec.id'),
+        nullable=True,
+    )
+    company_ous_id = Column(
+        Integer,
+        ForeignKey('companies_ous.id'),
+        nullable=True,
+    )
+    file_id = Column(
+        Integer,
+        ForeignKey('files.id'),
+        nullable=False,
+    )
+    type = Column(String(50), nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
