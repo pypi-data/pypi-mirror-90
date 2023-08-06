10e02b7573fb4837b3662f32e23c1203a603e79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:15 +0000

    Add package Mopidy-Muse/0.0.23/Mopidy_Muse-0.0.23-py3-none-any.whl

---
+++ packages/Mopidy-Muse/Mopidy_Muse-0.0.23-py3-none-any.whl/mopidy_muse/static/client/client.c1715502.js
@@ -0,0 +1,1 @@
+import"./client.f6218726.js";
---
+++ packages/Mopidy-Muse/Mopidy_Muse-0.0.23-py3-none-any.whl/mopidy_muse/static/client/legacy/clickOutside.eb6c57be.js
@@ -0,0 +1,1 @@
+function e(e){var t=function(t){!e||e.contains(t.target)||t.defaultPrevented||e.dispatchEvent(new CustomEvent("click_outside",e))};return document.addEventListener("click",t,!0),{destroy:function(){document.removeEventListener("click",t,!0)}}}export{e as c};
