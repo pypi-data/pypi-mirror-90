6543fa4074fe7358b8bafd2023c4f7910cf389b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package pybungie/0.5.0/pybungie-0.5.0.tar.gz

---
+++ packages/pybungie/pybungie-0.5.0.tar.gz/pybungie-0.5.0/pybungie/__init__.py
@@ -0,0 +1,5 @@
+import destiny_components
+import destiny_definitions
+import destiny_enums
+import destiny_vendor_hashes
+from api import BungieAPI
---
+++ packages/pybungie/pybungie-0.5.0.tar.gz/pybungie-0.5.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pybungie",
+    version="0.5.0",
+    author="Hayden Cole",
+    author_email="cole.haydenj@gmail.com",
+    description="For interacting with the Bungie.net API",
+    url="https://github.com/Hayden-J-C/pybungie.git",
+    packages=['pybungie'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
