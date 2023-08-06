02c4053c573183c5af6c9a741ee10ca5b9d08ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:16 +0000

    Add package discodo/2.3.0/discodo-2.3.0-py3-none-any.whl

---
+++ packages/discodo/discodo-2.3.0-py3-none-any.whl/discodo/natives/AudioFifo.py
@@ -0,0 +1,44 @@
+import logging
+import threading
+
+import av
+
+from ..config import Config
+
+log = logging.getLogger("discodo.natives.AudioFifo")
+
+
+class AudioFifo(av.AudioFifo):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.AUDIOBUFFERLIMITMS = Config.BUFFERLIMIT * 50 * 960
+
+        self.haveToFillBuffer = threading.Event()
+        self.haveToFillBuffer.set()
+
+    def check_buffer(self) -> None:
+        if self.samples < self.AUDIOBUFFERLIMITMS:
+            self.haveToFillBuffer.set()
+        else:
+            self.haveToFillBuffer.clear()
+
+    def read(self, samples: int = 960) -> bytes:
+        AudioFrame = super().read(samples)
+        if not AudioFrame:
+            return
+
+        self.check_buffer()
+
+        return AudioFrame.planes[0].to_bytes()
+
+    def write(self, *args, **kwargs) -> None:
+        try:
+            super().write(*args, **kwargs)
+        except:
+            log.warning("while writing on fifo, an error occured, ignored.")
+
+        self.check_buffer()
+
+    def reset(self) -> None:
+        super().read(samples=max(self.samples - 960, 0), partial=True)
