0b4eaa43427b8f1e2fc57a4222b9f6c80766b58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:11 +0000

    Add package plone.app.users/2.6.6/plone.app.users-2.6.6.tar.gz

---
+++ packages/plone.app.users/plone.app.users-2.6.6.tar.gz/plone.app.users-2.6.6/setup.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+version = '2.6.6'
+
+long_description = u'{0}\n\n{1}'.format(
+    open('README.rst').read(),
+    open('CHANGES.rst').read()
+)
+
+extras_require = {
+    'test': [
+        'Products.MailHost',
+        'plone.app.testing',
+        'plone.keyring',
+    ]
+}
+
+setup(
+    name='plone.app.users',
+    version=version,
+    description='A package for all things users and groups related (specific '
+                'to plone)',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.2',
+        'Framework :: Plone :: Core',
+        'Framework :: Zope2',
+        'Framework :: Zope :: 4',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='Zope CMF Plone Users Groups',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.org/project/plone.app.users',
+    license='GPL version 2',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=extras_require,
+    install_requires=[
+        'AccessControl',
+        'Acquisition',
+        'Products.CMFCore',
+        'Products.CMFPlone >= 5.1a2',
+        'Products.PlonePAS >= 5.0.1',
+        'Products.statusmessages',
+        'Zope2 >= 2.12.3',
+        'plone.app.layout',
+        'plone.autoform >= 1.2',
+        'plone.formwidget.namedfile >= 1.0.3',
+        'plone.namedfile',
+        'plone.protect',
+        'plone.schema',
+        'plone.uuid',
+        'setuptools',
+        'six',
+        'z3c.form',
+        'zope.component',
+        'zope.event',
+        'zope.interface',
+        'zope.schema',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
