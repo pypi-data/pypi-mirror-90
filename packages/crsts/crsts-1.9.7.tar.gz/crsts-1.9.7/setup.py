8b4f7a2e290cc9d701c04e6e3cd49c965e8bfeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package crsts/1.9.7/crsts-1.9.7.tar.gz

---
+++ packages/crsts/crsts-1.9.7.tar.gz/crsts-1.9.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crsts",
+    version="1.9.7",
+    author="Chris Chen",
+    author_email="wsywddr@163.com",
+    description="This is my personal toolkit.",
+    longe_description=long_description,
+    longe_description_content_type="text/markdown",
+    url="https://github.com/wsywddr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
