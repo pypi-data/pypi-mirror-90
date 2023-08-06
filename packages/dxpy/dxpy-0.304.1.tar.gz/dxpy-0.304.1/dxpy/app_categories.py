bbe3b494223760b610848fac75c914554d8104e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package dxpy/0.304.1/dxpy-0.304.1.tar.gz

---
+++ packages/dxpy/dxpy-0.304.1.tar.gz/dxpy-0.304.1/dxpy/app_categories.py
@@ -0,0 +1,15 @@
+APP_CATEGORIES = frozenset([
+    'Annotation',
+    'Assembly',
+    'Debugging',
+    'Export',
+    'Import',
+    'Mappings Manipulation',
+    'Read Manipulation',
+    'Read Mapping',
+    'Reports',
+    'RNA-Seq',
+    'Statistics',
+    'Structural Variation',
+    'Variation Calling'
+])
