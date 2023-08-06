5b4024094b8eaeb7c36e2bf0b9727ad317685291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:32 +0000

    Add package hystats/0.1/hystats-0.1.tar.gz

---
+++ packages/hystats/hystats-0.1.tar.gz/hystats-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="hystats",
+version="0.1",
+description="Calculates hydrological model's performance indices",
+long_description = "Calculates hydrological model's performance indices",
+author="UPS Bhadoriya",
+install_requires=['pandas','numpy'])
