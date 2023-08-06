a88c89a79025f19bc881af1bcc71fd126d5f61af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:29 +0000

    Add package simpervisor/0.4/simpervisor-0.4.tar.gz

---
+++ packages/simpervisor/simpervisor-0.4.tar.gz/simpervisor-0.4/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="simpervisor",
+    version='0.4',
+    url="https://github.com/yuvipanda/simpervisor",
+    description='Simple async process supervisor',
+    author="Yuvi Panda",
+    author_email="yuvipanda@gmail.com",
+    license="BSD 3-Clause",
+    packages=setuptools.find_packages(),
+)
