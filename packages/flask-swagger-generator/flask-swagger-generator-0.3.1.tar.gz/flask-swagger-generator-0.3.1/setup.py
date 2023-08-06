6ba472c7985cf0055e34922cb84d8fac40089ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:20 +0000

    Add package flask-swagger-generator/0.3.1/flask-swagger-generator-0.3.1.tar.gz

---
+++ packages/flask-swagger-generator/flask-swagger-generator-0.3.1.tar.gz/flask-swagger-generator-0.3.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from flask_swagger_generator import get_version
+
+VERSION = get_version()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="flask-swagger-generator",
+    version=VERSION,
+    license="MIT",
+    author="coding kitties",
+    description="A library for generating swagger open api specifications for "
+                "the Flask web framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coding-kitties/flask-swagger-generator.git",
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    keywords=['Flask', 'swagger', 'swagger generator', 'OpenAPI'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Topic :: Software Development",
+    ],
+    install_requires=required,
+    python_requires='>=3',
+    include_package_data=True,
+)
