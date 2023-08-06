afc3893351979fd684a0649e1dc3a06b307e82f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package luis-avilarz-distributions/0.1/luis_avilarz_distributions-0.1.tar.gz

---
+++ packages/luis-avilarz-distributions/luis_avilarz_distributions-0.1.tar.gz/luis_avilarz_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='luis_avilarz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['luis_avilarz_distributions'],
+      zip_safe=False)
