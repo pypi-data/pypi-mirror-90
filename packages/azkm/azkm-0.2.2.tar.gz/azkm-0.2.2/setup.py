a159eb89df725bf92a7539c82d0dca2b48bc2170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package azkm/0.2.2/azkm-0.2.2.tar.gz

---
+++ packages/azkm/azkm-0.2.2.tar.gz/azkm-0.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+
+# https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='azkm',
+    description='azure knowledge mining cli',
+    version="0.2.2",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/frogrammer/azure-knowledgemining-cli',
+    author='Luke Vinton',
+    author_email='luke0vinton@gmail.com',
+    license='Apache 2.0',
+    packages=find_packages(),
+    install_requires=['fire', 'fire-cli-helper', 'cdktf', 'tabulate', 'azure-cli==2.14.0', 'azure-mgmt-core==1.2.0', 'azure-storage-blob', 'clint'],
+    tests_require=[],
+    classifiers=[],
+    test_suite='',
+    entry_points={
+        'console_scripts': [
+            'azkm = azkm.__main__:main',
+        ]
+    },
+    package_data={
+        '': ['*.json', '*.tgz']
+    },
+    include_package_data=True
+)
