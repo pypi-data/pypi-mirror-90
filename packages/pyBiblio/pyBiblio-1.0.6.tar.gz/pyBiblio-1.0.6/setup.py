86b7ebfc8a14353702b0e1a3dcf3fcf5a4303247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:03 +0000

    Add package pyBiblio/1.0.6/pyBiblio-1.0.6.tar.gz

---
+++ packages/pyBiblio/pyBiblio-1.0.6.tar.gz/pyBiblio-1.0.6/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="pyBiblio",
+    version="1.0.6",
+    author="Marie Dumaz,Aldo Romero",
+    author_email="mcd0029@mix.wvu.edu,alromero@mail.wvu.edu",
+    url="https://github.com/romerogroup/pyBiblio",
+    packages=["pybiblio"],
+    license="LICENSE.txt",
+    data_files=[("", ["LICENSE.txt"])],
+    package_data={"pybiblio": ["FU.csv"]},
+    description="A Python library for basic bibliometic measures.",
+    install_requires=["pandas", "numpy", "nltk"],
+)
