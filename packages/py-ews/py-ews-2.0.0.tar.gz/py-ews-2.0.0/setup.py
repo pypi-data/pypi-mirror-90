a219b7cab0d24deb2dcc387967ee4e87c516899c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:46 +0000

    Add package py-ews/2.0.0/py-ews-2.0.0.tar.gz

---
+++ packages/py-ews/py-ews-2.0.0.tar.gz/py-ews-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(requirement_file):
+    with open(requirement_file) as f:
+        return f.readlines()
+
+setup(
+    name='py-ews',
+    version='2.0.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A Python package to interact with both on-premises and Office 365 Exchange Web Services',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=parse_requirements('./requirements.txt'),
+    keywords=['ews', 'exchange', 'office365', 'email', 'ediscovery', 'swimlane'],
+    url='https://github.com/swimlane/pyews',
+    author='Swimlane',
+    author_email='info@swimlane.com',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+)
