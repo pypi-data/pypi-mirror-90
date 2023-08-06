b7ef7645b5eff7be278533f4d570610d612582d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:19 +0000

    Add package tableone/0.7.10/tableone-0.7.10-py2.py3-none-any.whl

---
+++ packages/tableone/tableone-0.7.10-py2.py3-none-any.whl/tableone/__init__.py
@@ -0,0 +1,5 @@
+# Import tableone objects
+from .tableone import TableOne, load_dataset, tableone
+
+__author__ = "Tom Pollard <tpollard@mit.edu>, Alistair Johnson, Jesse Raffa"
+__version__ = "0.7.10"
