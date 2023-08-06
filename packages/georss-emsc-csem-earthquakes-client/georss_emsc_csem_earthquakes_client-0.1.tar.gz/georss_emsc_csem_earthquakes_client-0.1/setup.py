9e3b0d0cec78da02685fb21ac661e33408e0fdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:38 +0000

    Add package georss-emsc-csem-earthquakes-client/0.1/georss_emsc_csem_earthquakes_client-0.1.tar.gz

---
+++ packages/georss-emsc-csem-earthquakes-client/georss_emsc_csem_earthquakes_client-0.1.tar.gz/georss_emsc_csem_earthquakes_client-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    'georss_client>=0.12',
+]
+
+setup(
+    name="georss_emsc_csem_earthquakes_client",
+    version="0.1",
+    author="Matej Sekoranja",
+    author_email="matej.sekoranja@gmail.com",
+    description="A GeoRSS client library for the EMSC CSEM Earthquakes feed.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msekoranja/python-georss-emsc-csem-earthquakes-client",
+    packages=find_packages(exclude=("tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
