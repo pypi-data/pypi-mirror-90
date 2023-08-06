7ece09bbd268422e8bf2fa2779365ef99cbe3822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package wsl-tools/0.2.1/wsl-tools-0.2.1.tar.gz

---
+++ packages/wsl-tools/wsl-tools-0.2.1.tar.gz/wsl-tools-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['wsl_tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyxdg>=0.27,<0.28']
+
+setup_kwargs = {
+    'name': 'wsl-tools',
+    'version': '0.2.1',
+    'description': 'WSL utilities library',
+    'long_description': None,
+    'author': 'Andrea Ghensi',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
