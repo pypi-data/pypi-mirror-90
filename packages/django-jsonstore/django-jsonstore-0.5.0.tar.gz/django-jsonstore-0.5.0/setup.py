c21880f1679885a233ea3f2cd3f43246bd8061cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:18 +0000

    Add package django-jsonstore/0.5.0/django-jsonstore-0.5.0.tar.gz

---
+++ packages/django-jsonstore/django-jsonstore-0.5.0.tar.gz/django-jsonstore-0.5.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='django-jsonstore',
+    version='0.5.0',
+    author='Mikhail Podgurskiy',
+    author_email='kmmbvnr@gmail.com',
+    description='Expose JSONField data as a virtual django model fields.',
+    long_description=open('README.rst').read(),
+    platforms=['Any'],
+    keywords=['django', 'json', 'orm'],
+    url='http://github.com/viewflow/jsonstore',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    ],
+    install_requires=[
+        'Django>=1.11',
+        'six',
+    ],
+    packages=['jsonstore'],
+    include_package_data=True,
+    zip_safe=False
+)
