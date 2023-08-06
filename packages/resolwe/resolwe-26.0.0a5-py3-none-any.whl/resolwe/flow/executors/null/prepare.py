9d52dc5962de4fcefdae448056f2bf1044c94af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:59 +0000

    Add package resolwe/26.0.0a5/resolwe-26.0.0a5-py3-none-any.whl

---
+++ packages/resolwe/resolwe-26.0.0a5-py3-none-any.whl/resolwe/flow/executors/null/prepare.py
@@ -0,0 +1,11 @@
+"""Null workflow executor.
+
+To customize the settings the manager serializes for the executor
+at runtime, properly subclass
+:class:`~resolwe.flow.executors.prepare.BaseFlowExecutorPreparer` into
+``FlowExecutorPreparer`` and override its
+:meth:`~resolwe.flow.executors.prepare.BaseFlowExecutorPreparer.extend_settings`
+method.
+"""
+
+from ..prepare import BaseFlowExecutorPreparer as FlowExecutorPreparer  # noqa: F401
