1820e311f52e8614f193489616f21eaf45304dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:42 +0000

    Add package YTVdownload/1.0.0/YTVdownload-1.0.0.tar.gz

---
+++ packages/YTVdownload/YTVdownload-1.0.0.tar.gz/YTVdownload-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="YTVdownload", # Replace with your own username
+    version="1.0.0",
+    author="Akshat Tiwari",
+    author_email="thealphacoding@gmail.com",
+    description="This package can Download YouTube videos in MP3 or MP4 format....",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/alphacodingat/YTVdownload.git' ,
+    packages=setuptools.find_packages(),
+    install_requires = ['pafy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+)
