afb2f0c8b5db70ee1674b2f6dc129bb668715df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package lite-bo/0.6.0/lite-bo-0.6.0.tar.gz

---
+++ packages/lite-bo/lite-bo-0.6.0.tar.gz/lite-bo-0.6.0/litebo/core/base.py
@@ -0,0 +1,111 @@
+from litebo.acquisition_function.acquisition import *
+from litebo.acquisition_function.multi_objective_acquisition import *
+from litebo.surrogate.base.rf_with_instances import RandomForestWithInstances
+from litebo.surrogate.base.build_gp import create_gp_model
+from litebo.utils.util_funcs import get_types
+from litebo.utils.constants import MAXINT
+from collections import namedtuple
+
+Observation = namedtuple('Observation', ['config', 'trial_state', 'constraints', 'objectives'])
+
+
+def build_acq_func(func_str='ei', model=None, constraint_models=None, **kwargs):
+    func_str = func_str.lower()
+    if constraint_models is None:
+        if func_str == 'ei':
+            acq_func = EI
+        elif func_str == 'eips':
+            acq_func = EIPS
+        elif func_str == 'logei':
+            acq_func = LogEI
+        elif func_str == 'pi':
+            acq_func = PI
+        elif func_str == 'lcb':
+            acq_func = LCB
+        elif func_str == 'lpei':
+            acq_func = LPEI
+        elif func_str == 'mesmo':
+            acq_func = MESMO
+        elif func_str == 'usemo':   # todo single acq type
+            acq_func = USeMO
+        else:
+            raise ValueError('Invalid string %s for acquisition function!' % func_str)
+        return acq_func(model=model, **kwargs)
+    else:
+        if func_str == 'eic':
+            acq_func = EIC
+        elif func_str == 'mesmoc':
+            acq_func = MESMOC
+        elif func_str == 'mesmoc2':
+            acq_func = MESMOC2
+        else:
+            raise ValueError('Invalid string %s for acquisition function!' % func_str)
+        return acq_func(model=model, constraint_models=constraint_models, **kwargs)
+
+
+def build_optimizer(func_str='local_random', acq_func=None, config_space=None, rng=None):
+    assert config_space is not None
+    func_str = func_str.lower()
+
+    if func_str == 'local_random':
+        from litebo.acq_maximizer.ei_optimization import InterleavedLocalAndRandomSearch
+        optimizer = InterleavedLocalAndRandomSearch
+    elif func_str == 'random_scipy':
+        from litebo.acq_maximizer.ei_optimization import RandomScipyOptimizer
+        optimizer = RandomScipyOptimizer
+    elif func_str == 'mesmo_optimizer':
+        from litebo.acq_maximizer.ei_optimization import MESMO_Optimizer
+        optimizer = MESMO_Optimizer
+    elif func_str == 'usemo_optimizer':
+        from litebo.acq_maximizer.ei_optimization import USeMO_Optimizer
+        optimizer = USeMO_Optimizer
+    elif func_str == 'cma_es':
+        from litebo.acq_maximizer.ei_optimization import CMAESOptimizer
+        optimizer = CMAESOptimizer
+    else:
+        raise ValueError('Invalid string %s for acq_maximizer!' % func_str)
+
+    return optimizer(acquisition_function=acq_func,
+                     config_space=config_space,
+                     rng=rng)
+
+
+def build_surrogate(func_str='prf', config_space=None, rng=None, history_hpo_data=None):
+    assert config_space is not None
+    func_str = func_str.lower()
+    types, bounds = get_types(config_space)
+    seed = rng.randint(MAXINT)
+    if func_str == 'prf':
+        return RandomForestWithInstances(types=types, bounds=bounds, seed=seed)
+    elif func_str.startswith('gp'):
+        return create_gp_model(model_type=func_str,
+                               config_space=config_space,
+                               types=types,
+                               bounds=bounds,
+                               rng=rng)
+    elif func_str.startswith('mfgpe'):
+        from litebo.surrogate.tlbo.mfgpe import MFGPE
+        inner_surrogate_type = 'prf'
+        return MFGPE(config_space, history_hpo_data, seed,
+                     surrogate_type=inner_surrogate_type, num_src_hpo_trial=-1)
+    elif func_str.startswith('tlbo'):
+        print('the current surrogate is', func_str)
+        if 'rgpe' in func_str:
+            from litebo.surrogate.tlbo.rgpe import RGPE
+            inner_surrogate_type = func_str.split('_')[-1]
+            return RGPE(config_space, history_hpo_data, seed,
+                        surrogate_type=inner_surrogate_type, num_src_hpo_trial=-1)
+        elif 'sgpr' in func_str:
+            from litebo.surrogate.tlbo.stacking_gpr import SGPR
+            inner_surrogate_type = func_str.split('_')[-1]
+            return SGPR(config_space, history_hpo_data, seed,
+                        surrogate_type=inner_surrogate_type, num_src_hpo_trial=-1)
+        elif 'topov3' in func_str:
+            from litebo.surrogate.tlbo.topo_variant3 import TOPO_V3
+            inner_surrogate_type = func_str.split('_')[-1]
+            return TOPO_V3(config_space, history_hpo_data, seed,
+                           surrogate_type=inner_surrogate_type, num_src_hpo_trial=-1)
+        else:
+            raise ValueError('Invalid string %s for tlbo surrogate!' % func_str)
+    else:
+        raise ValueError('Invalid string %s for surrogate!' % func_str)
---
+++ packages/lite-bo/lite-bo-0.6.0.tar.gz/lite-bo-0.6.0/litebo/core/sync_batch_advisor.py
@@ -0,0 +1,129 @@
+import copy
+import numpy as np
+
+from litebo.config_space.util import convert_configurations_to_array
+from litebo.utils.constants import MAXINT, SUCCESS, FAILED, TIMEOUT
+from litebo.core.advisor import Advisor
+
+
+class SyncBatchAdvisor(Advisor):
+    def __init__(self, config_space,
+                 batch_size=4,
+                 initial_trials=10,
+                 initial_configurations=None,
+                 init_strategy='random_explore_first',
+                 optimization_strategy='bo',
+                 batch_strategy='median_imputation',
+                 history_bo_data=None,
+                 surrogate_type='prf',
+                 output_dir='logs',
+                 task_id=None,
+                 rng=None):
+
+        self.batch_size = batch_size
+        self.batch_strategy = batch_strategy
+        super().__init__(config_space,
+                         initial_trials=initial_trials,
+                         initial_configurations=initial_configurations,
+                         init_strategy=init_strategy,
+                         optimization_strategy=optimization_strategy,
+                         history_bo_data=history_bo_data,
+                         surrogate_type=surrogate_type,
+                         output_dir=output_dir,
+                         task_id=task_id,
+                         rng=rng)
+
+        if batch_strategy == 'median_imputation':
+            acq_type = 'ei'
+        elif batch_strategy == 'local_penalization':
+            acq_type = 'lpei'
+        else:
+            raise ValueError('Unsupported batch strategy - %s.' % batch_strategy)
+        super(SyncBatchAdvisor, self).setup_bo_basics(acq_type=acq_type)
+
+    def get_suggestions(self):
+        if len(self.configurations) == 0:
+            X = np.array([])
+        else:
+            failed_configs = list() if self.max_y is None else self.failed_configurations.copy()
+            X = convert_configurations_to_array(self.configurations + failed_configs)
+
+        num_failed_trial = len(self.failed_configurations)
+        failed_perfs = list() if self.max_y is None else [self.max_y] * num_failed_trial
+        Y = np.array(self.perfs + failed_perfs, dtype=np.float64)
+
+        num_config_evaluated = len(self.perfs)
+        batch_configs_list = list()
+
+        if num_config_evaluated < self.init_num:
+            return self.sample_random_configs(self.init_num)
+
+        if self.optimization_strategy == 'random':
+            return self.sample_random_configs(self.batch_size)
+
+        if self.batch_strategy == 'median_imputation':
+            estimated_y = np.median(Y)
+            batch_history_container = copy.deepcopy(self.history_container)
+            for i in range(self.batch_size):
+                self.surrogate_model.train(X, Y)
+                incumbent_value = batch_history_container.get_incumbents()[0][1]
+                self.acquisition_function.update(model=self.surrogate_model, eta=incumbent_value,
+                                                 num_data=len(batch_history_container.data))
+
+                challengers = self.optimizer.maximize(
+                    runhistory=batch_history_container,
+                    num_points=5000
+                )
+
+                is_repeated_config = True
+                repeated_time = 0
+                curr_batch_config = None
+                while is_repeated_config:
+                    try:
+                        curr_batch_config = challengers.challengers[repeated_time]
+                        batch_history_container.add(curr_batch_config, estimated_y)
+                    except ValueError:
+                        is_repeated_config = True
+                        repeated_time += 1
+                    else:
+                        is_repeated_config = False
+
+                batch_configs_list.append(curr_batch_config)
+                X = np.append(X, curr_batch_config.get_array().reshape(1, -1), axis=0)
+                Y = np.append(Y, estimated_y)
+
+        elif self.batch_strategy == 'local_penalization':
+            self.surrogate_model.train(X, Y)
+            incumbent_value = self.history_container.get_incumbents()[0][1]
+            # L = self.estimate_L(X)
+            for i in range(self.batch_size):
+                self.acquisition_function.update(model=self.surrogate_model, eta=incumbent_value,
+                                                 num_data=len(self.history_container.data),
+                                                 batch_configs=batch_configs_list)
+
+                challengers = self.optimizer.maximize(
+                    runhistory=self.history_container,
+                    num_points=5000
+                )
+                batch_configs_list.append(challengers.challengers[0])
+        else:
+            raise ValueError('Invalid sampling strategy - %s.' % self.batch_strategy)
+        return batch_configs_list
+
+    def update_observation(self, observation):
+        config, perf, trial_state = observation
+        if not isinstance(perf, (int, float)):
+            perf = perf[-1]
+        if trial_state == SUCCESS and perf < MAXINT:
+            if len(self.configurations) == 0:
+                self.default_obj_value = perf
+
+            self.configurations.append(config)
+            self.perfs.append(perf)
+            self.history_container.add(config, perf)
+
+            self.perc = np.percentile(self.perfs, self.scale_perc)
+            self.min_y = np.min(self.perfs)
+            self.max_y = np.max(self.perfs)
+        else:
+            self.failed_configurations.append(config)
