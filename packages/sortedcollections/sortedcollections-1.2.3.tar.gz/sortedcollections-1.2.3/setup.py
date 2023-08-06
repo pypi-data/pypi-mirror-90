b28ecc10513fef83cef8aca2221124300162ad50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package sortedcollections/1.2.3/sortedcollections-1.2.3.tar.gz

---
+++ packages/sortedcollections/sortedcollections-1.2.3.tar.gz/sortedcollections-1.2.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.test_args)
+        exit(errno)
+
+
+with open('README.rst') as reader:
+    readme = reader.read()
+
+setup(
+    name='sortedcollections',
+    version='1.2.3',
+    description='Python Sorted Collections',
+    long_description=readme,
+    author='Grant Jenks',
+    author_email='contact@grantjenks.com',
+    url='http://www.grantjenks.com/docs/sortedcollections/',
+    license='Apache 2.0',
+    packages=['sortedcollections'],
+    install_requires=['sortedcontainers'],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
