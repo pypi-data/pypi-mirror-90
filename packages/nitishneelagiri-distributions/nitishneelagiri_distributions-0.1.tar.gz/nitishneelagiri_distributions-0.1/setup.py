9015c73e9ae0002f98b7a7a7917ca94125a1d16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package nitishneelagiri-distributions/0.1/nitishneelagiri_distributions-0.1.tar.gz

---
+++ packages/nitishneelagiri-distributions/nitishneelagiri_distributions-0.1.tar.gz/nitishneelagiri_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nitishneelagiri_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['nitishneelagiri_distributions'],
+      zip_safe=False)
