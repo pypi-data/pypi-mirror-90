11e6928d2885f407570e51e20459e389be0b029b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:59 +0000

    Add package pigment/0.4.0/pigment-0.4.0.tar.gz

---
+++ packages/pigment/pigment-0.4.0.tar.gz/pigment-0.4.0/pigment/exceptions.py
@@ -0,0 +1,32 @@
+class PigmentError(Exception):
+    """The base class for all Pigment exceptions"""
+
+    pass
+
+
+class WrongLengthError(PigmentError, ValueError):
+    """The argument provided had an invalid length
+
+    Note:
+        Also a subclass of ``ValueError``
+
+    Attributes:
+        argument (str): The name of the argument
+    """
+
+    def __init__(self, argument: str = None):
+        if argument:
+            super().__init__(f"argument {argument} has an invalid length")
+        else:
+            super().__init__(f"argument has an invalid length")
+
+
+class InvalidRGBValue(PigmentError, ValueError):
+    """A value was invalid when converting or saving a color in RGB form
+
+    Note:
+        Also a subclass of ``ValueError``
+    """
+
+    def __init__(self):
+        super().__init__("invalid RGB value")
