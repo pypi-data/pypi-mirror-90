3ad4c13b4748b6c2a76b644e7ce151799c181e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package edx-enterprise/3.16.7/edx_enterprise-3.16.7-py3-none-any.whl

---
+++ packages/edx-enterprise/edx_enterprise-3.16.7-py3-none-any.whl/enterprise/errors.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Errors thrown by the APIs in the Enterprise application.
+"""
+
+
+class CodesAPIRequestError(Exception):
+    """There was a problem with a request to the Codes application's APIs."""
