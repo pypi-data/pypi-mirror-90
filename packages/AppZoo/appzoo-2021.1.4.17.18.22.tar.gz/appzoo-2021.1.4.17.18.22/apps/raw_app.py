6da547beacd241d44cb4bfcaeca4ffe986a27697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:58 +0000

    Add package AppZoo/2021.1.4.17.18.22/appzoo-2021.1.4.17.18.22.tar.gz

---
+++ packages/AppZoo/appzoo-2021.1.4.17.18.22.tar.gz/appzoo-2021.1.4.17.18.22/apps/raw_app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : raw_app
+# @Time         : 2020/12/28 11:11 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+import time
+import uvicorn
+
+from datetime import datetime
+from typing import Optional
+from fastapi import FastAPI, Form, Depends, File, UploadFile
+from pydantic import BaseModel
+from starlette.staticfiles import StaticFiles
+from starlette.requests import Request
+from starlette.responses import \
+    RedirectResponse, FileResponse, HTMLResponse, PlainTextResponse
+from starlette.status import *
+
+app = FastAPI()
+
+
+@app.post('/')
+def read_root(kwargs:dict):
+    print(kwargs)
+    return kwargs.get("CHALLENGE")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
