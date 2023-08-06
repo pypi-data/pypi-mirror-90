90ffec3aa117fa13a89870997e3336282e49780b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:46 +0000

    Add package thermosteam/0.23.1/thermosteam-0.23.1.tar.gz

---
+++ packages/thermosteam/thermosteam-0.23.1.tar.gz/thermosteam-0.23.1/tests/test_biorefineries.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# BioSTEAM: The Biorefinery Simulation and Techno-Economic Analysis Modules
+# Copyright (C) 2020, Yoel Cortes-Pena <yoelcortes@gmail.com>
+# 
+# This module is under the UIUC open-source license. See 
+# github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
+# for license details.
+"""
+"""
+from biorefineries.tests.test_biorefineries import *
+
+
+def ignore_import_error(f):
+    def g():
+        try: f()  
+        except ImportError: pass
+    return g
+
+# To run theses tests, you'll need to get the github version.
+# If you have the PyPI installation, these tests will not run.
+# This is OK, the loss in coverage is very small.
+try: test_LAOs = ignore_import_error(test_LAOs)
+except NameError: pass
+try: test_lactic = ignore_import_error(test_lactic)
+except NameError: pass
+try: test_ethanol_adipic = ignore_import_error(test_annimal_bedding)
+except NameError: pass
+try: test_annimal_bedding = ignore_import_error(test_annimal_bedding)
+except NameError: pass
+try: test_wheatstraw = ignore_import_error(test_wheatstraw)
+except NameError: pass
