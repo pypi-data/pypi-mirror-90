ce5666f06dd4ad0f318d77fb1c6df6ef3fdcc391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:20 +0000

    Add package flask-resources/0.4.0/flask-resources-0.4.0.tar.gz

---
+++ packages/flask-resources/flask-resources-0.4.0.tar.gz/flask-resources-0.4.0/flask_resources/serializers/json.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Flask-Resources is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""JSON serializer."""
+
+import json
+
+from ..json_encoder import CustomJSONEncoder
+from .serializers import SerializerMixin
+
+
+class JSONSerializer(SerializerMixin):
+    """JSON serializer implementation."""
+
+    def serialize_object(self, obj, response_ctx=None, *args, **kwargs):
+        """Dump the object into a json string."""
+        return json.dumps(obj, cls=CustomJSONEncoder)
+
+    def serialize_object_list(self, obj_list, response_ctx=None, *args, **kwargs):
+        """Dump the object list into a json string."""
+        return json.dumps(obj_list, cls=CustomJSONEncoder)
