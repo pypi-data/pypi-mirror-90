50a828fa14513069e99b4cdc1647c3efe134c3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:07 +0000

    Add package py-laxz/0.1.19/py-laxz-0.1.19.tar.gz

---
+++ packages/py-laxz/py-laxz-0.1.19.tar.gz/py-laxz-0.1.19/pylaxz/__init__.py
@@ -0,0 +1,12 @@
+"""
+pylaxz
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright (c) 2020 Min Latt.
+License: MIT, see LICENSE for more details.
+
+A small utility for my personal use cases.
+"""
+
+from .utils.logxs import printf
+from .__version__ import version
