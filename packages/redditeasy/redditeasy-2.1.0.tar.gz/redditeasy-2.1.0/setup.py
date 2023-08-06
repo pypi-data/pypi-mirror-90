f418ede9fe513a030630e3ce0724373d605eed84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:34 +0000

    Add package redditeasy/2.1.0/redditeasy-2.1.0.tar.gz

---
+++ packages/redditeasy/redditeasy-2.1.0.tar.gz/redditeasy-2.1.0/setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redditeasy",
+    version="2.1.0",
+    author="MakufonSkifto",
+    author_email="emirsurmen@gmail.com",
+    license="GNU General Public License v3 (GPLv3)",
+    description="RedditEasy is an API wrapper for the Reddit JSON API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MakufonSkifto/RedditEasy",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires='>=3.6',
+)
