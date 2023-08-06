bd71498b6072e0c5cfb8800614076e36cd36933e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package NlpToolkit-NER-Cy/1.0.0/NlpToolkit-NER-Cy-1.0.0.tar.gz

---
+++ packages/NlpToolkit-NER-Cy/NlpToolkit-NER-Cy-1.0.0.tar.gz/NlpToolkit-NER-Cy-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["NER/AutoProcessor/ParseTree/*.pyx",
+                           "NER/AutoProcessor/Sentence/*.pyx"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-NER-Cy',
+    version='1.0.0',
+    packages=['NER', 'NER.AutoProcessor', 'NER.AutoProcessor.Sentence', 'NER.AutoProcessor.ParseTree'],
+    package_data={'NER.AutoProcessor.ParseTree': ['*.pxd', '*.pyx', '*.c', '*.py'],
+                  'NER.AutoProcessor.Sentence': ['*.pxd', '*.pyx', '*.c', '*.py']},
+    url='',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='NER library'
+)
