3e7d37b398375df065e774e58d03b83f89086ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:11 +0000

    Add package jaja-probability-dsnd/0.1/jaja_probability_dsnd-0.1.tar.gz

---
+++ packages/jaja-probability-dsnd/jaja_probability_dsnd-0.1.tar.gz/jaja_probability_dsnd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jaja_probability_dsnd',
+      version='0.1',
+      description='Gaussian & binomial distributions',
+      packages=['jaja_probability_dsnd'],
+      author='Tamuno-omi Jaja',
+      authoremail='tamjaja@yahoo.com',
+      zip_safe=False)
