87aa1e05f4764b49cca91af52ab02eb2d0b37700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:50 +0000

    Add package TextTron/0.2/TextTron-0.2.tar.gz

---
+++ packages/TextTron/TextTron-0.2.tar.gz/TextTron-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+name='TextTron',
+version='0.2',
+author='Ayan Gadpal',
+author_email='ayangadpal2@gmail.com',
+packages = ['TextTron'],
+license = "MIT",
+description = ("TextTron is a simple light-weight image processing based text detector in document images."),
+long_description=("TextTron is a simple light-weight image processing based text detector in document images."
+	"TextTron detects text with the help of Contours applied on a pre-processed image."
+	"This meant for fast text detection without using any machine learning or deep learning model."
+	"Though this will not work well in scene text detection, only meant for document images"),
+keywords = "Text Detection, lightweight Text Detection, Document Text Detector",
+url = "https://github.com/AyanGadpal/TextTron-Lightweight-text-detector",
+requires=['opencv_python','numpy'],
+)
