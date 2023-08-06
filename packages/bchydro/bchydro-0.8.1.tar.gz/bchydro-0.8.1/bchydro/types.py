b30b00baf6662860e5e26bbfcd360adf52914639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:18 +0000

    Add package bchydro/0.8.1/bchydro-0.8.1.tar.gz

---
+++ packages/bchydro/bchydro-0.8.1.tar.gz/bchydro-0.8.1/bchydro/types.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+from typing import List
+
+
+class BCHydroRates:
+    def __init__(
+        self,
+        days_since_billing: int,
+        consumption: float,
+        cost: float,
+        estimated_consumption: float,
+        estimated_cost: float,
+    ):
+        self.days_since_billing = days_since_billing
+        self.consumption = consumption
+        self.cost = cost
+        self.estimated_consumption = estimated_consumption
+        self.estimated_cost = estimated_cost
+
+
+class BCHydroInterval:
+    def __init__(self, start: datetime, end: datetime):
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return f"BCHydroInterval('{self.start}', '{self.end}')"
+
+
+class BCHydroDailyElectricity:
+    def __init__(
+        self,
+        type: str,
+        quality: str,
+        consumption: float,
+        interval: BCHydroInterval,
+        cost: float,
+    ):
+        self.type = type
+        self.quality = quality
+        self.consumption = consumption
+        self.interval = interval
+        self.cost = cost
+
+    def __repr__(self):
+        return f"BCHydroDailyElectricity('{self.type}', '{self.quality}', {self.consumption}, {self.interval}, {self.cost})"
+
+
+# Account details returned from the account JSON response.
+# Only used fields are stored.
+class BCHydroAccount:
+    def __init__(
+        self,
+        evpSlid,
+        evpAccount,
+        evpAccountId,
+        evpProfileId,
+        evpRateGroup,
+        evpBillingStart,
+        evpBillingEnd,
+        evpConsToDate,
+        evpCostToDate,
+        yesterdayPercentage,
+        evpEstConsCurPeriod,
+        evpEstCostCurPeriod,
+        evpCurrentDateTime,
+    ):
+        self.evpSlid = evpSlid
+        self.evpAccount = evpAccount
+        self.evpAccountId = evpAccountId
+        self.evpProfileId = evpProfileId
+        self.evpRateGroup = evpRateGroup
+        self.evpBillingStart = evpBillingStart
+        self.evpBillingEnd = evpBillingEnd
+        self.evpConsToDate = evpConsToDate
+        self.evpCostToDate = evpCostToDate
+        self.yesterdayPercentage = yesterdayPercentage
+        self.evpEstConsCurPeriod = evpEstConsCurPeriod
+        self.evpEstCostCurPeriod = evpEstCostCurPeriod
+        self.evpCurrentDateTime = evpCurrentDateTime
+
+
+class BCHydroDailyUsage:
+    def __init__(
+        self,
+        electricity: List[BCHydroDailyElectricity],
+        rates: BCHydroRates,
+        account: BCHydroAccount,
+    ):
+        self.electricity = electricity
+        self.rates = rates
+        self.account = account
---
+++ packages/bchydro/bchydro-0.8.1.tar.gz/bchydro-0.8.1/setup.py
@@ -0,0 +1,42 @@
+from os import path
+from setuptools import setup
+
+repo_url = "https://github.com/emcniece/bchydro"
+pwd = path.abspath(path.dirname(__file__))
+
+# Extract package version
+with open(path.join(pwd, "VERSION"), encoding="utf-8") as f:
+    version = f.read()
+
+# Pull PyPi description from README.md
+with open(path.join(pwd, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="bchydro",
+    packages=["bchydro"],
+    version=version,
+    license="MIT",
+    description="BCHydro API",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Eric McNiece",
+    author_email="emcniece@gmail.com",
+    url=repo_url,
+    download_url=f"{repo_url}/releases/latest/download/package.tar.gz",
+    keywords=["bchydro"],
+    install_requires=[
+        "aiohttp<=3.7.3",
+        "beautifulsoup4<=4.9.3",
+        "tenacity<=6.3.1",
+        "ratelimit<=2.2.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
