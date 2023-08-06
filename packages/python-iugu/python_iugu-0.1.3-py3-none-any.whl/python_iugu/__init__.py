63eaf8ef4da614ec4692cdbd64e8b62a55ac0def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package python-iugu/0.1.3/python_iugu-0.1.3-py3-none-any.whl

---
+++ packages/python-iugu/python_iugu-0.1.3-py3-none-any.whl/python_iugu/__init__.py
@@ -0,0 +1,14 @@
+from python_iugu.service import (
+    InvoiceService,
+    SubscriptionService,
+    CustomerService
+)
+
+from python_iugu.request import (
+    InvoiceRequest,
+    CustomerRequest,
+    SubscriptionRequest,
+    CustomVariableRequest
+)
+
+from python_iugu.version import __version__
