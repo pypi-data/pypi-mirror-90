6276cbfbe1a6f47c66f61cbfda7bc333d3e98222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:58 +0000

    Add package giotto-tda-nightly/20210108.7/giotto_tda_nightly-20210108.7-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/giotto-tda-nightly/giotto_tda_nightly-20210108.7-cp39-cp39-macosx_10_15_x86_64.whl/gtda/homology/__init__.py
@@ -0,0 +1,16 @@
+"""The module :mod:`gtda.homology` implements transformers to generate
+persistence diagrams."""
+# License: GNU AGPLv3
+
+from .simplicial import VietorisRipsPersistence, SparseRipsPersistence, \
+    WeakAlphaPersistence, EuclideanCechPersistence, FlagserPersistence
+from .cubical import CubicalPersistence
+
+__all__ = [
+    'VietorisRipsPersistence',
+    'SparseRipsPersistence',
+    'WeakAlphaPersistence',
+    'EuclideanCechPersistence',
+    'FlagserPersistence',
+    'CubicalPersistence',
+    ]
