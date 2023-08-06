afc5317eab9d6697dba4b579d6b251724bac7fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package popsicle/0.0.6/popsicle-0.0.6-cp39-cp39-win_amd64.whl

---
+++ packages/popsicle/popsicle-0.0.6-cp39-cp39-win_amd64.whl/popsicle/juce_audio_utils.py
@@ -0,0 +1,29 @@
+from functools import lru_cache as __lru_cache
+
+import cppyy as __cppyy
+
+from .utils import juce_bootstrap as __juce_bootstrap
+from . import juce_audio_processors as __juce_audio_processors
+from . import juce_audio_formats as __juce_audio_formats
+from . import juce_audio_devices as __juce_audio_devices
+
+__all__ = []
+
+@__lru_cache(maxsize=1024)
+def __juce_include():
+    __cppyy.include("juce_audio_utils/juce_audio_utils.h")
+
+    def __pythonize(klass, name):
+        if name == "AudioThumbnail":
+            original_set_source = klass.setSource
+
+            def set_source(obj, source):
+                source.__python_owns__ = False
+                original_set_source(obj, source)
+
+            klass.setSource = set_source
+
+    __cppyy.py.add_pythonization(__pythonize, "juce")
+
+__juce_bootstrap()
+__juce_include()
