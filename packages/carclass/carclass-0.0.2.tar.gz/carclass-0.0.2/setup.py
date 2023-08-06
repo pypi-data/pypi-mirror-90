74b648307aed0d04893a1733888135ea36166a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:36 +0000

    Add package carclass/0.0.2/carclass-0.0.2.tar.gz

---
+++ packages/carclass/carclass-0.0.2.tar.gz/carclass-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'carclass',
+  author_email = 'example@example.com',
+  version = '0.0.2',
+  classifiers = [
+      "Programming Language :: Python :: 3",
+  ],
+  description = ' class test',
+  licence = 'MIT',
+  instal_requires =[],
+  packages = ['carclass'],
+  author = 'ninjamar',
+  python_require = '>=3.7',
+  url = 'https://repl.it/@ninjamar/classes'
+)
