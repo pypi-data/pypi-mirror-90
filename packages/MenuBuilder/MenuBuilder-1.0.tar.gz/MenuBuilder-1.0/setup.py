a404e299cd7efe9691319b76d06e1fda14aa5a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:21 +0000

    Add package MenuBuilder/1.0/MenuBuilder-1.0.tar.gz

---
+++ packages/MenuBuilder/MenuBuilder-1.0.tar.gz/MenuBuilder-1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+
+
+setuptools.setup(
+    name ="MenuBuilder",
+    version="1.0",
+    author = "Ewen",
+    author_email = "1lorimerewe2@gmail.com",
+    description = "A simple commandline menu builder",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/OOgaboogaboog/MenuBuilder",
+    packages= setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+
+        
+        "Operating System :: OS Independent"
+
+    ],
+    python_requires='<=3.9',
+)
+
