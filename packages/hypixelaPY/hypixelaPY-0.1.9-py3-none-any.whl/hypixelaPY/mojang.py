bdea2bd6ea638b39296959a2cb5f1baa50e4a74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:39 +0000

    Add package hypixelaPY/0.1.9/hypixelaPY-0.1.9-py3-none-any.whl

---
+++ packages/hypixelaPY/hypixelaPY-0.1.9-py3-none-any.whl/hypixelaPY/mojang.py
@@ -0,0 +1,22 @@
+from .exceptions import NoPlayerFoundError
+from .objects.objects import MojangPlayer, NameHistory
+import aiohttp
+
+MOJANG_API = "https://api.mojang.com"
+MOJANG_SESSION_SERVER = "https://sessionserver.mojang.com"
+
+
+async def get_player_by_name(name: str) -> MojangPlayer:
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(f"{MOJANG_API}/users/profiles/minecraft/{name}")
+        if response.status != 200:
+            raise NoPlayerFoundError
+    return MojangPlayer(await response.json())
+
+
+async def get_player_name_history(uuid: str) -> NameHistory:
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(f"{MOJANG_API}/user/profiles/{uuid}/names")
+        if response.status != 200:
+            raise NoPlayerFoundError
+    return NameHistory(await response.json())
