54c1055c16715d4dedc0994c6920d3739eca0c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:12 +0000

    Add package udacity-z-distributions/0.1/udacity_z_distributions-0.1.tar.gz

---
+++ packages/udacity-z-distributions/udacity_z_distributions-0.1.tar.gz/udacity_z_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity_z_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_z_distributions'],
+      zip_safe=False)
