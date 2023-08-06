fdd62195fb12043dd4176c83207eb0cb81694aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:51 +0000

    Add package youtube-related/1.0.2/youtube_related-1.0.2-py3-none-any.whl

---
+++ packages/youtube-related/youtube_related-1.0.2-py3-none-any.whl/youtube_related/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Youtube Related Video Fetcher
+"""
+
+__title__ = "youtube_related"
+__author__ = "kijk2869"
+__lisence__ = "MIT"
+__version__ = "1.0.2"
+
+from collections import namedtuple
+
+from .client import async_fetch, fetch, preventDuplication
+from .error import *
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=0, minor=0, micro=4, releaselevel="final", serial=0)
