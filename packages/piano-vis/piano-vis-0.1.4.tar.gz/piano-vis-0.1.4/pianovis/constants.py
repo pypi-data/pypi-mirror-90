a11e51568ccb7fa31d5d4590657f0e9e50cd61e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:23 +0000

    Add package piano-vis/0.1.4/piano-vis-0.1.4.tar.gz

---
+++ packages/piano-vis/piano-vis-0.1.4.tar.gz/piano-vis-0.1.4/pianovis/constants.py
@@ -0,0 +1,36 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+# Colors
+BLACK = (0, 0, 0)
+GRAY = (0, 0, 0.5)
+WHITE = (0, 0, 1)
+
+RED1 = (0, 1, 1)
+ORANGE = (0.083, 1, 1)
+YELLOW = (0.166, 1, 1)
+GREEN = (0.333, 1, 1)
+CYAN = (0.5, 1, 1)
+BLUE = (0.6, 1, 1)
+MAGENTA = (0.8, 1, 1)
+PINK = (0.9, 1, 1)
+RED2 = (1, 1, 1)
+
+
+# Block presets
+BLOCK_PRESET_RAINBOW = ((0, RED1), (1, RED2))
