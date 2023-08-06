02024d86fce9931d8b04ed7bf35a28a5a05f7b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:49 +0000

    Add package oarepo-multilingual/2.4.1/oarepo_multilingual-2.4.1.tar.gz

---
+++ packages/oarepo-multilingual/oarepo_multilingual-2.4.1.tar.gz/oarepo_multilingual-2.4.1/oarepo_multilingual/config.py
@@ -0,0 +1,11 @@
+"""Multilingual extension default configuration."""
+
+
+ELASTICSEARCH_DEFAULT_LANGUAGE_TEMPLATE = {
+    "type": "text",
+    "fields": {
+        "raw": {
+            "type": "keyword"
+        }
+    }
+}
