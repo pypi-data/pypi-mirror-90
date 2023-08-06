af7cd53b85dffe671309818ecbb70ea832cda411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:02 +0000

    Add package zerotk.url2env/1.2/zerotk.url2env-1.2.tar.gz

---
+++ packages/zerotk.url2env/zerotk.url2env-1.2.tar.gz/zerotk.url2env-1.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from os import popen
+from setuptools import setup, find_packages
+import re
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="zerotk.url2env",
+    use_scm_version=True,
+
+    author="Alexandre Andrade",
+    author_email='kaniabi@gmail.com',
+    # author="Stuart Campbell",
+    # author_email="stuart@harto.org",
+
+    url="https://github.com/zerotk/url2env",
+
+    description="Produces env vars from Heroku-style database URLs",
+    long_description=long_description,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords="heroku postgresql shell",
+
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "url2env=url2env:main"
+        ],
+    },
+
+    install_requires=[
+        'click',
+    ],
+    setup_requires=['setuptools_scm'],
+    tests_require=[],
+
+    license="MIT",
+)
