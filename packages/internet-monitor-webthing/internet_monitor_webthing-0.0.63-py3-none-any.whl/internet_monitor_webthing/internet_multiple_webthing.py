205cebfc59a031450825c3b176ee370203db31a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:28 +0000

    Add package internet-monitor-webthing/0.0.63/internet_monitor_webthing-0.0.63-py3-none-any.whl

---
+++ packages/internet-monitor-webthing/internet_monitor_webthing-0.0.63-py3-none-any.whl/internet_monitor_webthing/internet_multiple_webthing.py
@@ -0,0 +1,25 @@
+from internet_monitor_webthing.connectivity_monitor_webthing import InternetConnectivityMonitorWebthing
+from internet_monitor_webthing.speedtest_monitor_webthing import InternetSpeedMonitorWebthing
+from webthing import (MultipleThings, WebThingServer)
+import logging
+
+
+def run_server(hostname: str, port: int, description: str, speedtest_period: int, connecttest_period: int, connecttest_url: str):
+    services = []
+    if speedtest_period > 0:
+        services.append(InternetSpeedMonitorWebthing(description, speedtest_period))
+    if connecttest_period > 0:
+        services.append(InternetConnectivityMonitorWebthing(description, connecttest_period, connecttest_url))
+
+    if len(services) > 0:
+        print("running Internet " + ", ".join([service.get_title() for service in services]) + " on " + hostname + ":" + str(port))
+        server = WebThingServer(MultipleThings(services, "Internet Monitor"), hostname=hostname, port=port)
+        try:
+            logging.info('starting the server')
+            server.start()
+        except KeyboardInterrupt:
+            logging.info('stopping the server')
+            server.stop()
+            logging.info('done')
+    else:
+        print("no service activated")
