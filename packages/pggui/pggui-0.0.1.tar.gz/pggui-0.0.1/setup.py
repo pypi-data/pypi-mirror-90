4ed0ac1053b6832b314624572f146250082f38df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:58 +0000

    Add package pggui/0.0.1/pggui-0.0.1.tar.gz

---
+++ packages/pggui/pggui-0.0.1.tar.gz/pggui-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pggui
+setup(
+    name="pggui",
+    version=pggui.__version__,
+    packages=find_packages(),
+    author="LavaPower",
+    author_email="lavapower84@gmail.com",
+    description="A lib to make GUI made with PyGame",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning"
+    ],
+    install_requires=["pygame"]
+)
