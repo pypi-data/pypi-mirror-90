d1e2399b4dcc5d273c7364a23dc28560220d0476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:46 +0000

    Add package pccc/0.3.2/pccc-0.3.2-py3-none-any.whl

---
+++ packages/pccc/pccc-0.3.2-py3-none-any.whl/pccc/__init__.py
@@ -0,0 +1,30 @@
+"""Python Conventional Commit Checker.
+
+Contains the ConventionalCommit class and all the helper functions for
+loading, cleaning, and parsing a conventional commit.
+
+For programs, the main entry point should be pccc.main().  For
+scripts, pccc.ConventionalCommit() may be used directly:
+
+Examples
+--------
+>>> # Program:
+>>> import pccc
+>>> pccc.main()
+
+>>> # From script:
+>>> import pccc
+>>> ccr = pccc.ConventionalCommitRunner()
+>>> # Load configuration.
+>>> ccr.options.load()
+>>> # Get commit.
+>>> ccr.get()
+>>> # Clean commit.
+>>> ccr.clean()
+>>> # Parse commit.
+>>> ccr.parse()
+"""
+from .config import Config
+from .parser import ConventionalCommit
+from .parser import ConventionalCommitRunner
+from .parser import main
