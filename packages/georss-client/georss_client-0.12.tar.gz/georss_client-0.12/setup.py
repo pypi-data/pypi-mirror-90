fb0d00f4153d95dd80384f0811ed05ad2b0aa275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:28 +0000

    Add package georss-client/0.12/georss_client-0.12.tar.gz

---
+++ packages/georss-client/georss_client-0.12.tar.gz/georss_client-0.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    'haversine>=1.0.1',
+    'xmltodict>=0.11.0',
+    'requests>=2.20.0',
+    'dateparser>=0.7.0',
+]
+
+setup(
+    name="georss_client",
+    version="0.12",
+    author="Malte Franken",
+    author_email="coding@subspace.de",
+    description="A GeoRSS client library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exxamalte/python-georss-client",
+    packages=find_packages(exclude=("tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
