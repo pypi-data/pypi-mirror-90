f1cd93a357e953149c61abe4bcfa1dd4e74306ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:57 +0000

    Add package python-project-vik/1.2.0/python-project-vik-1.2.0.tar.gz

---
+++ packages/python-project-vik/python-project-vik-1.2.0.tar.gz/python-project-vik-1.2.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.rst", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="python-project-vik",
+    version="1.2.0",
+    author="Vikram Shinde",
+    author_email="vik.shinde@gmail.com",
+    description="Python project for testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vikramshinde12/python-project",
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+    install_requires=[],
+)
