cc5c419d351b5c8c3664a36339f3ed39a9ac996f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:13 +0000

    Add package iknowpy/1.0.10/iknowpy-1.0.10-cp35.cp36.cp37.cp38.cp39-cp35m.cp36m.cp37m.cp38.cp39-win_amd64.whl

---
+++ packages/iknowpy/iknowpy-1.0.10-cp35.cp36.cp37.cp38.cp39-cp35m.cp36m.cp37m.cp38.cp39-win_amd64.whl/iknowpy/labels.py
@@ -0,0 +1,19 @@
+class Labels:
+	"""User Dictionary labels"""
+
+	# role labels
+	CONCEPT = "UDConcept"
+	RELATION = "UDRelation"
+	NONRELEVANT = "UDNonRelevant"
+
+	# attribute labels
+	NEGATION = "UDNegation"
+	POS_SENTIMENT = "UDPosSentiment"
+	NEG_SENTIMENT = "UDNegSentiment"
+	UNIT = "UDUnit"
+	NUMBER = "UDNumber"
+	TIME = "UDTime"
+
+	# sentence ending (pseudo label)
+	SENTENCE_END = "UDSentenceEnd"
+	SENTENCE_NO_END = "UDSentenceNoEnd"
