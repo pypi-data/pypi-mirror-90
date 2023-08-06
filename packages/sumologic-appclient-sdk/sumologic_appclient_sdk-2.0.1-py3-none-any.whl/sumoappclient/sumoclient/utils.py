0067bd34aca64a3aefcaed360016e27975423068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:28 +0000

    Add package sumologic-appclient-sdk/2.0.1/sumologic_appclient_sdk-2.0.1-py3-none-any.whl

---
+++ packages/sumologic-appclient-sdk/sumologic_appclient_sdk-2.0.1-py3-none-any.whl/sumoappclient/sumoclient/utils.py
@@ -0,0 +1,16 @@
+# -*- coding: future_fstrings -*-
+import json
+
+
+def get_body(data, jsondump=True):
+    if isinstance(data, list):
+        if jsondump:
+            out = [json.dumps(d) for d in data]
+        else:
+            out = data
+        body = "\n".join(out).encode("utf-8")
+    else:
+        body = json.dumps(data).encode("utf-8")
+    return body
+
+
