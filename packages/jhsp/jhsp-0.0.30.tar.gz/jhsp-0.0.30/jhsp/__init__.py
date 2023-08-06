631f63a7afda909f2afee00ce90f742de1c511a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package jhsp/0.0.30/jhsp-0.0.30.tar.gz

---
+++ packages/jhsp/jhsp-0.0.30.tar.gz/jhsp-0.0.30/jhsp/__init__.py
@@ -0,0 +1,71 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+
+# 导入sann模块的所有包
+import configparser
+
+
+# 定义SANN类
+# # 在调用时实例化，因为要传入一个工作目录
+class Sann():
+    def __init__(self,input_path,task_name_list):
+
+        cg = configparser.ConfigParser()
+        cg['path'] = {'input_path' : input_path,
+
+                             }
+
+        task_name_list_str = ''
+        for task_name in task_name_list:
+            task_name_list_str += ','
+            task_name_list_str += task_name
+
+        cg['task'] = {'task_name_list': task_name_list_str,
+                      'cur_task_path': 'None'
+                      }
+
+        with open(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)),'sann'),'initial.ini'), 'w',encoding='utf-8') as f:
+            cg.write(f)
+
+        from jhsp.sann.bin import config
+        from jhsp.sann.bin import other_utils
+        from jhsp.sann.bin import data_utils
+        from jhsp.sann.bin import model_utils
+
+        self.config = config.config
+        self.other_utils = other_utils.utils
+        self.data_utils = data_utils.utils
+        self.model_utils = model_utils.utils
+
+class Preprocessing():
+    def __init__(self):
+        from jhsp.preprocessing import codingdata
+        from jhsp.preprocessing import describingdata
+        from jhsp.preprocessing import processingdata
+        self.CodingData = codingdata.CodingData
+        self.DescribingData = describingdata.DescribingData
+        self.ProcessingData = processingdata.ProcessingData
+
+
+
+class Tools():
+    def __init__(self):
+        from jhsp.tools import collatingwords
+        self.CollatingWords = collatingwords.CollatingWords
+
+
+
+
+
+
+
+
+
+
+
+
+
+
