7c10c09ebaf26bf28d079a5523f40350bdd602da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:10 +0000

    Add package jpe-types/0.0.0.4.4/jpe_types-0.0.0.4.4.tar.gz

---
+++ packages/jpe-types/jpe_types-0.0.0.4.4.tar.gz/jpe_types-0.0.0.4.4/jpe_types/setup.py
@@ -0,0 +1,18 @@
+"setup script"
+
+import json
+from os.path import dirname, join
+from os import system
+from sys import executable
+
+def run():
+    #update logg utils json file
+
+    path = dirname(__file__)
+    #print( f"{executable} {join(path, u"utils/Optimizations/build.py")}")
+    buildScript_path = join(path, "utils", "Optimizations", "build.py")
+    outputScriptPath = join(path, "utils", "Optimizations")
+    system(f"{executable} {buildScript_path} build_ext --build-lib {outputScriptPath}")
+
+if __name__ == "__main__":
+    run()
