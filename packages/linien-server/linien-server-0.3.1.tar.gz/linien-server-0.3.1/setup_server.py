ebeabe156779b4894ef5ddfbda14d1c3b2a60ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:18 +0000

    Add package linien-server/0.3.1/linien-server-0.3.1.tar.gz

---
+++ packages/linien-server/linien-server-0.3.1.tar.gz/linien-server-0.3.1/setup_server.py
@@ -0,0 +1,33 @@
+import setuptools
+
+import linien
+assert linien.__version__ != 'dev'
+
+setuptools.setup(
+    name="linien-server",
+    version=linien.__version__,
+    author="Benjamin Wiegand",
+    author_email="highwaychile@posteo.de",
+    description="Server of linien that runs on RedPitaya.",
+    long_description="Have a look at the \"linien\" package for installation instructions.",
+    long_description_content_type="text/x-rst",
+    url="https://github.com/hermitdemschoenenleben/linien",
+    packages=['linien', 'linien.server', 'linien.server.optimization'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'rpyc>=4.1.0', 'myhdl', 'click', 'cma', 'plumbum'
+    ],
+    scripts=[
+        'linien/server/linien_start_server',
+        'linien/server/linien_stop_server',
+        'linien/server/linien_install_requirements'
+    ],
+    package_data={
+        '': ['linien.bin', 'linien_start_server', 'linien_stop_server',
+             'linien_install_requirements', 'VERSION']
+    }
+)
