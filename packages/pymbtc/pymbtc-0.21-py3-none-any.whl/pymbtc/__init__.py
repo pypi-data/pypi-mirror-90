835c3b20539c31dbffca8781a0fe965ab0cea9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:32 +0000

    Add package pymbtc/0.21/pymbtc-0.21-py3-none-any.whl

---
+++ packages/pymbtc/pymbtc-0.21-py3-none-any.whl/pymbtc/__init__.py
@@ -0,0 +1,13 @@
+""" 
+API de Negociações
+Documentação Oficial: 
+https://www.mercadobitcoin.com.br/trade-api/
+
+API de Dados
+Documentação Oficial:
+https://www.mercadobitcoin.com.br/api-doc/
+
+"""
+
+from .api import Api
+from .tradeapi import TradeApi
