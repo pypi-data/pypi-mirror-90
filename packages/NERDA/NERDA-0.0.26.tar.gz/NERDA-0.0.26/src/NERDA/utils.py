7461520d15eaa3d35306bbaef6d90aa60c156f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:16 +0000

    Add package NERDA/0.0.26/NERDA-0.0.26.tar.gz

---
+++ packages/NERDA/NERDA-0.0.26.tar.gz/NERDA-0.0.26/src/NERDA/utils.py
@@ -0,0 +1,15 @@
+# Helper function that flattens a list of lists
+def flatten(xs):
+    return [item for sublist in xs for item in sublist]
+
+def match_kwargs(function, **kwargs):
+    
+    arg_count = function.__code__.co_argcount
+    args = function.__code__.co_varnames[:arg_count]
+
+    args_dict = {}
+    for k, v in kwargs.items():
+        if k in args:
+            args_dict[k] = v
+
+    return args_dict
