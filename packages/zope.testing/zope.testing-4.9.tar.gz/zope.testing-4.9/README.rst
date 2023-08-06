ebbd4491662cc63d0a5b258ffa08dc7d0a7d9620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package zope.testing/4.9/zope.testing-4.9.tar.gz

---
+++ packages/zope.testing/zope.testing-4.9.tar.gz/zope.testing-4.9/README.rst
@@ -0,0 +1,78 @@
+=================
+``zope.testing``
+=================
+
+.. image:: https://img.shields.io/pypi/v/zope.testing.svg
+    :target: https://pypi.python.org/pypi/zope.testing/
+    :alt: Latest Version
+
+.. image:: https://travis-ci.com/zopefoundation/zope.testing.svg?branch=master
+        :target: https://travis-ci.com/zopefoundation/zope.testing
+
+.. image:: https://readthedocs.org/projects/zopetesting/badge/?version=latest
+        :target: http://zopetesting.readthedocs.org/en/latest/
+        :alt: Documentation Status
+
+This package provides a number of testing frameworks.
+
+cleanup
+  Provides a mixin class for cleaning up after tests that
+  make global changes.
+
+formparser
+  An HTML parser that extracts form information.
+
+  **Python 2 only**
+
+  This is intended to support functional tests that need to extract
+  information from HTML forms returned by the publisher.
+
+  See formparser.txt.
+
+loggingsupport
+  Support for testing logging code
+
+  If you want to test that your code generates proper log output, you
+  can create and install a handler that collects output.
+
+loghandler
+  Logging handler for tests that check logging output.
+
+module
+  Lets a doctest pretend to be a Python module.
+
+  See module.txt.
+
+renormalizing
+  Regular expression pattern normalizing output checker.
+  Useful for doctests.
+
+server
+  Provides a simple HTTP server compatible with the zope.app.testing
+  functional testing API.  Lets you interactively play with the system
+  under test.  Helpful in debugging functional doctest failures.
+
+  **Python 2 only**
+
+setupstack
+  A simple framework for automating doctest set-up and tear-down.
+  See setupstack.txt.
+
+wait
+  A small utility for dealing with timing non-determinism
+  See wait.txt.
+
+doctestcase
+  Support for defining doctests as methods of ``unittest.TestCase``
+  classes so that they can be more easily found by test runners, like
+  nose, that ignore test suites.
+
+.. contents::
+
+Getting started developing zope.testing
+=======================================
+
+zope.testing uses buildout.  To start, run ``python bootstrap.py``.  It will
+create a number of directories and the ``bin/buildout`` script.  Next, run
+``bin/buildout``.  It will create a test script for you.  Now, run ``bin/test``
+to run the zope.testing test suite.
---
+++ packages/zope.testing/zope.testing-4.9.tar.gz/zope.testing-4.9/setup.py
@@ -0,0 +1,97 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.testing package
+"""
+import os
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+chapters = [
+    read((os.path.join('src', 'zope', 'testing', name)))
+    for name in [
+        'formparser.txt',
+        'loggingsupport.txt',
+        'renormalizing.txt',
+        'setupstack.txt',
+        'wait.txt',
+        'doctestcase.txt',
+    ]
+]
+
+
+long_description = '\n\n'.join(
+    [read('README.rst')] +
+    chapters +
+    [read('CHANGES.rst')]
+)
+keywords = "zope testing doctest RENormalizing OutputChecker timeout logging"
+
+setup(
+    name='zope.testing',
+    version='4.9',
+    url='https://github.com/zopefoundation/zope.testing',
+    license='ZPL 2.1',
+    description='Zope testing helpers',
+    long_description=long_description,
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Zope :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+    ],
+    keywords=keywords,
+    packages=[
+        "zope",
+        "zope.testing",
+    ],
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'zope.testrunner',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='zope.testing.tests.test_suite',
+)
