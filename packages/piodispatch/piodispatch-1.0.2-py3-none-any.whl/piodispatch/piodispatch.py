49cb3e16ed4347f3f2c083ffc488019b39f19bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:16 +0000

    Add package piodispatch/1.0.2/piodispatch-1.0.2-py3-none-any.whl

---
+++ packages/piodispatch/piodispatch-1.0.2-py3-none-any.whl/piodispatch/piodispatch.py
@@ -0,0 +1,21 @@
+"""PioDispatch library's main module.
+Copyright (c) Kiruse 2020. See license in LICENSE."""
+from asyncio.futures import wrap_future
+from concurrent.futures import ThreadPoolExecutor
+from typing import Awaitable, Callable, TypeVar
+import functools
+
+pool = ThreadPoolExecutor(100, 'io-runner-')
+T = TypeVar('T')
+
+def ascoroutine(fn: Callable[..., T]) -> Callable[..., T]:
+    @functools.wraps(fn)
+    async def inner(*args, **kwargs):
+        return await dispatch(fn, *args, **kwargs)
+    return inner
+
+def dispatch(callback: Callable[..., T], *args, **kwargs) -> Awaitable[T]:
+    return wrap_future(pool.submit(callback, *args, **kwargs))
+
+def shutdown():
+    pool.shutdown()
