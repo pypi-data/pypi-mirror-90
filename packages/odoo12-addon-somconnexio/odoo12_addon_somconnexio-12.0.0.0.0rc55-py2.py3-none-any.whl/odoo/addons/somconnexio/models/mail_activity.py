40606d12486dccfbeb70d77089440d78e90f1c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:51 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc55/odoo12_addon_somconnexio-12.0.0.0.0rc55-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-somconnexio/odoo12_addon_somconnexio-12.0.0.0.0rc55-py2.py3-none-any.whl/odoo/addons/somconnexio/models/mail_activity.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+
+
+class MailActivity(models.Model):
+    _inherit = 'mail.activity'
+
+    reference = fields.Char(
+        string='Reference',
+        compute='_compute_reference',
+        readonly=True,
+        store=False
+    )
+    activity_type_name = fields.Char(
+        related="activity_type_id.name"
+    )
+    date_done = fields.Date(readonly=False)
+    location = fields.Char()
+
+    @api.depends('res_model', 'res_id')
+    def _compute_reference(self):
+        for res in self:
+            res.reference = "%s,%s" % (res.res_model, res.res_id)
