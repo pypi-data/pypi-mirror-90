510854b70836bfe11028a90fe600fdab73a36370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package prettyqt/0.171.1/PrettyQt-0.171.1.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.171.1.tar.gz/PrettyQt-0.171.1/prettyqt/__init__.py
@@ -0,0 +1,34 @@
+"""Top-level package for PrettyQt."""
+
+__author__ = """Philipp Temminghoff"""
+__email__ = "phil65@kodi.tv"
+__version__ = "0.171.1"
+
+
+def debug():
+    """Print the local variables in the caller's frame."""
+    import inspect
+
+    frame = inspect.currentframe()
+    if frame is None or frame.f_back is None:
+        del frame
+        raise RuntimeError()
+    try:
+        from prettyqt.objbrowser import objectbrowser
+
+        objectbrowser.ObjectBrowser.browse(frame.f_back.f_globals)
+    finally:
+        del frame
+
+
+__all__ = [
+    "core",
+    "gui",
+    "widgets",
+    "custom_widgets",
+    "custom_models",
+    "syntaxhighlighters",
+    "charts",
+    "webenginewidgets",
+    "debug",
+]
---
+++ packages/prettyqt/PrettyQt-0.171.1.tar.gz/PrettyQt-0.171.1/prettyqt/location/placeicon.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from prettyqt import core
+from prettyqt.qt import QtLocation
+
+
+class PlaceIcon(QtLocation.QPlaceIcon):
+    def __bool__(self):
+        return not self.isEmpty()
+
+    def __setitem__(self, index: str, val):
+        attrs = self.parameters()
+        attrs[index] = val
+        self.setParameters(attrs)
+
+    def __getitem__(self, index: str):
+        attr = self.parameters()
+        if index not in attr:
+            raise KeyError(f"Key {index!r} does not exist.")
+        return attr[index]
+
+    # def get_manager(self) -> location.PlaceManager:
+    #     return location.PlaceManager(self.manager())
+
+    def get_url(self) -> core.Url:
+        return core.Url(self.url())
+
+
+if __name__ == "__main__":
+    icon = PlaceIcon()
+    print(bool(icon))
