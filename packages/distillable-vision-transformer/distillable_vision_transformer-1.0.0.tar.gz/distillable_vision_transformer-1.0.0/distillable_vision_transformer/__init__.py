2527d40f4f51cd685f3a7221328eb18db32b89ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:39 +0000

    Add package distillable-vision-transformer/1.0.0/distillable_vision_transformer-1.0.0.tar.gz

---
+++ packages/distillable-vision-transformer/distillable_vision_transformer-1.0.0.tar.gz/distillable_vision_transformer-1.0.0/distillable_vision_transformer/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "1.0.0"
+from .model import DistillableVisionTransformer, DistillationTrainer, DistilledLoss, VALID_MODELS
+from .utils import load_pretrained_weights, get_model_params
