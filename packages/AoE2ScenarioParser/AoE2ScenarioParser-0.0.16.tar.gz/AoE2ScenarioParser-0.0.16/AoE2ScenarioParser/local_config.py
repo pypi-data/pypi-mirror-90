5caf9a5a7ee04af21a78ace0937b6e1fdcd2df7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:56 +0000

    Add package AoE2ScenarioParser/0.0.16/AoE2ScenarioParser-0.0.16.tar.gz

---
+++ packages/AoE2ScenarioParser/AoE2ScenarioParser-0.0.16.tar.gz/AoE2ScenarioParser-0.0.16/AoE2ScenarioParser/local_config.py
@@ -0,0 +1,4 @@
+# Configuration file
+folder_de = "C:/Users/Kerwin Sneijders/Games/Age of Empires 2 DE/76561198140740017/resources/_common/scenario/"
+folder_local = "C:/Users/Kerwin Sneijders/Documents/Python/AoE2ScenarioParser/resources/maps/"
+
