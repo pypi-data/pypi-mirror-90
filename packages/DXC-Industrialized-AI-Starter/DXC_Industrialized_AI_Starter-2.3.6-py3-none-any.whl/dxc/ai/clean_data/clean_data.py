cf3bc45b3e2b67487f1e073b3c0978815dc44ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:50 +0000

    Add package DXC-Industrialized-AI-Starter/2.3.6/DXC_Industrialized_AI_Starter-2.3.6-py3-none-any.whl

---
+++ packages/DXC-Industrialized-AI-Starter/DXC_Industrialized_AI_Starter-2.3.6-py3-none-any.whl/dxc/ai/clean_data/clean_data.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import janitor #data cleaning
+from ftfy import fix_text #data cleaning
+import nltk #data cleaning
+nltk.download('punkt') #data cleaning
+import scrubadub #data cleaning
+import arrow #normalizing dates
+import numpy as np
+from sklearn.base import TransformerMixin
+from sklearn.impute import KNNImputer ##using KNN as imputer for categorical fields
+from sklearn.preprocessing import OrdinalEncoder ##Ordinal encoder is being used for encoding categorical objects
+from dxc.ai.global_variables import globals_file
+from datetime import datetime
+
+def encode(data):
+
+    '''function to encode non-null data and replace it in the original data'''
+    encoder = OrdinalEncoder()
+    #retains only non-null values
+    nonulls = np.array(data.dropna())
+    #reshapes the data for encoding
+    impute_reshape = nonulls.reshape(-1,1)
+    #encode date
+    impute_ordinal = encoder.fit_transform(impute_reshape)
+    #encoders_store[column_name]=encoder
+    #Assign back encoded values to non-null values
+    data.loc[data.notnull()] = np.squeeze(impute_ordinal)
+    return (data,encoder)
+
+def impute_df(df):
+    # imputer = KNN()
+    imputer = KNNImputer(n_neighbors=2)
+    object_types = list(df.select_dtypes(include=['object']).columns)
+    num_types = list(set(df.columns) - set(object_types))
+    encoders_store={}
+    for column in num_types:
+        skew=df[column].skew()
+        if (-1 < skew < 1):
+            df[column]=df[column].fillna(df[column].mean())
+        else :
+            df[column]=df[column].fillna(df[column].median())
+    #create a for loop to iterate through each column in the data
+    for columns in object_types:
+        new=encode(df[columns])
+        encoders_store[columns]=new[1]
+    imputed_data = pd.DataFrame(np.round(imputer.fit_transform(df)),columns = df.columns)
+    for columns in object_types:
+        imputed_data[columns]=encoders_store[columns].inverse_transform(np.array(imputed_data[columns]).reshape(-1,1))
+    return imputed_data
+
+#CLEANING FILE
+def clean_dataframe(df, impute = False, text_fields = [], date_fields = [], numeric_fields = [], categorical_fields = []):
+    clean_df = (
+      df
+      #make the column names lower case and remove spaces
+      .clean_names()
+
+      #remove empty columns
+      .remove_empty()
+
+      #remove empty rows and columns
+      .dropna(how='all')
+    )
+
+    #remove harmful characters. remove personal identifiers. make lowercase
+    for field in text_fields:
+        field = '_'.join(field.split()).lower()
+        clean_df[field] = clean_df[field].fillna(' ').apply(fix_text)
+        clean_df[field] = clean_df[field].apply(scrubadub.clean, replace_with='identifier')
+        clean_df[field] = clean_df[field].str.lower()
+
+    #impute missing values
+    if impute:
+        clean_df = impute_df(clean_df)
+
+    #standardize the format of all date fields
+    for field in date_fields:
+        field = '_'.join(field.split()).lower()
+        try:
+          clean_df[field] = clean_df[field].apply(arrow.get)
+        except ParserError:
+          try:
+            clean_df[field] = clean_df[field].apply(lambda x: arrow.get(datetime.strptime(x,'%m/%d/%Y')))
+          except ValueError:
+            clean_df[field] = clean_df[field].apply(lambda x: arrow.get(datetime.strptime(x,'%m/%d/%y')))
+
+    #make sure all numeric fields have the proper data type
+    for field in numeric_fields:
+        field = '_'.join(field.split()).lower()
+        clean_df[field] = pd.to_numeric(clean_df[field])
+
+    #make sure all categorical variables have the proper data type
+    for field in categorical_fields:
+        field = '_'.join(field.split()).lower()
+        clean_df[field] = clean_df[field].astype('category')
+
+    clean_df=clean_df.clean_names()
+
+    globals_file.clean_data_used = True
+
+    return(clean_df)
---
+++ packages/DXC-Industrialized-AI-Starter/DXC_Industrialized_AI_Starter-2.3.6-py3-none-any.whl/dxc/ai/run_model/run_model.py
@@ -0,0 +1,123 @@
+import arrow #normalizing dates
+import numpy as np
+from sklearn.base import TransformerMixin #impute missing data
+from auto_ml import Predictor #ML models
+from sklearn.model_selection import train_test_split
+import os
+import pickle
+from contextlib import redirect_stdout
+import warnings
+import io
+from dxc.ai.global_variables import globals_file
+from .TimeSeriesModels import getBestForcastingModel
+from dxc.ai.logging import experiment_design_logging
+from .model_pipeline import regressor
+from .model_pipeline import train_model
+from .model_pipeline import classifier
+
+
+# define the general class of models
+class model:
+    __model = []
+    def build(self, meta_data): raise NotImplementedError()
+    def train_and_score(self, data): raise NotImplementedError()
+    def interpret(self): raise NotImplementedError()
+    def python_object(): raise NotImplementedError()
+
+    @staticmethod
+    def meta_data_key(meta_data, value):
+        key_list = list(meta_data.keys())
+        val_list = list(meta_data.values())
+
+        return key_list[val_list.index(value)]
+
+#define the model lifecycle
+
+
+# define a prediction model
+class prediction(model):
+
+    @property
+    def estimator(self):
+        raise NotImplementedError()
+
+    def build(self, meta_data, verbose, max_time_mins, max_eval_time_mins, config_dict, warm_start, scoring):
+        if self.estimator == 'TPOTRegressor':
+            if globals_file.run_experiment_warm_start == False or warm_start == False:
+                self.__model = regressor(verbose, max_time_mins, max_eval_time_mins, config_dict, warm_start, scoring)
+            else:
+                self.__model = globals_file.run_experiment_model
+        elif self.estimator == 'TPOTClassifier':
+            if globals_file.run_experiment_warm_start == False or warm_start == False:
+                self.__model = classifier(verbose, max_time_mins, max_eval_time_mins, config_dict, warm_start, scoring)
+            else:
+                self.__model = globals_file.run_experiment_model
+        else:    
+            self.__model = Predictor(type_of_estimator=self.estimator, column_descriptions=meta_data)
+        self.__label = self.meta_data_key(meta_data, "output")
+
+    def train_and_score(self, data, labels, verbose, interpret, warm_start, export_pipeline):
+        ##Train and score 
+        if self.estimator == 'TPOTRegressor' or self.estimator == 'TPOTClassifier':
+            self.__model = train_model(data, self.__label, self.__model, self.estimator, interpret, warm_start, export_pipeline)
+        else:
+            # create training and test data
+            training_data, test_data = train_test_split(data, test_size=0.2)
+
+        # train the model
+            if verbose == False:
+                warnings.filterwarnings('ignore')
+                text_trap = io.StringIO()
+                with redirect_stdout(text_trap):
+                    self.__model.train(training_data, verbose=False, ml_for_analytics= False)
+            else:
+                warnings.filterwarnings('ignore')
+                self.__model.train(training_data, verbose=True, ml_for_analytics=False)
+
+        # score the model
+            if verbose == False:
+                self.__model.score(test_data, test_data[self.__label], verbose=0)
+            else:
+                self.__model.score(test_data, test_data[self.__label], verbose=1)
+
+    def interpret(self):
+        pass
+
+    def python_object(self):
+        return self.__model
+
+# define a regressor model
+class regression(prediction):
+    @property
+    def estimator(self):
+        return("regressor")
+
+# define a classification model
+class classification(prediction):
+    @property
+    def estimator(self):
+        return("classifier")
+    
+# define a tpot classification model
+class tpot_classification(prediction):
+    @property
+    def estimator(self):
+        return("TPOTClassifier")
+    
+# define a Tpot regressor model
+class tpot_regression(prediction):
+    @property
+    def estimator(self):
+        return("TPOTRegressor")
+
+def run_experiment(design, verbose = False, interpret = False, max_time_mins = 5, max_eval_time_mins = 0.04 , config_dict = None, warm_start = False, export_pipeline = True, scoring = None):
+    experiment_design_logging.experiment_design_log(design)
+    if design["model"] == 'timeseries':
+        trained_model = getBestForcastingModel(design['labels'], no_predictions=7, debug=verbose, visualize = False)
+        return trained_model
+    globals_file.run_experiment_used = True
+    design["model"].build(design["meta_data"], verbose, max_time_mins, max_eval_time_mins, config_dict,warm_start, scoring)
+    design["model"].train_and_score(design["data"], design["labels"], verbose, interpret, warm_start, export_pipeline)
+    design["model"].interpret()
+    return design["model"].python_object()
+    
