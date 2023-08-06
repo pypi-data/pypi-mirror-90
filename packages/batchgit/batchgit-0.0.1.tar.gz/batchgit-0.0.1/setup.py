d2387b99b9aa1937579aa9173928299332d8d8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:28 +0000

    Add package batchgit/0.0.1/batchgit-0.0.1.tar.gz

---
+++ packages/batchgit/batchgit-0.0.1.tar.gz/batchgit-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.rst").read_text()
+
+setup(
+    name="batchgit",
+    version="0.0.1",
+    description="Tool to manage multiple git repos",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author="Hong Jiang",
+    author_email="lazyseq@gmail.com",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=["gitpython", "pyyaml"],
+    entry_points={
+        "console_scripts": [
+            "batchgit = batchgit.cli:main",
+        ],
+    },
+)
