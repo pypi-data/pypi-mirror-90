955f615bb1eafd575e75a4957b87aa90c73b6618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:43 +0000

    Add package tonicapp-helpers/2.4.4/tonicapp-helpers-2.4.4.tar.gz

---
+++ packages/tonicapp-helpers/tonicapp-helpers-2.4.4.tar.gz/tonicapp-helpers-2.4.4/helpers/middleware/locale.py
@@ -0,0 +1,33 @@
+import logging
+
+from re import fullmatch
+
+from django.utils.translation import activate
+from django.http import HttpResponseBadRequest
+
+logger = logging.getLogger()
+
+
+class LocaleMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logger.debug(request.headers)
+        accept_language = request.META.get('HTTP_ACCEPT_LANGUAGE')
+        if not accept_language:
+            return HttpResponseBadRequest(
+                'Add Accept-Language to the header. Example: Accept-Language: pt-PT'
+            )
+        if not fullmatch('^[a-z]{2}\-[A-Z]{2}$', accept_language):
+            return HttpResponseBadRequest(
+                'Incorrect Accept-Language. Example: Accept-Language: pt-PT'
+            )
+
+        activate(accept_language)
+
+        request.locale = accept_language
+
+        response = self.get_response(request)
+
+        return response
