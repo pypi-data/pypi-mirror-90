d56a6972997a14726f40c2909dcd83f2536c7746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:03 +0000

    Add package distributions-dangat/0.1/distributions_dangat-0.1.tar.gz

---
+++ packages/distributions-dangat/distributions_dangat-0.1.tar.gz/distributions_dangat-0.1/README.md
@@ -0,0 +1,3 @@
+**This i a brief description about this package**
+
+## DESCRIPTION: This package is about distributions such as Binomial and Gaussian. In order to create them and operate instances with them.
---
+++ packages/distributions-dangat/distributions_dangat-0.1.tar.gz/distributions_dangat-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_dangat',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_dangat'],
+      zip_safe=False)
