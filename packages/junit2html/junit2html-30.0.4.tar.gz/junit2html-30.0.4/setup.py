fa670c307e6b116e323eed4c733f6089589b6f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:10 +0000

    Add package junit2html/30.0.4/junit2html-30.0.4.tar.gz

---
+++ packages/junit2html/junit2html-30.0.4.tar.gz/junit2html-30.0.4/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+
+files = [os.path.join("templates", "*.css"),
+         os.path.join("templates", "*.html")]
+
+
+setup(
+    name="junit2html",
+    version="30.0.4",
+    description="Generate HTML reports from Junit results",
+    author="Ian Norton",
+    author_email="inorton@gmail.com",
+    url="https://gitlab.com/inorton/junit2html",
+    install_requires=["jinja2>=2.11.2"],
+    packages=["junit2htmlreport"],
+    package_data={"junit2htmlreport": files},
+    entry_points={'console_scripts': ['junit2html=junit2htmlreport.runner:start']},
+    platforms=["any"],
+    license="License :: OSI Approved :: MIT License",
+    long_description="Genearate a single file HTML report from a Junit or XUnit XML results file"
+)
