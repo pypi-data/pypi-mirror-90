0ab40aee990832e218f993da51baab1594302dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:32 +0000

    Add package advent-of-code/2019.12.267/advent_of_code-2019.12.267-cp38-cp38-win_amd64.whl

---
+++ packages/advent-of-code/advent_of_code-2019.12.267-cp38-cp38-win_amd64.whl/cli/main.py
@@ -0,0 +1,18 @@
+from advent_of_code import solve
+import sys
+
+
+def main():
+    if len(sys.argv) != 4:
+        sys.exit("usage: advent-of-code-py YEAR DAY PART < INPUT")
+
+    year = sys.argv[1]
+    day = sys.argv[2]
+    part = sys.argv[3]
+    problem_input = sys.stdin.read()
+
+    try:
+        problem_output = solve(year, day, part, problem_input)
+        print(problem_output)
+    except Exception as error:
+        sys.exit("{0}".format(error))
