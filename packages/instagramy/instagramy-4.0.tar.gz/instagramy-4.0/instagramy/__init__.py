0a400ccad6e9196a0cfb005717c23b5d11413d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:19 +0000

    Add package instagramy/4.0/instagramy-4.0.tar.gz

---
+++ packages/instagramy/instagramy-4.0.tar.gz/instagramy-4.0/instagramy/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    Instagramy
+    ~~~~~~~~~~
+
+    A python package for Instagram. It scarpe the Instagram
+    contents.
+
+    :license: MIT License
+"""
+
+from .InstagramUser import InstagramUser
+from .InstagramPost import InstagramPost
+from .InstagramHashTag import InstagramHashTag
