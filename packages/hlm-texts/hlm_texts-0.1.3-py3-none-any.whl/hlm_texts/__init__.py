8b7a702be009d4b5d78478f2535880e41005f653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package hlm-texts/0.1.3/hlm_texts-0.1.3-py3-none-any.whl

---
+++ packages/hlm-texts/hlm_texts-0.1.3-py3-none-any.whl/hlm_texts/__init__.py
@@ -0,0 +1,18 @@
+"""Init."""
+from pathlib import Path
+from .load_text import load_text
+from .seg_text import seg_text
+
+__version__ = "0.1.3"
+
+# pylint: disable=invalid-name
+cdir = Path(__file__).parent
+hlm_zh = load_text(cdir / "340-脂砚斋重批红楼梦.txt")
+hlm_en = load_text(cdir / "david-hawks-the-story-of-the-stone.txt")
+hlm_en1 = load_text(cdir / "yang-hlm.txt")
+hlm_en2 = load_text(cdir / "joly-hlm.txt")
+
+fangfang_en = load_text(cdir / "fangfang-en.txt")
+fangfang_zh = load_text(cdir / "fangfang-zh.txt")
+
+__all__ = ("load_text", "seg_text")
