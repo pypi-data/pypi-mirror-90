55deed272fc21f0efa9177cbc625d8a991ec920e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:00 +0000

    Add package bluedot-rest-framework/2.0.98/bluedot_rest_framework-2.0.98-py3-none-any.whl

---
+++ packages/bluedot-rest-framework/bluedot_rest_framework-2.0.98-py3-none-any.whl/bluedot_rest_framework/wechat/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import SerializerMethodField
+from bluedot_rest_framework import import_string
+from bluedot_rest_framework.utils.serializers import CustomSerializer
+
+from bluedot_rest_framework.wechat.models import WeChatUser
+
+User = import_string('user.models')
+UserSerializer = import_string('user.serializers')
+
+
+class WeChatUserSerializer(CustomSerializer):
+    profile = SerializerMethodField()
+
+    class Meta:
+        model = WeChatUser
+        fields = '__all__'
+
+    def get_profile(self, queryset):
+        user_queryset = User.objects.filter(
+            wechat_id=queryset.pk).first()
+        if user_queryset:
+            serializer = UserSerializer(user_queryset)
+            return serializer.data
+        else:
+            return None
