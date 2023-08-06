4ca7a258b6166ec20b148db47b7efeca401e3401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:39 +0000

    Add package django-config-models/2.0.4/django-config-models-2.0.4.tar.gz

---
+++ packages/django-config-models/django-config-models-2.0.4.tar.gz/django-config-models-2.0.4/config_models/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+config_models Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class ConfigModelsConfig(AppConfig):
+    """
+    Configuration for the config_models Django application.
+    """
+
+    name = 'config_models'
