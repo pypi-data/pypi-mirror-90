2971d12d2334ddba1e03ab467fc23a3ef6d8112f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:18 +0000

    Add package icli/0.0.10/icli-0.0.10.tar.gz

---
+++ packages/icli/icli-0.0.10.tar.gz/icli-0.0.10/setup.py
@@ -0,0 +1,24 @@
+__version__ = '0.0.10'
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='icli',
+    version=__version__,
+    author='Altertech',
+    author_email='div@altertech.com',
+    description='Interactive CLI builder',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alttch/icli',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=['argcomplete'],
+    classifiers=('Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: User Interfaces'),
+)
