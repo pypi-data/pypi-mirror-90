9d78a0acda0a3f0152c2c72105cb9568c91957ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:20 +0000

    Add package cacho/0.0.5/cacho-0.0.5.tar.gz

---
+++ packages/cacho/cacho-0.0.5.tar.gz/cacho-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Distribution, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cacho',
+    version='0.0.5',
+    author="Sanix-darker",
+    author_email="s4nixd@gmail.com",
+    description="A tiny C++ in-memory cache system with Least recently Used algorithm!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/osscameroon/cacho",
+    packages=find_packages(),
+    package_data={
+        'cacho': ['./build/lib/cacho.so'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
