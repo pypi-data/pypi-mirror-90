3edc011fbbf1d267f8e0a92004adb91a87dfa635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:48 +0000

    Add package mlen-kk-distribution/0.1/mlen_kk_distribution-0.1.tar.gz

---
+++ packages/mlen-kk-distribution/mlen_kk_distribution-0.1.tar.gz/mlen_kk_distribution-0.1/README.md
@@ -0,0 +1,23 @@
+# mlen_kk_distribution package (test-upload)
+
+Summary of the package...
+
+
+## Install
+
+```
+```
+
+## Usage
+
+```
+```
+
+## Contributing
+
+PRs accepted.
+
+## License
+
+```
+```
---
+++ packages/mlen-kk-distribution/mlen_kk_distribution-0.1.tar.gz/mlen_kk_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlen_kk_distribution',
+      version='0.1',
+      description='Gaussian, Binomial and General distributions',
+      packages=['mlen_kk_distribution'],
+      author= 'Konstantinos Katsinas',
+      author_email= 'konstantinoskatsinas@gmail.com',
+      zip_safe=False)
