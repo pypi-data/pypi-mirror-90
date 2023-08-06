02e28278361aab46db2e2f3a903f640a2ad1d678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:47 +0000

    Add package exoplanet-core/0.0.1/exoplanet_core-0.0.1-cp38-cp38-macosx_10_14_x86_64.whl

---
+++ packages/exoplanet-core/exoplanet_core-0.0.1-cp38-cp38-macosx_10_14_x86_64.whl/exoplanet_core/numpy/ops.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["kepler", "quad_solution_vector", "contact_points"]
+
+
+import numpy as np
+
+from .. import driver
+
+
+def kepler(mean_anomaly, eccentricity):
+    mean_anomaly = np.ascontiguousarray(mean_anomaly, dtype=np.float64)
+    eccentricity = np.ascontiguousarray(eccentricity, dtype=np.float64)
+    cosf = np.empty_like(mean_anomaly)
+    sinf = np.empty_like(mean_anomaly)
+    return driver.solve_kepler(mean_anomaly, eccentricity, cosf, sinf)
+
+
+def quad_solution_vector(b, r):
+    b = np.ascontiguousarray(b, dtype=np.float64)
+    r = np.ascontiguousarray(r, dtype=np.float64)
+    s = np.empty(r.shape + (3,), dtype=np.float64)
+    return driver.quad_solution_vector(b, r, s)
+
+
+def contact_points(a, e, cosw, sinw, cosi, sini, L):
+    a = np.ascontiguousarray(a, dtype=np.float64)
+    e = np.ascontiguousarray(e, dtype=np.float64)
+    cosw = np.ascontiguousarray(cosw, dtype=np.float64)
+    sinw = np.ascontiguousarray(sinw, dtype=np.float64)
+    cosi = np.ascontiguousarray(cosi, dtype=np.float64)
+    sini = np.ascontiguousarray(sini, dtype=np.float64)
+    L = np.ascontiguousarray(L, dtype=np.float64)
+    M_left = np.empty_like(a)
+    M_right = np.empty_like(a)
+    flag = np.empty_like(a, dtype=np.int32)
+    return driver.contact_points(
+        a, e, cosw, sinw, cosi, sini, L, M_left, M_right, flag
+    )
