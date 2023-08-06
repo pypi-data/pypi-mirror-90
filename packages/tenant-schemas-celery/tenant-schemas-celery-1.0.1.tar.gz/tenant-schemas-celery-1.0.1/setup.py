0cf0783264545fde6a56dfa0b885cac5a98c5af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:04 +0000

    Add package tenant-schemas-celery/1.0.1/tenant-schemas-celery-1.0.1.tar.gz

---
+++ packages/tenant-schemas-celery/tenant-schemas-celery-1.0.1.tar.gz/tenant-schemas-celery-1.0.1/setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('VERSION', 'rb') as v:
+    version = v.read().decode().strip()
+
+with open('README.md', 'rb') as ld:
+    long_description = ld.read().decode().strip()
+
+setup(
+    author='Maciej Gol',
+    author_email='1kroolik1@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+    ],
+    description='Celery integration for django-tenant-schemas and django-tenants',
+    install_requires=[
+        'celery',
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    name='tenant-schemas-celery',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/maciej-gol/tenant-schemas-celery',
+    version=version,
+)
---
+++ packages/tenant-schemas-celery/tenant-schemas-celery-1.0.1.tar.gz/tenant-schemas-celery-1.0.1/tenant_schemas_celery/test_tasks.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+
+from celery import shared_task, Task
+from django.db import connection
+from test_app.tenant.models import DummyModel
+
+from .test_app import app
+
+
+class DoesNotExist(Exception):
+    pass
+
+
+@app.task
+def update_task(model_id, name):
+    try:
+        dummy = DummyModel.objects.get(pk=model_id)
+
+    except DummyModel.DoesNotExist:
+        raise DoesNotExist()
+
+    dummy.name = name
+    dummy.save()
+
+
+@app.task(bind=True)
+def update_retry_task(self, model_id, name):
+    if update_retry_task.request.retries:
+        return update_task(model_id, name)
+
+    # Don't throw the Retry exception.
+    self.retry(countdown=0.1)
+
+
+@shared_task
+def get_schema_name():
+    return connection.schema_name
+
+
+class SchemaClassTask(Task):
+    @property
+    def connection_schema_name(self):
+        return connection.schema_name
+
+
+@shared_task(base=SchemaClassTask, bind=True)
+def get_schema_from_class_task(self):
+    '''
+        NOTICE: decorator tasks using a custom base like this are not supported
+    '''
+    return self.connection_schema_name
