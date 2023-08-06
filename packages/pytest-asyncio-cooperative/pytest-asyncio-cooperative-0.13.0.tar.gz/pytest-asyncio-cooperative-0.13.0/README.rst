2cef5b92e590cb1ef246e70f4127b630360c40bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:39 +0000

    Add package pytest-asyncio-cooperative/0.13.0/pytest-asyncio-cooperative-0.13.0.tar.gz

---
+++ packages/pytest-asyncio-cooperative/pytest-asyncio-cooperative-0.13.0.tar.gz/pytest-asyncio-cooperative-0.13.0/README.rst
@@ -0,0 +1,96 @@
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/psf/black
+
+Use asyncio (cooperative multitasking) to run your I/O bound test suite efficiently and quickly.
+
+.. code-block:: python
+   :class: ignore
+   
+   import asyncio
+
+   import pytest
+   
+   @pytest.mark.asyncio_cooperative
+   async def test_a():
+       await asyncio.sleep(2)
+   
+   
+   @pytest.mark.asyncio_cooperative
+   async def test_b():
+       await asyncio.sleep(2)
+
+
+.. code-block:: bash
+   :class: ignore
+
+   ========== 2 passed in 2.05 seconds ==========
+
+
+Quickstart
+----------
+.. code-block:: bash
+   :class: ignore
+
+   pip install pytest-asyncio-cooperative
+
+
+Compatibility
+-------------
+pytest-asyncio is NOT compatible with this plugin. Please uninstall pytest-asyncio or pass this flag to pytest `-p no:asyncio`
+
+Fixtures
+--------
+It's recommended that async tests use async fixtures.
+
+.. code-block:: bash
+   :class: ignore
+
+   import asyncio
+   import pytest
+
+
+   @pytest.fixture
+   async def my_fixture():
+       await asyncio.sleep(2)
+       yield "XXX"
+       await asyncio.sleep(2)
+
+
+   @pytest.mark.asyncio_cooperative
+   async def test_a(my_fixture):
+       await asyncio.sleep(2)
+       assert my_fixture == "XXX"
+
+
+Goals
+-----
+
+- Reduce the total run time of I/O bound test suites via cooperative multitasking
+
+- Reduce system resource usage via cooperative multitasking
+
+
+Pros
+----
+
+- An I/O bound test suite will run faster (ie. individual tests will take just as long. The total runtime of the entire test suite will be faster)
+
+- An I/O bound test suite will use less system resources (ie. only a single thread is used)
+
+Cons
+----
+
+- All tests MUST be coroutines (ie. have the `async` keyword)
+
+- Order of tests is not guaranteed (ie. some blocking operations might taken longer and affect the order of test results)
+
+- Tests MUST be isolated from each other (ie. NO shared resources, NO `mock.patch`)
+
+- There is NO parallelism, CPU bound tests will NOT get a performance benefit
+
+Known Issues
+------------
+
+**Synchronous tests fail to run when async tests fail**
+
+Async tests are run BEFORE synchronous tests. If an async test fails then synchronous tests will NOT run. Therefore the synchronous tests will not report to have passed or failed.
---
+++ packages/pytest-asyncio-cooperative/pytest-asyncio-cooperative-0.13.0.tar.gz/pytest-asyncio-cooperative-0.13.0/pytest_asyncio_cooperative/plugin.py
@@ -0,0 +1,274 @@
+import asyncio
+import functools
+import inspect
+import time
+
+import pytest
+
+from .assertion import activate_assert_rewrite
+from .fixtures import fill_fixtures
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--max-asyncio-tasks",
+        action="store",
+        default=100,
+        help="asyncio: maximum number of tasks to run concurrently (int)",
+    )
+    parser.addoption(
+        "--asyncio-task-timeout",
+        action="store",
+        default=120,
+        help="asyncio: number of seconds before a test will be cancelled (int)",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers",
+        "asyncio_cooperative: run an async test cooperatively with other async tests.",
+    )
+    config.addinivalue_line(
+        "markers", "flakey: if this test fails then run it one more time."
+    )
+
+
+@pytest.hookspec
+def pytest_runtest_makereport(item, call):
+    # Tests are run outside of the normal place, so we have to inject our timings
+
+    if call.when == "call":
+        if hasattr(item, "start") and hasattr(item, "stop"):
+            call.start = item.start
+            call.stop = item.stop
+
+    elif call.when == "setup":
+        if hasattr(item, "start_setup") and hasattr(item, "stop_setup"):
+            call.start = item.start_setup
+            call.stop = item.stop_setup
+
+    elif call.when == "teardown":
+        if hasattr(item, "start_teardown") and hasattr(item, "stop_teardown"):
+            call.start = item.start_teardown
+            call.stop = item.stop_teardown
+
+
+def not_coroutine_failure(function_name: str, *args, **kwargs):
+    raise Exception(
+        f"Function {function_name} is not a coroutine.\n"
+        f"Tests with the `@pytest.mark.asyncio_cooperative` mark MUST be coroutines.\n"
+        f"Please add the `async` keyword to the test function."
+    )
+
+
+async def test_wrapper(item):
+    # Do setup
+    item.start_setup = time.time()
+    fixture_values, teardowns = await fill_fixtures(item)
+    item.stop_setup = time.time()
+
+    # This is a class method test so prepend `self`
+    if item.instance:
+        fixture_values.insert(0, item.instance)
+
+    # Run test
+    item.start = time.time()
+    await item.function(*fixture_values)
+    item.stop = time.time()
+
+    # Do teardowns
+    item.start_teardown = time.time()
+    for teardown in teardowns:
+        if inspect.isgenerator(teardown):
+            try:
+                teardown.__next__()
+            except StopIteration:
+                pass
+        else:
+            try:
+                await teardown.__anext__()
+            except StopAsyncIteration:
+                pass
+    item.stop_teardown = time.time()
+
+
+# TODO: move to hypothesis module
+async def hypothesis_test_wrapper(item):
+    """
+    Hypothesis is synchronous, let's run inside an executor to keep asynchronicity
+    """
+
+    # Do setup
+    item.start_setup = time.time()
+    fixture_values, teardowns = await fill_fixtures(item)
+    item.stop_setup = time.time()
+
+    default_loop = asyncio.get_running_loop()
+    inner_test = item.function.hypothesis.inner_test
+
+    def async_to_sync(*args, **kwargs):
+        # FIXME: can we cache this loop across multiple runs?
+        loop = asyncio.new_event_loop()
+        task = inner_test(*args, **kwargs)
+        try:
+            loop.run_until_complete(task)
+        finally:
+            loop.close()
+
+    # Run test
+    item.function.hypothesis.inner_test = async_to_sync
+    wrapped_func_with_fixtures = functools.partial(item.function, *fixture_values)
+    await default_loop.run_in_executor(None, wrapped_func_with_fixtures)
+
+    # Do teardowns
+    item.start_teardown = time.time()
+    for teardown in teardowns:
+        try:
+            await teardown.__anext__()
+        except StopAsyncIteration:
+            pass
+    item.stop_teardown = time.time()
+
+
+class NotCoroutine(Exception):
+    pass
+
+
+def item_to_task(item):
+    if inspect.iscoroutinefunction(item.function):
+        return test_wrapper(item)
+    elif getattr(item.function, "is_hypothesis_test", False):
+        return hypothesis_test_wrapper(item)
+    else:
+        raise NotCoroutine
+
+
+def _run_test_loop(tasks, session, run_tests):
+    loop = asyncio.new_event_loop()
+    try:
+        task = run_tests(tasks, int(session.config.getoption("--max-asyncio-tasks")))
+        loop.run_until_complete(task)
+    finally:
+        loop.close()
+
+
+@pytest.hookspec
+def pytest_runtestloop(session):
+    if session.config.pluginmanager.is_registered("asyncio"):
+        raise Exception(
+            "pytest-asyncio-cooperative is NOT compatible with pytest-asyncio\n"
+            "Uninstall pytest-asyncio or pass this option to pytest: `-p no:asyncio`\n"
+        )
+
+    session.wrapped_fixtures = {}
+
+    flakes_to_retry = []
+
+    # Collect our coroutines
+    regular_items = []
+    item_by_coro = {}
+    tasks = []
+    for item in session.items:
+        markers = [m.name for m in item.own_markers]
+
+        if "skip" in markers:
+            continue
+
+        # Coerce into a task
+        if "asyncio_cooperative" in markers:
+            try:
+                task = item_to_task(item)
+            except NotCoroutine:
+                item.runtest = functools.partial(not_coroutine_failure, item.name)
+                item.ihook.pytest_runtest_protocol(item=item, nextitem=None)
+                continue
+
+            item._flakey = "flakey" in markers
+            item_by_coro[task] = item
+            tasks.append(task)
+        else:
+            regular_items.append(item)
+
+    async def run_tests(tasks, max_tasks):
+        sidelined_tasks = tasks[max_tasks:]
+        tasks = tasks[:max_tasks]
+
+        task_timeout = int(session.config.getoption("--asyncio-task-timeout"))
+
+        completed = []
+        while tasks:
+
+            # Mark when the task was started
+            for task in tasks:
+                if isinstance(task, asyncio.Task):
+                    item = item_by_coro[task._coro]
+                else:
+                    item = item_by_coro[task]
+                if not hasattr(item, "enqueue_time"):
+                    item.enqueue_time = time.time()
+
+            done, pending = await asyncio.wait(
+                tasks, return_when=asyncio.FIRST_COMPLETED, timeout=30
+            )
+
+            # Cancel tasks that have taken too long
+            tasks = []
+            for task in pending:
+                now = time.time()
+                item = item_by_coro[task._coro]
+                if task_timeout < now - item.enqueue_time:
+                    task.cancel()
+                tasks.append(task)
+
+            for result in done:
+                item = item_by_coro[result._coro]
+
+                # Flakey tests will be run again if they failed
+                # TODO: add retry count
+                if item._flakey:
+                    try:
+                        result.result()
+                    except:
+                        item._flakey = None
+                        new_task = item_to_task(item)
+                        flakes_to_retry.append(new_task)
+                        item_by_coro[new_task] = item
+                        continue
+
+                item.runtest = lambda: result.result()
+                item.ihook.pytest_runtest_protocol(item=item, nextitem=None)
+                # Hack: See rewrite comment below
+                # pytest_runttest_protocl will disable the rewrite assertion
+                # so we renable it here
+                activate_assert_rewrite(item)
+
+                completed.append(result)
+
+            if sidelined_tasks:
+                tasks.append(sidelined_tasks.pop(0))
+
+        return completed
+
+    # Do assert rewrite
+    # Hack: pytest's implementation sets up assert rewriting as a shared
+    # resource. This causes a race condition between async tests. Therefore we
+    # need to activate the assert rewriting here
+    if tasks:
+        item = item_by_coro[tasks[0]]
+        activate_assert_rewrite(item)
+
+    # Run the tests using cooperative multitasking
+    _run_test_loop(tasks, session, run_tests)
+
+    # Run failed flakey tests
+    if flakes_to_retry:
+        _run_test_loop(flakes_to_retry, session, run_tests)
+
+    # Hack: because we might be running synchronous tests later (ie.
+    # regular_items) we to set this to zero otherwise pytest bails out early
+    # NOTE: this doesn't work, the return code is not non-negative
+    # if session.testsfailed:
+    #     session.testsfailed = 0
+
+    session.items = regular_items
