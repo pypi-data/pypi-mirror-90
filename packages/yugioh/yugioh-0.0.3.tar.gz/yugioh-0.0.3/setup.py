435fcde0a51604a7e36d6768f3a7f5c8c4767b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package yugioh/0.0.3/yugioh-0.0.3.tar.gz

---
+++ packages/yugioh/yugioh-0.0.3.tar.gz/yugioh-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        
+setuptools.setup(name='yugioh',
+                 py_modules=["yugioh_api"],
+                 entry_points={"console_scripts": ["yugioh=clt:main"]},
+                 version='0.0.3',
+                 description='Yu-Gi-Oh API Wrapper & CLT',
+                 long_description=readme(),
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/pclt-dev/yugioh',
+                 author='diogenesjunior',
+                 author_email='diogenesjunior@protonmail.com',
+                 packages=['yugioh'],
+                 keywords='Yu-Gi-Oh API')
