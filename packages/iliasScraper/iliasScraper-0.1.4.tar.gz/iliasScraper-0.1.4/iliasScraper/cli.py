fc10756fea756db2a8d85ab55b42f8d55120275f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:33 +0000

    Add package iliasScraper/0.1.4/iliasScraper-0.1.4.tar.gz

---
+++ packages/iliasScraper/iliasScraper-0.1.4.tar.gz/iliasScraper-0.1.4/iliasScraper/cli.py
@@ -0,0 +1,104 @@
+
+import os
+import sys
+import glob
+import click
+from pathlib import Path
+from colorama import Fore, Back, Style
+
+from .scraper import Scraper
+from .auth_manager import remove_pwd
+
+CONFIG_FILE = ".config"
+HOME = str(Path.home())
+PATH = os.path.join(HOME, ".iliasScraper")
+
+version_path = os.path.join(os.path.dirname(__file__),"VERSION")
+
+with open(version_path) as f:
+    line = f.readline()
+    __version__ = line
+
+@click.group()
+@click.version_option(__version__)
+def main():
+    """CLI for the ilias-scraper"""
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+
+@main.command()
+@click.option('--url', help='Ilias course url', required=True)
+@click.option('--username', help='Your Ilias username', required=True)
+@click.option('--course-name', help='The name of the course, will be used as the name of this scraper', required=True)
+@click.option('--target-dir', help="The ABSOLUTE path for this course folder", required=False)
+# @click.option('--ignore', help="Files you want to ignore, seperated by ,")
+def create(url, username, course_name, target_dir):
+    """
+    Create a new scraper with a url and name
+    """
+    # ignore = ignore.split(",")
+    scraper_source = f"""
+from iliasScraper import scraper
+user = '{username}'
+sc = scraper.Scraper(url='{url}', name='{course_name}', target_dir='{target_dir}')
+sc.setup(user)
+sc.run()"""
+    if course_name == None:
+        print("please use ")
+        return 0
+    course_name = course_name.replace(" ", "_")
+    with open(f"{PATH}/{course_name}.py", "w") as scraper_file:
+        scraper_file.write(scraper_source)
+    print(Fore.GREEN + f"Scraper {course_name} has been created!")
+
+
+@main.command()
+@click.option('--name', help='Name of the scraper', required=True)
+@click.option('--fallback-extension', required=False)
+def run(name, fallback_extension="txt"):
+    """
+    Run a previously created scraper, name can be specified with
+    and without the .py ending
+    """
+    scraper_file = f"{PATH}/{name}"
+    if not ".py" in scraper_file:
+        scraper_file += ".py"
+    if not os.path.exists(scraper_file):
+        print(Fore.RED + "The specified name does not match any configured scrapers.")
+        print("Use <<iliasScraper list>> to list all current scrapers.")
+        return 0
+    os.system(f"python3 {scraper_file}")
+
+@main.command()
+def list(**kwargs):
+    """
+    List all existing scraper
+    """
+    files = [f for f in glob.glob(PATH+"/*.py", recursive=True)]
+    if not files:
+        print(Fore.RED+"There are no scrapers configured."+Style.RESET_ALL)
+        return 0
+    print(Fore.BLUE + "Currently configured scrapers:"+Style.RESET_ALL)
+    for f in files:
+        filename_printable = f.split("/")[-1:][0]
+        print(f"- {filename_printable}")
+
+
+@main.command()
+@click.option('--username', required=True)
+def remove_password(username):
+    """
+    Remove the stored password
+    """
+    remove_pwd(username)
+    print(Fore.RED + f"Password for {username} has been removed!")
+
+if __name__ == '__main__':
+    args = sys.argv
+    if "--help" in args or len(args) == 1:
+        print(Fore.BLUE + "ILIAS SCRAPER")
+        print(Style.RESET)
+        print(Style.DIM + "scraper for ilias platform of of the uni constance")
+        print("create a scraper via <create> <url> <title> or run via <run> <name>")
+    main()
---
+++ packages/iliasScraper/iliasScraper-0.1.4.tar.gz/iliasScraper-0.1.4/iliasScraper/file_parser.py
@@ -0,0 +1,130 @@
+
+import re
+from tqdm import tqdm
+from colorama import Fore, Back, Style
+
+class FileParser:
+    """
+    docstring for FileParser.
+    """
+
+    def __init__(self, request_handler):
+        self.parser = 'html.parser'
+        self.ignore = ["weblink", "test", "forum", "übung", "inhaltsseite"]
+        self.file_id = "datei"
+        self.folder_id = "ordner"
+        self.current_path = "/"
+        self.dir_dict = {}
+        self.done_folders = []
+        self.folder_todo = []
+        self.todo_folders_index = 0
+        self.layer = 0
+        self.request_handler = request_handler
+        self.all_urls = []
+        self.base_soup = None
+
+
+    def filter_ignored(self, img_list):
+        def filter_(title):
+            return any(map(lambda el: el in title.lower(), self.ignore))
+        return [tag for tag in img_list if not filter_(tag["title"])]
+
+    def _extract_href(self, img):
+        return img.parent["href"]
+
+    def _pathify_folder_name(self, folder_name):
+        folder_name = "_".join(folder_name.split(" "))
+        folder_name = "_".join(folder_name.split("|"))
+        folder_name = "_".join(folder_name.split("'"))
+        folder_name = re.sub("_+", "_", folder_name)
+        return folder_name
+
+    def _extract_file_name(self, img):
+        return img.parent.parent.parent.find_all("a")[1].text
+
+    def _extract_symbol_name(self, a_tag):
+        imgs = a_tag.find_all("img")
+        if imgs:            
+            return str(imgs[0]["title"].lower())
+        return ""
+
+    def update_dir_dict(self, img_list, path):
+        """
+        """
+        dir_dict = {}
+        for img in img_list:
+            img_title = img["title"].lower()
+            if self.file_id in img_title:
+                if not path in dir_dict:
+                    dir_dict[path] = []
+                dir_dict[path].append({
+                        "url": self._extract_href(img),
+                        "file_name": self._extract_file_name(img)
+                        })
+        return dir_dict
+
+    def _is_sitzung(self, tag):
+        if isinstance(tag, str):
+            return False
+        img = tag.parent.parent.parent.parent.parent.find_all("img", class_="ilListItemIcon")
+        if img:
+            img = img[0]
+            title = img["title"].lower()
+            if "sitzung" in title or "session" in title:
+                return True
+        return False
+
+    def get_all_urls(self, url):
+        soup = self.request_handler.get_soup(url)
+        if self.layer == 0:
+            self.base_soup = soup
+        links = soup.find_all('a', class_="il_ContainerItemTitle")
+        if not links or url in self.all_urls:
+            return 1
+        else:
+            for link in links:
+                if not link.has_attr("href"):
+                    continue
+                href = link['href']
+                if not "https" in href:
+                    href = f"https://ilias.uni-konstanz.de/ilias/{href}"
+
+                # res = self._is_sitzung(link)
+                # sitz = res[0]
+                # title = res[1]
+                # print(f"{sitz=}")
+                # print(f"{title=}")
+                added = False
+                if "fold" in href:
+                    self.all_urls.append(url)
+                    added = True
+                if self._is_sitzung(link):
+                    self.all_urls.append(href)
+                    added = True
+                if added:
+                    print(Style.DIM + url + Style.RESET_ALL)
+                    self.get_all_urls(href)
+        return list(set(self.all_urls))
+
+    def extract_path_from_breadcrumb(self, breadcrumb_string):
+        total_path_list = breadcrumb_string.split("\n")
+        split_index = list(map(lambda item: "Lehrveranstaltung" in item, total_path_list)).index(True)
+        path_list = total_path_list[split_index+1:][:-1]
+        return path_list
+
+    def parse(self, soup):
+
+        breadcrumb_string = soup.find_all("ol", class_="breadcrumb")
+        if not breadcrumb_string:
+            return {}
+        breadcrumb_string = breadcrumb_string[0].text
+        path = "/".join(list(map(lambda x: self._pathify_folder_name(x),
+                        self.extract_path_from_breadcrumb(breadcrumb_string))))
+
+
+        all_img_tags = soup.find_all("img", class_="ilListItemIcon")
+        f_img_tags = self.filter_ignored(all_img_tags)
+
+        dir_dict = self.update_dir_dict(f_img_tags, path)
+
+        return dir_dict
