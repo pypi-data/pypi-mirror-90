6b5f43bda8a5fa66d402c8b67261d0925de9bdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:56 +0000

    Add package mglg/0.2.22/mglg-0.2.22-cp36-cp36m-win_amd64.whl

---
+++ packages/mglg/mglg-0.2.22-cp36-cp36m-win_amd64.whl/mglg/graphics/__init__.py
@@ -0,0 +1,7 @@
+from .drawable import DrawableGroup
+from .framebuffer import Framebuffer
+from .image import Image
+from .particles import Particles
+from .shapes import Shape, Circle, Rect, Cross, Arrow, Polygon, RoundedRect
+from .text import Text, DynamicText
+from .win import Win
