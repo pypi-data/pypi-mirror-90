d0aed45d3911d5f4a8efe965cc5418642dae1d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:10 +0000

    Add package django-ajax-datatable/4.1.5/django_ajax_datatable-4.1.5-py2.py3-none-any.whl

---
+++ packages/django-ajax-datatable/django_ajax_datatable-4.1.5-py2.py3-none-any.whl/ajax_datatable/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+__version__ = '4.1.5'
+
+from .columns import (
+    Column,
+    ForeignColumn,
+    ColumnLink,
+    PlaceholderColumnLink,
+    Order,
+)
+
+from .exceptions import (
+    ColumnOrderError,
+)
+
+from .views import (
+    AjaxDatatableView
+)
