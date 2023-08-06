c743e1176212c03a5ddaf3667a2662dafafcc211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:11 +0000

    Add package actionpi/1.1.13/actionpi-1.1.13-py3-none-any.whl

---
+++ packages/actionpi/actionpi-1.1.13-py3-none-any.whl/actionpi/app.py
@@ -0,0 +1,2 @@
+name='ActionPi'
+version='1.1.13'
---
+++ packages/actionpi/actionpi-1.1.13-py3-none-any.whl/actionpi/static/precache-manifest.611bc6362a9a5ad461bd137c0747742e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32d0fab780d187a17f532348c713fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c2d1f2c6c37bce8601",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "6b03c0f226757de3c880",
+    "url": "/static/js/2.17949a82.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.17949a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c2d1f2c6c37bce8601",
+    "url": "/static/js/main.cb1a5f95.chunk.js"
+  },
+  {
+    "revision": "98c1327435369df6cc45",
+    "url": "/static/js/runtime-main.c63fbe4a.js"
+  },
+  {
+    "revision": "cabc4b0d6882524fc0e7872c7fc50e0d",
+    "url": "/static/media/logo.cabc4b0d.png"
+  }
+]);
