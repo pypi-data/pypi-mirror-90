cd212ddf2fdfb06673c8b0e8def1b2d20ea5b2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package acquisition-sanitizer/0.1.0/acquisition_sanitizer-0.1.0-py3-none-any.whl

---
+++ packages/acquisition-sanitizer/acquisition_sanitizer-0.1.0-py3-none-any.whl/acquisition_sanitizer/sc_decision.py
@@ -0,0 +1,52 @@
+from html_sanitizer import Sanitizer
+from html_sanitizer.sanitizer import (
+    bold_span_to_strong,
+    italic_span_to_em,
+    sanitize_href,
+    tag_replacer,
+    target_blank_noopener,
+)
+
+case_sanitizer = Sanitizer(
+    {
+        "tags": {
+            "a",
+            "h1",
+            "h2",
+            "h3",
+            "strong",
+            "center",
+            "em",
+            "p",
+            "ul",
+            "ol",
+            "li",
+            "br",
+            "blockquote",
+            "sub",
+            "sup",
+            "hr",
+        },
+        "attributes": {
+            "a": ("href", "name", "target", "title", "id", "rel"),
+            "sup": ("id"),
+        },
+        "empty": {"hr", "a", "br"},
+        "separate": {"a", "p", "li"},
+        "whitespace": {"br"},
+        "keep_typographic_whitespace": False,
+        "add_nofollow": False,
+        "autolink": False,
+        "sanitize_href": sanitize_href,
+        "element_preprocessors": [
+            bold_span_to_strong,
+            italic_span_to_em,
+            tag_replacer("b", "strong"),
+            tag_replacer("i", "em"),
+            tag_replacer("form", "p"),
+            target_blank_noopener,
+        ],
+        "element_postprocessors": [],
+        "is_mergeable": lambda e1, e2: True,
+    }
+)
