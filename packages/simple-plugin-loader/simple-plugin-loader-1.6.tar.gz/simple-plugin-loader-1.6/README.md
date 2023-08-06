623ca71a1a95e3d4804ec24a36e4af0a72eee784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:16 +0000

    Add package simple-plugin-loader/1.6/simple-plugin-loader-1.6.tar.gz

---
+++ packages/simple-plugin-loader/simple-plugin-loader-1.6.tar.gz/simple-plugin-loader-1.6/README.md
@@ -0,0 +1,68 @@
+# Simple Python Plugin Loader
+
+![PyPI package](https://github.com/mammo0/py-simple-plugin-loader/workflows/PyPI%20package/badge.svg)
+[![PyPI version](https://badge.fury.io/py/simple-plugin-loader.svg)](https://badge.fury.io/py/simple-plugin-loader)
+
+This module provides a simple way to dynamically load other Python modules as Plugins to your current project.
+
+
+### Install
+
+You can install this python module via **pip**:
+```shell
+pip install simple-plugin-loader
+```
+
+Otherwise the module can be downloaded from PyPI: https://pypi.org/project/simple-plugin-loader/
+
+
+### Usage
+
+1. Import the module:
+    ```python
+    from simple_plugin_loader import Loader
+    ```
+2. Load your plugins/modules:
+    ```python
+    # initialize the loader
+    loader = Loader()
+
+    # load your plugins
+    plugins = loader.load_plugins(<plugin_path>, <plugin_base_class>, <specific_plugins>, <recursive>, <verbose>)
+    ```
+3. **(Optional)** The already loaded plugins/modules can be accessed via the `plugins` property of the loader instance:
+   ```python
+   plugins = loader.plugins
+   ```
+
+### `load_plugins(...)` Method
+This method does the actual plugin loading.
+
+It loads only Python modules that can be executed in the current environment. If a module e.g. contains syntax errors or depends on other not installed Python modules, it will be skipped. So the main program does not fail completely.
+
+##### Arguments
+
+- `<plugin_path>`: _str_</br>
+  This string represents the path (relative or absolute) to the directory from where the plugins/modules should be loaded.
+- `<plugin_base_class>`: _class_ (Default: `SamplePlugin`)</br>
+  The Loader does not load all found modules that are in the above directory. It only loads classes that are **sub-classes** of the here specified class.</br>
+  The default value of this argument is the `SamplePlugin` class. You can use this class as the base class for your plugins:
+  ```python
+  from simple_plugin_loader.sample_plugin import SamplePlugin
+
+  class YourPlugin(SamplePlugin):
+      pass
+  ```
+- `<specific_plugins>`: _List[str]_ (Default: `[]`)</br>
+  This list can contain **case sensitive** class names, that should be loaded. Then no other plugins will be loaded. The argument `<plugin_base_class>` will also be ignored, so any class can be loaded.
+- `<recursive>`: _bool_ (Default: `False`)</br>
+  Set this flag to `True` if you wish to load plugins/modules recursively to the above directory.
+- `<verbose>`: _bool_ (Default: `False`)</br>
+  This flag controls the debug output of this method. If enabled, information about (not) loaded plugins/modules are printed.
+
+##### Return value
+
+The method returns a dictionary that has the following structure:
+
+- **Key**: The name of the plugin/module. This name is the module name of the module that contains the plugin class.
+- **Value**: The plugin class. Not an instance!
---
+++ packages/simple-plugin-loader/simple-plugin-loader-1.6.tar.gz/simple-plugin-loader-1.6/simple_plugin_loader/loader.py
@@ -0,0 +1,131 @@
+"""
+This module can load Python modules by path.
+"""
+
+from importlib import import_module
+import inspect
+import os
+import pkgutil
+import sys
+from typing import List
+
+from simple_plugin_loader.sample_plugin import SamplePlugin
+
+
+class _Loader():
+    def __init__(self):
+        self.__available_plugins = {}
+
+    @property
+    def plugins(self):
+        """
+        Get all already loaded plugins.
+        """
+        return self.__available_plugins
+
+    def load_plugins(self, path: str,
+                     plugin_base_class: type=SamplePlugin,
+                     specific_plugins: List[str]=[],
+                     recursive: bool=False,
+                     verbose: bool=False) -> dict:
+        """
+        Load all classes in a directory specified by 'path' that match the 'plugin_base_class' class.
+        Alternatively if the 'specific_plugins' list contains class names, only those will be loaded.
+        They don't need to be subclasses of e.g. 'SamplePlugin'.
+
+        All other classes or methods are ignored.
+        """
+        # normalize the path
+        path = os.path.abspath(os.path.normpath(path))
+
+        # add the parent path to the system PATH if it doesn't already exists
+        # this is needed for the import later
+        sys_path_modified = False
+        if os.path.dirname(path) not in sys.path:
+            # insert the parent path at index zero, because the loader should look at this location first
+            sys.path.insert(0, os.path.dirname(path))
+            sys_path_modified = True
+
+        # do the actual import
+        plugins = self.__load(path,
+                              os.path.basename(path),  # the module main package is the last directory of the path
+                              plugin_base_class,
+                              specific_plugins,
+                              recursive,
+                              verbose)
+
+        # reset the modified path again
+        if (sys_path_modified and
+                os.path.dirname(path) in sys.path):
+            sys.path.remove(os.path.dirname(path))
+
+        self.__available_plugins.update(plugins)
+        return plugins
+
+    def __load(self, path: str,
+               package_name: str,
+               plugin_base_class: type=SamplePlugin,
+               specific_plugins: List[str]=[],
+               recursive: bool=False,
+               verbose: bool=False) -> dict:
+        plugins = {}
+        # iterate over the modules that are within the path
+        for (_, name, ispkg) in pkgutil.iter_modules([path]):
+            if ispkg:
+                if recursive:
+                    plugins.update(self.__load(os.path.join(path, name),
+                                               ".".join([package_name, name]),
+                                               plugin_base_class,
+                                               specific_plugins,
+                                               recursive,
+                                               verbose))
+                    continue
+                else:
+                    # do not try to import it, since it's not a module
+                    continue
+
+            # import the module
+            try:
+                imported_module = import_module(".".join([package_name, name]))
+            except ModuleNotFoundError as e:
+                if verbose:
+                    print("\n".join(["Can't import module '%s'! Skipping it." % ".".join([package_name, name]),
+                                     "Error: %s" % str(e)]),
+                          file=sys.stderr)
+                continue
+
+            plugin_found = False
+            # try to find a subclass of the plugin class
+            for i in dir(imported_module):
+                attribute = getattr(imported_module, i)
+
+                # first check if it's a class
+                if (inspect.isclass(attribute) and
+                        # check if only specific plugins should be loaded
+                        ((specific_plugins and
+                            # they must match the name case sensitive
+                            attribute.__name__ in specific_plugins) or
+                         # otherwise check for plugin subclass
+                         (not specific_plugins and
+                            issubclass(attribute, plugin_base_class) and
+                            # but do not match the plugin class itself
+                            attribute != plugin_base_class))):
+                    # if the plugin is derived from 'SamplePlugin' class,
+                    if issubclass(attribute, SamplePlugin):
+                        # use the 'plugin_name' property as name
+                        pn = attribute.plugin_name
+                    else:
+                        # otherwise simply use the class name
+                        pn = attribute.__name__
+
+                    plugins[pn.casefold()] = attribute
+                    plugin_found = True
+
+                    if verbose:
+                        print("Imported plugin %s as %s" % (pn, pn.casefold()))
+
+            # remove imported module again if no plugin class is found
+            if not plugin_found:
+                del imported_module
+
+        return plugins
