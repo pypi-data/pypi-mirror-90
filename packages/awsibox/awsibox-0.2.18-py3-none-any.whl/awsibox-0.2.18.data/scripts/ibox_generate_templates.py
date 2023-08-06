410c6f10074ae4a4c69f4d239e416ad515041446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package awsibox/0.2.18/awsibox-0.2.18-py3-none-any.whl

---
+++ packages/awsibox/awsibox-0.2.18-py3-none-any.whl/awsibox-0.2.18.data/scripts/ibox_generate_templates.py
@@ -0,0 +1,98 @@
+#!python
+import sys
+import os
+import json
+import concurrent.futures
+from traceback import print_exc
+
+from troposphere import Template
+from awsibox import args, cfg, RP, discover, generate
+
+args = args.get_args()
+
+
+def output_template(template, brand=None, role=None):
+    if args.Output == 'json':
+        output = template.to_json()
+        extension = '.json'
+    elif args.Output == 'cjson':
+        output = template.to_json(indent=None)
+        extension = '.json'
+    elif args.Output == 'yaml':
+        output = template.to_yaml(long_form=True)
+        extension = '.yaml'
+
+    if args.action == 'view':
+        print(output)
+    elif args.action == 'write':
+        file_path = os.path.join(os.getcwd(), 'templates', brand)
+        try:
+            os.makedirs(file_path)
+        except Exception:
+            pass
+        file_name = os.path.join(file_path, role + extension)
+        with open(file_name, 'w') as f:
+            f.write(output + '\n')
+
+
+def get_template():
+    # save base cfg
+    cfg_base = cfg.__dict__.copy()
+
+    cfg.template = Template()
+    RP.build_RP()
+    template = generate.generate()
+
+    # restore base cfg
+    cfg.__dict__.clear()
+    cfg.__dict__.update(cfg_base)
+
+    return template
+
+
+def do_write(role): 
+    cfg.envrole = role
+    template = get_template()
+    output_template(template, brand, role)
+    print('Brand: %s - EnvRole: %s' % (brand, role))
+
+
+def concurrent_exec(roles, kwargs):
+    with concurrent.futures.ProcessPoolExecutor(**kwargs) as executor:
+        data = {}
+        future_to_role = {}
+        for n in roles:
+            ex_sub = executor.submit(do_write, n)
+            future_to_role[ex_sub] = n
+
+        for future in concurrent.futures.as_completed(future_to_role):
+            role = future_to_role[future]
+            try:
+                data[role] = future.result()
+            except Exception as e:
+                print(f'{role} generated an exception: {e}')
+                print_exc()
+                break
+        for future in future_to_role:
+            future.cancel()
+
+
+if args.action == 'view':
+    cfg.envrole = args.EnvRole
+    cfg.brand = args.Brand
+
+    template = get_template()
+
+    output_template(template)
+
+elif args.action == 'write':
+    discover_map = discover.discover(
+        args.Brands, args.EnvRoles, args.StackTypes)
+
+    for brand, roles in discover_map.items():
+        cfg.brand = brand
+        if args.jobs:
+            kwargs = {'max_workers': args.jobs}
+        else:
+            kwargs = {}
+        concurrent_exec(set(roles), kwargs)
---
+++ packages/awsibox/awsibox-0.2.18-py3-none-any.whl/awsibox/discover.py
@@ -0,0 +1,106 @@
+import sys
+import os
+import mmap
+
+from . import cfg
+
+
+def get_brands():
+    brand_int = os.listdir(cfg.PATH_INT)
+    brand_ext = os.listdir(cfg.PATH_EXT)
+
+    brands = set(brand_int + brand_ext)
+
+    brands.remove('BASE')
+
+    return brands
+
+
+def get_files(brand):
+    files = []
+
+    path_int = os.path.join(cfg.PATH_INT, brand)
+    path_ext = os.path.join(cfg.PATH_EXT, brand)
+
+    for n in [path_int, path_ext]:
+        for root, directories, filenames in os.walk(n, topdown=True):
+            try:
+                directories.remove('UNUSED')
+            except Exception:
+                pass
+            for filename in filenames:
+                if not filename.startswith('.'):
+                    files.append(os.path.join(root, filename))
+
+    return files
+
+
+def build_discover_map(brand, files, stacktypes):
+    roles = []
+
+    for n in files:
+        try:
+            with open(n, 'r', encoding='utf-8') as f:
+                s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+                if any(
+                        s.find((f'{pattern}{t}').encode('utf-8')) != -1
+                        for t in stacktypes):
+                    base_name = os.path.basename(n)
+                    role = os.path.splitext(base_name)[0]
+                    roles.append(role)
+        except IOError:
+            pass
+
+    if brand == 'BASE':
+        for n in ext_brands:
+            add_to_map(n, roles)
+    else:
+        add_to_map(brand, roles)
+
+
+def add_to_map(brand, roles):
+    global discover_map
+
+    try:
+        discover_map[brand].extend(roles)
+    except Exception:
+        discover_map[brand] = roles
+
+
+def discover(brands, envroles, stacktypes):
+    global discover_map
+    global pattern
+    global ext_brands
+
+    pattern = ' StackType: '
+
+    discover_map = {}
+
+    if brands:
+        ext_brands = list(brands)
+    else:
+        ext_brands = get_brands()
+        brands = list(ext_brands)
+
+    brands.append('BASE')
+
+    for brand in brands:
+        if envroles:
+            files = []
+            for role in envroles:
+                files.append(
+                    os.path.join(cfg.PATH_EXT, brand, role + '.yml'))
+                if brand == 'BASE':
+                    files.append(
+                        os.path.join(cfg.PATH_INT, brand, role + '.yml'))
+            build_discover_map(brand, files, stacktypes=[''])
+        if stacktypes:
+            files = get_files(brand)
+            build_discover_map(brand, files, stacktypes)
+        if not stacktypes and not envroles:
+            # match all
+            files = get_files(brand)
+            build_discover_map(brand, files, stacktypes=[''])
+
+    return discover_map
