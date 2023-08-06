0c39d6d0812679770220a93dc2e9e6702e047ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:25 +0000

    Add package neuropod/0.3.0rc1/neuropod-0.3.0rc1-cp35-cp35m-manylinux2014_x86_64.whl

---
+++ packages/neuropod/neuropod-0.3.0rc1-cp35-cp35m-manylinux2014_x86_64.whl/neuropod-0.3.0rc1.data/purelib/neuropod/tests/gpu_test_basic_environment_support.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2020 UATC, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+
+class TestGPUEnvSupport(unittest.TestCase):
+    """
+    Tests that the environment running the tests has GPUs available
+    and that various frameworks used in the tests support using the
+    GPUs
+    """
+
+    def test_torch_gpu(self):
+        import torch
+
+        self.assertTrue(torch.cuda.is_available())
+
+    def test_tf_gpu(self):
+        import tensorflow as tf
+
+        self.assertTrue(tf.test.is_built_with_cuda() and tf.test.is_gpu_available())
+
+
+if __name__ == "__main__":
+    unittest.main()
