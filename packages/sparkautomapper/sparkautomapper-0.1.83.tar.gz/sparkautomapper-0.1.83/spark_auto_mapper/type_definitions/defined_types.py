10311aa8ffb2196bb6ae3602a580e741f27534dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:18 +0000

    Add package sparkautomapper/0.1.83/sparkautomapper-0.1.83.tar.gz

---
+++ packages/sparkautomapper/sparkautomapper-0.1.83.tar.gz/sparkautomapper-0.1.83/spark_auto_mapper/type_definitions/defined_types.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
+from spark_auto_mapper.type_definitions.native_types import AutoMapperNativeSimpleType, AutoMapperNativeListType, \
+    AutoMapperNativeTextType, AutoMapperNativeNumberType, AutoMapperNativeBooleanType, AutoMapperNativeDateType, \
+    AutoMapperNativeAmountType
+from spark_auto_mapper.type_definitions.wrapper_types import AutoMapperColumnOrColumnLikeType
+
+AutoMapperTextInputType = Union[AutoMapperNativeTextType,
+                                AutoMapperColumnOrColumnLikeType]
+
+AutoMapperNumberInputType = Union[AutoMapperNativeNumberType,
+                                  AutoMapperColumnOrColumnLikeType]
+
+AutoMapperBooleanInputType = Union[AutoMapperNativeBooleanType,
+                                   AutoMapperColumnOrColumnLikeType]
+
+AutoMapperDateInputType = Union[AutoMapperNativeDateType,
+                                AutoMapperColumnOrColumnLikeType]
+
+AutoMapperAmountInputType = Union[AutoMapperNativeAmountType,
+                                  AutoMapperColumnOrColumnLikeType]
+
+AutoMapperAnyDataType = Union[AutoMapperNativeSimpleType,
+                              AutoMapperNativeListType, AutoMapperDataTypeBase]
