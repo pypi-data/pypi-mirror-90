de49a7db9ad5ec3ee05b2747d4006074e5d0f47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:13 +0000

    Add package nester-cocvu9x/1.0.0/nester_cocvu9x-1.0.0.tar.gz

---
+++ packages/nester-cocvu9x/nester_cocvu9x-1.0.0.tar.gz/nester_cocvu9x-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_cocvu9x',
+    version     = '1.0.0',
+    py_modules  = ['nester'],
+    author      = 'cocvu9x',
+    author_email= 'cocvu99@gmail.com',
+    url         = 'https://github.com/cocvu99',
+    description = 'Phien ban don gian nhat nested lists cua vu nguyen',
+)
