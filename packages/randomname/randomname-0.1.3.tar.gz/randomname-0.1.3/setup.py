33c124eeea20b76df0e0a8b96f9ef23acbc3b4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:50 +0000

    Add package randomname/0.1.3/randomname-0.1.3.tar.gz

---
+++ packages/randomname/randomname-0.1.3.tar.gz/randomname-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+USERNAME = 'beasteers'
+NAME = 'randomname'
+
+setuptools.setup(
+    name=NAME,
+    version='0.1.3',
+    description='Generate random adj-noun names like docker and github.',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type='text/markdown',
+    author='Bea Steers',
+    author_email='bea.steers@gmail.com',
+    url='https://github.com/{}/{}'.format(USERNAME, NAME),
+    packages=setuptools.find_packages(),
+    package_data={NAME: ['wordlists/**/*.txt']},
+    entry_points={'console_scripts': ['{name}={name}:main'.format(name=NAME)]},
+    install_requires=['fire'],
+    license='MIT License',
+    keywords='random name generator docker container github repo '
+             'word list noun adjective verb')
