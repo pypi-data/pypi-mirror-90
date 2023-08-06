80ca67d58766138e2e9f1471255c020ad3c1721a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:46 +0000

    Add package ro-py/1.0.0/ro_py-1.0.0-py3-none-any.whl

---
+++ packages/ro-py/ro_py-1.0.0-py3-none-any.whl/ro_py/utilities/errors.py
@@ -0,0 +1,35 @@
+"""
+
+ro.py > errors.py
+
+This file houses custom exceptions unique to this module.
+
+"""
+
+
+class NotLimitedError(Exception):
+    """Called when code attempts to read limited-only information."""
+    pass
+
+
+class InvalidIconSizeError(Exception):
+    """Called when code attempts to pass in an improper size to a thumbnail function."""
+    pass
+
+
+class InvalidShotTypeError(Exception):
+    """Called when code attempts to pass in an improper avatar image type to a thumbnail function."""
+    pass
+
+
+class ApiError(Exception):
+    """Called in requests when an API request fails."""
+    pass
+
+
+class ChatError(Exception):
+    """Called in chat when a chat action fails."""
+
+
+class InvalidPageError(Exception):
+    """Called when an invalid page is requested."""
