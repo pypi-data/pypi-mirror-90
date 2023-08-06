2ef91703375252a2daee6dca96966fe8d10bad7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:45 +0000

    Add package batch22/0.1.2021.1.2.3/batch22-0.1.2021.1.2.3-py3-none-any.whl

---
+++ packages/batch22/batch22-0.1.2021.1.2.3-py3-none-any.whl/batch22/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+"""
+batch22 (b22)
+
+
+"""
+from batch22._version import __version__
+
+from batch22 import eeg
+# from batch22 import temp
+
+if __name__ == "__main__":
+    pass
