17ad6425fb58282e1efc321b8b827b1effa93e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:16 +0000

    Add package 32blit/0.4.0/32blit-0.4.0.tar.gz

---
+++ packages/32blit/32blit-0.4.0.tar.gz/32blit-0.4.0/ttblit/core/assetbuilder.py
@@ -0,0 +1,130 @@
+import logging
+import pathlib
+import re
+
+from .outputformat import (CHeader, OutputFormat, output_formats,
+                           parse_output_format)
+from .tool import Tool
+
+
+class AssetBuilder(Tool):
+    formats = output_formats
+    no_output_file_default_format = CHeader
+
+    options = {
+        'input_file': pathlib.Path,
+        'input_type': str,
+        'output_file': pathlib.Path,
+        'output_format': parse_output_format,
+        'symbol_name': str,
+        'force': bool,
+        'prefix': str,
+        'working_path': pathlib.Path
+    }
+
+    def __init__(self, parser=None):
+        Tool.__init__(self, parser)
+
+        if self.parser is not None:
+            self.parser.add_argument('--input_file', type=pathlib.Path, required=True, help='Input file')
+            if(len(self.types) > 1):
+                self.parser.add_argument('--input_type', type=str, default=None, choices=self.types, help='Input file type')
+            self.parser.add_argument('--output_file', type=pathlib.Path, default=None)
+            self.parser.add_argument('--output_format', type=str, default=None, choices=self.formats.keys(), help='Output file format')
+            self.parser.add_argument('--symbol_name', type=str, default=None, help='Output symbol name')
+            self.parser.add_argument('--force', action='store_true', help='Force file overwrite')
+
+    def prepare(self, opts):
+        """Imports a dictionary of options to class variables.
+
+        Requires options to already be in their correct types.
+
+        """
+        Tool.prepare(self, opts)
+
+        if self.symbol_name is None:
+            if self.working_path is None:
+                name = '_'.join(self.input_file.parts)
+            else:
+                name = '_'.join(self.input_file.relative_to(self.working_path).parts)
+            name = name.replace('.', '_')
+            name = re.sub('[^0-9A-Za-z_]', '_', name)
+            self.symbol_name = name.lower()
+
+        if type(self.prefix) is str:
+            self.symbol_name = self.prefix + self.symbol_name
+
+        if self.output_format is None:
+            self._guess_format()
+        elif type(self.output_format) is str:
+            self.output_format = parse_output_format(self.output_format)
+        elif not issubclass(self.output_format, OutputFormat):
+            raise ValueError(f'Invalid format {self.output_format}, choices {self.formats.keys()}')
+
+        if self.input_type is None:
+            self._guess_type()
+        elif self.input_type not in self.types:
+            raise ValueError(f'Invalid type {self.input_type}, choices {self.types}')
+
+    def run(self, args):
+        self.prepare_options(vars(args))
+
+        output_data = self.build()
+
+        self.output(output_data, self.output_file, self.output_format, self.force)
+
+    def prepare_options(self, opts):
+        """Imports a dictionary of options to class variables.
+
+        Used for external callers which don't invoke `run` on this class.
+
+        Converts all options into their correct types via the specified validators.
+
+        """
+        if self.options is not None and opts is not None:
+            for option_name, option_value in opts.items():
+                if option_name in self.options:
+                    option_type = self.options[option_name]
+                    default_value = None
+                    if type(option_type) is tuple:
+                        option_type, default_value = option_type
+                    if option_value is not None:
+                        opts[option_name] = option_type(option_value)
+                    else:
+                        opts[option_name] = default_value
+                else:
+                    logging.info(f'Ignoring unsupported {self.command} option {option_name}')
+
+        self.prepare(opts)
+
+    def build(self):
+        input_data = open(self.input_file, 'rb').read()
+
+        output_data = self.to_binary(input_data)
+
+        return self.output_format().build(output_data, self.symbol_name)
+
+    def _guess_type(self):
+        for input_type, extensions in self.typemap.items():
+            for extension in extensions:
+                if self.input_file.name.endswith(extension):
+                    self.input_type = input_type
+                    logging.info(f"Guessed type {input_type} for {self.input_file}")
+                    return
+
+        raise TypeError(f"Unable to identify type of input file {self.input_file}")
+
+    def _guess_format(self):
+        if self.output_file is None:
+            self.output_format = self.no_output_file_default_format
+            logging.warning(f"No --output given, writing to stdout assuming {self.no_output_file_default_format.name}")
+            return
+
+        for format_name, format_class in self.formats.items():
+            for extension in format_class.extensions:
+                if self.output_file.name.endswith(extension):
+                    self.output_format = format_class
+                    logging.info(f"Guessed output format {format_class.name} for {self.output_file}")
+                    return
+
+        raise TypeError(f"Unable to identify type of output file {self.output_file}")
---
+++ packages/32blit/32blit-0.4.0.tar.gz/32blit-0.4.0/ttblit/tool/cmake.py
@@ -0,0 +1,157 @@
+import logging
+import pathlib
+
+import yaml
+
+from ..core.tool import Tool
+from ..tool.packer import AssetTarget
+
+
+class CMake(Tool):
+    command = 'cmake'
+    help = 'Generate CMake configuration for the asset packer'
+
+    def __init__(self, parser):
+        Tool.__init__(self, parser)
+        self.parser.add_argument('--config', type=pathlib.Path, help='Asset config file')
+        self.parser.add_argument('--cmake', type=pathlib.Path, help='Output CMake file', required=True)
+        self.parser.add_argument('--output', type=pathlib.Path, help='Name for output file(s) or root path when using --config')
+
+        self.config = {}
+        self.general_options = {}
+
+    def parse_config(self, config_file):
+        config = open(config_file).read()
+        config = yaml.safe_load(config)
+
+        self.config = config
+
+    def get_general_options(self):
+        for key, value in self.config.items():
+            if key in ():
+                self.general_options[key] = value
+
+        for key, value in self.general_options.items():
+            self.config.items.pop(key)
+
+    def run(self, args):
+        self.working_path = pathlib.Path('.')
+
+        if args.config is not None:
+            if args.config.is_file():
+                self.working_path = args.config.parent
+            else:
+                logging.warning(f'Unable to find config at {args.config}')
+
+        if args.config is not None:
+            self.parse_config(args.config)
+            logging.info(f'Using config at {args.config}')
+
+        if args.output is not None:
+            self.destination_path = args.output
+        else:
+            self.destination_path = self.working_path
+
+        if 'title' in self.config and 'description' in self.config:
+            logging.info('Detected metadata config')
+            self.run_for_metadata_config(args)
+        else:
+            logging.info('Detected asset config')
+            self.run_for_asset_config(args)
+
+    def run_for_metadata_config(self, args):
+        all_inputs = []
+
+        if 'splash' in self.config:
+            file = pathlib.Path(self.config['splash']['file'])
+            if file.is_absolute():
+                all_inputs += [file]
+            else:
+                all_inputs += list(self.working_path.glob(str(file)))
+
+        if 'icon' in self.config:
+            file = pathlib.Path(self.config['icon']['file'])
+            if file.is_absolute():
+                all_inputs += [file]
+            else:
+                all_inputs += list(self.working_path.glob(str(file)))
+
+        all_inputs = '\n'.join(f'"{x}"'.replace('\\', '/') for x in all_inputs)
+
+        open(args.cmake, 'w').write(f'''# Auto Generated File - DO NOT EDIT!
+    set(METADATA_DEPENDS
+    {all_inputs}
+    )
+    set(METADATA_TITLE "{self.config['title']}")
+    set(METADATA_AUTHOR "{self.config['author']}")
+    set(METADATA_DESCRIPTION "{self.config['description']}")
+    set(METADATA_VERSION "{self.config['version']}")
+    ''')
+
+    def run_for_asset_config(self, args):
+        self.get_general_options()
+
+        all_inputs = []
+        all_outputs = []
+
+        # Top level of our config is filegroups and general settings
+        for target, options in self.config.items():
+            target = pathlib.Path(target)
+
+            if target.suffix in AssetTarget.output_formats:
+                output_formatter = AssetTarget.output_formats[target.suffix]
+            else:
+                logging.warning(f'Unable to guess type of {target}, assuming raw/binary')
+                output_formatter = AssetTarget.output_formats['.raw']
+
+            if output_formatter.components is None:
+                all_outputs.append(self.destination_path / target.name)
+            else:
+                for suffix in output_formatter.components:
+                    all_outputs.append(self.destination_path / target.with_suffix(f'.{suffix}').name)
+
+            # Strip high-level options from the dict
+            # Leaving just file source globs
+            for key in AssetTarget.supported_options:
+                options.pop(key, None)
+
+            for file_glob, file_options in options.items():
+                # Rewrite a single string option to `name: option`
+                # This handles: `inputfile.bin: filename` entries
+                if type(file_options) is str:
+                    file_options = {'name': file_options}
+
+                # Handle both an array of options dicts or a single dict
+                if type(file_options) is not list:
+                    file_options = [file_options]
+
+                input_files = []
+                # Parse the options for any references to input files
+                for file_opts in file_options:
+                    for key, value in file_opts.items():
+                        if key in ('palette') and type(value) is str:
+                            input_files += list(self.working_path.glob(value))
+
+                # Treat the input string as a glob, and get an input filelist
+                if type(file_glob) is str:
+                    input_files += list(self.working_path.glob(file_glob))
+                else:
+                    input_files += [file_glob]
+                if len(input_files) == 0:
+                    logging.warning(f'Input file(s) not found {self.working_path / file_glob}')
+                    continue
+
+                all_inputs += input_files
+
+        all_inputs = '\n'.join(f'"{x}"'.replace('\\', '/') for x in all_inputs)
+        all_outputs = '\n'.join(f'"{x}"'.replace('\\', '/') for x in all_outputs)
+
+        open(args.cmake, 'w').write(f'''# Auto Generated File - DO NOT EDIT!
+set(ASSET_DEPENDS
+{all_inputs}
+)
+
+set(ASSET_OUTPUTS
+{all_outputs}
+)
+''')
