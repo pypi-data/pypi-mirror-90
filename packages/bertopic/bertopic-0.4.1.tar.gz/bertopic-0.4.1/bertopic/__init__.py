7ae01a82b72335bde01b0cd75e555830f7292159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:35 +0000

    Add package bertopic/0.4.1/bertopic-0.4.1.tar.gz

---
+++ packages/bertopic/bertopic-0.4.1.tar.gz/bertopic-0.4.1/bertopic/__init__.py
@@ -0,0 +1,12 @@
+from bertopic._bertopic import BERTopic
+from bertopic._ctfidf import ClassTFIDF
+from bertopic._embeddings import languages, embedding_models
+
+__version__ = "0.4.1"
+
+__all__ = [
+    "BERTopic",
+    "ClassTFIDF",
+    "languages",
+    "embedding_models",
+]
