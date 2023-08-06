255e6ec60d1bbdd0f1a83053dc84a7bac9b4ae48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:16 +0000

    Add package snakai/1.1.dev1/snakai-1.1.dev1.tar.gz

---
+++ packages/snakai/snakai-1.1.dev1.tar.gz/snakai-1.1.dev1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='snakai',  
+    version='1.1.dev1',
+    entry_points={
+        'console_scripts': [
+            'run_snake=snakai.run:main',
+        ],
+    },
+    author="fishshrimp",
+    author_email="readonlyfile@hotmail.com",
+    description="snake with ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fseasy/snakai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.6'
+ )
