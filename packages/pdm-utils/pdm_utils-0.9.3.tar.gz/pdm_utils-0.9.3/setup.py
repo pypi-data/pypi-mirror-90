322b089941f974353c52f2da38e6d4ad4f3b5f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package pdm-utils/0.9.3/pdm_utils-0.9.3.tar.gz

---
+++ packages/pdm-utils/pdm_utils-0.9.3.tar.gz/pdm_utils-0.9.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdm_utils",
+    version="0.9.3",
+    author="Christian Gauthier",
+    author_email="chg60@pitt.edu",
+    description="MySQL phage genomics database management utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SEA-PHAGES/pdm_utils",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"":"src"},
+    install_requires=[
+       'biopython>=1.70',
+       'pymysql>=0.9.0',
+       'paramiko>=2.5.0',
+       'tabulate>=0.8.0',
+       'sqlalchemy>=1.3.0'
+    ],
+    project_urls={
+        'Documentation': 'https://pdm-utils.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/SEA-PHAGES/pdm_utils/',
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Science/Research",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    python_requires='>=3.6'
+)
