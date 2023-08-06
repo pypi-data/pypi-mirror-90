258b8dba5355da700139c6af0dd979b91a9a5691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:50 +0000

    Add package intersect/1.0/intersect-1.0.tar.gz

---
+++ packages/intersect/intersect-1.0.tar.gz/intersect-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="intersect",
+    version="1.0",
+    packages=find_packages(),
+    description="Intersection Of two curves",
+    author="sukhbinder",
+    author_email="sukh2010@yahoo.com",
+    license='MIT',
+    url = 'https://github.com/sukhbinder/intersection',
+    keywords = ["Intersection", "curves", "numpy"],
+    
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 2.7',
+  ], 
+)
