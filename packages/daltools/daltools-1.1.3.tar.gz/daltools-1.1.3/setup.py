d7aef9d01621075a8fe42c7d7bf046cf392873dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:38 +0000

    Add package daltools/1.1.3/daltools-1.1.3.tar.gz

---
+++ packages/daltools/daltools-1.1.3.tar.gz/daltools-1.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from daltools import __version__
+
+setup(
+    name="daltools",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    version=__version__,
+    url="https://github.com/vahtras/daltools",
+    packages=["daltools"],
+    install_requires=["blocked-matrix-utils", "fortran-binary"],
+    description="Misc utilities for extracting data from Dalton files",
+    )
