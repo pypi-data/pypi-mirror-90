e8905fb72d9ab0e38d23c8ad535e14eb78e47419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:34 +0000

    Add package ethermine/0.1.1/ethermine-0.1.1.tar.gz

---
+++ packages/ethermine/ethermine-0.1.1.tar.gz/ethermine-0.1.1/setup.py
@@ -0,0 +1,17 @@
+"""Setup ethermine library."""
+
+from setuptools import find_packages, setup
+setup(
+    name='ethermine',
+    packages=find_packages(include=['ethermine']),
+    version='0.1.1',
+    description='Ethermine python wrapper',
+    author='Chris Landa',
+    author_email='stylesuxx@gmail.com',
+    license='MIT',
+    keywords=['etherium', 'ethermine', 'eth'],
+    install_requires=['requests'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
