76314f25cc5914cffd5429c775a6cf5cbad2fd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package pydbclib/2.2.2/pydbclib-2.2.2-py3-none-any.whl

---
+++ packages/pydbclib/pydbclib-2.2.2-py3-none-any.whl/pydbclib/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    Python Database Connectivity lib
+"""
+from pydbclib.database import Database
+from pydbclib.drivers import CommonDriver, SQLAlchemyDriver
+
+__author__ = "liyatao"
+__version__ = '2.2.2'
+
+
+def connect(*args, **kwargs):
+    driver = kwargs.get("driver", "sqlalchemy")
+    kwargs.update(driver=driver)
+    if isinstance(driver, str):
+        driver_class = {"sqlalchemy": SQLAlchemyDriver}.get(driver.lower(), CommonDriver)
+    elif hasattr(driver, "cursor"):
+        driver_class = CommonDriver
+    else:
+        driver_class = SQLAlchemyDriver
+    return Database(driver_class(*args, **kwargs))
