56381fe9ed1938abdf5a6ab4e41c90891811292b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:17 +0000

    Add package naefdistributions/0.2/naefdistributions-0.2.tar.gz

---
+++ packages/naefdistributions/naefdistributions-0.2.tar.gz/naefdistributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='naefdistributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['naefdistributions'],
+      author = 'Rico Naef',
+      author_email = 'naefrico@gmail.com',
+      zip_safe=False)
