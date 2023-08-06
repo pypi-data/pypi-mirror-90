02b9c0f34797c72a061a2d99435b9603fccb3626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:52 +0000

    Add package scikit-sequitur/0.1.4/scikit_sequitur-0.1.4-cp39-cp39-win_amd64.whl

---
+++ packages/scikit-sequitur/scikit_sequitur-0.1.4-cp39-cp39-win_amd64.whl/sksequitur/__init__.py
@@ -0,0 +1,24 @@
+"""Sequitur
+
+Sequitur is a method for inferring compositional hierarchies from strings. It
+detects repetition and factors it out of the string by forming rules in a
+grammar. The rules can be composed of non-terminals, giving rise to a
+hierarchy. It is useful for recognizing lexical structure in strings, and
+excels at very long sequences.
+
+The algorithm was developed by:
+    Craig Nevill-Manning, Google
+    Ian Witten, University of Waikato, New Zealand
+
+More details are available online at http://www.sequitur.info/
+
+"""
+
+try:
+    from ._core import Grammar, Parser, Stop, parse
+except ImportError:
+    from .core import Grammar, Parser, Stop, parse
+
+__all__ = ["Grammar", "Parser", "Stop", "parse"]
+__title__ = "sksequitur"
+__version__ = "0.1.4"
