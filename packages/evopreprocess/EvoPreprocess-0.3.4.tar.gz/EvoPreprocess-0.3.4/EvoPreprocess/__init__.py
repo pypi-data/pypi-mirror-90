b8d6ac86a2dc152ca5b5172ccc71d48283f157c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:24 +0000

    Add package evopreprocess/0.3.4/EvoPreprocess-0.3.4.tar.gz

---
+++ packages/evopreprocess/EvoPreprocess-0.3.4.tar.gz/EvoPreprocess-0.3.4/EvoPreprocess/__init__.py
@@ -0,0 +1,22 @@
+"""``EvoPreprocess``: Data preprocessing Python toolkit with evolutionary and nature inspired algorithms.
+The main problems tackled by the toolkit are **data sampling** (over- and under-sampling data instances
+and mixture of both), **instance weighting** and **feature selection**.
+
+Subpackages
+-----------
+feature_selection
+    Module for feature selection with evolutionary and nature inspired algorithms.
+data_sampling
+    Module for sampling datasets with evolutionary and nature inspired algorithms.
+data_weighting
+    Module for weighting instances with evolutionary and nature inspired algorithms.
+"""
+
+from . import data_sampling
+from . import data_weighting
+from . import feature_selection
+
+name = 'EvoPreprocess'
+__project__ = 'EvoPreprocess'
+__version__ = '0.3.4'
+__all__ = ['feature_selection', 'data_sampling', 'data_weighting']
