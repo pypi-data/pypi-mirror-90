587b354a8d9884bb5c1830183bd2e30dda039c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:27 +0000

    Add package c2cwsgiutils/4.1.1/c2cwsgiutils-4.1.1-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-4.1.1-py3-none-any.whl/c2cwsgiutils/sqlalchemylogger/_filters.py
@@ -0,0 +1,29 @@
+import re
+import logging
+from typing import Any
+
+
+class ContainsExpression(logging.Filter):
+    """
+    Returns True if the regex is matched in the log message
+    """
+
+    def __init__(self, regex: str) -> None:
+        super().__init__(name="ContainsExpression")
+        self.regex = re.compile(regex)
+
+    def filter(self, record: Any) -> bool:
+        return bool(self.regex.search(record.msg))
+
+
+class DoesNotContainExpression(logging.Filter):
+    """
+    Returns True if the regex is NOT matched in the log message
+    """
+
+    def __init__(self, regex: str) -> None:
+        super().__init__(name="DoesNotContainExpression")
+        self.regex = re.compile(regex)
+
+    def filter(self, record: Any) -> bool:
+        return not bool(self.regex.search(record.msg))
