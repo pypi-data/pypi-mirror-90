fd5418cd9ff26b6ef7fe097a0053ff1969ba4ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:24 +0000

    Add package vitessce/0.1.0a6/vitessce-0.1.0a6.tar.gz

---
+++ packages/vitessce/vitessce-0.1.0a6.tar.gz/vitessce-0.1.0a6/vitessce/routes.py
@@ -0,0 +1,29 @@
+
+from .constants import DataType as dt
+
+def create_obj_routes(obj, base_url, dataset_uid, obj_i):
+	"""
+	For a particular data object, simultaneously set up:
+	
+	* its server routes and their responses, and
+	* the corresponding view config dataset file definitions
+
+	:param obj: An object representing a single-cell data analysis result or microscopy image.
+	:type obj: anndata.AnnData or loompy.LoomConnection or zarr.hierarchy.Group
+	
+	:returns: A list of view config file definitions and a list of server routes.
+	:rtype: tuple[list[dict], list[starlette.routing.Route]]
+	"""
+	obj_file_defs = []
+	obj_routes = []
+
+	for data_type in dt:
+		try:
+			dt_obj_file_defs, dt_obj_routes = obj._get_data(data_type, base_url, dataset_uid, obj_i)
+			obj_file_defs += dt_obj_file_defs
+			obj_routes += dt_obj_routes
+		except NotImplementedError:
+			pass
+
+	return obj_file_defs, obj_routes
+	
