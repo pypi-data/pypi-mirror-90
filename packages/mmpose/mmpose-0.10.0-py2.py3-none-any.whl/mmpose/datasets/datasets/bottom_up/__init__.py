8f5cfcd75b756efc58c038941d7fe8d92cf6d730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:29 +0000

    Add package mmpose/0.10.0/mmpose-0.10.0-py2.py3-none-any.whl

---
+++ packages/mmpose/mmpose-0.10.0-py2.py3-none-any.whl/mmpose/datasets/datasets/bottom_up/__init__.py
@@ -0,0 +1,7 @@
+from .bottom_up_coco import BottomUpCocoDataset
+from .bottom_up_crowdpose import BottomUpCrowdPoseDataset
+from .bottom_up_mhp import BottomUpMhpDataset
+
+__all__ = [
+    'BottomUpCocoDataset', 'BottomUpCrowdPoseDataset', 'BottomUpMhpDataset'
+]
