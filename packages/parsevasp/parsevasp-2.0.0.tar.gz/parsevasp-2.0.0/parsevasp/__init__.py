a4e930837c1c7d52685766f510f061c786745f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:19 +0000

    Add package parsevasp/2.0.0/parsevasp-2.0.0.tar.gz

---
+++ packages/parsevasp/parsevasp-2.0.0.tar.gz/parsevasp-2.0.0/parsevasp/__init__.py
@@ -0,0 +1,10 @@
+"""A parser for VASP."""
+
+__version__ = '2.0.0'
+
+import parsevasp.vasprun
+import parsevasp.poscar
+import parsevasp.incar
+import parsevasp.kpoints
+import parsevasp.outcar
+import parsevasp.stream
