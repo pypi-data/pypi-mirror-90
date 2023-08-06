e921453c370ea2b0017153a6d6d96af9ab08ae13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:56 +0000

    Add package xrayutilities/1.7.1/xrayutilities-1.7.1-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/xrayutilities/xrayutilities-1.7.1-cp37-cp37m-manylinux2010_x86_64.whl/xrayutilities/gridder3d.py
@@ -0,0 +1,237 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2009-2010, 2013
+#               Eugen Wintersberger <eugen.wintersberger@desy.de>
+# Copyright (C) 2009 Mario Keplinger <mario.keplinger@jku.at>
+# Copyright (C) 2009-2016 Dominik Kriegner <dominik.kriegner@gmail.com>
+
+import numpy
+
+from . import cxrayutilities, exception, utilities
+from .gridder import Gridder, axis, delta, ones
+
+
+class Gridder3D(Gridder):
+
+    def __init__(self, nx, ny, nz):
+        Gridder.__init__(self)
+
+        # check input
+        if nx <= 0 or ny <= 0 or nz <= 0:
+            raise exception.InputError('None of nx, ny and nz can be smaller '
+                                       'than 1!')
+
+        self.xmin = 0
+        self.xmax = 0
+        self.ymin = 0
+        self.ymax = 0
+        self.zmin = 0
+        self.zmax = 0
+
+        self.nx = nx
+        self.nz = nz
+        self.ny = ny
+
+        self._allocate_memory()
+
+    def _allocate_memory(self):
+        """
+        Class method to allocate memory for the gridder based on the nx, ny
+        class attributes.
+        """
+        self._gdata = numpy.zeros((self.nx, self.ny, self.nz),
+                                  dtype=numpy.double)
+        self._gnorm = numpy.zeros((self.nx, self.ny, self.nz),
+                                  dtype=numpy.double)
+
+    def SetResolution(self, nx, ny, nz):
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+
+        self._allocate_memory()
+
+    def __get_xaxis(self):
+        return axis(self.xmin, self.xmax, self.nx)
+
+    def __get_yaxis(self):
+        return axis(self.ymin, self.ymax, self.ny)
+
+    def __get_zaxis(self):
+        return axis(self.zmin, self.zmax, self.nz)
+
+    def __get_xmatrix(self):
+        return ones(self.nx, self.ny, self.nz) *\
+            self.xaxis[:, numpy.newaxis, numpy.newaxis]
+
+    def __get_ymatrix(self):
+        return ones(self.nx, self.ny, self.nz) *\
+            self.yaxis[numpy.newaxis, :, numpy.newaxis]
+
+    def __get_zmatrix(self):
+        return ones(self.nx, self.ny, self.nz) *\
+            self.zaxis[numpy.newaxis, numpy.newaxis, :]
+
+    zaxis = property(__get_zaxis)
+    zmatrix = property(__get_zmatrix)
+    xaxis = property(__get_xaxis)
+    xmatrix = property(__get_xmatrix)
+    yaxis = property(__get_yaxis)
+    ymatrix = property(__get_ymatrix)
+
+    def dataRange(self, xmin, xmax, ymin, ymax, zmin, zmax, fixed=True):
+        """
+        define minimum and maximum data range, usually this is deduced
+        from the given data automatically, however, for sequential
+        gridding it is useful to set this before the first call of the
+        gridder. data outside the range are simply ignored
+
+        Parameters
+        ----------
+        xmin, ymin, zmin :  float
+            minimum value of the gridding range in x, y, z
+        xmax, ymax, zmax :  float
+            maximum value of the gridding range in x, y, z
+        fixed :             bool, optional
+            flag to turn fixed range gridding on (True (default)) or off
+            (False)
+        """
+        self.fixed_range = fixed
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        self.zmin = zmin
+        self.zmax = zmax
+
+    def _checktransinput(self, x, y, z, data):
+        """
+        common checks and reshape commands for the input data. This function
+        checks the data type and shape of the input data.
+        """
+        if not self.keep_data:
+            self.Clear()
+
+        x = self._prepare_array(x)
+        y = self._prepare_array(y)
+        z = self._prepare_array(z)
+        data = self._prepare_array(data)
+
+        if x.size != y.size or y.size != z.size or z.size != data.size:
+            raise exception.InputError("XU.%s: size of given datasets "
+                                       "(x, y, z, data) is not equal!"
+                                       % self.__class__.__name__)
+
+        if not self.fixed_range:
+            # assume that with setting keep_data the user wants to call the
+            # gridder more often and obtain a reasonable result
+            self.dataRange(x.min(), x.max(),
+                           y.min(), y.max(),
+                           z.min(), z.max(),
+                           self.keep_data)
+
+        return x, y, z, data
+
+    def __call__(self, x, y, z, data):
+        """
+        Perform gridding on a set of data. After running the gridder
+        the 'data' object in the class is holding the gridded data.
+
+        Parameters
+        ----------
+        x :     ndarray
+            numpy array of arbitrary shape with x positions
+        y :	ndarray
+            numpy array of arbitrary shape with y positions
+        z :	ndarray
+            numpy array fo arbitrary shape with z positions
+        data :	ndarray
+            numpy array of arbitrary shape with data values
+        """
+
+        x, y, z, data = self._checktransinput(x, y, z, data)
+
+        # remove normalize flag for C-code
+        flags = utilities.set_bit(self.flags, 2)
+        cxrayutilities.gridder3d(x, y, z, data, self.nx, self.ny, self.nz,
+                                 self.xmin, self.xmax,
+                                 self.ymin, self.ymax,
+                                 self.zmin, self.zmax,
+                                 self._gdata, self._gnorm, flags)
+
+
+class FuzzyGridder3D(Gridder3D):
+    """
+    An 3D binning class considering every data point to have a finite volume.
+    If necessary one data point will be split fractionally over different
+    data bins. This is numerically more effort but represents better the
+    typical case of a experimental data, which do not represent a mathematical
+    point but have a finite size.
+
+    Currently only a quader can be considered as volume during the gridding.
+    """
+
+    def __call__(self, x, y, z, data, **kwargs):
+        """
+        Perform gridding on a set of data. After running the gridder
+        the 'data' object in the class is holding the gridded data.
+
+        Parameters
+        ----------
+        x :	ndarray
+            numpy array of arbitrary shape with x positions
+        y :	ndarray
+            numpy array of arbitrary shape with y positions
+        z :     ndarray
+            numpy array fo arbitrary shape with z positions
+        data :	ndarray
+            numpy array of arbitrary shape with data values
+        width :	float, tuple or list, optional
+            width of one data point. If not given half the bin size will be
+            used. The width can be given as scalar if it is equal for all three
+            dimensions, or as sequence of length 3.
+        """
+
+        valid_kwargs = {'width': 'specifiying fuzzy data size'}
+        utilities.check_kwargs(kwargs, valid_kwargs,
+                               self.__class__.__name__)
+
+        x, y, z, data = self._checktransinput(x, y, z, data)
+
+        if 'width' in kwargs:
+            try:
+                length = len(kwargs['width'])
+            except TypeError:
+                length = 1
+            if length == 3:
+                wx, wy, wz = kwargs['width']
+            else:
+                wx = kwargs['width']
+                wy = wx
+                wz = wx
+        else:
+            wx = delta(self.xmin, self.xmax, self.nx) / 2.
+            wy = delta(self.ymin, self.ymax, self.ny) / 2.
+            wz = delta(self.zmin, self.zmax, self.nz) / 2.
+
+        # remove normalize flag for C-code
+        flags = utilities.set_bit(self.flags, 2)
+        cxrayutilities.fuzzygridder3d(x, y, z, data, self.nx, self.ny, self.nz,
+                                      self.xmin, self.xmax,
+                                      self.ymin, self.ymax,
+                                      self.zmin, self.zmax,
+                                      self._gdata, self._gnorm,
+                                      wx, wy, wz, flags)
---
+++ packages/xrayutilities/xrayutilities-1.7.1-cp37-cp37m-manylinux2010_x86_64.whl/xrayutilities/normalize.py
@@ -0,0 +1,543 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2010-2011 Dominik Kriegner <dominik.kriegner@gmail.com>
+
+"""
+module to provide functions that perform block averaging
+of intensity arrays to reduce the amount of data (mainly
+for PSD and CCD measurements
+
+and
+
+provide functions for normalizing intensities for
+
+* count time
+* absorber (user-defined function)
+* monitor
+* flatfield correction
+"""
+
+import numpy
+
+from . import config, cxrayutilities, math, utilities
+from .exception import InputError
+
+
+def blockAverage1D(data, Nav):
+    """
+    perform block average for 1D array/list of Scalar values
+    all data are used. at the end of the array a smaller cell
+    may be used by the averaging algorithm
+
+    Parameters
+    ----------
+    data :  array-like
+        data which should be contracted (length N)
+    Nav :   int
+        number of values which should be averaged
+
+    Returns
+    -------
+    ndarray
+        block averaged numpy array of data type numpy.double
+        (length ceil(N/Nav))
+    """
+
+    if not isinstance(data, (numpy.ndarray, list)):
+        raise TypeError("first argument data must be of type list or "
+                        "numpy.ndarray")
+
+    data = numpy.array(data, dtype=numpy.double)
+    block_av = cxrayutilities.block_average1d(data, Nav)
+
+    return block_av
+
+
+def blockAverage2D(data2d, Nav1, Nav2, **kwargs):
+    """
+    perform a block average for 2D array of Scalar values
+    all data are used therefore the margin cells may differ in size
+
+    Parameters
+    ----------
+    data2d :        ndarray
+        array of 2D data shape (N, M)
+    Nav1, Nav2 :    int
+        a field of (Nav1 x Nav2) values is contracted
+
+    kwargs :        dict, optional
+        optional keyword argument
+    roi :           tuple or list, optional
+        region of interest for the 2D array. e.g. [20, 980, 40, 960],
+        reduces M, and M!
+
+    Returns
+    -------
+    ndarray
+        block averaged numpy array with type numpy.double with shape
+        (ceil(N/Nav1), ceil(M/Nav2))
+    """
+
+    if not isinstance(data2d, (numpy.ndarray)):
+        raise TypeError("first argument data2d must be of type numpy.ndarray")
+
+    roi = kwargs.get('roi', [0, data2d.shape[0], 0, data2d.shape[1]])
+    data = numpy.array(data2d[roi[0]:roi[1], roi[2]:roi[3]],
+                       dtype=numpy.double)
+
+    if config.VERBOSITY >= config.DEBUG:
+        N, M = (roi[1] - roi[0], roi[3] - roi[2])
+        print("xu.normalize.blockAverage2D: roi: %s" % (str(roi)))
+        print("xu.normalize.blockAverage2D: Nav1, 2: %d,%d" % (Nav1, Nav2))
+        print("xu.normalize.blockAverage2D: number of points: (%d,%d)"
+              % (numpy.ceil(N / float(Nav1)), numpy.ceil(M / float(Nav2))))
+
+    block_av = cxrayutilities.block_average2d(data, Nav1, Nav2,
+                                              config.NTHREADS)
+
+    return block_av
+
+
+def blockAveragePSD(psddata, Nav, **kwargs):
+    """
+    perform a block average for serveral PSD spectra
+    all data are used therefore the last cell used for
+    averaging may differ in size
+
+    Parameters
+    ----------
+    psddata :   ndarray
+        array of 2D data shape (Nspectra, Nchannels)
+    Nav :       int
+        number of channels which should be averaged
+
+    kwargs :    dict, optional
+        optional keyword argument
+    roi :       tuple or list
+        region of interest for the 2D array. e.g. [20, 980] Nchannels = 980-20
+
+    Returns
+    -------
+    ndarray
+        block averaged psd spectra as numpy array with type numpy.double of
+        shape (Nspectra , ceil(Nchannels/Nav))
+    """
+
+    if not isinstance(psddata, (numpy.ndarray)):
+        raise TypeError("first argument psddata must be of type numpy.ndarray")
+
+    roi = kwargs.get('roi', [0, psddata.shape[1]])
+    data = numpy.array(psddata[:, roi[0]:roi[1]], dtype=numpy.double)
+
+    block_av = cxrayutilities.block_average_PSD(data, Nav, config.NTHREADS)
+
+    return block_av
+
+
+def blockAverageCCD(data3d, Nav1, Nav2, **kwargs):
+    """
+    perform a block average for 2D frames inside a 3D array.
+    all data are used therefore the margin cells may differ in size
+
+    Parameters
+    ----------
+    data3d :        ndarray
+        array of 3D data shape (Nframes, N, M)
+    Nav1, Nav2 :    int
+        a field of (Nav1 x Nav2) values is contracted
+
+    kwargs :        dict, optional
+        optional keyword argument
+    roi :           tuple or list, optional
+        region of interest for the 2D array. e.g. [20, 980, 40, 960],
+        reduces M, and M!
+
+    Returns
+    -------
+    ndarray
+        block averaged numpy array with type numpy.double with shape
+        (Nframes, ceil(N/Nav1), ceil(M/Nav2))
+    """
+
+    if not isinstance(data3d, (numpy.ndarray)):
+        raise TypeError("first argument data3d must be of type numpy.ndarray")
+
+    roi = kwargs.get('roi', [0, data3d.shape[1], 0, data3d.shape[2]])
+    data = numpy.array(data3d[:, roi[0]:roi[1], roi[2]:roi[3]],
+                       dtype=numpy.double)
+
+    if config.VERBOSITY >= config.DEBUG:
+        N, M = (roi[1] - roi[0], roi[3] - roi[2])
+        print("xu.normalize.blockAverageCCD: roi: %s" % (str(roi)))
+        print("xu.normalize.blockAverageCCD: Nav1, 2: %d,%d" % (Nav1, Nav2))
+        print("xu.normalize.blockAverageCCD: number of points: (%d,%d)"
+              % (numpy.ceil(N / float(Nav1)), numpy.ceil(M / float(Nav2))))
+
+    block_av = cxrayutilities.block_average_CCD(data, Nav1, Nav2,
+                                                config.NTHREADS)
+
+    return block_av
+
+# #####################################
+# #    Intensity correction class    ##
+# #####################################
+
+
+class IntensityNormalizer(object):
+
+    """
+    generic class for correction of intensity (point detector, or MCA,
+    single CCD frames) for count time and absorber factors
+    the class must be supplied with a absorber correction function
+    and works with data structures provided by xrayutilities.io classes or the
+    corresponding objects from hdf5 files
+    """
+
+    def __init__(self, det='', **keyargs):
+        """
+        initialization of the corrector class
+
+        Parameters
+        ----------
+        det :       str
+            detector field name of the data structure
+
+        mon :       str, optional
+            monitor field name
+        time:       float or str, optional
+            count time field name or count time as float
+        av_mon :    float, optional
+            average monitor value (default: data[mon].mean())
+        smoothmon : int
+            number of monitor values used to get a smooth monitor signal
+        absfun :    callable, optional
+            absorber correction function to be used as in
+            ``absorber_corrected_intensity = data[det]*absfun(data)``
+        flatfield : ndarray
+            flatfield of the detector; shape must be the same as data[det], and
+            is only applied for MCA detectors
+        darkfield : ndarray
+            darkfield of the detector; shape must be the same as data[det], and
+            is only applied for MCA detectors
+
+        Examples
+        --------
+        >>> detcorr = IntensityNormalizer("MCA", time="Seconds",
+        >>>     absfun=lambda d: d["PSDCORR"]/d["PSD"].astype(numpy.float))
+        """
+        valid_kwargs = {'mon': 'monitor field name',
+                        'time': 'count time field/value',
+                        'smoothmon': 'number of monitor values to average',
+                        'av_mon': 'average monitor value',
+                        'absfun': 'absorber correction function',
+                        'flatfield': 'detector flatfield',
+                        'darkfield': 'detector darkfield'}
+        utilities.check_kwargs(keyargs, valid_kwargs,
+                               self.__class__.__name__)
+
+        # check input arguments
+        self._setdet(det)
+
+        self._setmon(keyargs.get('mon', None))
+        self._settime(keyargs.get('time', None))
+        self._setavmon(keyargs.get('av_mon', None))
+        self._setabsfun(keyargs.get('absfun', None))
+        self._setflatfield(keyargs.get('flatfield', None))
+        self._setdarkfield(keyargs.get('darkfield', None))
+        self.smoothmon = keyargs.get('smoothmon', 1)
+
+    def _getdet(self):
+        """
+        det property handler
+
+        returns the detector field name
+        """
+        return self._det
+
+    def _setdet(self, det):
+        """
+        det  property handler
+
+        sets the detector field name
+        """
+        if isinstance(det, str):
+            self._det = det
+        else:
+            self._det = None
+            raise TypeError("argument det must be of type str")
+
+    def _gettime(self):
+        """
+        time property handler
+
+        returns the count time or the field name of the count time
+        or None if time is not set
+        """
+        return self._time
+
+    def _settime(self, time):
+        """
+        time property handler
+
+        sets the count time field or value
+        """
+        if isinstance(time, str):
+            self._time = time
+        elif isinstance(time, (float, int)):
+            self._time = float(time)
+        elif isinstance(time, type(None)):
+            self._time = None
+        else:
+            raise TypeError("argument time must be of type str, float or None")
+
+    def _getmon(self):
+        """
+        mon property handler
+
+        returns the monitor field name or None if not set
+        """
+        return self._mon
+
+    def _setmon(self, mon):
+        """
+        mon property handler
+
+        sets the monitor field name
+        """
+        if isinstance(mon, str):
+            self._mon = mon
+        elif isinstance(mon, type(None)):
+            self._mon = None
+        else:
+            raise TypeError("argument mon must be of type str")
+
+    def _getavmon(self):
+        """
+        av_mon property handler
+
+        returns the value of the average monitor or None
+        if average is calculated from the monitor field
+        """
+        return self._avmon
+
+    def _setavmon(self, avmon):
+        """
+        avmon property handler
+
+        sets the average monitor field name
+        """
+        if isinstance(avmon, (float, int)):
+            self._avmon = float(avmon)
+        elif isinstance(avmon, type(None)):
+            self._avmon = None
+        else:
+            raise TypeError("argument avmon must be of type float or None")
+
+    def _getabsfun(self):
+        """
+        absfun property handler
+
+        returns the costum correction function or None
+        """
+        return self._absfun
+
+    def _setabsfun(self, absfun):
+        """
+        absfun property handler
+
+        sets the costum correction function
+        """
+        if hasattr(absfun, '__call__'):
+            self._absfun = absfun
+            if self._absfun.__code__.co_argcount != 1:
+                raise TypeError("argument absfun must be a function with one "
+                                "argument (data object)")
+        elif isinstance(absfun, type(None)):
+            self._absfun = None
+        else:
+            raise TypeError("argument absfun must be of type function or None")
+
+    def _getflatfield(self):
+        """
+        flatfield property handler
+
+        returns the current set flatfield of the detector
+        or None if not set
+        """
+        return self._flatfield
+
+    def _setflatfield(self, flatf):
+        """
+        flatfield property handler
+
+        sets the flatfield of the detector
+        """
+        if isinstance(flatf, (list, tuple, numpy.ndarray)):
+            self._flatfield = numpy.array(flatf, dtype=numpy.float)
+            self._flatfieldav = numpy.mean(self._flatfield[
+                                           self._flatfield.nonzero()])
+            self._flatfield[self.flatfield < 1.e-5] = 1.0
+        elif isinstance(flatf, type(None)):
+            self._flatfield = None
+        else:
+            raise TypeError("argument flatfield must be of type list, tuple, "
+                            "numpy.ndarray or None")
+
+    def _getdarkfield(self):
+        """
+        flatfield property handler
+
+        returns the current set darkfield of the detector
+        or None if not set
+        """
+        return self._darkfield
+
+    def _setdarkfield(self, darkf):
+        """
+        flatfield property handler
+
+        sets the darkfield of the detector
+        """
+        if isinstance(darkf, (list, tuple, numpy.ndarray)):
+            self._darkfield = numpy.array(darkf, dtype=numpy.float)
+            self._darkfieldav = numpy.mean(self._darkfield)
+        elif isinstance(darkf, type(None)):
+            self._darkfield = None
+        else:
+            raise TypeError("argument flatfield must be of type list, tuple, "
+                            "numpy.ndarray or None")
+
+    det = property(_getdet, _setdet)
+    time = property(_gettime, _settime)
+    mon = property(_getmon, _setmon)
+    avmon = property(_getavmon, _setavmon)
+    absfun = property(_getabsfun, _setabsfun)
+    flatfield = property(_getflatfield, _setflatfield)
+    darkfield = property(_getdarkfield, _setdarkfield)
+
+    def __call__(self, data, ccd=None):
+        """
+        apply the correction method which was initialized to the measured data
+
+        Parameters
+        ----------
+        data :  numpy.recarray
+            data object from xrayutilities.io classes
+        ccd :   ndarray, optional
+            optionally CCD data can be given as separate ndarray of shape
+            (len(data), n1, n2), where n1, n2 is the shape of the CCD image.
+
+        Returns
+        -------
+        corrint :   ndarray
+            corrected intensity as numpy.ndarray of the same shape as data[det]
+            (or ccd.shape)
+        """
+        if numpy.any(ccd):
+            rawdata = ccd
+        else:
+            rawdata = data[self._det]
+
+        corrint = numpy.zeros(rawdata.shape, dtype=numpy.float)
+
+        # set needed variables
+        # monitor intensity
+        if self._mon:
+            if self.smoothmon == 1:
+                mon = data[self._mon]
+            else:
+                mon = math.smooth(data[self._mon], self.smoothmon)
+        else:
+            mon = 1.
+        # count time
+        if isinstance(self._time, str):
+            time = data[self._time]
+        elif isinstance(self._time, float):
+            time = self._time
+        else:
+            time = 1.
+        # average monitor counts
+        if self._avmon:
+            avmon = self._avmon
+        else:
+            avmon = numpy.mean(mon)
+        # absorber correction function
+        if self._absfun:
+            abscorr = self._absfun(data)
+        else:
+            abscorr = 1.
+
+        c = abscorr * avmon / (mon * time)
+        # correct the correction factor if it was evaluated to an incorrect
+        # value
+        if isinstance(c, numpy.ndarray):
+            c[numpy.isnan(c)] = 1.0
+            c[numpy.isinf(c)] = 1.0
+            c[c == 0] = 1.0
+        else:
+            if numpy.isnan(c) or numpy.isinf(c) or c == 0:
+                c = 1.0
+
+        if len(rawdata.shape) == 1:
+            corrint = rawdata * c
+        elif len(rawdata.shape) == 2 and isinstance(c, numpy.ndarray):
+            # 1D detector c.shape[0] should be rawdata.shape[0]
+            if self._darkfield is not None:
+                if self._darkfield.shape[0] != rawdata.shape[1]:
+                    raise InputError("data[det] second dimension must have "
+                                     "the same length as darkfield")
+
+                if isinstance(time, numpy.ndarray):
+                    # darkfield correction
+                    corrint = (rawdata -
+                               self._darkfield[numpy.newaxis, :] *
+                               time[:, numpy.newaxis])
+                elif isinstance(time, float):
+                    # darkfield correction
+                    corrint = (rawdata -
+                               self._darkfield[numpy.newaxis, :] * time)
+                else:
+                    print("XU.normalize.IntensityNormalizer: check "
+                          "initialization and your input")
+                    return None
+                corrint[corrint < 0.] = 0.
+
+            else:
+                corrint = rawdata
+
+            corrint = corrint * c[:, numpy.newaxis]
+
+            if self._flatfield is not None:
+                if self._flatfield.shape[0] != rawdata.shape[1]:
+                    raise InputError("data[det] second dimension must have "
+                                     "the same length as flatfield")
+                # flatfield correction
+                corrint = (corrint / self._flatfield[numpy.newaxis, :] *
+                           self._flatfieldav)
+
+        elif len(rawdata.shape) == 2 and isinstance(c, numpy.float):
+            # single 2D detector frame
+            corrint = rawdata * c
+
+        elif len(rawdata.shape) == 3:
+            # darkfield and flatfield correction is still missing!
+            corrint = rawdata * c[:, numpy.newaxis, numpy.newaxis]
+
+        else:
+            raise InputError("data[det] must be an array of dimension one "
+                             "or two or three")
+
+        return corrint
