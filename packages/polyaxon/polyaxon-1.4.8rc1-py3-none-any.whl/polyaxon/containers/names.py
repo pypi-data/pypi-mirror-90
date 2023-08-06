aeccaab70adf425142d11a0a11af27b33e2fc908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:33 +0000

    Add package polyaxon/1.4.8rc1/polyaxon-1.4.8rc1-py3-none-any.whl

---
+++ packages/polyaxon/polyaxon-1.4.8rc1-py3-none-any.whl/polyaxon/containers/names.py
@@ -0,0 +1,20 @@
+MAIN_JOB_CONTAINER = "polyaxon-main"
+INIT_AUTH_CONTAINER = "polyaxon-init-auth"
+SIDECAR_CONTAINER = "polyaxon-sidecar"
+TFJOBS_CONTAINER = "tensorflow"
+PYTORCHJOBS_CONTAINER = "pytorch"
+INIT_DOCKERFILE_CONTAINER_PREFIX = "polyaxon-init-dockerfile"
+INIT_GIT_CONTAINER_PREFIX = "polyaxon-init-git"
+INIT_CUSTOM_CONTAINER_PREFIX = "polyaxon-init-custom"
+INIT_ARTIFACTS_CONTAINER_PREFIX = "polyaxon-init-artifacts"
+POLYAXON_INIT_PREFIX = "polyaxon-init"
+INIT_PREFIX = "init"
+SIDECAR_PREFIX = "sidecar"
+
+
+def generate_container_name(prefix: str, suffix: str = None) -> str:
+    import uuid
+
+    prefix = prefix or "container"
+    suffix = suffix or uuid.uuid4().hex[:10]
+    return "{}-{}".format(prefix, suffix)
---
+++ packages/polyaxon/polyaxon-1.4.8rc1-py3-none-any.whl/polyaxon/polyaxonfile/specs/operation.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+#
+# Copyright 2018-2020 Polyaxon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections.abc import Mapping
+from typing import Dict, Union
+
+from polyaxon.polyaxonfile.specs import kinds
+from polyaxon.polyaxonfile.specs.base import BaseSpecification
+from polyaxon.polyflow import (
+    V1IO,
+    V1CompiledOperation,
+    V1Component,
+    V1Operation,
+    validate_run_patch,
+)
+from polyaxon.utils.list_utils import to_list
+
+
+class OperationSpecification(BaseSpecification):
+    """The polyaxonfile specification for operations."""
+
+    _SPEC_KIND = kinds.OPERATION
+
+    CONFIG = V1Operation
+
+    @classmethod
+    def compile_operation(
+        cls, config: V1Operation, override: Dict = None
+    ) -> V1CompiledOperation:
+        if override:
+            preset = OperationSpecification.read(override, is_preset=True)
+            config = config.patch(preset, preset.patch_strategy)
+        # Patch run
+        component = config.component  # type: V1Component
+        if config.run_patch:
+            component.run = component.run.patch(
+                validate_run_patch(config.run_patch, component.run.kind),
+                strategy=config.patch_strategy,
+            )
+        # Gather contexts io
+        config_params = config.params or {}
+        contexts = [
+            V1IO(name=p) for p in config_params if config_params[p].context_only
+        ]
+        patch_compiled = V1CompiledOperation(
+            name=config.name,
+            description=config.description,
+            contexts=contexts,
+            tags=config.tags,
+            presets=config.presets,
+            queue=config.queue,
+            cache=config.cache,
+            hooks=config.hooks,
+            events=config.events,
+            plugins=config.plugins,
+            termination=config.termination,
+            matrix=config.matrix,
+            joins=config.joins,
+            schedule=config.schedule,
+            dependencies=config.dependencies,
+            trigger=config.trigger,
+            conditions=config.conditions,
+            skip_on_upstream_skip=config.skip_on_upstream_skip,
+        )
+
+        values = [
+            {cls.VERSION: config.version},
+            component.to_dict(),
+            {cls.KIND: kinds.COMPILED_OPERATION},
+        ]
+        compiled = V1CompiledOperation.read(values)  # type: V1CompiledOperation
+        return compiled.patch(patch_compiled, strategy=config.patch_strategy)
+
+    @classmethod
+    def apply_preset(
+        cls, config: V1CompiledOperation, preset: Union[Dict, str] = None
+    ) -> V1CompiledOperation:
+        if not preset:
+            return config
+        preset = OperationSpecification.read(
+            preset, is_preset=True
+        )  # type: V1Operation
+        if preset.run_patch:
+            config.run = config.run.patch(
+                validate_run_patch(preset.run_patch, config.run.kind),
+                strategy=preset.patch_strategy,
+            )
+        patch_compiled = V1CompiledOperation(
+            name=preset.name,
+            description=preset.description,
+            tags=preset.tags,
+            presets=preset.presets,
+            queue=preset.queue,
+            cache=preset.cache,
+            hooks=preset.hooks,
+            events=preset.events,
+            plugins=preset.plugins,
+            termination=preset.termination,
+            matrix=preset.matrix,
+            joins=preset.joins,
+            schedule=preset.schedule,
+            dependencies=preset.dependencies,
+            trigger=preset.trigger,
+            conditions=preset.conditions,
+            skip_on_upstream_skip=preset.skip_on_upstream_skip,
+        )
+        return config.patch(patch_compiled, strategy=preset.patch_strategy)
+
+    @classmethod
+    def read(cls, values, is_preset: bool = False):
+        if is_preset:
+            if isinstance(values, cls.CONFIG):
+                values.is_preset = True
+                return values
+            elif isinstance(values, Mapping):
+                values[cls.IS_PRESET] = True
+            else:
+                values = to_list(values)
+                values = [{cls.IS_PRESET: True}] + values
+
+        return super().read(values)
