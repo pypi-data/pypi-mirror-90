c30f4d28eef043d1e394589f0cab09e807727432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:52 +0000

    Add package pyShelly/0.2.15/pyShelly-0.2.15.tar.gz

---
+++ packages/pyShelly/pyShelly-0.2.15.tar.gz/pyShelly-0.2.15/pyShelly/loop.py
@@ -0,0 +1,40 @@
+import threading
+from datetime import timedelta, datetime
+import time
+
+class Loop(object):
+    def __init__(self, name, root, interval, delay=5 ):
+        self._loop_root = root
+        self._loop_delay = delay
+        self._loop_interval = interval
+        self._loop_name = name
+        self._loop_thread = None
+        self._last_run = None
+
+    def start_loop(self):
+        self._loop_thread = threading.Thread(target=self._loop_start)
+        self._loop_thread.name = self._loop_name
+        self._loop_thread.daemon = True
+        self._loop_thread.start()
+
+    def _loop_start(self):
+        try:
+            self.loop_start()
+        except:
+            LOGGER.error("Error start loop %s, %s", self._loop_name, ex)
+        while not self._loop_root.stopped.isSet():
+            self.loop()
+
+    def loop_start(self):
+        pass
+
+    def loop(self):
+        if self._last_run is None or \
+            datetime.now() - self._last_run \
+                        > self._loop_interval:
+            self._last_run = datetime.now()
+            self.loop_timer()
+        self._loop_root.stopped.wait(self._loop_delay)
+
+    def loop_timer(self):
+        pass
---
+++ packages/pyShelly/pyShelly-0.2.15.tar.gz/pyShelly-0.2.15/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+import re
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^VERSION = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setuptools.setup(
+    name="pyShelly",
+    version=find_version("pyShelly", "const.py"),
+    license="MIT",
+    author="StyraHem / Tarra AB",
+    author_email="info@styrahem.se",
+    description="Library for Shelly smart home devices",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    install_requires=['zeroconf'],
+    url="https://github.com/StyraHem/pyShelly",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
