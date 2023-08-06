5fbd42cba5a7779c35b68b8e67d3128f22c3e1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:13 +0000

    Add package django-thumbnails/0.4.0/django-thumbnails-0.4.0.tar.gz

---
+++ packages/django-thumbnails/django-thumbnails-0.4.0.tar.gz/django-thumbnails-0.4.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='django-thumbnails',
+    version='0.4.0',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['thumbnails'],
+    url='https://github.com/ui/django-thumbnails',
+    license='MIT',
+    description='A simple Django app to manage image/photo thumbnails. Supports remote/cloud storage systems like Amazon S3.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    install_requires=['django>=2.0', 'da-vinci', 'shortuuid'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
