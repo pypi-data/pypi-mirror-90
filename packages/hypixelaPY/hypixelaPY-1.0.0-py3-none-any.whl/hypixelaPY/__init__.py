4387ad8ad8f419bab1d650cd005ff34e64defa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:01 +0000

    Add package hypixelaPY/1.0.0/hypixelaPY-1.0.0-py3-none-any.whl

---
+++ packages/hypixelaPY/hypixelaPY-1.0.0-py3-none-any.whl/hypixelaPY/__init__.py
@@ -0,0 +1,13 @@
+__title__ = "hypixelaPY"
+__author__ = "myerfire"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+from .exceptions import *
+from .client import Hypixel, Mojang
+from objects import bedwars
+from objects import duels
+from objects import leaderboards
+from objects import skywars
+from objects import stats
+import utils
