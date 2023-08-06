19b1ad9fdad36d698c2f99b16d304cfa96c22d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:36 +0000

    Add package GsuiteToMd/0.9/GsuiteToMd-0.9.tar.gz

---
+++ packages/GsuiteToMd/GsuiteToMd-0.9.tar.gz/GsuiteToMd-0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='GsuiteToMd',
+    version='0.9',
+    author='Laurent Maumet',
+    author_email='laurent@aurora-5r.fr',
+    url='https://github.com/laurentmau/GsuiteToMd',
+    description='Tools to convert gsuite Documents to markdown',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+
+)
