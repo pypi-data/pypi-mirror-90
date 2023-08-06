86f787be0c88c01e378e082b7f4f376f3d219051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:50 +0000

    Add package automate-insurance-pricing/1.0.1/automate_insurance_pricing-1.0.1-py3-none-any.whl

---
+++ packages/automate-insurance-pricing/automate_insurance_pricing-1.0.1-py3-none-any.whl/automate_insurance_pricing/preprocessing/encoding_functions.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+
+from copy import deepcopy
+from datetime import date, timedelta, datetime
+
+
+
+def min_max_scale(df, features, min_max_scaler):
+    """
+        Makes a min-max scaling method on the features
+        Arguments --> The df and the list of features to rescale
+        Returns --> a new df, copy of the original df but with the features scaled
+    """
+
+    new_df = deepcopy(df)
+
+    for feature in features:
+        new_df[feature + '_scaled'] = min_max_scaler.fit_transform(new_df[feature].to_frame())
+
+    return new_df, min_max_scaler
+
+
+def hot_encode(df, features, features_hot_encoded, encoder):
+    """
+        Hot encodes the features
+        Arguments --> df, features to encode, the names of the new features created and the encoder
+        Returns --> A new df with features hot encoded, i.e. with new binary variables
+    """
+
+    data_encoded = pd.DataFrame(encoder.fit_transform(df[features].astype('str')), columns=features_hot_encoded)
+
+    new_df = pd.concat([df, data_encoded], axis=1, join='inner').reindex()
+
+    for feature in features:
+        value = new_df[feature].unique()[0]
+        col_to_remove = feature + '_' + str(value)
+        # Drops the original feature as not needed anymore
+        # Drops the first value of the encoded feature as it can be directly infered from the other values
+        new_df.drop(columns=[feature, col_to_remove], inplace=True)
+
+    return new_df, encoder
+
+
+def label_encode(df, features, encoder, sort_features=None):
+    """
+        Label encodes the features
+        Arguments --> the df, the features to encode, the list storing the features and their encoded versions names
+                    if the column values need to be sorted before being encoded (ordinal feature)
+                    and which suffixe name to give to the newly created column
+        Returns --> A new df, copy of the original df but with features label encoded
+    """
+
+    if features is None or len(features) == 0:
+        print("No features to encode have been specified in the 'features' argument")
+        return df, encoder
+
+    new_df = deepcopy(df)
+
+    list_features = features if isinstance(features, list) else [features]
+    list_sort_features = sort_features if sort_features is None or isinstance(features, list)  == True else [features]
+
+    for index, feature in enumerate(list_features):
+        new_feature = feature + '_label_enc'
+
+        if list_sort_features is None or list_sort_features[index] == True:
+            data = new_df[feature]
+            encoder.fit(data.sort_values().values.astype('str'))
+            new_df[new_feature] = encoder.transform(data.values.astype('str'))
+
+        else:
+            new_df[new_feature] = encoder.fit_transform(new_df[feature].astype('str'))
+
+    return new_df, encoder
