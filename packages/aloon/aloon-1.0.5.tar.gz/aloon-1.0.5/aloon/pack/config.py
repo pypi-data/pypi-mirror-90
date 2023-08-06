632b0d23a48c30d17bef743bbdb86bcb8dbd4133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package aloon/1.0.5/aloon-1.0.5.tar.gz

---
+++ packages/aloon/aloon-1.0.5.tar.gz/aloon-1.0.5/aloon/pack/config.py
@@ -0,0 +1,25 @@
+from ..simple_parsing import ArgumentParser, field, subparsers
+from ..simple_parsing.helpers import Serializable
+from dataclasses import dataclass
+
+@dataclass
+class Dependency(Serializable):
+    name:                str = ''
+    gradle_placeholder:  str = ''
+
+@dataclass
+class Module(Serializable):
+    name:                        str  = ''
+    alias:                       str  = ''
+    path:                        str  = ''
+    build:                       str = ''
+    upload_version:              str = ''
+    upload_version_placeholder:  str = ''
+    dependencies:                list[Dependency] = field(default_factory=list)
+
+@dataclass
+class Config(Serializable):
+    suffix:              str = ''
+    tools_version:       str = ''
+    outputs:             str = ''
+    projects:            list[Module] = field(default_factory=list)
