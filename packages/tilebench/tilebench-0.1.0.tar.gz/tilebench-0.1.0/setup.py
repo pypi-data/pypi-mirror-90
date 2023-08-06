183f3f9038a75a254cc3dc63f0c696212a626474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:25 +0000

    Add package tilebench/0.1.0/tilebench-0.1.0.tar.gz

---
+++ packages/tilebench/tilebench-0.1.0.tar.gz/tilebench-0.1.0/setup.py
@@ -0,0 +1,48 @@
+"""Setup tilebench."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+inst_reqs = [
+    "wurlitzer",
+    "loguru",
+    "rasterio",
+    "rio-tiler>=2.0.0rc4",
+    "boto3",
+    "supermercado",
+    "fastapi",
+    "uvicorn",
+    "geojson_pydantic",
+    "aiofiles",
+    "jinja2",
+]
+extra_reqs = {
+    "test": ["pytest", "pytest-cov", "pytest-asyncio"],
+    "dev": ["pytest", "pytest-cov", "pytest-asyncio", "pre-commit"],
+}
+
+
+setup(
+    name="tilebench",
+    version="0.1.0",
+    description=u"",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    keywords="",
+    author=u"Vincent Sarago",
+    author_email="vincent@developmentseed.org",
+    url="https://github.com/developmentseed/tilebench",
+    license="MIT",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+    entry_points="""
+      [console_scripts]
+      tilebench=tilebench.scripts.cli:cli
+      """,
+)
