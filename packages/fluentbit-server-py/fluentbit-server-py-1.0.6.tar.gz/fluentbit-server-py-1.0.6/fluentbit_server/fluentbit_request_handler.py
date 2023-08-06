52630c12e36012e63e8c6f20c6a74b36e4faea9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:01 +0000

    Add package fluentbit-server-py/1.0.6/fluentbit-server-py-1.0.6.tar.gz

---
+++ packages/fluentbit-server-py/fluentbit-server-py-1.0.6.tar.gz/fluentbit-server-py-1.0.6/fluentbit_server/fluentbit_request_handler.py
@@ -0,0 +1,36 @@
+import socketserver
+import msgpack
+import logging
+
+class FluentbitRequestHandler(socketserver.BaseRequestHandler):
+
+    def __init__(self, request, client_address, server):
+        self._log = logging.getLogger("FluentbitRequestHandler")
+        self._log.debug("Init for {}".format(client_address))
+        socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
+
+    def setup(self):
+        self._log.debug("setup")
+        return socketserver.BaseRequestHandler.setup(self)
+
+    def handle(self):
+        self._log.debug("handle")
+        if self.server.get_authentication():
+            auth = self.server.get_authentication()()
+            authorized = auth.process_authentication(self.request)
+        else:
+            authorized = True
+
+        if authorized:
+            try:
+                t = self.server.get_transport()()
+                for message_bin in t.collect(self.request):
+                    t.process(message_bin)
+            except Exception as e:
+                self._log.error("Drop message ({})".format(repr(e)))
+        else:
+            self._log.debug("Authentication failed")
+
+    def finish(self):
+        self._log.debug("finish")
+        return socketserver.BaseRequestHandler.finish(self)
