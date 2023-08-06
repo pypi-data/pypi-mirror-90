612929ab58bdcd11050a63da416a56654e0d7276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:17 +0000

    Add package eyecu-bumblebee/0.4.1/eyecu-bumblebee-0.4.1.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.4.1.tar.gz/eyecu-bumblebee-0.4.1/bumblebee/effects/GoTo.py
@@ -0,0 +1,16 @@
+import cv2
+
+from ..sources import FileStream
+from ..bases import IEffect
+
+
+class GoTo(IEffect):
+
+    def __init__(self, src: FileStream):
+        self.src = src
+
+    def __call__(self,frame_number : int,*args,**kwargs):
+        self.goto(frame_number)
+
+    def goto(self,frame_number : int):
+        self.src.cap.set(cv2.CAP_PROP_POS_FRAMES,float(frame_number))
