d38b5a4653a44a878eca98a2d2fd36e4dc003d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:10 +0000

    Add package tsfeatures/0.2.0/tsfeatures-0.2.0.tar.gz

---
+++ packages/tsfeatures/tsfeatures-0.2.0.tar.gz/tsfeatures-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tsfeatures",
+    version="0.2.0",
+    description="Calculates various features from time series data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FedericoGarza/tsfeatures",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "arch==4.11",
+        "pandas==1.0.5",
+        "scikit-learn==0.23.1",
+        "statsmodels==0.11.1",
+        "supersmoother==0.4"
+    ]
+)
