b89f84e0313daea28055bbe792bb891223452c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:33 +0000

    Add package easyvanity.py/0.0.3/easyvanity.py-0.0.3-py3-none-any.whl

---
+++ packages/easyvanity.py/easyvanity.py-0.0.3-py3-none-any.whl/easyvanity/lib/util/exceptions.py
@@ -0,0 +1,14 @@
+class NotFound(Exception):
+    def __init__(*args, **kwargs):
+        Exception.__init__(*args, **kwargs)
+
+class LibraryUpdateAvailable(Exception):
+    def __init__(*args, **kwargs):
+        Exception.__init__(*args, **kwargs)
+
+def CheckException(data):
+    errorCode: str = data["error"]
+    errorMessage: str = data["response"]
+
+    if errorCode != "true": raise NotFound(errorMessage)
+    else: raise Exception(errorMessage)
