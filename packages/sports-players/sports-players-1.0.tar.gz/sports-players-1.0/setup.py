f0a2fadf3e7ec4d3584eae8ed372cce4e9170359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:21 +0000

    Add package sports-players/1.0/sports-players-1.0.tar.gz

---
+++ packages/sports-players/sports-players-1.0.tar.gz/sports-players-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="sports-players",
+    version="1.0",
+    description="Sports Players Objects",
+    packages=["sports_players"],
+    install_requires=[
+        "pandas",
+    ],
+    author="Frantz Paul",
+    author_email="fpaul787@gmail.com",
+    zip_safe=False,
+)
