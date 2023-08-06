d189e7317d260f81935a2ea587451e3b9e40dfed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package servicelayer/1.16.1/servicelayer-1.16.1-py2.py3-none-any.whl

---
+++ packages/servicelayer/servicelayer-1.16.1-py2.py3-none-any.whl/servicelayer/archive/util.py
@@ -0,0 +1,30 @@
+from hashlib import sha1
+from pathlib import Path
+
+BUF_SIZE = 1024 * 1024 * 16
+
+
+def ensure_path(file_path):
+    if file_path is None or isinstance(file_path, Path):
+        return file_path
+    return Path(file_path).resolve()
+
+
+def ensure_posix_path(file_path):
+    if isinstance(file_path, Path):
+        file_path = file_path.as_posix()
+    return file_path
+
+
+def checksum(file_name):
+    """Generate a hash for a given file name."""
+    file_name = ensure_path(file_name)
+    if file_name is not None and file_name.is_file():
+        digest = sha1()
+        with open(file_name, "rb") as fh:
+            while True:
+                block = fh.read(BUF_SIZE)
+                if not block:
+                    break
+                digest.update(block)
+        return str(digest.hexdigest())
---
+++ packages/servicelayer/servicelayer-1.16.1-py2.py3-none-any.whl/tests/archive/test_file.py
@@ -0,0 +1,58 @@
+import shutil
+import tempfile
+from unittest import TestCase
+
+from servicelayer.archive import init_archive
+from servicelayer.archive.util import checksum, ensure_path
+
+
+class FileArchiveTest(TestCase):
+    def setUp(self):
+        tempdir = ensure_path(tempfile.gettempdir())
+        self.path = tempdir.joinpath("sltest")
+        self.archive = init_archive("file", path=self.path)
+        self.file = ensure_path(__file__)
+
+    def tearDown(self):
+        if self.path.exists():
+            shutil.rmtree(self.path)
+
+    def test_basic_archive(self):
+        checksum_ = checksum(self.file)
+        assert checksum_ is not None, checksum_
+        out = self.archive.archive_file(self.file)
+        assert checksum_ == out, (checksum_, out)
+        out2 = self.archive.archive_file(self.file)
+        assert out == out2, (out, out2)
+
+    def test_basic_archive_with_checksum(self):
+        checksum_ = "banana"
+        out = self.archive.archive_file(self.file, checksum_)
+        assert checksum_ == out, (checksum_, out)
+
+    def test_generate_url(self):
+        out = self.archive.archive_file(self.file)
+        url = self.archive.generate_url(out)
+        assert url is None, url
+
+    def test_load_file(self):
+        out = self.archive.archive_file(self.file)
+        path = self.archive.load_file(out)
+        assert path is not None, path
+        assert self.path in path.parents
+        assert path.is_file(), path
+
+    def test_cleanup_file(self):
+        out = self.archive.archive_file(self.file)
+        self.archive.cleanup_file(out)
+        path = self.archive.load_file(out)
+        assert path.is_file(), path
+
+    def test_delete_file(self):
+        out = self.archive.archive_file(self.file)
+        path = self.archive.load_file(out)
+        assert path is not None, path
+        self.archive.cleanup_file(out)
+        self.archive.delete_file(out)
+        path = self.archive.load_file(out)
+        assert path is None, path
