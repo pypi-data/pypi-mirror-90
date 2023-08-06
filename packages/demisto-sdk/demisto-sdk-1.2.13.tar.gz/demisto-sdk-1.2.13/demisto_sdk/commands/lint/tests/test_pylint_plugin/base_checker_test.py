37ec87c1785ef7840a56a5e25c63f6a6b55f0a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:57 +0000

    Add package demisto-sdk/1.2.13/demisto-sdk-1.2.13.tar.gz

---
+++ packages/demisto-sdk/demisto-sdk-1.2.13.tar.gz/demisto-sdk-1.2.13/demisto_sdk/commands/lint/tests/test_pylint_plugin/base_checker_test.py
@@ -0,0 +1,351 @@
+import astroid
+import pylint.testutils
+from demisto_sdk.commands.lint.resources.pylint_plugins import base_checker
+
+# You can find documentation about adding new test checker here:
+# http://pylint.pycqa.org/en/latest/how_tos/custom_checkers.html#write-a-checker
+
+
+class TestPrintChecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests the functionality of print checker .
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_print(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - print function exists in the code
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint
+        """
+        _, node_b, _ = astroid.extract_node("""
+            def test_function(): #@
+                print('catch this print') #@
+                return True #@
+        """)
+        assert node_b is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='print-exists',
+                    node=node_b,
+                ),
+        ):
+            self.checker.visit_call(node_b)
+
+    def test_no_print(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - print function does not exists in the code
+        Then:
+            - Ensure that it does not raise any errors, Check that there is no error message.
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_function():  #@
+                return True #@
+        """)
+        assert node_a is not None and node_b is not None
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+
+    def test_print_in_docstr(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - print function exists in the code but only as a comment
+        Then:
+            - Ensure that it does not raise any errors, Check that there is no error message.
+        """
+        node_a = astroid.extract_node("""
+            def test_function():
+                '''this is doc string of print('test') function''' #@
+                return True
+        """)
+        assert node_a is None
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
+
+    def test_number_of_prints(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - print function exists in the code couple of times.
+        Then:
+            - Ensure that it catches all the prints .
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_function():
+                print("first") #@
+                a=1
+                if(a==1):
+                    print("second") #@
+                return True
+        """)
+        assert node_a is not None and node_b is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='print-exists',
+                    node=node_b,
+                ),
+                pylint.testutils.Message(
+                    msg_id='print-exists',
+                    node=node_a,
+                ),
+        ):
+            self.checker.visit_call(node_b)
+            self.checker.visit_call(node_a)
+
+
+class TestSleepChecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests the functionality of Sleep checker .
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_sleep_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - time.sleep(0) exists in the code twice
+            - sleep(0) exists in the code.
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint for each appearance
+        """
+        _, node_a, node_b, node_c, _ = astroid.extract_node("""
+            def test_function(): #@
+                a=9
+                time.sleep(60) #@
+                time.sleep(a) #@
+                sleep(100) #@
+                return True #@
+        """)
+        assert node_b is not None and node_a is not None and node_c is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='sleep-exists',
+                    node=node_a,
+                ),
+                pylint.testutils.Message(
+                    msg_id='sleep-exists',
+                    node=node_b,
+                ),
+                pylint.testutils.Message(
+                    msg_id='sleep-exists',
+                    node=node_c,
+                ),
+        ):
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+            self.checker.visit_call(node_c)
+
+    def test_no_sleep(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - sleep does not exists in the code .
+        Then:
+            - Ensure that there is no errors, Check that there is no error message.
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_function(): #@
+                return True #@
+        """)
+        assert node_a is not None and node_b is not None
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+
+
+class TestExitChecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests the functionality of exit checker .
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_exit_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - exit() exists in the code.
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint for each appearance
+        """
+        _, node_a, node_b, = astroid.extract_node("""
+            def test_function(): #@
+                if True:
+                    exit() #@
+                return True
+            exit() #@
+        """)
+        assert node_b is not None and node_a is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='exit-exists',
+                    node=node_a,
+                ),
+                pylint.testutils.Message(
+                    msg_id='exit-exists',
+                    node=node_b,
+                ),
+        ):
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+
+    def test_no_exit(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - exit() does not exists in the code .
+        Then:
+            - Ensure that there is no errors, Check that there is no error message.
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_function(): #@
+                return True #@
+        """)
+        assert node_a is not None and node_b is not None
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+
+
+class TestQuithecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests the functionality of quit checker .
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_exit_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - quit() exists in the code.
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint for each appearance
+        """
+        _, node_a = astroid.extract_node("""
+            def test_function(): #@
+                return True
+            quit() #@
+        """)
+        assert node_a is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='quit-exists',
+                    node=node_a,
+                ),
+        ):
+            self.checker.visit_call(node_a)
+
+    def test_no_quit(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - quit() does not exists in the code .
+        Then:
+            - Ensure that there is no errors, Check that there is no error message.
+        """
+        node_a, node_b, node_c = astroid.extract_node("""
+            def test_function(): #@
+                return True #@
+                # quit() #@
+        """)
+        assert node_a is not None and node_b is not None
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
+            self.checker.visit_call(node_b)
+            self.checker.visit_call(node_c)
+
+
+class TestImportCommonServerPythonChecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests the functionality of commonServerPython import checker .
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_valid_common_server_python_import(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - valid import of commonServerPython exists in the code.
+        Then:
+            - Ensure that no being added to the message errors of pylint for each appearance
+        """
+        node_a = astroid.extract_node("""from CommonServerPython import *""")
+        assert node_a
+        with self.assertNoMessages():
+            self.checker.visit_importfrom(node_a)
+
+    def test_invalid_common_server_python_import(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Invalid import of commonServerPython exists in the code.
+        Then:
+            - Ensure that there is no errors, Check that there is no error message.
+        """
+        node_a = astroid.extract_node("""from CommonServerPython import DemistoException""")
+        assert node_a
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='invalid-import-common-server-python',
+                    node=node_a,
+                ),
+        ):
+            self.checker.visit_importfrom(node_a)
+
+
+class TestCommandResultsIndicatorsChecker(pylint.testutils.CheckerTestCase):
+    """
+    """
+    CHECKER_CLASS = base_checker.CustomBaseChecker
+
+    def test_indicators_exist(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Invalid use of indicators inside of CommandResults in the code.
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint for each appearance
+
+        """
+        node_a = astroid.extract_node("""CommandResults(name=name,test=test,indicators=indicators)""")
+        assert node_a
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='commandresults-indicators-exists',
+                    node=node_a,
+                ),
+        ):
+            self.checker.visit_call(node_a)
+
+    def test_indicators_doesnt_exist(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - No use of  indicators inside of CommandResults in the code.
+            - Use of indicator instead of indicators inside of CommandResults.
+        Then:
+            - Ensure that there is no errors, Check that there is no error message.
+        """
+        node_a = astroid.extract_node("""CommandResults(name=name,test=test,indicator=indicators)""")
+        assert node_a
+        with self.assertNoMessages():
+            self.checker.visit_call(node_a)
---
+++ packages/demisto-sdk/demisto-sdk-1.2.13.tar.gz/demisto-sdk-1.2.13/demisto_sdk/commands/lint/tests/test_pylint_plugin/xsoar_level_checker_test.py
@@ -0,0 +1,191 @@
+import astroid
+import pylint.testutils
+from demisto_sdk.commands.lint.resources.pylint_plugins import \
+    xsoar_level_checker
+
+# You can find documentation about adding new test checker here:
+# http://pylint.pycqa.org/en/latest/how_tos/custom_checkers.html#write-a-checker
+
+
+class TestTypeAnnotationsChecker(pylint.testutils.CheckerTestCase):
+    """
+    Class which tests that all functions have type annotations .
+    """
+    CHECKER_CLASS = xsoar_level_checker.XsoarChecker
+
+    def test_type_annotations_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - return_output exists in the code
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_num1(a: str, b:int) ->str: #@
+                '''
+                function docs
+                '''
+                return "test function"
+            def test_num2(a: bool, b: bool, c:int) -> bool: #@
+                '''
+                function docs
+                '''
+                if a:
+                    return True
+                if b:
+                    return False
+                else:
+                    return None
+        """)
+        assert node_b is not None and node_a is not None
+        with self.assertNoMessages():
+            self.checker.visit_functiondef(node_a)
+            self.checker.visit_functiondef(node_b)
+
+    def test_args_annotations_doesnt_exist(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Two given function, One does not have type annotations and the other does.
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint to the relevent function.
+        """
+        node_a, node_b = astroid.extract_node("""
+            def test_num1(a: str, b:int) ->str: #@
+                '''
+                function docs
+                '''
+                return "test function"
+            def test_num2(a, b: bool, c:int) -> bool: #@
+                '''
+                function docs
+                '''
+                if a:
+                    return True
+                if b:
+                    return False
+                else:
+                    return None
+        """)
+        assert node_b is not None and node_a is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='missing-arg-type-annoation',
+                    node=node_b,
+                ),
+        ):
+            self.checker.visit_functiondef(node_a)
+            self.checker.visit_functiondef(node_b)
+
+        node_a, node_b = astroid.extract_node("""
+            def test_num1(a, b) ->str: #@
+                '''
+                function docs
+                '''
+                return "test function"
+            def test_num2(a, b: bool, c:int) -> bool: #@
+                '''
+                function docs
+                '''
+                if a:
+                    return True
+                if b:
+                    return False
+                else:
+                    return None
+        """)
+
+        assert node_b is not None and node_a is not None
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='missing-arg-type-annoation',
+                    node=node_a,
+                ),
+                pylint.testutils.Message(
+                    msg_id='missing-arg-type-annoation',
+                    node=node_b,
+                ),
+        ):
+            self.checker.visit_functiondef(node_a)
+            self.checker.visit_functiondef(node_b)
+
+    def test_not_implemented_error_doesnt_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Main function that in the else claus doesnt raises a NotImplementedError but raises a DemistoError
+            - Main function that in the if claus raises a ValueError
+        Then:
+            - Ensure that the correct message id is being added to the message errors of pylint to the relevent function.
+        """
+        node = astroid.extract_node("""
+            def main() -> bool:
+                if True:
+                    return True
+                if b:
+                    raise ValueError("this is an error")
+                else:
+                    raise DemistoError("this is an error")
+        """)
+        with self.assertAddsMessages(
+                pylint.testutils.Message(
+                    msg_id='not-implemented-error-doesnt-exist',
+                    node=node,
+                ),
+        ):
+            self.checker.visit_functiondef(node)
+
+    def test_not_implemented_error_exists(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Main function that in the else claus raises a NotImplementedError
+        Then:
+            - Ensure that the there was not message added to the checker.
+        """
+        node = astroid.extract_node("""
+            def main() -> bool:
+                try:
+                    if True:
+                        return True
+                    if b:
+                        return False
+                    else:
+                        raise NotImplementedError("this command wasnt implemented")
+                except Exception:
+                    pass
+        """)
+        assert node is not None
+        with self.assertNoMessages():
+            self.checker.visit_functiondef(node)
+
+    def test_not_implemented_error_doesnt_exists_on_Script(self):
+        """
+        Given:
+            - String of a code part which is being examined by pylint plugin.
+        When:
+            - Main function that in the else claus raises a NotImplementedError but its in a script path
+        Then:
+            - Ensure that the there was not message added to the checker.
+        """
+        self.checker.is_script = True
+        node = astroid.extract_node("""
+            def main() -> bool:
+                try:
+                    if True:
+                        return True
+                    if b:
+                        return False
+                    else:
+                        raise DemistoException("this command wasnt implemented")
+                except Exception:
+                    pass
+        """)
+        assert node is not None
+        with self.assertNoMessages():
+            self.checker.visit_functiondef(node)
