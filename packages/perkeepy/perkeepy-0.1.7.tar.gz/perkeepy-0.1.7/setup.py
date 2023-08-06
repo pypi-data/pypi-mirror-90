58c3f239954551ca6e9fb8fd5494899442f70e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:27 +0000

    Add package perkeepy/0.1.7/perkeepy-0.1.7.tar.gz

---
+++ packages/perkeepy/perkeepy-0.1.7.tar.gz/perkeepy-0.1.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description: str = f.read()
+
+setup(
+    name="perkeepy",
+    version="0.1.7",
+    author="Alexandre Viau",
+    author_email="alexandre@alexandreviau.net",
+    description="Python utilities for Perkeep",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/aviau/perkeepy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+)
