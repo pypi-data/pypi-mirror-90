2fd8edb36f304a63a838d83d9dbf6c3cc0a89465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:55 +0000

    Add package turbo-lazy/0.2.1/turbo_lazy-0.2.1.tar.gz

---
+++ packages/turbo-lazy/turbo_lazy-0.2.1.tar.gz/turbo_lazy-0.2.1/tests/test_lazy.py
@@ -0,0 +1,10 @@
+from turbo_lazy.templatetags.lazy import is_int
+
+
+def test_is_int_with_int():
+    assert is_int(123)
+    assert is_int("123")
+
+
+def test_is_int_with_string():
+    assert not is_int("Hello")
