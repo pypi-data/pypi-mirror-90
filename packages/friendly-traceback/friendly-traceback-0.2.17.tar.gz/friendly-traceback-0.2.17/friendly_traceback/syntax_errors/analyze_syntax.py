95acbbfd6adbc63bba1b88e2d8fd139e0939e074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package friendly-traceback/0.2.17/friendly-traceback-0.2.17.tar.gz

---
+++ packages/friendly-traceback/friendly-traceback-0.2.17.tar.gz/friendly-traceback-0.2.17/friendly_traceback/syntax_errors/analyze_syntax.py
@@ -0,0 +1,169 @@
+"""analyze_syntax.py
+
+Collection of functions useful attempting to determine the
+cause of a SyntaxError and providing a somewhat detailed explanation.
+
+In an ideal world, one would write a custom parser for Python, extending
+the existing one with enhanced error messages about where in the parsing
+process a SyntaxError occurred, what kind of token was expected, etc.,
+and use that information to give feedback to users.
+
+Unfortunately, we do not live in such a world.
+
+Friendly-traceback uses some ad-hoc heuristics to analyze the information
+given by Python or the code itself and makes an attempt at guessing
+as often as possible what went wrong while trying to avoid giving
+incorrect information.
+"""
+
+from friendly_traceback.my_gettext import current_lang
+from friendly_traceback.source_cache import cache
+from . import source_analyzer
+from . import line_analyzer
+from . import statement_analyzer
+from . import message_analyzer
+from .. import debug_helper
+from .. import token_utils
+
+# TODO invalid digit '8' in octal literal
+
+
+def set_cause_syntax(value, tb_data):
+    """Gets the likely cause of a given exception based on some information
+    specific to a given exception.
+    """
+    _ = current_lang.translate
+    hint = None
+    try:
+        cause, hint = find_syntax_error_cause(value, tb_data)
+    except Exception as e:
+        debug_helper.log_error(e)
+        cause = _(
+            "Exception raised by Friendly-traceback itself.\n"
+            "Please report this example to\n"
+            "https://github.com/aroberge/friendly-traceback/issues\n"
+        )
+    return cause, hint
+
+
+def find_syntax_error_cause(value, tb_data):
+    """Attempts to find the cause of a SyntaxError"""
+    # value = tb_data.value
+    _ = current_lang.translate
+
+    filepath = value.filename
+    linenumber = value.lineno
+    offset = value.offset
+    message = value.msg
+    source_lines = cache.get_source_lines(filepath)
+
+    statement = tb_data.statement
+    if "invalid syntax" in message:
+        cause, hint = statement_analyzer.analyze_statement(statement)
+        if cause is not None:
+            return cause, hint
+
+    # SyntaxError in f-strings are handled differently by Python
+    # than other types of errors. They are effectively handled internally
+    # as special files.  Before Python 3.9, Python will tell us
+    # that we have a
+    #   SyntaxError: invalid syntax
+    # that the error occurred in file <fstring>  (which is not a real file).
+    #
+    # Starting with Python 3.9, Python will indicates:
+    #     SyntaxError: f-string: invalid syntax
+    # but will give us the real name of the file.
+    #
+    # So, given this line with a SyntaxError:
+    #   f"{x y}"
+    # the traceback will indicate that the error is on line
+    #   (x y)
+    # which does not correspond to an actual line in the source file
+    # that we can retrieve when using Python 3.9.
+
+    if source_lines and "f-string: invalid syntax" not in message:
+        if linenumber is None:  # can happen in some rare cases where Python
+            # apparently gives up processing a file
+            if "too many statically nested blocks" not in message:
+                debug_helper.log(
+                    "linenumber is None in analyze_syntax._find_likely_cause"
+                )
+            offending_line = "\n"
+            source_lines = ["\n"]
+        else:
+            offending_line = source_lines[linenumber - 1]
+    else:
+        offending_line = tb_data.bad_line
+        source_lines = [offending_line]  # create a fake file for analysis
+    line = offending_line.rstrip()
+
+    # If Python includes a descriptive enough message, we rely
+    # on the information that it provides. We know that sometimes
+    # this will yield to the wrong diagnostic but one of our objectives
+    # is to explain in simpler language what Python means when it
+    # raises a particular exception.
+
+    if "invalid syntax" not in message:
+        cause, hint = message_analyzer.analyze_message(
+            message=message,
+            statement=statement,
+            line=line,
+            linenumber=linenumber,
+            source_lines=source_lines,
+            offset=offset,
+        )
+        if cause:
+            return cause, hint
+        else:
+            notice = _(
+                "Python gave us the following informative message\n"
+                "about the possible cause of the error:\n\n"
+                "    {message}\n\n"
+                "However, I do not recognize this information and I have\n"
+                "to guess what caused the problem, but I might be wrong.\n\n"
+            ).format(message=message)
+    else:
+        notice = _(
+            "I make an effort below to guess what caused the problem\n"
+            "but I might guess incorrectly.\n\n"
+        )
+
+    try:
+        tokens, brackets, end_bracket, bad_token = source_analyzer.isolate_bad_statement(
+            source_lines=source_lines, linenumber=linenumber, offset=offset
+        )
+        statement = token_utils.untokenize(tokens)  # noqa
+    except Exception as e:
+        debug_helper.log_error(e)
+
+    # If not cause has been identified, we look at a single line
+    # where the error has been found by Python, and try to find the source
+    # of the error
+
+    cause, hint = line_analyzer.analyze_last_line(line, offset=offset)
+    if cause:
+        return notice + cause, hint
+
+    # TODO: check to see if the offset corresponds to the first token
+    # of a line; if so, the error might be found by looking at the
+    # previous line.
+
+    # Failing that, we look for another type of common mistake. Note that
+    # while we look for missing or mismatched brackets, such as (],
+    # we also can sometimes identify other problems during this step.
+
+    cause = source_analyzer.scan_source(source_lines, linenumber, offset)
+    if cause:
+        return notice + cause, hint
+
+    cause = _(
+        "Currently, I cannot guess the likely cause of this error.\n"
+        "Try to examine closely the line indicated as well as the line\n"
+        "immediately above to see if you can identify some misspelled\n"
+        "word, or missing symbols, like (, ), [, ], :, etc.\n"
+        "\n"
+        "You might want to report this case to\n"
+        "https://github.com/aroberge/friendly-traceback/issues\n"
+        "\n"
+    )
+    return cause, hint
---
+++ packages/friendly-traceback/friendly-traceback-0.2.17.tar.gz/friendly-traceback-0.2.17/friendly_traceback/syntax_errors/statement_analyzer.py
@@ -0,0 +1,444 @@
+"""This module contains functions that are used to
+   analyze a single statement which has been identified
+   as containing a syntax error with the message "invalid syntax".
+"""
+import sys
+
+from . import fixers
+from ..my_gettext import current_lang
+from .. import debug_helper
+
+STATEMENT_ANALYZERS = []
+
+
+def add_statement_analyzer(func):
+    """A simple decorator that adds a function to the list
+    of all functions that analyze a single statement."""
+    STATEMENT_ANALYZERS.append(func)
+
+    def wrapper(tokens, offset=None):
+        return func(tokens, offset=offset)
+
+    return wrapper
+
+
+# ========================================================
+# Main calling function
+# ========================================================
+
+
+def analyze_statement(statement):
+    """Analyzes the statement as identified by Python as that
+    on which the error occurred."""
+    if not statement.tokens:
+        debug_helper.log("Statement with no tokens in statement_analyser.py")
+        return None, None
+
+    for analyzer in STATEMENT_ANALYZERS:
+        cause, hint = analyzer(statement)
+        if cause:
+            return cause, hint
+    return None, None
+
+
+# ==================
+# IMPORTANT: causes are looked at in the same order as they appear below.
+# Changing the order could possibly yield incorrect results
+# ==================
+
+
+@add_statement_analyzer
+def copy_pasted_code(statement):
+    """Detecting code that starts with a Python prompt"""
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.nb_tokens < 2:
+        return cause, hint
+
+    tokens = statement.tokens
+    if tokens[0] == ">>" and tokens[1] == ">":
+        cause = _(
+            "It looks like you copy-pasted code from an interactive interpreter.\n"
+            "The Python prompt, `>>>`, should not be included in your code.\n"
+        )
+        hint = _("Did you use copy-paste?\n")
+    return cause, hint
+
+
+@add_statement_analyzer
+def detect_walrus(statement):
+    """Detecting if code uses named assignment operator := with an
+    older version of Python.
+    """
+    _ = current_lang.translate
+    cause = hint = None
+    if sys.version_info >= (3, 8):
+        return cause, hint
+
+    # Normally, the token identified as the bad token should be
+    # '='; however, in some test cases where a named assignment
+    # is not allowed, it is ':' that is identified as the
+    # bad token.
+
+    bad = statement.bad_token
+    prev = statement.prev_token
+    next_token = statement.next_token
+
+    if (prev == ":" and bad == "=" and bad.immediately_after(prev)) or (
+        bad == ":" and next_token == "=" and bad.immediately_before(next_token)
+    ):
+        hint = _("Your Python version does not support this f-string feature.\n")
+        cause = _(
+            "You appear to be using the operator `:=`, sometimes called\n"
+            "the walrus operator. This operator requires the use of\n"
+            "Python 3.8 or newer. You are using version {version}.\n"
+        ).format(version=f"{sys.version_info.major}.{sys.version_info.minor}")
+
+    return cause, hint
+
+
+@add_statement_analyzer
+def debug_fstring(statement):
+    """detect debug feature of f-string introduced in Python 3.8"""
+    _ = current_lang.translate
+    cause = hint = None
+    if sys.version_info >= (3, 8) or not statement.fstring_error:
+        return cause, hint
+
+    if statement.bad_token == "=" and statement.prev_token.is_identifier():
+        if statement.next_token == ")":
+            hint = _("Your Python version does not support this f-string feature.\n")
+            cause = _(
+                "You are likely using a 'debug' syntax of f-strings introduced\n"
+                "in Python version 3.8. You are using version {version}.\n"
+            ).format(version=f"{sys.version_info.major}.{sys.version_info.minor}")
+        else:
+            cause = _(
+                "You are likely trying to assign a value within an f-string.\n"
+                "This is not allowed.\n"
+            )
+    return cause, hint
+
+
+@add_statement_analyzer
+def detect_backquote(statement):
+    """Detecting if the error is due to using `x` which was allowed
+    in Python 2.
+    """
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.bad_token == "`":
+        hint = _("You should not use the backquote character.\n")
+        cause = _(
+            "You are using the backquote character.\n"
+            "Either you meant to write a single quote, ', "
+            "or copied Python 2 code;\n"
+            "in this latter case, use the function `repr(x)`."
+        )
+    return cause, hint
+
+
+@add_statement_analyzer
+def assign_to_a_keyword(statement):
+    """Checks to see if line is of the form 'keyword = ...'"""
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.bad_token == "=" and statement.prev_token.is_keyword():
+        cause = _(
+            "You were trying to assign a value to the Python keyword `{keyword}`.\n"
+            "This is not allowed.\n"
+            "\n"
+        ).format(keyword=statement.prev_token)
+    return cause, hint
+
+
+@add_statement_analyzer
+def confused_elif(statement):
+    _ = current_lang.translate
+    cause = hint = None
+    name = None
+    if statement.bad_token == "elseif" or statement.prev_token == "elseif":
+        name = "elseif"
+    elif statement.bad_token == "if" and statement.prev_token == "else":
+        name = "else if"
+    if name:
+        hint = _("Perhaps you meant to write `elif`.\n")
+        cause = _(
+            "You likely meant to use Python's `elif` keyword\n"
+            "but wrote `{name}` instead\n"
+            "\n"
+        ).format(name=name)
+    return cause, hint
+
+
+@add_statement_analyzer
+def import_from(statement):
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.bad_token != "from" or statement.tokens[0] != "import":
+        return cause, hint
+
+    function = statement.prev_token
+    module = statement.next_token
+
+    hint = _("Did you mean `from {module} import {function}`?\n").format(
+        module=module, function=function
+    )
+    cause = _(
+        "You wrote something like\n\n"
+        "    import {function} from {module}\n"
+        "instead of\n\n"
+        "    from {module} import {function}\n\n"
+        "\n"
+    ).format(module=module, function=function)
+    return cause, hint
+
+
+@add_statement_analyzer
+def keyword_as_attribute(statement):
+    """Will identify something like  obj.True ..."""
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.prev_token != ".":
+        return cause, hint
+
+    word = statement.bad_token
+    if word.is_keyword():
+        cause = _(
+            "You cannot use the Python keyword `{word}` as an attribute.\n\n"
+        ).format(word=word)
+    elif word == "__debug__":
+        cause = _("You cannot use the constant `__debug__` as an attribute.\n\n")
+
+    if cause is not None:
+        hint = _("`{word}` cannot be used as an attribute.\n").format(word=word)
+
+    return cause, hint
+
+
+@add_statement_analyzer
+def misplaced_quote(statement):
+    """This looks for a misplaced quote, something like
+       info = 'don't ...
+
+    The clue we are looking for is a STRING token ('don')
+    followed by something else than a string.
+    """
+    _ = current_lang.translate
+    cause = hint = None
+
+    if not statement.prev_token.is_string():
+        return cause, hint
+
+    bad_token = statement.bad_token
+    if bad_token.is_identifier():
+        bad_string = statement.prev_token.string
+        for fn in [int, float, complex]:
+            try:
+                fn(bad_string)  # Definitely not a word!
+                return cause, hint
+            except Exception:
+                pass
+
+        hint = _("Perhaps you misplaced a quote.\n")
+        cause = _(
+            "There appears to be a Python identifier (variable name)\n"
+            "immediately following a string.\n"
+            "I suspect that you were trying to use a quote inside a string\n"
+            "that was enclosed in quotes of the same kind.\n"
+        )
+
+    return cause, hint
+
+
+@add_statement_analyzer
+def assign_instead_of_equal(statement):
+    """Checks to see if an assignment sign, '=', has been used instead of
+    an equal sign, '==', in an if or elif statement."""
+    _ = current_lang.translate
+    cause = hint = None
+
+    if not statement.bad_token == "=":
+        return cause, hint
+
+    if not statement.first_token.is_in(["if", "elif", "while"]):
+        return cause, hint
+
+    new_statement = fixers.modify_source(statement.tokens, statement.bad_token, "==")
+    if not fixers.check_statement(new_statement):
+        # TODO: find a way to confirm that new error is later.
+        debug_helper.log("Fix did not work in assign_instead_of_equal")
+        additional_cause = _(
+            "However, there might be some other errors in this statement.\n"
+        )
+    else:
+        additional_cause = ""
+
+    equal = _("Perhaps you needed `==` instead of `=`.\n")
+    equal_or_walrus = _("Perhaps you needed `==` or `:=` instead of `=`.\n")
+    if sys.version_info < (3, 8):
+        cause = _(
+            "You used an assignment operator `=` instead of an equality operator `==`.\n"
+        )
+        hint = equal
+    else:
+        cause = _(
+            "You used an assignment operator `=`; perhaps you meant to use \n"
+            "an equality operator, `==`, or the walrus operator `:=`.\n"
+        )
+        hint = equal_or_walrus
+
+    return cause + additional_cause, hint
+
+
+@add_statement_analyzer
+def print_as_statement(statement):
+    _ = current_lang.translate
+    cause = hint = None
+
+    if statement.prev_token != "print":
+        return cause, hint
+
+    if statement.bad_token != "(":
+        cause = _(
+            "In older version of Python, `print` was a keyword.\n"
+            "Now, `print` is a function; you need to use parentheses to call it.\n"
+        )
+    return cause, hint
+
+
+@add_statement_analyzer
+def calling_pip(statement):
+    _ = current_lang.translate
+    cause = hint = None
+    if not statement.first_token.is_in(["pip", "python"]):
+        return cause, hint
+
+    use_pip = _(
+        "It looks as if you are attempting to use pip to install a module.\n"
+        "`pip` is a command that needs to run in a terminal,\n"
+        "not from a Python interpreter.\n"
+    )
+
+    for tok in statement.tokens:
+        if tok == "pip":
+            hint = _("Pip cannot be used in a Python interpreter.\n")
+            return use_pip, hint
+    return cause, hint
+
+
+@add_statement_analyzer
+def dot_followed_by_bracket(statement):
+    _ = current_lang.translate
+    cause = hint = None
+
+    if statement.bad_token.is_in("()[]{}") and statement.prev_token == ".":
+        cause = _("You cannot have a dot `.` followed by `{bracket}`.\n").format(
+            bracket=statement.bad_token
+        )
+
+    new_statement = fixers.modify_source(statement.tokens, statement.prev_token, ",")
+    if fixers.check_statement(new_statement):
+        cause += _("Perhaps you need to replace the dot by a comma.\n")
+        return cause, hint
+
+    return cause, hint
+
+
+@add_statement_analyzer
+def raise_single_exception(statement):
+    _ = current_lang.translate
+    cause = hint = None
+    if statement.first_token != "raise":
+        return cause, hint
+
+    if statement.bad_token == "," and statement.prev_token.is_identifier():
+        cause = _(
+            "It looks like you are trying to raise an exception using Python 2 syntax.\n"
+        )
+    return cause, hint
+
+
+@add_statement_analyzer
+def invalid_double_star_operator(statement):
+    _ = current_lang.translate
+    cause = hint = None
+
+    if statement.bad_token == "**":
+        cause = _(
+            "The double star operator `**` is likely interpreted to mean that\n"
+            "dict unpacking is to be used which does not make sense here.\n"
+        )
+
+    return cause, hint
+
+
+@add_statement_analyzer
+def missing_colon(statement):
+    """look for missing colon at the end of statement"""
+    _ = current_lang.translate
+    cause = hint = None
+
+    if statement.last_token == ":" or statement.bad_token != statement.last_token:
+        return cause, hint
+
+    name = statement.first_token
+    if name.is_not_in(
+        [
+            "class",
+            "def",
+            "if",
+            "elif",
+            "else",
+            "for",
+            "while",
+            "try",
+            "except",
+            "finally",
+            "with",
+        ]
+    ):
+        return cause, hint
+
+    new_statement = fixers.modify_source(
+        statement.tokens, statement.bad_token, ":", add=True
+    )
+    if not fixers.check_statement(new_statement):
+        return cause, hint
+
+    name = statement.first_token
+
+    forgot_a_colon = _("Did you forget a colon `:`?\n")
+
+    if name.is_in(["for", "while"]):
+        cause = _(
+            "You wrote a `{for_while}` loop but\n"
+            "forgot to add a colon `:` at the end\n"
+            "\n"
+        ).format(for_while=name)
+        hint = forgot_a_colon
+    elif name.is_in(["def", "elif", "else", "except", "finally", "if", "try", "with"]):
+        cause = _(
+            "You wrote a statement beginning with\n"
+            "`{name}` but forgot to add a colon `:` at the end\n"
+            "\n"
+        ).format(name=name)
+        hint = forgot_a_colon
+
+    return cause, hint
+
+
+# # --------- Keep this last
+# @add_statement_analyzer
+# def general_fstring_problem(statement=None):
+#     # General f-string problems are outside of our main priorities.
+#     _ = current_lang.translate
+#     cause = hint = None
+#     if not statement.fstring_error:
+#         return cause, hint
+#
+#     cause = _(
+#         "The content of your f-string is invalid. Please consult the documentation:\n"
+#         "https://docs.python.org/3/reference/lexical_analysis.html#f-strings\n"
+#     )
+#     return cause, hint
