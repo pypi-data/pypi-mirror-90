5291c62dd034052169b07139bc5c9ab17229eaca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:32 +0000

    Add package hpcomt/0.1/hpcomt-0.1.tar.gz

---
+++ packages/hpcomt/hpcomt-0.1.tar.gz/hpcomt-0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+packages = ['hpcomt']
+
+requires = [
+    'platform'
+]
+
+setuptools.setup(
+    name="hpcomt",
+    version="0.1",
+    author="Honey Pots",
+    author_email="honeypots124@gmail.com",
+    description="Detect OS Name, E.g. 'Android', 'Linux', 'Windows' or 'Java' , Etc. Specially For Android",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/honeypots0/hpcomt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+) 
