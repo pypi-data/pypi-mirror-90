eed74eb40d61fa1651ca4ebf4d0bc23e5d9b5d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:42 +0000

    Add package pwork/0.0.2/pwork-0.0.2.tar.gz

---
+++ packages/pwork/pwork-0.0.2.tar.gz/pwork-0.0.2/pwork/__main__.py
@@ -0,0 +1,4 @@
+from pwork.app import main
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pwork/pwork-0.0.2.tar.gz/pwork-0.0.2/pwork/app.py
@@ -0,0 +1,10 @@
+def main():
+    print("please input such script to start:")
+    print("1. cookiecutter https://github.com/gaianote/python-framework")
+    print("2. pipenv shell")
+    print("3. pipenv install --dev")
+    print('4. pip install -e ".[dev]"')
+
+
+if __name__ == "__main__":
+    main()
