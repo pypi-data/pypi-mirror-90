72c4cc3b2759e4d958b641942d40f92b576a7c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:42 +0000

    Add package pytanga/1.0.9/pytanga-1.0.9.tar.gz

---
+++ packages/pytanga/pytanga-1.0.9.tar.gz/pytanga-1.0.9/pytanga/components/Cisco/xe/__init__.py
@@ -0,0 +1,5 @@
+from .native import nativeComponent
+from .router import routerComponent
+from .rollback import rollbackComponent
+from .saveconfig import saveConfigComponent
+from .checkpoint import checkpointComponent
