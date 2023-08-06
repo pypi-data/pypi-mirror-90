e5c2a4341a1f603c990ab30e5219581ef3cc2658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:48 +0000

    Add package yandex-cloud-client/1.0.4/yandex_cloud_client-1.0.4-py3.6.egg

---
+++ packages/yandex-cloud-client/yandex_cloud_client-1.0.4-py3.6.egg/yandex_cloud_client/utils/endpoints.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This module contains base endpoint urls."""
+
+BASE_URL = 'https://api.cloud.yandex.net'
+RESOURCE_MANAGER_URL = 'https://resource-manager.api.cloud.yandex.net'
+IAM_URL = 'https://iam.api.cloud.yandex.net'
+COMPUTE_URL = 'https://compute.api.cloud.yandex.net'
+OPERATION_URL = 'https://operation.api.cloud.yandex.net'
+CERTIFICATE_URL = 'https://certificate-manager.api.cloud.yandex.net'
+CERTIFICATE_DATA_URL = 'https://data.certificate-manager.api.cloud.yandex.net'
