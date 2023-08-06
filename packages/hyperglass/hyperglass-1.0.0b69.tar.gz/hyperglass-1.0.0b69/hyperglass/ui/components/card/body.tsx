fc906ba2b1cfa1f6f5e6edc6e8de9dfe4d93b093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:54 +0000

    Add package hyperglass/1.0.0b69/hyperglass-1.0.0b69.tar.gz

---
+++ packages/hyperglass/hyperglass-1.0.0b69.tar.gz/hyperglass-1.0.0b69/hyperglass/ui/components/card/body.tsx
@@ -0,0 +1,23 @@
+import { Flex } from '@chakra-ui/react';
+import { useColorValue } from '~/context';
+
+import type { ICardBody } from './types';
+
+export const CardBody = (props: ICardBody) => {
+  const { onClick, ...rest } = props;
+  const bg = useColorValue('white', 'dark.500');
+  const color = useColorValue('dark.500', 'white');
+  return (
+    <Flex
+      bg={bg}
+      w="100%"
+      rounded="md"
+      color={color}
+      onClick={onClick}
+      overflow="hidden"
+      borderWidth="1px"
+      direction="column"
+      {...rest}
+    />
+  );
+};
