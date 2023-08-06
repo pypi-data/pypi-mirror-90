6e2ca1747a8e8a644e24620252c6c9c89f877bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:27 +0000

    Add package MathLibrary/17.0/MathLibrary-17.0.tar.gz

---
+++ packages/MathLibrary/MathLibrary-17.0.tar.gz/MathLibrary-17.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name = 'MathLibrary',
+    version = '17.0',
+    download_url = 'https://github.com/abdulsamedkeskin/MathLibrary/archive/V17.tar.gz'
+)
