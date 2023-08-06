ce95e39a5dacb1148454da95673af140f5e9eedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:15 +0000

    Add package cppiniter/1.0.1/cppiniter-1.0.1.tar.gz

---
+++ packages/cppiniter/cppiniter-1.0.1.tar.gz/cppiniter-1.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from shutil import make_archive
+import os
+import sys
+
+setup(
+    name='cppiniter',
+    version="1.0.1",
+    description="C++项目脚手架",
+    long_description="""用于提供C++项目开发环境的初始化""",
+    keywords='c++ scaffolding',
+    author='xyz1001',
+    author_email='zgzf1001@gmail.com',
+    url='https://github.com/xyz1001/cppiniter',
+    license='MIT',
+    include_package_data=True,
+    package_dir={
+        'cppiniter': 'cppiniter',
+    },
+    packages=[
+        'cppiniter',
+    ],
+    install_requires=['pystache', 'docopt', 'inquirer'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points={'console_scripts': [
+        'cppiniter = cppiniter.cppiniter:main',
+    ]},
+)
