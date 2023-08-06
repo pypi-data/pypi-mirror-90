dafeaf62460193a30dd49a1944e6f09ca0e0cdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:07 +0000

    Add package diagnose/3.1.3/diagnose-3.1.3.tar.gz

---
+++ packages/diagnose/diagnose-3.1.3.tar.gz/diagnose-3.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+ext_modules = []
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="diagnose",
+    version="3.1.3",
+    author="Robert Brewer",
+    author_email="dev@crunch.io",
+    description="A library for instrumenting Python code at runtime.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Crunch-io/diagnose",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["hunter>=2.2.0", "mock", "six",],
+    ext_modules=ext_modules,
+    entry_points={},
+)
