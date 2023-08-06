a849dd824522e488b31bc6c27e983087d8053699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:34 +0000

    Add package django-admin-search/0.3.6/django-admin-search-0.3.6.tar.gz

---
+++ packages/django-admin-search/django-admin-search-0.3.6.tar.gz/django-admin-search-0.3.6/django_admin_search/admin.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from django.contrib import messages
+from django.contrib.admin import ModelAdmin
+from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+
+from django_admin_search import utils
+
+
+class AdvancedSearchAdmin(ModelAdmin):
+    """
+        class to add custom filters in django admin
+    """
+    change_list_template = 'admin/custom_change_list.html'
+    advanced_search_fields = {}
+    search_form_data = None
+
+    def get_queryset(self, request):
+        """
+            override django admin 'get_queryset'
+        """
+        queryset = super().get_queryset(request)
+        try:
+            return queryset.filter(self.advanced_search_query(request))
+        except Exception:
+            messages.add_message(request, messages.ERROR, 'Filter not applied, error has occurred')
+            return queryset.none()
+
+    def changelist_view(self, request, extra_context=None):
+        self.advanced_search_fields = {}
+        self.search_form_data = self.search_form(request.GET)
+        self.extract_advanced_search_terms(request.GET)
+        extra_context = {'asf': self.search_form_data}
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def extract_advanced_search_terms(self, request):
+        request._mutable = True  # pylint: disable=W0212
+
+        if self.search_form_data is not None:
+            for key in self.search_form_data.fields.keys():
+                temp = request.pop(key, None)
+                if temp:  # there is a field but it's empty so it's useless
+                    self.advanced_search_fields[key] = temp
+
+        request._mutable = False  # pylint: disable=W0212
+
+    def get_field_value(self, field):
+        """
+            check if field has value passed on request
+        """
+        if field in self.advanced_search_fields:
+            return True, self.advanced_search_fields[field][0]
+
+        return False, None
+
+    def get_field_value_override(self, field, field_value, form_field, request):
+        """
+            allow to override default field query
+        """
+        if hasattr(self, ('search_' + field)):
+            return getattr(self, 'search_' + field)(field, field_value, form_field, request,
+                                                    self.advanced_search_fields)
+        return Q()
+
+    @staticmethod
+    def get_field_value_default(field, form_field, field_value, has_field_value, request):
+        """
+            mount default field value
+        """
+        if has_field_value:
+            field_name = form_field.widget.attrs.get('filter_field', field)
+            field_filter = field_name + form_field.widget.attrs.get('filter_method', '')
+
+            try:
+                field_value = utils.format_data(form_field, field_value)  # format by field type
+                return Q(**{field_filter: field_value})
+            except ValidationError:
+                messages.add_message(request, messages.ERROR, _(f"Filter in field `{field_name}` "
+                                                                "ignored, because value "
+                                                                f"`{field_value}` isn't valid"))
+            except Exception:
+                messages.add_message(request, messages.ERROR, _(f"Filter in field `{field_name}` "
+                                                                "ignored, error has occurred."))
+
+        return Q()
+
+    def advanced_search_query(self, request):
+        """
+            Get form and mount filter query if form is not none
+        """
+        query = Q()
+
+        if self.search_form_data is None:
+            return query
+
+        for field, form_field in self.search_form_data.fields.items():
+            has_field_value, field_value = self.get_field_value(field)
+
+            if hasattr(self, ('search_' + field)):
+                query &= self.get_field_value_override(field, field_value, form_field, request)
+            else:
+                query &= self.get_field_value_default(field, form_field, field_value, has_field_value, request)
+
+        return query
---
+++ packages/django-admin-search/django-admin-search-0.3.6.tar.gz/django-admin-search-0.3.6/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from io import open
+
+from setuptools import find_packages, setup
+
+from django_admin_search.meta import VERSION
+
+extras_require = {
+    'dev': [
+        'django',
+        'django_mock_queries',
+        'pylint',
+        'pytest-pylint',
+        'pytest',
+        'pytest-cov',
+        'pytest-watch'
+    ]
+}
+
+setup(
+    name='django-admin-search',
+    version=str(VERSION),
+    description='The "Django Admin Search" is a advanced search modal for django admin',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Shinneider Libanio da Silva',
+    author_email='shinneider-libanio@hotmail.com',
+    url='https://github.com/shinneider/django_admin_search',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    python_requires=">=3.3",
+    extras_require=extras_require,
+    install_requires=[
+        'django',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
