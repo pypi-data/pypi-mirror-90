2cfbfc0f6701c6f8592ba2ec66a0020ef297846d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package newsie/0.0.2/newsie-0.0.2-py3-none-any.whl

---
+++ packages/newsie/newsie-0.0.2-py3-none-any.whl/newsie/weather.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+from datetime import datetime, timedelta, timezone
+
+from ambient_api.ambientapi import AmbientAPI
+
+import config
+
+def get_weather():
+
+    weather = {}
+
+    aapi = AmbientAPI(
+        AMBIENT_ENDPOINT = config.AMBIENT_ENDPOINT,
+        AMBIENT_APPLICATION_KEY = config.AMBIENT_APPLICATION_KEY,
+        AMBIENT_API_KEY = config.AMBIENT_API_KEY
+    )
+    devices = aapi.get_devices()
+
+    if len(devices) > 0:
+        device = devices[0]
+        time.sleep(1)
+        device_data = device.get_data()
+        current_conditions = device_data[0]
+
+        weather = {
+            "temp": current_conditions['tempf'],
+            "humidity": current_conditions['humidity'],
+            "pressure": current_conditions['baromrelin'],
+            "windspeed": current_conditions['windspeedmph'],
+            "wind_direction": current_conditions['winddir']
+        }
+    else:
+        weather = {"temp": "69", "humidity": "0"}
+
+    return weather
