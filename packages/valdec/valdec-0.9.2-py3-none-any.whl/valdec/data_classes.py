4239e0223efd8727f6968c66a1f7b4691bb1c8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:49 +0000

    Add package valdec/0.9.2/valdec-0.9.2-py3-none-any.whl

---
+++ packages/valdec/valdec-0.9.2-py3-none-any.whl/valdec/data_classes.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, field
+from typing import Any, Callable
+
+
+@dataclass
+class FieldData:
+
+    name: str
+    value: Any
+    annotation: Any
+
+
+@dataclass
+class Settings:
+    """ Настройки для валидации.
+
+        :function_for_validation: Cсылка на функцию, которая будет
+                                  использоваться непосредственно для валидации.
+                                  Реализация поддерживаемых функций находится
+                                  в файлах val_*.py.
+                                  Можно сделать свою функцию для валидации и
+                                  указать ее в этом поле.
+                                  Функция "по умолчанию" установлена в файле
+                                  dec.py.
+
+        :is_replace_the_args:     Если True, то будет производиться замена
+                                  исходных значений полей аргументов на
+                                  экземпляры классов валидации.
+
+        :is_replace_the_result:   Если True, то будет производиться замена
+                                  значения результата на экземпляры классов
+                                  валидации.
+        :extra:                   Словарь с дополнительными значениями,
+                                  который будет передаваться в
+                                  function_for_validation (например, можно
+                                  передать класс для валидации данных)
+    """
+
+    function_for_validation: Callable
+    is_replace_the_args: bool = True
+    is_replace_the_result: bool = True
+    extra: dict = field(default_factory=dict)
