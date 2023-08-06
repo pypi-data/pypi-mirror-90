60fb6f5b7069551ce5d3faf87734139d6883ad0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:22 +0000

    Add package librecell-common/0.0.8/librecell-common-0.0.8.tar.gz

---
+++ packages/librecell-common/librecell-common-0.0.8.tar.gz/librecell-common-0.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(name='librecell-common',
+      version='0.0.8',
+      description='Common utility functions for LibreCell suite.',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='cmos cell vlsi asic',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Development Status :: 3 - Alpha',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Programming Language :: Python :: 3'
+      ],
+      url='https://codeberg.org/tok/librecell',
+      author='T. Kramer',
+      author_email='code@tkramer.ch',
+      license='GPLv3',
+      packages=find_packages(),
+      install_requires=[
+          'networkx==2.5',  # BSD
+          'pyspice==1.4.3',  # GPLv3
+      ],
+      zip_safe=False)
