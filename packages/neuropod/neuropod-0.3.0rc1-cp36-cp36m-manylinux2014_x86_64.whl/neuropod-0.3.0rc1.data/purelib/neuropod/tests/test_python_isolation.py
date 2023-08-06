3f54fde148cee2cb4cf879b4f23406a4fef2c3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:25 +0000

    Add package neuropod/0.3.0rc1/neuropod-0.3.0rc1-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/neuropod/neuropod-0.3.0rc1-cp36-cp36m-manylinux2014_x86_64.whl/neuropod-0.3.0rc1.data/purelib/neuropod/tests/test_python_isolation.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2020 UATC, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import os
+import unittest
+from testpath.tempdir import TemporaryDirectory
+
+from neuropod.loader import load_neuropod
+from neuropod.packagers import create_python_neuropod
+
+DUMMY_MODEL_SOURCE = """
+import numpy as np
+
+def model(x):
+    return {{
+        "out": np.array([{}], dtype=np.float32)
+    }}
+
+def get_model(_):
+    return model
+"""
+
+
+class TestPythonIsolation(unittest.TestCase):
+    def package_dummy_model(self, test_dir, target):
+        neuropod_path = os.path.join(test_dir, "test_neuropod")
+        model_code_dir = os.path.join(test_dir, "model_code")
+        os.makedirs(model_code_dir)
+
+        with open(os.path.join(model_code_dir, "dummy_model.py"), "w") as f:
+            f.write(DUMMY_MODEL_SOURCE.format(target))
+
+        # `create_python_neuropod` runs inference with the test data immediately
+        # after creating the neuropod. Raises a ValueError if the model output
+        # does not match the expected output.
+        create_python_neuropod(
+            neuropod_path=neuropod_path,
+            model_name="dummy_model",
+            data_paths=[],
+            code_path_spec=[
+                {
+                    "python_root": model_code_dir,
+                    "dirs_to_package": [""],  # Package everything in the python_root
+                }
+            ],
+            entrypoint_package="dummy_model",
+            entrypoint="get_model",
+            input_spec=[{"name": "x", "dtype": "float32", "shape": (1,)}],
+            output_spec=[{"name": "out", "dtype": "float32", "shape": (1,)}],
+            test_input_data={"x": np.array([0.0], dtype=np.float32)},
+            test_expected_out={"out": np.array([target], dtype=np.float32)},
+        )
+
+        return neuropod_path
+
+    def test_model_isolation(self):
+        # Test that we can correctly load two different models with the same entrypoint in the same process
+        with TemporaryDirectory() as test_dir1:
+            with TemporaryDirectory() as test_dir2:
+                path1 = self.package_dummy_model(test_dir1, 1.0)
+                path2 = self.package_dummy_model(test_dir2, 2.0)
+
+                with load_neuropod(path1, _always_use_native=False) as n1:
+                    with load_neuropod(path2, _always_use_native=False) as n2:
+                        input_data = {"x": np.array([0], dtype=np.float32)}
+                        self.assertEqual(n1.infer(input_data)["out"][0], 1.0)
+                        self.assertEqual(n2.infer(input_data)["out"][0], 2.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/neuropod/neuropod-0.3.0rc1-cp36-cp36m-manylinux2014_x86_64.whl/neuropod-0.3.0rc1.data/purelib/neuropod/tests/test_tensorflow_strings.py
@@ -0,0 +1,77 @@
+# Copyright (c) 2020 UATC, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import tensorflow as tf
+import unittest
+from testpath.tempdir import TemporaryDirectory
+
+from neuropod.packagers import create_tensorflow_neuropod
+from neuropod.tests.utils import get_string_concat_model_spec, check_strings_model
+
+
+def create_tf_strings_model():
+    """
+    A model that concatenates two input strings
+    """
+    g = tf.Graph()
+    with g.as_default():
+        with tf.name_scope("some_namespace"):
+            x = tf.placeholder(tf.string, name="in_x")
+            y = tf.placeholder(tf.string, name="in_y")
+
+            # Assigned to a variable for clarity
+            out = tf.string_join([x, y], separator=" ", name="out")  # noqa: F841
+
+    return g.as_graph_def()
+
+
+@unittest.skipIf(tf.__version__[0] == "2", "Skipping TF 1.x tests for TF 2.x")
+class TestTensorflowStrings(unittest.TestCase):
+    def package_strings_model(self, do_fail=False):
+        with TemporaryDirectory() as test_dir:
+            neuropod_path = os.path.join(test_dir, "test_neuropod")
+
+            # `create_tensorflow_neuropod` runs inference with the test data immediately
+            # after creating the neuropod. Raises a ValueError if the model output
+            # does not match the expected output.
+            create_tensorflow_neuropod(
+                neuropod_path=neuropod_path,
+                model_name="strings_model",
+                graph_def=create_tf_strings_model(),
+                node_name_mapping={
+                    "x": "some_namespace/in_x:0",
+                    "y": "some_namespace/in_y:0",
+                    "out": "some_namespace/out:0",
+                },
+                # Get the input/output spec along with test data
+                **get_string_concat_model_spec(do_fail=do_fail)
+            )
+
+            # Run some additional checks
+            check_strings_model(neuropod_path)
+
+    def test_strings_model(self):
+        # Tests a case where packaging works correctly and
+        # the model output matches the expected output
+        self.package_strings_model()
+
+    def test_strings_model_failure(self):
+        # Tests a case where the output does not match the expected output
+        with self.assertRaises(ValueError):
+            self.package_strings_model(do_fail=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
