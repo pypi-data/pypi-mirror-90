c5854adc804af97746f1b05a8f23d854cf6cb8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:19 +0000

    Add package ddnmath/0.0.2/ddnmath-0.0.2.tar.gz

---
+++ packages/ddnmath/ddnmath-0.0.2.tar.gz/ddnmath-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ddnmath",  # Replace with your own username
+    version="0.0.2",
+    author="Davoud Nasrabadi",
+    author_email="davoud.nasrabadi@gmail.com",
+    description="davoud nasrabadi math package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davoudnasrabadi/ddnmath",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
