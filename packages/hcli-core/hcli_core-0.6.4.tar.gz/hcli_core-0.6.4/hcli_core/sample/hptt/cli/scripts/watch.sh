b8d2a74d0576efc37fa795ed7985ab7c6490806f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:23 +0000

    Add package hcli-core/0.6.4/hcli_core-0.6.4.tar.gz

---
+++ packages/hcli-core/hcli_core-0.6.4.tar.gz/hcli_core-0.6.4/hcli_core/sample/hptt/cli/scripts/watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while :
+do
+    status=`curl 'https://hcli.io/hcli/cli/exec/getexecute?command=hptt%20channel%20ptt%20%27default%27'`
+    if [[ "$status" == "active" ]]
+    then
+        hptt channel stream -r 'default' | sox -v 10.0 --ignore-length -t wav - -t wav - | play -
+    fi
+    
+    sleep 1
+done
