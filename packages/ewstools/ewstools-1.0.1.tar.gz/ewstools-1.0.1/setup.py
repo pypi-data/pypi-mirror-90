d50dfe40a6ed184e4313ff2565bb2dc22e024e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:27 +0000

    Add package ewstools/1.0.1/ewstools-1.0.1.tar.gz

---
+++ packages/ewstools/ewstools-1.0.1.tar.gz/ewstools-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ["lmfit>=0.9", "arch>=4.7"]
+
+setuptools.setup(
+    name="ewstools",
+    version="1.0.1",
+    author="Thomas M Bury",
+    author_email="tombury182@gmail.com",
+    description="""Python package to compute early warning signals (EWS) 
+    from time series data""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThomasMBury/ewstools",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
