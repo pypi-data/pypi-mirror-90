702dd788566b949e1a6ba50d92e5e8baf7fa9025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:24 +0000

    Add package edgegallery-sdk-face-recognition/1.4/edgegallery_sdk_face_recognition-1.4-py3.6.egg

---
+++ packages/edgegallery-sdk-face-recognition/edgegallery_sdk_face_recognition-1.4-py3.6.egg/edgegallery_sdk_face_recognition/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from edgegallery_sdk_face_recognition.api.default_api import DefaultApi
+from edgegallery_sdk_face_recognition.api.recognition_api import RecognitionApi
+from edgegallery_sdk_face_recognition.api.upload_api import UploadApi
+from edgegallery_sdk_face_recognition.api.video_api import VideoApi
