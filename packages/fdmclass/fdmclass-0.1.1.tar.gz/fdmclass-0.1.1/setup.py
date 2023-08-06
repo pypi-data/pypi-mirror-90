2a9f5549a06bd8966d3b4d3c54835b73d5fb232e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package fdmclass/0.1.1/fdmclass-0.1.1.tar.gz

---
+++ packages/fdmclass/fdmclass-0.1.1.tar.gz/fdmclass-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fdmclass',
+    version='0.1.1',
+    author='fdmseven',
+    author_email='wrp2003000@163.com',
+    packages=["fdmclass"],
+    url='https://github.com/fdmseven/fdmclass',
+    license='LICENSE.txt',
+    description='independent class',
+    long_description=open('README.md').read(),
+    python_requires='>=3',
+    install_requires=[
+        "fdmutils==0.1.1",
+    ],
+)
