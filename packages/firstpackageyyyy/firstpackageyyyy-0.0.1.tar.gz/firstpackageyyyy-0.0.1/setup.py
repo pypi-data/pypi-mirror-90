c0f738ddc8ab55964691b0a30e1042a58d891f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:19 +0000

    Add package firstpackageyyyy/0.0.1/firstpackageyyyy-0.0.1.tar.gz

---
+++ packages/firstpackageyyyy/firstpackageyyyy-0.0.1.tar.gz/firstpackageyyyy-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='firstpackageyyyy',
+    version='0.0.1',
+    packages=['myapp']
+)
+
+# sdist: source code discribute, format: zip, gztar, bztar, xztar, ztar, tar; need compile in host machine
+# bdist: bulit distribute, already compiled, install will get faster; format: gztar, bztar, xztar, ztar, tar, zip, rpm, pkgtool, sdux, wininst, msi
