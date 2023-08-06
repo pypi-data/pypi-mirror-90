a8dc215aa779eb8d255e52b40d0b45f0e4201972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:41 +0000

    Add package eyecu-good-guys/0.2.0/eyecu_good_guys-0.2.0-py3-none-any.whl

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.0-py3-none-any.whl/good_guys/layers/__init__.py
@@ -0,0 +1,12 @@
+from . import basic
+from . import misc
+
+from .Inception import Inception
+from .InceptionMerge import InceptionMerge
+from .MinibatchDiscrimination import MinibatchDiscrimination
+
+from .Reshape import Reshape
+
+from .TemporalConcatenation import TemporalConcatenation
+from .BiFPN import BiFPN
+from .DepthwiseSeparableConvolution import DepthwiseSeparableConvolution
