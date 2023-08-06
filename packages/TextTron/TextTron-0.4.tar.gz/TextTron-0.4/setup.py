f6dcdd78d722f6d64e0603531a958823e411cc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:56 +0000

    Add package TextTron/0.4/TextTron-0.4.tar.gz

---
+++ packages/TextTron/TextTron-0.4.tar.gz/TextTron-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+name='TextTron',
+version='0.4',
+author='Ayan Gadpal',
+author_email='ayangadpal2@gmail.com',
+packages = ['TextTron'],
+license = "MIT",
+description = ("TextTron is a simple light-weight image processing based text detector in document images."),
+long_description_content_type='text/markdown',
+long_description=long_description,
+keywords = "Text Detection, lightweight Text Detection, Document Text Detector",
+url = "https://github.com/AyanGadpal/TextTron-Lightweight-text-detector",
+requires=['opencv_python','numpy'],
+)
