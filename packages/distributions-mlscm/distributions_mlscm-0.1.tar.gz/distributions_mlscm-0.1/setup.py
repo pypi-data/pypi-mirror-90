16ee2f6a9d0bb546e9281a0820d233d07b323d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:55 +0000

    Add package distributions-mlscm/0.1/distributions_mlscm-0.1.tar.gz

---
+++ packages/distributions-mlscm/distributions_mlscm-0.1.tar.gz/distributions_mlscm-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mlscm',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mlscm'],
+      zip_safe=False)
