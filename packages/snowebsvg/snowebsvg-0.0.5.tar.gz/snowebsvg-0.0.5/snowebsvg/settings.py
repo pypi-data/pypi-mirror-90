0b10c3a0f5d9be47c538773db0242d73c32d1ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:30 +0000

    Add package snowebsvg/0.0.5/snowebsvg-0.0.5.tar.gz

---
+++ packages/snowebsvg/snowebsvg-0.0.5.tar.gz/snowebsvg-0.0.5/snowebsvg/settings.py
@@ -0,0 +1,17 @@
+import os
+import json
+import pkg_resources
+
+BASE_DIR = os.environ['BASE_DIR']
+
+try:
+    #  Production part
+    VERSION = pkg_resources.get_distribution("snowebsvg").version
+except pkg_resources.DistributionNotFound:
+    #  Develop part
+    with open(os.path.join(BASE_DIR, 'package.json')) as package:
+        data = json.load(package)
+        VERSION = data['version']
+
+SVG_DEFAULT_THEME = 'dark'
+SVG_DEFAULT_SIZE = 'x3'
