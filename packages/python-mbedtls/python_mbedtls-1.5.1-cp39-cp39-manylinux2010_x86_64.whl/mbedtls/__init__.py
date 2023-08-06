8a187f2eec0d2946a2a4326647a030bf82700aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:22 +0000

    Add package python-mbedtls/1.5.1/python_mbedtls-1.5.1-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/python-mbedtls/python_mbedtls-1.5.1-cp39-cp39-manylinux2010_x86_64.whl/mbedtls/__init__.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2015, Elaborated Networks GmbH
+# Copyright (c) 2018, Mathias Laurin
+
+"""python-mbedtls is a this wrapper to ARM's mbed TLS library."""
+
+
+import mbedtls.cipher as cipher
+import mbedtls.exceptions as exceptions
+import mbedtls.hashlib as hashlib
+import mbedtls.hkdf as hkdf
+import mbedtls.hmac as hmac
+import mbedtls.pk as pk
+import mbedtls.secrets as secrets
+import mbedtls.tls as tls
+import mbedtls.version as version
+import mbedtls.x509 as x509
+
+__version__ = "1.5.1"
+
+__all__ = (
+    "cipher",
+    "exceptions",
+    "hash",
+    "hashlib",
+    "hkdf",
+    "hmac",
+    "pk",
+    "secrets",
+    "tls",
+    "version",
+    "x509",
+)
+
+
+has_feature = version.has_feature
