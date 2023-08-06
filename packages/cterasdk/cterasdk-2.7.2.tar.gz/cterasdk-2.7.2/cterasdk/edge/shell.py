9fba3e25f53dd32d541d09bd76e5e2feaad00ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:02 +0000

    Add package cterasdk/2.7.2/cterasdk-2.7.2.tar.gz

---
+++ packages/cterasdk/cterasdk-2.7.2.tar.gz/cterasdk-2.7.2/cterasdk/edge/shell.py
@@ -0,0 +1,27 @@
+import logging
+
+from ..lib.task_manager_base import TaskError
+from . import taskmgr as TaskManager
+from ..exception import CTERAException
+from .base_command import BaseCommand
+
+
+class Shell(BaseCommand):
+    """ Gateway Shell command """
+
+    def run_command(self, shell_command):
+        """
+        Execute a shell command on the gateway
+
+        :param str shell_command: The shell command to execute
+        :return: The command result
+        """
+        logging.getLogger().info("Executing shell command. %s", {'shell_command': shell_command})
+
+        task = self._gateway.execute("/config/device", "bgshell", shell_command)
+        try:
+            task = TaskManager.wait(self._gateway, task)
+            logging.getLogger().info("Shell command executed. %s", {'shell_command': shell_command})
+            return task.result.result
+        except TaskError as error:
+            raise CTERAException('An error occurred while executing task', error)
---
+++ packages/cterasdk/cterasdk-2.7.2.tar.gz/cterasdk-2.7.2/cterasdk/edge/taskmgr.py
@@ -0,0 +1,38 @@
+import re
+import logging
+
+from ..lib.task_manager_base import TaskBase
+from ..exception import InputError
+
+
+class Task(TaskBase):
+
+    def _get_task_id(self, ref):
+        uid = None
+        if isinstance(ref, int):
+            uid = str(ref)
+        elif isinstance(ref, str):
+            match = re.search('[1-9][0-9]*', ref)
+            if match is not None:
+                start, end = match.span()
+                uid = ref[start: end]
+        if uid is not None:
+            return '/proc/bgtasks/' + uid
+        logging.getLogger().error('Could not parse task id. %s', {'ref': ref})
+        raise InputError('Invalid task id', ref, [64, '/proc/bgtasks/64'])
+
+    def _get_task_status(self):
+        return self.CTERAHost.get(self.path)
+
+
+def running(CTERAHost):
+    return CTERAHost.query('/proc/bgtasks', 'status', 'running')
+
+
+def by_name(CTERAHost, name):
+    return CTERAHost.query('/proc/bgtasks', 'name', name)
+
+
+def wait(CTERAHost, ref):
+    task = Task(CTERAHost, ref)
+    return task.wait()
