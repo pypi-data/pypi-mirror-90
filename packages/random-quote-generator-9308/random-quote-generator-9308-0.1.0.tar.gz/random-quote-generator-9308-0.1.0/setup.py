8f13da8938883dc5c23d5340f95f6b350c8b3a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:23 +0000

    Add package random-quote-generator-9308/0.1.0/random-quote-generator-9308-0.1.0.tar.gz

---
+++ packages/random-quote-generator-9308/random-quote-generator-9308-0.1.0.tar.gz/random-quote-generator-9308-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['random_quote_generator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'random-quote-generator-9308',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Michael Herman',
+    'author_email': 'hermanmu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
