c89758f78f86c6b11d055574ee9bc928002e5ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:16 +0000

    Add package proxy-randomizer/1.3.0/proxy_randomizer-1.3.0-py2.py3-none-any.whl

---
+++ packages/proxy-randomizer/proxy_randomizer-1.3.0-py2.py3-none-any.whl/proxy_randomizer/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for proxy-randomizer."""
+
+__author__ = """Esequiel Albornoz"""
+__email__ = "esequielalbornoz7@gmail.com"
+__version__ = "__version__ = '1.3.0'"
+
+from .providers import RegisteredProviders
+from .proxy import Proxy
