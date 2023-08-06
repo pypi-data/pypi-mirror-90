ac00b7f43e68780f66315d2854c72ad413451f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:47 +0000

    Add package Tergame/0.0.3/Tergame-0.0.3.tar.gz

---
+++ packages/Tergame/Tergame-0.0.3.tar.gz/Tergame-0.0.3/README.md
@@ -0,0 +1,11 @@
+Make text-based command line games using this module.
+
+Release notes version 0.0.3
+===
+- Added error messages to literally everything
+- Added Button sets to easily add buttons to main menus and actions
+
+Documentation
+===
+
+Coming soon. Try reading the source code if you want to use this module, the documentation will release sometime soon, join the Discord for notifications: https://discord.gg/DQTRTWQ7yd
---
+++ packages/Tergame/Tergame-0.0.3.tar.gz/Tergame-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Tergame",
+    version="0.0.3",
+    author="BruhDev",
+    author_email="mr.bruh.dev@gmail.com",
+    description="Make text-based command line games using this module.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bruhdev.com",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires=">=3.7",
+)
