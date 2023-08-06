6ed3649f3fbf45cf0a617c4c5f5cab20cd67c4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:24 +0000

    Add package certspotter/1.0.12/certspotter-1.0.12.tar.gz

---
+++ packages/certspotter/certspotter-1.0.12.tar.gz/certspotter-1.0.12/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = "1.0.12"
+
+
+def readme():
+  """print long description"""
+  with open('README.md') as f:
+    return f.read()
+
+
+setup(name='certspotter',
+      version=VERSION,
+      description='sslmate CertSpotter API',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      url="https://github.com/fartbagxp/certspotter-api",
+      author='Boris Ning',
+      author_email='fartbagxp@gmail.com',
+      py_modules=['certspotter'],
+      packages=['certspotter'],
+      package_dir={'certspotter': 'src/certspotter'},
+      license='MIT')
