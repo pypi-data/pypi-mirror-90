127fd66821325b2ad0d8af12c351f626a6bb391c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:10 +0000

    Add package scripture-burrito/0.0.1/scripture-burrito-0.0.1.tar.gz

---
+++ packages/scripture-burrito/scripture-burrito-0.0.1.tar.gz/scripture-burrito-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name="scripture-burrito",
+    version="0.0.1",
+    description="Python library for the Scripture Burrito data interchange format",
+    url="http://github.com/bible-technology/scripture-burrito-python",
+    author="BT Tech Consortium",
+    author_email="jtauber@jtauber.com",
+    license="MIT",
+    packages=["scripture_burrito"],
+    package_data={
+        "scripture_burrito": ["schema"]
+    },
+    install_requires=["jsonschema"],
+    entry_points={
+        "console_scripts": [
+            "validate-sb = scripture_burrito.validate:main",
+        ],
+    },
+)
