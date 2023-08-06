5541e07238ff1a2b7a7b46b0bfea8f6b9e5f7bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:34 +0000

    Add package pfinsim/0.0.3/pfinsim-0.0.3.tar.gz

---
+++ packages/pfinsim/pfinsim-0.0.3.tar.gz/pfinsim-0.0.3/pfinsim/common.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+import yaml
+import numpy as np
+
+
+def load_settings(file_name='settings.yml'):
+    with open(file_name) as file:
+        settings = yaml.load(file, Loader=yaml.FullLoader)
+    return settings
+
+
+def perc_2_float(perc: str):
+    return float(perc.replace('%', '')) / 100
+
+
+def month_to_year(month: int):
+    return 2021 + np.floor(month / 12)
+
+
+class Period(Enum):
+    YEAR = 'year'
+    MONTH = 'month'
