1eee8a6d9b9fc28f5921acc61b99a6de8b3a3e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:12 +0000

    Add package lboxd/0.1.4/lboxd-0.1.4.tar.gz

---
+++ packages/lboxd/lboxd-0.1.4.tar.gz/lboxd-0.1.4/lboxd/__init__.py
@@ -0,0 +1,15 @@
+from .lboxd import *
+
+parser = argparse.ArgumentParser(description='letterboxd args')
+parser.add_argument('--user', '-u', dest='user', help='letterboxd.com user')
+parser.add_argument('--reviews', '-r', dest='reviews', action="store_true", default=False, help='Gets reviews')
+parser.add_argument('--testing', '-t', dest='testing', action='store_true', default=False, help='Testing flag - for development only')
+parser.add_argument('--save-json', '-j', dest='json', action="store_true", default=False, help='Saves a JSON file of the reviews dictionary')
+parser.add_argument('--save-html', '-w', dest='html', action="store_true", default=False, help='Saves an HTML document for easily viewing reviews')
+parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_true", default=False, help='Opens saved HTML document in the browser')
+parser.add_argument('--search', '-s', nargs='+', dest='search', default=())
+args = parser.parse_args()
+
+
+
+console = Console()
