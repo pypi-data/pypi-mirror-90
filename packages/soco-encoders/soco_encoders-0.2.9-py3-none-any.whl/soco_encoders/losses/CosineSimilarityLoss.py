f5406bc543bf7c6c53c7ee5397aaf39117a3bb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:36 +0000

    Add package soco-encoders/0.2.9/soco_encoders-0.2.9-py3-none-any.whl

---
+++ packages/soco-encoders/soco_encoders-0.2.9-py3-none-any.whl/soco_encoders/losses/CosineSimilarityLoss.py
@@ -0,0 +1,27 @@
+from typing import Iterable, Dict
+
+import torch
+from torch import nn, Tensor
+
+from soco_encoders.SentenceTransformer import SentenceTransformer
+
+
+class CosineSimilarityLoss(nn.Module):
+    def __init__(self, model: SentenceTransformer):
+        super(CosineSimilarityLoss, self).__init__()
+        self.model = model
+
+
+    def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
+        reps = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+        rep_a, rep_b = reps
+
+        output = torch.cosine_similarity(rep_a, rep_b)
+        # output = torch.sum(rep_a*rep_b, dim=1)
+        loss_fct = nn.MSELoss()
+
+        if labels is not None:
+            loss = loss_fct(output, labels.view(-1))
+            return loss
+        else:
+            return reps, output
