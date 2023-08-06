ae9a87af254ecb40ccd0b059cf558d933cdf8f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:42 +0000

    Add package FASTRAL/0.5.2/FASTRAL-0.5.2.tar.gz

---
+++ packages/FASTRAL/FASTRAL-0.5.2.tar.gz/FASTRAL-0.5.2/FASTRAL/fastral_infer.py
@@ -0,0 +1,42 @@
+"""
+@author: Payam Dibaeinia
+"""
+
+import argparse
+from FASTRAL.fastral import FASTRAL
+from FASTRAL.utils import make_dir
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--ns', type=str, default=None, help='number of samples, input multiple sample sizes with comma seperated values', required = True)
+    parser.add_argument('--nt', type=str, default=None, help='number of trees per sample, if there are multuple samples use comma seperated values', required = True)
+    parser.add_argument('--k', type=int, default=None, help='total number of input gene trees', required = True)
+    parser.add_argument('--it', type=str, default=None, help='path to input gene trees', required = True)
+    parser.add_argument('--os', type=str, default=None, help='path to samples folders to write sampled trees', required = True)
+    parser.add_argument('--aggregate', type=str, default=None, help='path to write aggeregated species trees', required = True)
+    parser.add_argument('--o', type=str, default=None, help='path to write FASTRAL output species tree', required = True)
+    parser.add_argument('--time', type=str, default=None, help='path to write running times', required = True)
+    parser.add_argument('--rep', action='store_true', help='whether draw samples with replacement | Default: False', required = False)
+    parser.add_argument('--path_ASTRID', type=str, default='ASTRID/ASTRID-linux', help='path to ASTRID | Default: linux runtime of ASTRID-2 (version untagged-fdc5326080d364b87c5a) is used (see: https://github.com/pranjalv123/ASTRID/releases/tag/untagged-fdc5326080d364b87c5a)', required = False)
+    parser.add_argument('--path_ASTRAL', type=str, default='ASTRAL-modified/astral.5.7.3.jar', help='path to ASTRAL | Default: modified ASTRAL 5.7.3 is used', required = False)
+    parser.add_argument('--heuristics', type=int, default=0, help='heuristics level of ASTRAL | Default: 0', required = False)
+    parser.add_argument('--multi', type=str, default=None, help='if input gene trees contain multiple individuals, specify the path to the mapping file', required = False)
+    parser.add_argument('--incomp_id', type=str, default=None, help='(Optional) Path to a file containing the IDs of incomplete gene trees. If specified, sampling step makes sure that each sub-sample contains complete gene trees.', required = False)
+
+    FLAGS = parser.parse_args()
+
+    make_dir(FLAGS.os)
+    make_dir(FLAGS.time, writable = True)
+    make_dir(FLAGS.aggregate, writable = True)
+    make_dir(FLAGS.o, writable = True)
+
+    method = FASTRAL(FLAGS)
+    method.run()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/FASTRAL/FASTRAL-0.5.2.tar.gz/FASTRAL-0.5.2/README.md
@@ -0,0 +1,4 @@
+# FASTRAL
+
+## Description
+FASTRAL is a novel approach for running ASTRAL, a widely used and popular species tree reconstruction method. FASTRAL runs up to ~800 times faster than ASTRAL but with a similar (or sometimes better) accuracy.
