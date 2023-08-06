7204266412c8b49a4ff601e71db09eb501894c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:16 +0000

    Add package librecell/0.0.7/librecell-0.0.7.tar.gz

---
+++ packages/librecell/librecell-0.0.7.tar.gz/librecell-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(name='librecell',
+      version='0.0.7',
+      description='Meta-package for the LibreCell suite.',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='cmos cell generator layout characterization vlsi asic',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Programming Language :: Python :: 3'
+      ],
+      url='https://codeberg.org/tok/librecell',
+      author='T. Kramer',
+      author_email='code@tkramer.ch',
+      license='',  # ???
+      install_requires=[
+          'librecell-common==0.0.7',
+          'librecell-layout==0.0.7',
+          'librecell-lib==0.0.7',
+      ],
+      zip_safe=False)
