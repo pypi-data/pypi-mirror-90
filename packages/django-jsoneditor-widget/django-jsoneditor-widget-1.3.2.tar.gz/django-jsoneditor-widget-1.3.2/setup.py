5cf9eca0fcd8761e251108e2413460fba4152fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:10 +0000

    Add package django-jsoneditor-widget/1.3.2/django-jsoneditor-widget-1.3.2.tar.gz

---
+++ packages/django-jsoneditor-widget/django-jsoneditor-widget-1.3.2.tar.gz/django-jsoneditor-widget-1.3.2/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='django-jsoneditor-widget',
+    license='MIT',
+    version=os.environ['CIRCLE_TAG'],
+    author='Arthur Chang',
+    author_email='arthurc0102@gmail.com',
+    url='https://github.com/arthurc0102/django-jsoneditor-widget',
+    packages=['jsoneditor'],
+    package_data={
+        'jsoneditor': [
+            'static/jsoneditor/*.css',
+            'static/jsoneditor/*.js',
+            'static/jsoneditor/*.map',
+            'static/jsoneditor/img/*.svg',
+            'templates/jsoneditor/*.html',
+        ],
+    },
+    description='Django form widget form JSONField',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+)
