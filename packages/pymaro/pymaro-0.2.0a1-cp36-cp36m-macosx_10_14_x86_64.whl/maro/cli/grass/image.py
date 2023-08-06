3da59d6f15fca56e52b9dd44379a761b843fbce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package pymaro/0.2.0a1/pymaro-0.2.0a1-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/pymaro/pymaro-0.2.0a1-cp36-cp36m-macosx_10_14_x86_64.whl/maro/cli/grass/image.py
@@ -0,0 +1,29 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+
+from maro.cli.grass.executors.grass_azure_executor import GrassAzureExecutor
+from maro.cli.utils.checkers import check_details_validity
+from maro.cli.utils.details import load_cluster_details
+from maro.cli.utils.lock import lock
+from maro.utils.exception.cli_exception import BadRequestError
+
+
+@check_details_validity
+@lock
+def push_image(
+    cluster_name: str, image_name: str, image_path: str, remote_context_path: str, remote_image_name: str,
+    **kwargs
+):
+    cluster_details = load_cluster_details(cluster_name=cluster_name)
+
+    if cluster_details["mode"] in ["grass/azure", "grass/on-premises"]:
+        executor = GrassAzureExecutor(cluster_name=cluster_name)
+        executor.push_image(
+            image_name=image_name,
+            image_path=image_path,
+            remote_context_path=remote_context_path,
+            remote_image_name=remote_image_name
+        )
+    else:
+        raise BadRequestError(f"Unsupported command in mode '{cluster_details['mode']}'.")
