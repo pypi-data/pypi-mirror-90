f1d0cdd1f971fcd02269494563f072b11f425ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:06 +0000

    Add package sqlite-rx/1.0.1/sqlite_rx-1.0.1.tar.gz

---
+++ packages/sqlite-rx/sqlite_rx-1.0.1.tar.gz/sqlite_rx-1.0.1/sqlite_rx/exception.py
@@ -0,0 +1,27 @@
+
+class SQLiteRxError(Exception):
+    pass
+
+
+class SQLiteRxAuthConfigError(SQLiteRxError):
+    pass
+
+
+class SQLiteRxZAPSetupError(SQLiteRxError):
+    pass
+
+
+class SQLiteRxTransportError(SQLiteRxError):
+    pass
+
+
+class SQLiteRxSerializationError(SQLiteRxError):
+    pass
+
+
+class SQLiteRxCompressionError(SQLiteRxError):
+    pass
+
+
+class SQLiteRxConnectionError(SQLiteRxError):
+    pass
