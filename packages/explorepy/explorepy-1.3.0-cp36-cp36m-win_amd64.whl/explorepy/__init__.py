050ac009c432a2e33b1cb180243ec61481a8c3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package explorepy/1.3.0/explorepy-1.3.0-cp36-cp36m-win_amd64.whl

---
+++ packages/explorepy/explorepy-1.3.0-cp36-cp36m-win_amd64.whl/explorepy/__init__.py
@@ -0,0 +1,19 @@
+from .explore import Explore
+from . import tools, command, exploresdk
+from explorepy.dashboard.dashboard import Dashboard
+
+__version__ = '1.3.0'
+_bt_interface = 'pybluez'
+
+
+def set_bt_interface(bt_interface):
+    """Set Explorepy Bluetooth interface type
+
+    Args:
+        bt_interface (str): Bluetooth interfacce type. Options: 'sdk' or 'pybluez'
+
+    """
+    if bt_interface not in ['sdk', 'pybluez']:
+        raise ValueError
+    import explorepy
+    explorepy._bt_interface = bt_interface
