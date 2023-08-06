4ca4bfe76d559bf14bbbf76d44be4f25538c9181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package mbed-tools/4.6.0/mbed_tools-4.6.0-py3-none-any.whl

---
+++ packages/mbed-tools/mbed_tools-4.6.0-py3-none-any.whl/mbed_tools/devices/exceptions.py
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Public exceptions raised by the package."""
+from mbed_tools.lib.exceptions import ToolsError
+
+
+class MbedDevicesError(ToolsError):
+    """Base public exception for the mbed-devices package."""
+
+
+class DeviceLookupFailed(MbedDevicesError):
+    """Failed to look up data associated with the device."""
+
+
+class NoDevicesFound(MbedDevicesError):
+    """No Mbed Enabled devices were found."""
