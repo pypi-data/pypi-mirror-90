decfd75a7403eb4870fab1b877a784c45a548d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:38 +0000

    Add package distributions-pak/0.1/distributions_pak-0.1.tar.gz

---
+++ packages/distributions-pak/distributions_pak-0.1.tar.gz/distributions_pak-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pak',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pad'],
+      author = 'Yangfan',
+      author_email = 'xyf3333@gmail.com', 
+      zip_safe=False)
