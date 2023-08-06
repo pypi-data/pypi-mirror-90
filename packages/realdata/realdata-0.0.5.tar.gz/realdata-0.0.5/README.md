6e2762e804a397f434ac7304c8dcbcad59209a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:33 +0000

    Add package realdata/0.0.5/realdata-0.0.5.tar.gz

---
+++ packages/realdata/realdata-0.0.5.tar.gz/realdata-0.0.5/README.md
@@ -0,0 +1,12 @@
+# realdata
+
+Tired of using fake data for testing time series? 
+
+## Install
+
+    pip install realdata
+    
+### Usage 
+
+    from realdata import get_values
+    values = get_values()
---
+++ packages/realdata/realdata-0.0.5.tar.gz/realdata-0.0.5/realdata/__init__.py
@@ -0,0 +1,29 @@
+# See README
+
+from microprediction import MicroReader
+import random
+
+
+EXAMPLE_STREAMS = ['electricity-lbmp-nyiso-west.json','electricity-load-nyiso-nyc.json']
+
+
+def random_name():
+    """ Choose a random name of a stream """
+    mr = MicroReader()
+    names = [n for n in mr.get_stream_names() if not '~' in n ]
+    return random.choice(names)
+
+
+def get_values(name:str=None)->[float]:
+    """ Get a real world time series """
+    mr = MicroReader()
+    if name is None:
+        name = random_name()
+    lagged = mr.get_lagged_values(name=name)
+    values = list(reversed(lagged))
+    return values
+
+
+if __name__=='__main__':
+    values = get_values()
+    print(len(values))
