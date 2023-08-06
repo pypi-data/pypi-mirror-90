2619d000429b3e2254591ef098d43ba7b0697d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:58 +0000

    Add package Wrencher/0.1.8/Wrencher-0.1.8.tar.gz

---
+++ packages/Wrencher/Wrencher-0.1.8.tar.gz/Wrencher-0.1.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='Wrencher',
+    packages=find_packages(include=['Wrench']),
+    version='0.1.8',
+    description='Cool lib',
+    author='Rostcraft',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
