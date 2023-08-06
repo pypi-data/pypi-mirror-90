c791fa0678d80c452c18dd398bc8e241da4e7340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:43 +0000

    Add package bodega-bot/0.0.1/bodega_bot-0.0.1-py3-none-any.whl

---
+++ packages/bodega-bot/bodega_bot-0.0.1-py3-none-any.whl/app_configs/config_manager.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import json
+import os
+import pathlib
+
+
+class ConfigManager:
+
+    __instance = None
+
+    @staticmethod
+    def get_instance() -> ConfigManager:
+        """
+        Gets singleton class of config manager
+        @return: a config manager
+        """
+        if ConfigManager.__instance is None:
+            ConfigManager()
+        return ConfigManager.__instance
+
+
+    def __init__(self):
+        """
+        Private singleton constructor
+        """
+        if ConfigManager.__instance is not None:
+            raise Exception("This class is a singleton")
+        else:
+            ConfigManager.__instance = self
+
+        self.config_dict = {}
+        self.arg_dict = {}
+        self.load_config_file(f'{str(pathlib.Path(__file__).parent.absolute())}/config.json')
+
+    def load_config_file(self, filepath):
+        """
+        Attempts to load config file located at filepath.
+        """
+        if os.path.exists(filepath):
+            print(f'Loading config file: {filepath}')
+            self.config_dict = json.load(open(filepath))
+        else:
+            print(f'Not loading config file: {filepath} since it does not exist')
+
+
+    def get_db_url(self) -> str:
+        """
+        @return: The configured database name or default url
+        """
+        return self.config_dict['db_url'] if 'db_url' in self.config_dict else 'sqlite:///bodega_discord.db'
+
+
+    def get_bot_token(self) -> str:
+        """
+        @return: The configured bot token or default test bodega token
+        """
+        bot_token = ""
+        if 'bot_token' in self.config_dict:
+            if self.config_dict['bot_token']:
+                bot_token = self.config_dict['bot_token']
+
+        if 'bot_token' in self.arg_dict:
+            if self.arg_dict['bot_token']:
+                bot_token = self.arg_dict['bot_token']
+
+        return bot_token
+
+    def inject_parsed_arguments(self, arguments: dict) -> None:
+        """
+        Populates private argument dictionary.
+        """
+        for argument, value in arguments.items():
+            self.arg_dict[argument] = value
+
+
+
---
+++ packages/bodega-bot/bodega_bot-0.0.1-py3-none-any.whl/libdisc/models/message.py
@@ -0,0 +1,67 @@
+from typing import Set, Tuple
+
+from sqlalchemy import Column, Integer, ForeignKey, BigInteger
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from libdisc.models.base_mixin import Base
+
+class Message(Base):
+    """
+    Table used to store messages
+    """
+
+    __tablename__ = "message"
+    __table_args__ = {'mysql_engine':'InnoDB', 'mysql_charset': 'utf8mb4'}
+    id = Column(BigInteger, primary_key=True)
+    timestamp = Column(BigInteger, nullable=False, index=True)
+    user_id = Column(BigInteger, ForeignKey("user.id",
+                                         ondelete='cascade',
+                                         onupdate='cascade'), nullable=False)
+    channel_id = Column(BigInteger, index=True)
+    word_count = Column(BigInteger)
+    char_count = Column(BigInteger)
+
+    @staticmethod
+    def add_message(
+            db_session: Session,
+            user_id: int,
+            channel_id: int,
+            timestamp: int,
+            word_count: int,
+            char_count: int,
+            cache: Set[Tuple[int, int, int]] = None) -> None:
+
+        """
+        Adds message into the database
+        @param db_session: The current database session
+        @param user_id: The id of the user.
+        @param channel_id: The id of the channel.
+        @param timestamp: Given timestamp
+        @param word_count: The word count of the message
+        @param char_count: The character count of the message.
+        @param cache: Optional cache to determine if the message already exists in the database
+        @return: None
+        """
+
+        if cache and (user_id, channel_id, timestamp) in cache:
+            return
+
+        message = (db_session.query(Message)
+                   .filter(Message.timestamp == timestamp, Message.user_id == user_id, Message.channel_id == channel_id)
+                   .one_or_none())
+
+        if message is None:
+            message = Message(timestamp=timestamp,
+                              user_id=user_id,
+                              channel_id=channel_id,
+                              word_count=word_count,
+                              char_count=char_count)
+        db_session.add(message)
+
+        try:
+            db_session.commit()
+            if cache:
+                cache.add((user_id, channel_id, timestamp))
+        except SQLAlchemyError:
+            db_session.rollback()
+            raise
