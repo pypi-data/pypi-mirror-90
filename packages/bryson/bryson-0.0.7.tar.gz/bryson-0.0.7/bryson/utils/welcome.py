2fb2be8b85254021989b24eea18b98548c252af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:59 +0000

    Add package bryson/0.0.7/bryson-0.0.7.tar.gz

---
+++ packages/bryson/bryson-0.0.7.tar.gz/bryson-0.0.7/bryson/utils/welcome.py
@@ -0,0 +1,19 @@
+def print_welcome():
+    print(""""
+     _______                                                    
+    |       \                                                   
+    | $$$$$$$\  ______   __    __   _______   ______   _______  
+    | $$__/ $$ /      \ |  \  |  \ /       \ /      \ |       \ 
+    | $$    $$|  $$$$$$\| $$  | $$|  $$$$$$$|  $$$$$$\| $$$$$$$\\
+    | $$$$$$$\| $$   \$$| $$  | $$ \$$    \ | $$  | $$| $$  | $$
+    | $$__/ $$| $$      | $$__/ $$ _\$$$$$$\| $$__/ $$| $$  | $$
+    | $$    $$| $$       \$$    $$|       $$ \$$    $$| $$  | $$
+     \$$$$$$$  \$$       _\$$$$$$$ \$$$$$$$   \$$$$$$  \$$   \$$
+                        |  \__| $$                              
+                         \$$    $$                              
+                          \$$$$$$                               
+    """)
+
+    print("Created by- Jonathan Zavialov")
+    print("Owned by the BRYSON collaborative. Visit us at http://bryson.studio/")
+    print("Thank you for installing BRYSON. Please run \"pip install -r requirements.txt\" for full installation.")
