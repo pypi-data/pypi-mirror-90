dccc8b2af9166c6f56a3d39dd067740a70a6bc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:35 +0000

    Add package aws-lambda-typing/0.0.12/aws-lambda-typing-0.0.12.tar.gz

---
+++ packages/aws-lambda-typing/aws-lambda-typing-0.0.12.tar.gz/aws-lambda-typing-0.0.12/awslambdatyping/responses/s3_batch.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import enum
+import typing
+
+
+class S3BatchResponseResultCodeEnum(enum.Enum):
+    """
+    S3BatchResponseResultCodeEnum https://docs.aws.amazon.com/AmazonS3/latest/dev/batch-ops-invoke-lambda.html
+
+    Attributes:
+    ----------
+    SUCCEEDED: 'Succeeded'
+
+    TEMPORARY_FAILURE: 'TemporaryFailure'
+
+    PERMANENT_FAILURE: 'PermanentFailure'
+    """
+    SUCCEEDED = 'Succeeded'
+    TEMPORARY_FAILURE = 'TemporaryFailure'
+    PERMANENT_FAILURE = 'PermanentFailure'
+
+
+class S3BatchResponseResult(typing.TypedDict):
+    """
+    S3BatchRequestTask
+
+    Attributes:
+    ----------
+    taskId: str
+
+    resultCode: str
+
+    resultString: str
+    """
+
+    taskId: str
+    resultCode: str
+    resultString: str
+
+
+class S3BatchResponse(typing.TypedDict):
+    """
+    S3BatchResponse https://docs.aws.amazon.com/lambda/latest/dg/services-s3-batch.html
+
+    Attributes:
+    ----------
+    invocationSchemaVersion: str
+
+    treatMissingKeysAs: :class:`S3BatchResponseResultCodeEnum`
+
+    invocationId: str
+
+    results: typing.List[:class:`S3BatchResponseResult`]
+    """
+
+    invocationSchemaVersion: str
+    treatMissingKeysAs: S3BatchResponseResultCodeEnum
+    invocationId: str
+    results: typing.List[S3BatchResponseResult]
