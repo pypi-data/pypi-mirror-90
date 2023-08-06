8d0fd56930160a1ac2db24c0777baa3b9ee88813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:29 +0000

    Add package emailsanta/1.0.4/emailsanta-1.0.4-py3-none-any.whl

---
+++ packages/emailsanta/emailsanta-1.0.4-py3-none-any.whl/emailsanta/__init__.py
@@ -0,0 +1,10 @@
+"""
+emailsanta: Python module to simulate emailing the legendary Christmas 
+character Santa, powered by Alan Kerr's emailSanta.com.
+"""
+__author__ = "Félix An"
+__copyright__ = "Copyright (c) 2020 Félix An."
+__credits__ = ["emailSanta.com", "Alan Kerr"]
+__license__ = "AGPLv3"
+
+from emailsanta.emailsanta import *
