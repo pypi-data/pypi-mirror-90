21216bfc007d34e4b4f88449d3a72d00373c5861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package mcnp-input-reader/0.1.3/mcnp_input_reader-0.1.3.tar.gz

---
+++ packages/mcnp-input-reader/mcnp_input_reader-0.1.3.tar.gz/mcnp_input_reader-0.1.3/README.md
@@ -0,0 +1,19 @@
+# MCNP Input Reader
+
+MCNP Input Reader is a python package developed in order to facilitate the modifications and the check integrity 
+of large mcnp input files.
+
+## Install
+
+```shell
+pip install mcnp-input-reader
+```
+
+## TODO
+
+A lot of things...
+
+## Example
+
+Example taken from [here](https://www.utoledo.edu/med/depts/radther/pdf/MCNP5%20practical%20examples%20lecture%207%20companion.pdf) 
+
---
+++ packages/mcnp-input-reader/mcnp_input_reader-0.1.3.tar.gz/mcnp_input_reader-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mcnp_input_reader']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mcnp-input-reader',
+    'version': '0.1.3',
+    'description': 'MCNP Input reader',
+    'long_description': '# MCNP Input Reader\n\nMCNP Input Reader is a python package developed in order to facilitate the modifications and the check integrity \nof large mcnp input files.\n\n## Install\n\n```shell\npip install mcnp-input-reader\n```\n\n## TODO\n\nA lot of things...\n\n## Example\n\nExample taken from [here](https://www.utoledo.edu/med/depts/radther/pdf/MCNP5%20practical%20examples%20lecture%207%20companion.pdf) \n\n',
+    'author': 'gmariano',
+    'author_email': 'giovanni.mariano@enea.it',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ENEA-Fusion-Neutronics/MCNP-Input-Reader.git',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
