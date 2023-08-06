a7ecf5f8f23319d551f9c839e28152e52becb1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:54 +0000

    Add package sphinx-astrorefs/0.7/sphinx-astrorefs-0.7.tar.gz

---
+++ packages/sphinx-astrorefs/sphinx-astrorefs-0.7.tar.gz/sphinx-astrorefs-0.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sphinx-astrorefs",
+    version="0.7",
+    author="Jo Bovy",
+    author_email="bovy@astro.utoronto.ca",
+    description="Astro-style references in Sphinx documents",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=["sphinx_astrorefs"],
+    package_data={"sphinx_astrorefs":["_static/sphinx_astrorefs.css"]},
+    install_requires=["sphinx","sphinxcontrib-bibtex",
+                      "pybtex","latexcodec"]
+)
---
+++ packages/sphinx-astrorefs/sphinx-astrorefs-0.7.tar.gz/sphinx-astrorefs-0.7/sphinx_astrorefs/resolve_aas.py
@@ -0,0 +1,86 @@
+# Quick script to replace all AAS macros in a bibtex file
+import os.path
+from sphinx.errors import ExtensionError
+
+aas_macros_dict= {
+    '\\apjsupp' : 'Astrophys. J. Supp.',
+    '\\apjs'    : 'Astrophys. J. Supp.',
+    '\\appjlett': 'Astrophys. J. Lett.',
+    '\\appjl'   : 'Astrophys. J. Lett.',
+    '\\apj'     : 'Astrophys. J.',
+    '\\aj'      : 'Astron. J.',
+    '\\mnras'   : 'Mon. Not. Roy. Astron. Soc.',
+    '\\baas'    : 'Bull. AAS',
+    '\\bain'    : 'Bull. Astron. Inst. Netherlands',
+    '\\aapr'    : 'Astron. & Astrophys. Rev.',
+    '\\aaps'    : 'Astron. & Astrophys. Supp.',
+    '\\astap'   : 'Astron. & Astrophys.',
+    '\\aap'     : 'Astron. & Astrophys.',
+    '\\araa'    : 'Ann. Rev. Astron. Astrophys.',
+    '\\actaa'   : 'Acta Astronomica',
+    '\\apss'    : 'Astrophys. & Space Sci.',
+    '\\jcap'    : 'J. Cosmo & Astropart. Phys.',
+    '\\nat'     : 'Nature',
+    '\\nar'     : 'New Astron. Rev.',
+    '\\na'      : 'New Astron.',
+    '\\pra'     : 'Phys. Rev. A',
+    '\\prb'     : 'Phys. Rev. B',
+    '\\prc'     : 'Phys. Rev. C',
+    '\\prd'     : 'Phys. Rev. D',
+    '\\pre'     : 'Phys. Rev. E',
+    '\\prl'     : 'Phys. Rev. Lett.',
+    '\\pasa'    : 'Pub. Astron. Soc. Aus.',
+    '\\pasp'    : 'Pub. Astron. Soc. Pac.',
+    '\\pasj'    : 'Pub. Astron. Soc. Japan',
+    '\\rmxaa'   : 'Rev. Mex. Astron. & Astrofys.',
+    '\\ssr'     : 'Space Sci. Rev.',
+    '\\applopt' : 'Appl. Opt.',
+    '\\ao'      : 'Appl. Opt.',
+    '\\azh'     : 'Astron. Zhu.',
+    '\\bac'     : 'Bull. Astron. Czech.',
+    '\\caa'     : 'Chin. Astron. Astrophys.',
+    '\\cjaa'    : 'Chin. J. Astron. Astrophys.',
+    '\\icarus'  : 'Icarus',
+    '\\jrasc'   : 'J. RAS Can.',
+    '\\memras'  : 'Mem. RAS',
+    '\\qjras'   : 'Quat. J. RAS',
+    '\\skytel'  : 'Sky & Telescope',
+    '\\solphys' : 'Sol. Phys.',
+    '\\sovast'  : 'Sov. Astron.',
+    '\\zap'     : 'ZeitSch. Astrophys.',
+    '\\iaucirc' : 'IAU Circs.',
+    '\\aplett'  : 'Astrophys. Lett.',
+    '\\apspr'   : 'Astrophys. Space Phys. Res.',
+    '\\fcp'     : 'Fund. Cosm. Phys.',
+    '\\gca'     : 'Geochim. Cosmochim. Acta',
+    '\\grl'     : 'Geophys. Res. Lett',
+    '\\jcp'     : 'J. Chem. Phys.',
+    '\\jgr'     : 'J. Geophys. Res.',
+    '\\jqsrt'   : 'J. Quant. Spec. Rad. Trans.',
+    '\\memsai'  : 'Mem. Soc. Astron. Ital.',
+    '\\nphysa'  : 'Nucl. Phys. A',
+    '\\physrep' : 'Phys. Rep.',
+    '\\physscr' : 'Phys. Scrip.',
+    '\\planss'  : 'Plan. Space. Sci.',
+    '\\procspie': 'Proc. SPIE'
+    }
+
+def resolve(app):
+    if not app.config.astrorefs_resolve_aas_macros:
+        return
+    if app.config.astrorefs_resolve_aas_macros_infile is None \
+       or app.config.astrorefs_resolve_aas_macros_outfile is None:
+        raise ExtensionError('sphinx-astrorefs: when resolving AAS macros, need to give original and target bib file name as "astrorefs_resolve_aas_macros_infile" and "astrorefs_resolve_aas_macros_outfile"')
+    with open(os.path.join(app.env.srcdir,
+                        app.config.astrorefs_resolve_aas_macros_infile),'r') \
+                      as infile:
+        with open(os.path.join(app.env.srcdir,
+                        app.config.astrorefs_resolve_aas_macros_outfile),'w') \
+                        as outfile:
+            for line in infile:
+                for key in aas_macros_dict.keys():
+                    line= line.replace(key,aas_macros_dict[key])
+                outfile.write(line)
+    # Re-do this initialization to make sure the bibtex file is found
+    if hasattr(app.config,'bibtex_bibfiles'):
+        app.env.get_domain('cite').__init__(app.env)
