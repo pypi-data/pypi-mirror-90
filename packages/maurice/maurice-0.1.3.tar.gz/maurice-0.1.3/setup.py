bedb6fc47b46b5bb8c08cfd95ba88ef94326d550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:10 +0000

    Add package maurice/0.1.3/maurice-0.1.3.tar.gz

---
+++ packages/maurice/maurice-0.1.3.tar.gz/maurice-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['maurice', 'maurice.patchers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dill>=0.3.3,<0.4.0', 'wrapt>=1.12.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'maurice',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Tomas Pereira de Vasconcelos',
+    'author_email': 'tomas@tiqets.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
