3e4ece12c282942ea9fb50ed88603827361b5447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package bioregistry/0.0.6/bioregistry-0.0.6-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.0.6-py3-none-any.whl/bioregistry/compare.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+"""This script compares what's in OBO, OLS, and MIRIAM."""
+
+import itertools as itt
+import os
+import sys
+
+import click
+
+from bioregistry import read_bioregistry
+from bioregistry.constants import DOCS
+from bioregistry.external import get_miriam, get_n2t, get_obofoundry, get_ols, get_wikidata_registry
+
+
+@click.command()
+def compare():
+    """Compare the registries."""
+    try:
+        import matplotlib.pyplot as plt
+        from matplotlib_venn import venn2, venn3
+    except ImportError:
+        click.secho(
+            'Could not import matplotlib dependencies.'
+            ' Install bioregistry again with `pip install bioregistry[charts]`.',
+            fg='red',
+        )
+        return sys.exit(1)
+
+    directory = os.path.join(DOCS, 'img')
+    os.makedirs(directory, exist_ok=True)
+
+    bioregistry = read_bioregistry()
+    bioregistry_entries = set(bioregistry)
+
+    miriam = get_miriam(skip_deprecated=True)
+    miriam_entries = {
+        entry['prefix'].lower()
+        for entry in miriam
+    }
+
+    ols = get_ols()
+    ols_entries = {
+        entry['ontologyId'].lower()
+        for entry in ols
+    }
+
+    obofoundry = get_obofoundry(skip_deprecated=True)
+    obofoundry_entries = {
+        entry['id'].lower()
+        for entry in obofoundry
+    }
+
+    wikidata = get_wikidata_registry()
+
+    n2t_entries = set(get_n2t())
+    fig, axes = plt.subplots(ncols=2)
+    venn2(
+        subsets=[miriam_entries, n2t_entries],
+        set_labels=['MIRIAM', 'N2T'],
+        ax=axes[0],
+    )
+    venn2(
+        subsets=[obofoundry_entries, ols_entries],
+        set_labels=['OBO Foundry', 'OLS'],
+        ax=axes[1],
+    )
+    path = os.path.join(directory, 'comparison_1_way.png')
+    click.echo(f'output to {path}')
+    plt.tight_layout()
+    plt.savefig(path, dpi=300)
+    plt.close(fig)
+
+    keys = [
+        ('OBO Foundry', 'g', obofoundry_entries),
+        ('OLS', 'y', ols_entries),
+        ('MIRIAM', 'b', miriam_entries),
+        ('Wikidata', 'purple', wikidata),
+    ]
+
+    # 2-way
+    keys_2_way = [*keys, ('N2T', 'grey', n2t_entries)]
+    fig, axes = plt.subplots(ncols=2, nrows=(1 + len(keys_2_way)) // 2)
+    for (label, color, subset), ax in zip(keys_2_way, axes.ravel()):
+        venn2(
+            subsets=(bioregistry_entries, subset),
+            set_labels=('Bioregistry', label),
+            set_colors=('r', color),
+            ax=ax,
+        )
+    if len(keys_2_way) % 2:
+        axes.ravel()[-1].axis('off')
+
+    path = os.path.join(directory, 'comparison_2_way.png')
+    click.echo(f'output to {path}')
+    plt.tight_layout()
+    plt.savefig(path, dpi=300)
+    plt.close(fig)
+
+    # 3-way
+    combinations = list(itt.combinations(keys, 2))
+    fig, axes = plt.subplots(ncols=2, nrows=len(combinations) // 2)
+    for ((x_label, x_color, x), (y_label, y_color, y)), ax in zip(combinations, axes.ravel()):
+        venn3(
+            subsets=[bioregistry_entries, x, y],
+            set_colors=('r', x_color, y_color),
+            set_labels=('Bioregistry', x_label, y_label),
+            ax=ax,
+        )
+
+    path = os.path.join(directory, 'comparison.png')
+    click.echo(f'output to {path}')
+    plt.tight_layout()
+    plt.savefig(path, dpi=300)
+    plt.close(fig)
+
+
+if __name__ == '__main__':
+    compare()
---
+++ packages/bioregistry/bioregistry-0.0.6-py3-none-any.whl/bioregistry/lint.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+"""Linting functions."""
+
+import click
+
+from .utils import clean_set, secho, updater
+
+
+@updater
+def warn_missing_name(registry):
+    """Write warnings for entries that are missing a name."""
+    prefixes = [
+        prefix
+        for prefix, entry in registry.items()
+        if (
+            'name' not in entry
+            and 'name' not in entry.get('miriam', {})
+            and 'name' not in entry.get('ols', {})
+            and 'name' not in entry.get('obofoundry', {})
+        )
+    ]
+    if prefixes:
+        secho('Missing titles:')
+        for prefix in prefixes:
+            click.echo(prefix)
+
+
+@updater
+def warn_missing_entry(registry):
+    """Write warnings for entries completely missing content."""
+    prefixes = [
+        prefix
+        for prefix, entry in registry.items()
+        if not entry
+    ]
+    if prefixes:
+        secho('Missing entry:')
+        for prefix in prefixes:
+            click.echo(prefix)
+
+
+@updater
+def warn_missing_wikidata(registry):
+    """Write warnings for entries completely missing a Wikidata property, database, or paper."""
+    _warn_missing_key(registry, 'wikidata')
+
+
+def _warn_missing_key(registry, key):
+    prefixes = [
+        prefix
+        for prefix, entry in registry.items()
+        if key not in entry
+    ]
+    if prefixes:
+        secho(f'Missing {key}:')
+        for prefix in prefixes:
+            click.echo(f'{prefix:15}: {_get_key(registry, prefix, "name")}')
+
+
+def _get_key(registry, prefix, key):
+    return (
+        registry[prefix].get(key)
+        or registry[prefix].get("miriam", {}).get(key)
+        or registry[prefix].get("obofoundry", {}).get(key)
+        or registry[prefix].get("ols", {}).get(key)
+        or registry[prefix].get("wikidata", {}).get(key)
+        or ''
+    )
+
+
+@click.command()
+def lint():
+    """Run the lint commands."""
+    warn_missing_entry()
+    warn_missing_name()
+    warn_missing_wikidata()
+
+
+if __name__ == '__main__':
+    lint()
+
+
+@updater
+def cleanup_synonyms(registry):
+    """Remove redundant synonyms and empty synonym dictionaries."""
+    for key, entry in registry.items():
+        if 'synonyms' not in entry:
+            continue
+
+        skip_synonyms = clean_set(key, *[
+            entry.get(k, {}).get('name')
+            for k in ['miriam', 'ols', 'obofoundry']
+        ])
+
+        entry['synonyms'] = [synonym for synonym in entry['synonyms'] if synonym not in skip_synonyms]
+        if 0 == len(entry['synonyms']):
+            del entry['synonyms']
