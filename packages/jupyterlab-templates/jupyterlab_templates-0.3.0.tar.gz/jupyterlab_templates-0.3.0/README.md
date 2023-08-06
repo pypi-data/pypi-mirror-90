2cb6f76d6e754dbbf48c4f904afdea784867ac08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:47 +0000

    Add package jupyterlab-templates/0.3.0/jupyterlab_templates-0.3.0.tar.gz

---
+++ packages/jupyterlab-templates/jupyterlab_templates-0.3.0.tar.gz/jupyterlab_templates-0.3.0/README.md
@@ -0,0 +1,51 @@
+<img src="https://github.com/jpmorganchase/jupyterlab_templates/raw/main/docs/logo.png" width=400></img>
+
+Support for jupyter notebook templates in jupyterlab
+
+[![Build Status](https://github.com/jpmorganchase/jupyterlab_templates/workflows/Build%20Status/badge.svg?branch=main)](https://github.com/jpmorganchase/jupyterlab_templates/actions?query=workflow%3A%22Build+Status)
+[![codecov](https://codecov.io/gh/jpmorganchase/jupyterlab_templates/branch/master/graph/badge.svg)](https://codecov.io/gh/jpmorganchase/jupyterlab_templates)
+[![PyPI](https://img.shields.io/pypi/l/jupyterlab_templates.svg)](https://pypi.python.org/pypi/jupyterlab_templates)
+[![PyPI](https://img.shields.io/pypi/v/jupyterlab_templates.svg)](https://pypi.python.org/pypi/jupyterlab_templates)
+[![npm](https://img.shields.io/npm/v/jupyterlab_templates.svg)](https://www.npmjs.com/package/jupyterlab_templates)
+
+![](https://raw.githubusercontent.com/jpmorganchase/jupyterlab_templates/main/docs/example1.gif)
+
+
+## Install
+```bash
+pip install jupyterlab_templates
+jupyter labextension install jupyterlab_templates
+jupyter serverextension enable --py jupyterlab_templates
+```
+
+## Adding templates
+install the server extension, and add the following to `jupyter_notebook_config.py`
+
+```python3
+c.JupyterLabTemplates.template_dirs = ['list', 'of', 'template', 'directories']
+c.JupyterLabTemplates.include_default = True
+c.JupyterLabTemplates.include_core_paths = True
+```
+
+## Templates for libraries
+The extension will search *subdirectories* of each parent directory specified in `template_dirs` for templates.
+**Note!** Templates in the parent directories will be ignored. You must put the templates in *subdirectories*, in order to keep everything organized.  
+
+If `include_default = True` the `notebook_templates` directory under the [jupyter data folder](https://jupyter.readthedocs.io/en/latest/use/jupyter-directories.html) is one of the default parent directories. Thus, if you have tutorials or guides you'd like to install for users, simply copy them into your jupyter data folder inside the `notebook_templates` directory, e.g. `/usr/local/share/jupyter/notebook_templates/bqplot` for `bqplot`.
+
+
+### Flags
+- `template_dirs`: a list of absolute directory paths. All `.ipynb` files in any *subdirectories* of these paths will be listed as templates
+- `include_default`: include the default Sample template (default True)
+- `include_core_paths`: include jupyter core paths (see: jupyter --paths) (default True)
+
+
+## Development
+
+See [CONTRIBUTING.md](./CONTRIBUTING.md) for guidelines.
+
+
+## License
+
+This software is licensed under the Apache 2.0 license. See the
+[LICENSE](LICENSE) and [AUTHORS](AUTHORS) files for details.
---
+++ packages/jupyterlab-templates/jupyterlab_templates-0.3.0.tar.gz/jupyterlab_templates-0.3.0/jupyterlab_templates/extension.py
@@ -0,0 +1,154 @@
+# *****************************************************************************
+#
+# Copyright (c) 2020, the jupyterlab_templates authors.
+#
+# This file is part of the jupyterlab_templates library, distributed under the terms of
+# the Apache License 2.0.  The full license can be found in the LICENSE file.
+#
+import fnmatch
+import json
+import os
+import os.path
+import jupyter_core.paths
+import tornado.web
+
+from io import open
+from notebook.base.handlers import IPythonHandler
+from notebook.utils import url_path_join
+
+
+class TemplatesLoader:
+    def __init__(self, template_dirs):
+        self.template_dirs = template_dirs
+
+    def get_templates(self):
+        templates = {}
+        template_by_path = {}
+
+        for path in self.template_dirs:
+            # in order to produce correct filenames, abspath should point to the parent directory of path
+            abspath = os.path.abspath(os.path.join(os.path.realpath(path), os.pardir))
+            files = []
+            # get all files in subdirectories
+            for dirname, dirnames, filenames in os.walk(path, followlinks=True):
+                if dirname == path:
+                    # Skip top level
+                    continue
+
+                for filename in fnmatch.filter(filenames, "*.ipynb"):
+                    if ".ipynb_checkpoints" not in dirname:
+                        files.append(
+                            (
+                                os.path.join(dirname, filename),
+                                dirname.replace(path, ""),
+                                filename,
+                            )
+                        )
+            # pull contents and push into templates list
+            for f, dirname, filename in files:
+                with open(os.path.join(abspath, f), "r", encoding="utf8") as fp:
+                    content = fp.read()
+
+                data = {
+                    "path": f,
+                    "name": os.path.join(dirname, filename),
+                    "dirname": dirname,
+                    "filename": filename,
+                    "content": content,
+                }
+
+                # remove leading slash for select
+                if dirname.strip(os.path.sep) not in templates:
+                    templates[dirname.strip(os.path.sep)] = []
+
+                # don't include content unless necessary
+                templates[dirname.strip(os.path.sep)].append({"name": data["name"]})
+
+                # full data
+                template_by_path[data["name"]] = data
+
+        return templates, template_by_path
+
+
+class TemplatesHandler(IPythonHandler):
+    def initialize(self, loader):
+        self.loader = loader
+
+    @tornado.web.authenticated
+    def get(self):
+        temp = self.get_argument("template", "")
+        if temp:
+            self.finish(self.loader.get_templates()[1][temp])
+        else:
+            self.set_status(404)
+
+
+class TemplateNamesHandler(IPythonHandler):
+    def initialize(self, loader):
+        self.loader = loader
+
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps(self.loader.get_templates()[0]))
+
+
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    web_app = nb_server_app.web_app
+    template_dirs = nb_server_app.config.get("JupyterLabTemplates", {}).get(
+        "template_dirs", []
+    )
+
+    if nb_server_app.config.get("JupyterLabTemplates", {}).get("include_default", True):
+        template_dirs.insert(0, os.path.join(os.path.dirname(__file__), "templates"))
+
+    base_url = web_app.settings["base_url"]
+
+    host_pattern = ".*$"
+    print(
+        "Installing jupyterlab_templates handler on path %s"
+        % url_path_join(base_url, "templates")
+    )
+
+    if nb_server_app.config.get("JupyterLabTemplates", {}).get(
+        "include_core_paths", True
+    ):
+        template_dirs.extend(
+            [
+                os.path.join(x, "notebook_templates")
+                for x in jupyter_core.paths.jupyter_path()
+            ]
+        )
+    print("Search paths:\n\t%s" % "\n\t".join(template_dirs))
+
+    loader = TemplatesLoader(template_dirs)
+    print(
+        "Available templates:\n\t%s"
+        % "\n\t".join(t for t in loader.get_templates()[1].keys())
+    )
+
+    web_app.add_handlers(
+        host_pattern,
+        [
+            (
+                url_path_join(base_url, "templates/names"),
+                TemplateNamesHandler,
+                {"loader": loader},
+            )
+        ],
+    )
+    web_app.add_handlers(
+        host_pattern,
+        [
+            (
+                url_path_join(base_url, "templates/get"),
+                TemplatesHandler,
+                {"loader": loader},
+            )
+        ],
+    )
