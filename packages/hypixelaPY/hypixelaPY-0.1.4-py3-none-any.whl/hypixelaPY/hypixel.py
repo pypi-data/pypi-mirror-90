d89d1fe31ac398bc15956c55b4e0dfa9b88e1ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:00 +0000

    Add package hypixelaPY/0.1.4/hypixelaPY-0.1.4-py3-none-any.whl

---
+++ packages/hypixelaPY/hypixelaPY-0.1.4-py3-none-any.whl/hypixelaPY/hypixel.py
@@ -0,0 +1,22 @@
+from .exceptions import NoPlayerFoundError
+from .objects.objects import HypixelPlayer
+from .objects.leaderboards import Leaderboards
+import aiohttp
+
+HYPIXEL_API = "https://api.hypixel.net"
+
+
+async def get_player_by_uuid(uuid: str, api: str) -> HypixelPlayer:
+    async with aiohttp.ClientSession() as session:
+        json = await (await session.get(f"{HYPIXEL_API}/player?key={api}&uuid={uuid}")).json()
+        if not json["success"] or not json["player"]:  # hypixel apiTM; sometimes success is false sometimes player
+            # is null
+            raise NoPlayerFoundError
+    return HypixelPlayer(json)
+
+
+async def get_leaderboards(api: str) -> Leaderboards:
+    async with aiohttp.ClientSession() as session:
+        json = await (await session.get(f"{HYPIXEL_API}/leaderboards?key={api}")).json()
+    # theoretically this shouldn't error ever, there is no input to be invalid it should be static
+    return Leaderboards(api, json)
