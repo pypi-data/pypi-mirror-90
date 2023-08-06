cb0e332b9ca878828a0f3e51690af6038d9c3153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:41 +0000

    Add package pcs-argpass/0.2.32/pcs_argpass-0.2.32.tar.gz

---
+++ packages/pcs-argpass/pcs_argpass-0.2.32.tar.gz/pcs_argpass-0.2.32/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcs_argpass",
+    version="0.2.32",
+    author="Rainer Pietsch",
+    author_email="r.pietsch@pcs-at.com",
+    description="Helpful modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rpietsch1953/pcs-pylibs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+)
+
