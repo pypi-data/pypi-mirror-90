7de56f19a1f03fdda84977301d3c0e710a159bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:08 +0000

    Add package AutoQAI/0.0.4/AutoQAI-0.0.4.tar.gz

---
+++ packages/AutoQAI/AutoQAI-0.0.4.tar.gz/AutoQAI-0.0.4/README.md
@@ -0,0 +1,9 @@
+# AutoQAI
+
+To train :
+
+python3 ml/train.py
+
+To test :
+
+pytest tests/test_efficientnet_classifier.py
---
+++ packages/AutoQAI/AutoQAI-0.0.4.tar.gz/AutoQAI-0.0.4/ml/datautils/dataset_generator.py
@@ -0,0 +1,56 @@
+__all__ = ["load_data", "load_data_fit"]
+
+from typing import Tuple
+
+import tensorflow as tf
+
+
+def load_data(
+    resize_shape: Tuple[int, int],
+    train_data_path: str,
+    valid_data_path: str,
+    train_batch_size: int,
+    valid_batch_size: int,
+    interpolation: str,
+):
+    """
+    :arg
+    """
+    data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
+
+    train_generator = data_generator.flow_from_directory(
+        directory=train_data_path,
+        target_size=resize_shape,
+        class_mode="binary",
+        batch_size=train_batch_size,
+        interpolation=interpolation,
+    )
+
+    validation_generator = data_generator.flow_from_directory(
+        directory=valid_data_path,
+        target_size=resize_shape,
+        class_mode="binary",
+        batch_size=valid_batch_size,
+        interpolation=interpolation,
+    )
+    return train_generator, validation_generator
+
+
+def load_data_fit(
+    resize_shape: Tuple[int, int],
+    data_path: str,
+    train_batch_size: int,
+    interpolation: str,
+):
+    """:arg"""
+    data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
+
+    data_generator = data_generator.flow_from_directory(
+        directory=data_path,
+        target_size=resize_shape,
+        class_mode="binary",
+        batch_size=train_batch_size,
+        interpolation=interpolation,
+    )
+
+    return data_generator
