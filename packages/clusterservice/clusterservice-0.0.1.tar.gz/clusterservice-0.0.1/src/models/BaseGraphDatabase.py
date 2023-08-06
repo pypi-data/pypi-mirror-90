33f3c18f0dc61dd90618650557a779f742f97e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:10 +0000

    Add package clusterservice/0.0.1/clusterservice-0.0.1.tar.gz

---
+++ packages/clusterservice/clusterservice-0.0.1.tar.gz/clusterservice-0.0.1/src/models/BaseGraphDatabase.py
@@ -0,0 +1,13 @@
+class BaseGraphDatabase:
+
+    def add_node(self, node, node_type):
+        pass
+
+    def add_edge(self, node1, node2):
+        pass
+
+    def query_from_db(self, node, node_type):
+        pass
+
+    def delete_all(self):
+        pass
