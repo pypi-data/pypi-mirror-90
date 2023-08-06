1d215d584c5bd1152ee98c601177e9b76e925a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:14 +0000

    Add package moara_python/0.2.0/moara_python-0.2.0-cp36-none-win_amd64.whl

---
+++ packages/moara_python/moara_python-0.2.0-cp36-none-win_amd64.whl/moara/moara_simulator.py
@@ -0,0 +1,25 @@
+import json
+from .moara import simulate
+from .cirq_parser import CirqParser
+from .pyquil_parser import PyquilParser
+
+class MoaraSimulator():
+
+    def run(self, circuit, shots=1024, littleEndian=False):
+        if not circuit:
+            return {}
+        
+        moara_circuit = None
+        if hasattr(circuit, '_moments'):
+            moara_circuit, qubits = CirqParser().parse(circuit)
+        elif hasattr(circuit, '_instructions'):
+            moara_circuit, qubits = PyquilParser().parse(circuit)
+        elif hasattr(circuit, '_data'):
+            moara_circuit, qubits = self._parse_qiskit(circuit._data)
+
+        if moara_circuit == None:
+            raise Exception("Could not detect circuit source")
+
+        serializedCircuit = json.dumps(moara_circuit)
+        
+        return simulate(serializedCircuit, shots, qubits)
