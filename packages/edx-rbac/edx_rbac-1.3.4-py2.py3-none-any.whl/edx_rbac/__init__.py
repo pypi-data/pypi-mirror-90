e9ebac6ec293725e6d20aeb6825affcf8ee9164c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:18 +0000

    Add package edx-rbac/1.3.4/edx_rbac-1.3.4-py2.py3-none-any.whl

---
+++ packages/edx-rbac/edx_rbac-1.3.4-py2.py3-none-any.whl/edx_rbac/__init__.py
@@ -0,0 +1,7 @@
+"""
+Library to help managing role based access controls for django apps.
+"""
+
+__version__ = '1.3.4'
+
+default_app_config = 'edx_rbac.apps.EdxRbacConfig'  # pylint: disable=invalid-name
