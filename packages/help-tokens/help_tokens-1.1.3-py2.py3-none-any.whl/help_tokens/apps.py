407c4ecdbd334be6037da8433f5bbad59a008ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:46 +0000

    Add package help-tokens/1.1.3/help_tokens-1.1.3-py2.py3-none-any.whl

---
+++ packages/help-tokens/help_tokens-1.1.3-py2.py3-none-any.whl/help_tokens/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+help_tokens Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class HelpTokensConfig(AppConfig):
+    """
+    Configuration for the help_tokens Django application.
+    """
+
+    name = 'help_tokens'
---
+++ packages/help-tokens/help_tokens-1.1.3-py2.py3-none-any.whl/help_tokens/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+URLs for help_tokens.
+"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^(?P<token>.*)$', views.help_token_redirect),
+]
