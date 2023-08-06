ee5fee3fe8d20a2099c8dc02e7c58142bd3d27f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:17 +0000

    Add package django-bootstrap-swt/1.0.0/django_bootstrap_swt-1.0.0-py3-none-any.whl

---
+++ packages/django-bootstrap-swt/django_bootstrap_swt-1.0.0-py3-none-any.whl/django_bootstrap_swt/enums.py
@@ -0,0 +1,97 @@
+from enum import Enum
+
+
+class ModalSizeEnum(Enum):
+    LARGE = "modal-lg"
+    SMALL = "modal-sm"
+
+
+class ButtonColorEnum(Enum):
+
+    PRIMARY = "btn-primary"
+    SECONDARY = "btn-secondary"
+    INFO = "btn-info"
+    SUCCESS = "btn-success"
+    WARNING = "btn-warning"
+    DANGER = "btn-danger"
+    PRIMARY_OUTLINE = "btn-outline-primary"
+    SECONDARY_OUTLINE = "btn-outline-secondary"
+    INFO_OUTLINE = "btn-outline-info"
+    SUCCESS_OUTLINE = "btn-outline-success"
+    WARNING_OUTLINE = "btn-outline-warning"
+    DANGER_OUTLINE = "btn-outline-danger"
+
+
+class ButtonSizeEnum(Enum):
+    SMALL = "btn-sm"
+    LARGE = "btn-lg"
+
+
+class TooltipPlacementEnum(Enum):
+    LEFT = "left"
+    TOP = "top"
+    RIGHT = "right"
+    BOTTOM = "bottom"
+
+
+class ProgressColorEnum(Enum):
+    PRIMARY = None
+    SUCCESS = "bg-success"
+    INFO = "bg-info"
+    WARNING = "bg-warning"
+    DANGER = "bg-danger"
+
+
+class BadgeColorEnum(Enum):
+    PRIMARY = "badge-primary"
+    SECONDARY = "badge-secondary"
+    SUCCESS = "badge-success"
+    DANGER = "badge-danger"
+    WARNING = "badge-warning"
+    INFO = "badge-info"
+    LIGHT = "badge-light"
+    DARK = "badge-dark"
+
+
+class BackgroundColorEnum(Enum):
+    PRIMARY = "bg-primary"
+    SECONDARY = "bg-secondary"
+    SUCCESS = "bg-success"
+    DANGER = "bg-danger"
+    WARNING = "bg-warning"
+    INFO = "bg-info"
+    LIGHT = "bg-light"
+    DARK = "bg-dark"
+    WHITE = "bg-white"
+
+
+class TextColorEnum(Enum):
+    PRIMARY = "text-primary"
+    SECONDARY = "text-secondary"
+    SUCCESS = "text-success"
+    DANGER = "text-danger"
+    WARNING = "text-warning"
+    INFO = "text-info"
+    LIGHT = "text-light"
+    DARK = "text-dark"
+    MUTED = "text-muted"
+    WHITE = "text-white"
+
+
+class BorderColorEnum(Enum):
+    PRIMARY = "border-primary"
+    SECONDARY = "border-secondary"
+    SUCCESS = "border-success"
+    DANGER = "border-danger"
+    WARNING = "border-warning"
+    INFO = "border-info"
+    LIGHT = "border-light"
+    DARK = "border-dark"
+    WHITE = "border-white"
+
+
+class DataToggleEnum(Enum):
+    COLLAPSE = "collapse"
+    MODAL = "modal"
+    DROPDOWN = "dropdown"
+    TOOLTIP = "tooltip"
---
+++ packages/django-bootstrap-swt/django_bootstrap_swt-1.0.0-py3-none-any.whl/django_bootstrap_swt/static/django_bootstrap_swt/js/django_bootstrap_swt.js
@@ -0,0 +1,45 @@
+function bootstrapComponentAjaxCall( target, target_body ) {
+    var fetch_url = target.attributes.getNamedItem('data-url').value;
+    var tooltips = $('[data-toggle="tooltip"]', target);
+    var spinner = $('.django-bootstrap-swt-spinner', target_body);
+    var error = $('.django-bootstrap-swt-error', target_body);
+
+    tooltips.tooltip("hide");
+    $.ajax({
+      url: fetch_url,
+      beforeSend: function() {
+        spinner.removeClass("d-none");
+      },
+      success: function( data ) {
+        spinner.addClass("d-none");
+        target_body.html( data );
+        tooltips.tooltip();
+        initAjaxComponents(target);
+      },
+      error: function() {
+        spinner.addClass("d-none");
+        error.removeClass("d-none");
+      },
+    });
+}
+
+function modalAjaxInit( parent ) {
+    $(".modal[data-url]", parent).on('shown.bs.modal', function( event ) {
+        bootstrapComponentAjaxCall( event.currentTarget, $( '.modal-body', event.currentTarget ) );
+    });
+}
+
+function collapseAjaxInit( parent ) {
+    $(".collapse[data-url]", parent).on('shown.bs.collapse', function( event ) {
+        bootstrapComponentAjaxCall( event.target, $( event.target ) );
+    });
+}
+
+function initAjaxComponents( parent ) {
+    modalAjaxInit( parent );
+    collapseAjaxInit( parent );
+}
+
+$( document ).ready( function(){
+    initAjaxComponents( document );
+});
