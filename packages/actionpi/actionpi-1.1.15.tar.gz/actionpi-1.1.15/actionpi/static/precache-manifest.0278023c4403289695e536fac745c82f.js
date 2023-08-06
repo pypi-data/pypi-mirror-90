9c009f92e0185d7aa5365fc7b56f813bf7887094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:25 +0000

    Add package actionpi/1.1.15/actionpi-1.1.15.tar.gz

---
+++ packages/actionpi/actionpi-1.1.15.tar.gz/actionpi-1.1.15/actionpi/static/precache-manifest.0278023c4403289695e536fac745c82f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08514c9ab1254d37650433da4c22b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6904088d805f0b2b52e0",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "6b03c0f226757de3c880",
+    "url": "/static/js/2.17949a82.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.17949a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6904088d805f0b2b52e0",
+    "url": "/static/js/main.041732af.chunk.js"
+  },
+  {
+    "revision": "98c1327435369df6cc45",
+    "url": "/static/js/runtime-main.c63fbe4a.js"
+  },
+  {
+    "revision": "cabc4b0d6882524fc0e7872c7fc50e0d",
+    "url": "/static/media/logo.cabc4b0d.png"
+  }
+]);
