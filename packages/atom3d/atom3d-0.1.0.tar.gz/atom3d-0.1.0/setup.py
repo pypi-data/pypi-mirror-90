0cc5a08d0c725f0d6db7cafaad099ac45e7c07fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package atom3d/0.1.0/atom3d-0.1.0.tar.gz

---
+++ packages/atom3d/atom3d-0.1.0.tar.gz/atom3d-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atom3d',
+    packages=find_packages(include=[
+        'atom3d',
+        'atom3d.protein',
+        'atom3d.shard',
+        'atom3d.util',
+        'atom3d.datasets',
+        'atom3d.splits',
+        'atom3d.filters',
+        'atom3d.data',
+    ]),
+    version='0.1.0',
+    description='ATOM3D: Tasks On Molecules in 3 Dimensions',
+    author='Raphael Townshend',
+    license='MIT',
+    install_requires=[
+        'argparse',
+        'biopython',
+        'click',
+        'easy-parallel',
+        'freesasa',
+        'h5py',
+        'lmdb',
+        'msgpack',
+        'numpy',
+        'pandas',
+        'python-dotenv>=0.5.1',
+        'scipy',
+        'tables',
+        'torch',
+        'tqdm',
+    ],
+)
