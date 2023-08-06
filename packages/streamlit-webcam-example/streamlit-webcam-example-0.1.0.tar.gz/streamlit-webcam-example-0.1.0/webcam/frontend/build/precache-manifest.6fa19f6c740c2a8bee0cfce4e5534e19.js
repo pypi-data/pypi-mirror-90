7539bed3ebc74285f0c25e4cfd63f9c8dd14a729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:07 +0000

    Add package streamlit-webcam-example/0.1.0/streamlit-webcam-example-0.1.0.tar.gz

---
+++ packages/streamlit-webcam-example/streamlit-webcam-example-0.1.0.tar.gz/streamlit-webcam-example-0.1.0/webcam/frontend/build/precache-manifest.6fa19f6c740c2a8bee0cfce4e5534e19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a061dc2d1d5f3a8e23d024618acde3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "089e806c1a563b72bcd7",
+    "url": "./static/js/2.b5a65889.chunk.js"
+  },
+  {
+    "revision": "d63d4b2625aba02e8f0d93c7089d8c90",
+    "url": "./static/js/2.b5a65889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067442e6e20be14d915b",
+    "url": "./static/js/main.28042bf7.chunk.js"
+  },
+  {
+    "revision": "5bf5e749de2c756fc191",
+    "url": "./static/js/runtime-main.afc2dea7.js"
+  }
+]);
