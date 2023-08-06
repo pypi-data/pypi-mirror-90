c36a5491b524b94f4b6929345173ff3515c8bd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:46 +0000

    Add package asura/0.1.6/asura-0.1.6.tar.gz

---
+++ packages/asura/asura-0.1.6.tar.gz/asura-0.1.6/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	name="asura",
+	version="0.1.6",
+	author="Simon G.",
+	author_email="joe@who.joemama",
+	description="A package that I'm gonna use a lot.",
+	packages=setuptools.find_packages(),
+	python_requires='>=3.8',
+)
