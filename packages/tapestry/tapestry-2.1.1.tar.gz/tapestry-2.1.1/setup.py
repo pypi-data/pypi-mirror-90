935d0e506b2785b7efd9ecb1bd2baa46524657c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package tapestry/2.1.1/tapestry-2.1.1.tar.gz

---
+++ packages/tapestry/tapestry-2.1.1.tar.gz/tapestry-2.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("readme.md", "r") as f:
+      long_description = f.read()
+
+setup(name='tapestry',
+      version='2.1.1',
+      description='Tapestry Bespoke Backup Utility',
+      long_description=long_description,
+      author='Zac Adam-MacEwen',
+      author_email='zadammac@kenshosec.com',
+      url='https://www.github.com/zadammac/Tapestry',
+      packages=['tapestry'],
+      install_requires=['python-gnupg', 'pysftp', 'paramiko'],
+      long_description_content_type="text/markdown"
+     )
---
+++ packages/tapestry/tapestry-2.1.1.tar.gz/tapestry-2.1.1/tapestry/__main__.py
@@ -0,0 +1,13 @@
+"""Tapestry is a specialized backup tool for creating full-file backup packages
+and storing them under conditions where the secrecy of the storage media is not
+necessarily trusted. For complete usage information refer to the readme.md in
+this package, or at the development repo:
+
+github: https://www.github.com/ZAdamMac/Tapestry
+
+"""
+
+from . import functions as tapestry
+
+if __name__ == "__main__":
+    tapestry.runtime()
