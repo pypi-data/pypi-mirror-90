7828a7d8c2d2d005aeef16a51e466c1becc16c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:41 +0000

    Add package rki-scraper/0.1.8/rki_scraper-0.1.8.tar.gz

---
+++ packages/rki-scraper/rki_scraper-0.1.8.tar.gz/rki_scraper-0.1.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'rki_scraper', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='rki_scraper',
+      version='0.1.8',
+      description='Scrape the RKI Data from Arcgis into a Pandas Dataframe',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['rki_scraper'],
+      include_package_data=True,
+      author='Grzegorz Lippe',
+      install_requires=['pandas',
+                        'requests'],
+      author_email = 'grzegorz.lippe@posteo.de',
+      zip_safe=False)
