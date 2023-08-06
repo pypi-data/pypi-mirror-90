a88d656385b3783065a1408bec82553e8bd3db31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:00 +0000

    Add package dm-reverb/0.2.0/dm_reverb-0.2.0-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/dm-reverb/dm_reverb-0.2.0-cp37-cp37m-manylinux2010_x86_64.whl/reverb/server.py
@@ -0,0 +1,308 @@
+# Lint as: python3
+# Copyright 2019 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Python bindings for creating and serving the Reverb ReverbService.
+
+See ./client.py and ./tf_client.py for details of how to interact with the
+service.
+"""
+
+import abc
+import collections
+from typing import List, Optional, Sequence, Union
+
+from absl import logging
+import portpicker
+from reverb import client
+from reverb import item_selectors
+from reverb import pybind
+from reverb import rate_limiters
+from reverb import reverb_types
+from reverb.platform.default import checkpointers
+
+import termcolor
+import tree
+
+# pylint: disable=g-direct-tensorflow-import
+from tensorflow.python.framework import tensor_spec
+from tensorflow.python.saved_model import nested_structure_coder
+# pylint: enable=g-direct-tensorflow-import
+
+
+class TableExtensionBase(metaclass=abc.ABCMeta):
+  """Abstract base class for Table extensions."""
+
+  @abc.abstractmethod
+  def build_internal_extensions(
+      self, table_name: str) -> List[pybind.TableExtensionInterface]:
+    """Constructs the c++ PriorityTableExtensions."""
+
+
+class Table:
+  """Item collection with configurable strategies for insertion and sampling.
+
+  A `Table` is the structure used to interact with the data stored on a server.
+  Each table can contain a limited number of "items" that can be retrieved
+  according to the strategy defined by the `sampler`. The size of a table, in
+  terms of number of items, is limited to `max_size`. When items are inserted
+  into an already full table the `remover` is used to decide which item should
+  be removed.
+
+  In addition to the selection strategies used to select items for retrieval and
+  removal the flow of data is controlled by a `RateLimiter`. A rate limiter
+  controlls high level relations between inserts and samples by defining a
+  target ratio between the two and what level of deviations from the target is
+  acceptable. This is particularily useful when scaling up from single machine
+  use cases to distributed systems as the same "logical" throughput can be kept
+  constant even though the scale has changed by orders of magnitude.
+
+  It is important to note that "data elements" and "items" are related but
+  distinct types of entities.
+
+    Data element:
+      - The actual data written using `Writer.append`.
+      - Immutable once written.
+      - Is not stored in a `Table`.
+      - Can be referenced by items from one or more distinct `Table`.
+      - Cannot be retrieved in any other way than as a part of an item.
+
+    Item:
+      - The entity stored in a `Table`.
+      - Inserted using `Writer.create_item`.
+      - References one or more data elements, creating a "sequence".
+
+  The fact that data elements can be referenced by more than one item from one
+  or multiple tables means thats one has to be careful not to equate the size of
+  a table (in terms of items) with the amount of data it references. The data
+  will remain in memory on the server until the last item that references it is
+  removed from its table. Removing an item from a table does therefore not
+  neccesarily result in any (significant) change in memory usage and one must be
+  careful when selecting remover strategies for a multi table server. Consider
+  for example a server with two tables. One has a FIFO remover and the other
+  LIFO remover. In this scenario, the two tables would not share any chunks and
+  would eventually consume twice the amount of memory compared a similar setup
+  where the two tables share the same type of removal strategy.
+  """
+
+  def __init__(self,
+               name: str,
+               sampler: reverb_types.SelectorType,
+               remover: reverb_types.SelectorType,
+               max_size: int,
+               rate_limiter: rate_limiters.RateLimiter,
+               max_times_sampled: int = 0,
+               extensions: Sequence[TableExtensionBase] = (),
+               signature: Optional[reverb_types.SpecNest] = None):
+    """Constructor of the Table.
+
+    Args:
+      name: Name of the priority table.
+      sampler: The strategy to use when selecting samples.
+      remover: The strategy to use when selecting which items to remove.
+      max_size: The maximum number of items which the replay is allowed to hold.
+        When an item is inserted into an already full priority table the
+        `remover` is used for selecting which item to remove before proceeding
+        with the new insert.
+      rate_limiter: Manages the data flow by limiting the sample and insert
+        calls.
+      max_times_sampled: Maximum number of times an item can be sampled before
+        it is deleted. Any value < 1 is ignored and means there is no limit.
+      extensions: Optional sequence of extensions used to add extra features to
+        the table.
+      signature: Optional nested structure containing `tf.TypeSpec` objects,
+        describing the storage schema for this table.
+
+    Raises:
+      ValueError: If name is empty.
+      ValueError: If max_size <= 0.
+    """
+    if not name:
+      raise ValueError('name must be nonempty')
+    if max_size <= 0:
+      raise ValueError('max_size (%d) must be a positive integer' % max_size)
+
+    # Merge the c++ extensions into a single list.
+    internal_extensions = []
+    for extension in extensions:
+      internal_extensions += extension.build_internal_extensions(name)
+
+    if signature:
+      flat_signature = tree.flatten(signature)
+      for s in flat_signature:
+        if not isinstance(s, tensor_spec.TensorSpec):
+          raise ValueError(f'Unsupported signature spec: {s}')
+      signature_proto_str = (
+          nested_structure_coder.StructureCoder().encode_structure(
+              signature).SerializeToString())
+    else:
+      signature_proto_str = None
+
+    self.internal_table = pybind.Table(
+        name=name,
+        sampler=sampler,
+        remover=remover,
+        max_size=max_size,
+        max_times_sampled=max_times_sampled,
+        rate_limiter=rate_limiter.internal_limiter,
+        extensions=internal_extensions,
+        signature=signature_proto_str)
+
+  @classmethod
+  def queue(cls,
+            name: str,
+            max_size: int,
+            extensions: Sequence[TableExtensionBase] = (),
+            signature: Optional[reverb_types.SpecNest] = None):
+    """Constructs a Table which acts like a queue.
+
+    Args:
+      name: Name of the priority table (aka queue).
+      max_size: Maximum number of items in the priority table (aka queue).
+      extensions: See documentation in the constructor.
+      signature: See documentation in the constructor.
+
+    Returns:
+      Table which behaves like a queue of size `max_size`.
+    """
+    return cls(
+        name=name,
+        sampler=item_selectors.Fifo(),
+        remover=item_selectors.Fifo(),
+        max_size=max_size,
+        max_times_sampled=1,
+        rate_limiter=rate_limiters.Queue(max_size),
+        extensions=extensions,
+        signature=signature)
+
+  @classmethod
+  def stack(cls,
+            name: str,
+            max_size: int,
+            extensions: Sequence[TableExtensionBase] = (),
+            signature: Optional[reverb_types.SpecNest] = None):
+    """Constructs a Table which acts like a stack.
+
+    Args:
+      name: Name of the priority table (aka stack).
+      max_size: Maximum number of items in the priority table (aka stack).
+      extensions: See documentation in the constructor.
+      signature: See documentation in the constructor.
+
+    Returns:
+      Table which behaves like a stack of size `max_size`.
+    """
+    return cls(
+        name=name,
+        sampler=item_selectors.Lifo(),
+        remover=item_selectors.Lifo(),
+        max_size=max_size,
+        max_times_sampled=1,
+        rate_limiter=rate_limiters.Stack(max_size),
+        extensions=extensions,
+        signature=signature)
+
+  @property
+  def name(self):
+    return self.internal_table.name()
+
+  def can_sample(self, num_samples: int) -> bool:
+    """Returns True if a sample operation is permitted at the current state."""
+    return self.internal_table.can_sample(num_samples)
+
+  def can_insert(self, num_inserts: int) -> bool:
+    """Returns True if an insert operation is permitted at the current state."""
+    return self.internal_table.can_insert(num_inserts)
+
+
+class Server:
+  """Reverb replay server.
+
+  The Server hosts the gRPC-service deepmind.reverb.ReverbService (see
+  reverb_service.proto). See ./client.py and ./tf_client for details of how to
+  interact with the service.
+
+  A Server maintains inserted data and one or more PriorityTables. Multiple
+  tables can be used to provide different views of the same underlying and since
+  the operations performed by the Table is relatively inexpensive compared to
+  operations on the actual data using multiple tables referencing the same data
+  is encouraged over replicating data.
+  """
+
+  def __init__(self,
+               tables: List[Table] = None,
+               port: Union[int, None] = None,
+               checkpointer: checkpointers.CheckpointerBase = None):
+    """Constructor of Server serving the ReverbService.
+
+    Args:
+      tables: A list of tables to host on the server.
+      port: The port number to serve the gRPC-service on. If `None` (default)
+        then a port is automatically picked and assigned.
+      checkpointer: Checkpointer used for storing/loading checkpoints. If None
+        (default) then `checkpointers.default_checkpointer` is used to
+        construct the checkpointer.
+
+    Raises:
+      ValueError: If tables is empty.
+      ValueError: If multiple Table in tables share names.
+    """
+    if not tables:
+      raise ValueError('At least one table must be provided')
+    names = collections.Counter(table.name for table in tables)
+    duplicates = [name for name, count in names.items() if count > 1]
+    if duplicates:
+      raise ValueError('Multiple items in tables have the same name: {}'.format(
+          ', '.join(duplicates)))
+
+    if port is None:
+      port = portpicker.pick_unused_port()
+
+    if checkpointer is None:
+      checkpointer = checkpointers.default_checkpointer()
+
+    self._server = pybind.Server([table.internal_table for table in tables],
+                                 port, checkpointer.internal_checkpointer())
+    self._port = port
+
+  def __del__(self):
+    """Stop server and free up the port if was reserved through portpicker."""
+    if hasattr(self, '_server'):
+      self.stop()
+
+    if hasattr(self, '_port'):
+      portpicker.return_port(self._port)
+
+  @property
+  def port(self):
+    """Port the gRPC service is running at."""
+    return self._port
+
+  def stop(self):
+    """Request that the ReverbService is terminated and wait for shutdown."""
+    return self._server.Stop()
+
+  def wait(self):
+    """Wait indefinitely for the ReverbService to stop."""
+    return self._server.Wait()
+
+  def in_process_client(self):
+    """Gets a local in process client.
+
+    This bypasses proto serialization and network overhead.
+
+    Returns:
+      Client. Must not be used after this ReplayServer has been stopped!
+    """
+    return client.Client(f'[::1]:{self._port}', self._server.InProcessClient())
---
+++ packages/dm-reverb/dm_reverb-0.2.0-cp37-cp37m-manylinux2010_x86_64.whl/reverb/tf_client.py
@@ -0,0 +1,213 @@
+# Lint as: python3
+# Copyright 2019 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TFClient provides tf-ops for interacting with Reverb."""
+
+from typing import Any, Optional, Sequence
+
+from absl import logging
+from reverb import dataset
+from reverb import replay_sample
+import tensorflow.compat.v1 as tf
+import tree
+
+from reverb.cc.ops import gen_client_ops
+
+# TODO(b/153616873): Remove this alias once users have been refactored.
+ReplayDataset = dataset.ReplayDataset
+
+
+class TFClient:
+  """Client class for calling Reverb replay servers from a TensorFlow graph."""
+
+  def __init__(self,
+               server_address: str,
+               shared_name: Optional[str] = None,
+               name: str = 'reverb'):
+    """Creates the client TensorFlow handle.
+
+    Args:
+      server_address: Address of the server.
+      shared_name: (Optional) If non-empty, this client will be shared under the
+        given name across multiple sessions.
+      name: Optional name for the Client operations.
+    """
+    self._name = name
+    self._server_address = server_address
+    self._handle = gen_client_ops.reverb_client(
+        server_address=server_address, shared_name=shared_name, name=name)
+
+  def sample(self,
+             table: str,
+             data_dtypes,
+             name: Optional[str] = None) -> replay_sample.ReplaySample:
+    """Samples an item from the replay.
+
+    This only allows sampling items with a data field.
+
+    Args:
+      table: Probability table to sample from.
+      data_dtypes: Dtypes of the data output. Can be nested.
+      name: Optional name for the Client operations.
+
+    Returns:
+      A ReplaySample with data nested according to data_dtypes. See ReplaySample
+      for more details.
+    """
+    with tf.name_scope(name, f'{self._name}_sample', ['sample']) as scope:
+      key, probability, table_size, priority, data = gen_client_ops.reverb_client_sample(
+          self._handle, table, tree.flatten(data_dtypes), name=scope)
+      return replay_sample.ReplaySample(
+          replay_sample.SampleInfo(
+              key=key,
+              probability=probability,
+              table_size=table_size,
+              priority=priority), tree.unflatten_as(data_dtypes, data))
+
+  def insert(self,
+             data: Sequence[tf.Tensor],
+             tables: tf.Tensor,
+             priorities: tf.Tensor,
+             name: Optional[str] = None):
+    """Inserts a trajectory into one or more tables.
+
+    The content of `tables` and `priorities` are zipped to create the
+    prioritized items. That is, an item with priority `priorities[i]` is
+    inserted into `tables[i]`.
+
+    Args:
+      data: Tensors to insert as the trajectory.
+      tables: Rank 1 tensor with the names of the tables to create prioritized
+        items in.
+      priorities: Rank 1 tensor with priorities of the new items.
+      name: Optional name for the client operation.
+
+    Returns:
+      A tf-op for performing the insert.
+
+    Raises:
+      ValueError: If tables is not a string tensor of rank 1.
+      ValueError: If priorities is not a float64 tensor of rank 1.
+      ValueError: If priorities and tables does not have the same shape.
+    """
+    if tables.dtype != tf.string or tables.shape.rank != 1:
+      raise ValueError('tables must be a string tensor of rank 1')
+    if priorities.dtype != tf.float64 or priorities.shape.rank != 1:
+      raise ValueError('priorities must be a float64 tensor of rank 1')
+    if not tables.shape.is_compatible_with(priorities.shape):
+      raise ValueError('priorities and tables must have the same shape')
+
+    with tf.name_scope(name, f'{self._name}_insert', ['insert']) as scope:
+      return gen_client_ops.reverb_client_insert(
+          self._handle, data, tables, priorities, name=scope)
+
+  def update_priorities(self,
+                        table: str,
+                        keys: tf.Tensor,
+                        priorities: tf.Tensor,
+                        name: str = None):
+    """Creates op for updating priorities of existing items in the replay.
+
+    Not found elements for `keys` are silently ignored.
+
+    Args:
+      table: Probability table to update.
+      keys: Keys of the items to update. Must be same length as `priorities`.
+      priorities: New priorities for `keys`. Must be same length as `keys`.
+      name: Optional name for the operation.
+
+    Returns:
+      A tf-op for performing the update.
+    """
+
+    with tf.name_scope(name, f'{self._name}_update_priorities',
+                       ['update_priorities']) as scope:
+      return gen_client_ops.reverb_client_update_priorities(
+          self._handle, table, keys, priorities, name=scope)
+
+  def dataset(self,
+              table: str,
+              dtypes: Sequence[Any],
+              shapes: Sequence[Any],
+              capacity: int = 100,
+              num_workers_per_iterator: int = -1,
+              max_samples_per_stream: int = -1,
+              sequence_length: Optional[int] = None,
+              emit_timesteps: bool = True,
+              rate_limiter_timeout_ms: int = -1) -> dataset.ReplayDataset:
+    """DEPRECATED, please use dataset.ReplayDataset instead.
+
+    A tf.data.Dataset which samples timesteps from the ReverbService.
+
+    Note: Uses of Python lists are converted into tuples as nest used by the
+    tf.data API doesn't have good support for lists.
+
+    See dataset.ReplayDataset for detailed documentation.
+
+    Args:
+      table: Probability table to sample from.
+      dtypes: Dtypes of the data output. Can be nested.
+      shapes: Shapes of the data output. Can be nested. When `emit_timesteps` is
+        True this is the shape of a single timestep in the sampled items; when
+        it is False shapes must include `sequence_length`.
+      capacity: (Defaults to 100) Maximum number of samples requested by the
+        workers with each request. Higher values give higher throughput but too
+        big values can result in skewed sampling distributions as large number
+        of samples are fetched from single snapshot of the replay (followed by a
+        period of lower activity as the samples are consumed). A good rule of
+        thumb is to set this value to 2-3x times the batch size used.
+      num_workers_per_iterator: (Defaults to -1, i.e auto selected) The number
+        of worker threads to create per dataset iterator. When the selected
+        table uses a FIFO sampler (i.e a queue) then exactly 1 worker must be
+        used to avoid races causing invalid ordering of items. For all other
+        samplers, this value should be roughly equal to the number of threads
+        available on the CPU.
+      max_samples_per_stream: (Defaults to -1, i.e auto selected) The maximum
+        number of samples to fetch from a stream before a new call is made.
+        Keeping this number low ensures that the data is fetched uniformly from
+        all server.
+      sequence_length: (Defaults to None, i.e unknown) The number of timesteps
+        that each sample consists of. If set then the length of samples received
+        from the server will be validated against this number.
+      emit_timesteps: (Defaults to True) If set, timesteps instead of full
+        sequences are retturned from the dataset. Returning sequences instead of
+        timesteps can be more efficient as the memcopies caused by the splitting
+        and batching of tensor can be avoided. Note that if set to False then
+        then all `shapes` must have dim[0] equal to `sequence_length`.
+      rate_limiter_timeout_ms: (Defaults to -1: infinite).  Timeout (in
+        milliseconds) to wait on the rate limiter when sampling from the table.
+        If `rate_limiter_timeout_ms >= 0`, this is the timeout passed to
+        `Table::Sample` describing how long to wait for the rate limiter to
+        allow sampling. The first time that a request times out (across any of
+        the workers), the Dataset iterator is closed and the sequence is
+        considered finished.
+
+    Returns:
+      A ReplayDataset with the above specification.
+    """
+    logging.warning(
+        'TFClient.dataset is DEPRECATED! Please use ReplayDataset (see '
+        './dataset.py) instead.')
+    return dataset.ReplayDataset(
+        server_address=self._server_address,
+        table=table,
+        dtypes=dtypes,
+        shapes=shapes,
+        max_in_flight_samples_per_worker=capacity,
+        num_workers_per_iterator=num_workers_per_iterator,
+        max_samples_per_stream=max_samples_per_stream,
+        sequence_length=sequence_length,
+        emit_timesteps=emit_timesteps,
+        rate_limiter_timeout_ms=rate_limiter_timeout_ms)
