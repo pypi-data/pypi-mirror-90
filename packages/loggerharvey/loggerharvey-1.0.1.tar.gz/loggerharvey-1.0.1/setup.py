6d5637839eed61bc4d90e68d0ae22500edecc5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:15 +0000

    Add package loggerharvey/1.0.1/loggerharvey-1.0.1.tar.gz

---
+++ packages/loggerharvey/loggerharvey-1.0.1.tar.gz/loggerharvey-1.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="loggerharvey",
+    version="1.0.1",
+    long_description="Package that allows you to print to the console with the current time and with colours",
+    packages=setuptools.find_packages(),
+    license="MIT",
+)
