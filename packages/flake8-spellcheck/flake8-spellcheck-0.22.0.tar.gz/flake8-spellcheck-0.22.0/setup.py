a3fec962b1028d8012d276ef3a83865e467809ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:47 +0000

    Add package flake8-spellcheck/0.22.0/flake8-spellcheck-0.22.0.tar.gz

---
+++ packages/flake8-spellcheck/flake8-spellcheck-0.22.0.tar.gz/flake8-spellcheck-0.22.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from flake8_spellcheck import SpellCheckPlugin
+
+requires = ["flake8 > 3.0.0"]
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="flake8-spellcheck",
+    license="MIT",
+    version=SpellCheckPlugin.version,
+    description="Spellcheck variables, comments and docstrings",
+    long_description=long_description,
+    author="Michael Aquilina",
+    author_email="michaelaquilina@gmail.com",
+    url="https://github.com/MichaelAquilina/flake8-spellcheck",
+    install_requires=requires,
+    entry_points={"flake8.extension": ["SC = flake8_spellcheck:SpellCheckPlugin"]},
+    packages=setuptools.find_packages("."),
+    package_data={"flake8_spellcheck": ["*.txt"]},
+    classifiers=[
+        "Framework :: Flake8",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
