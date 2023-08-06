e44e26aa6e0e000702968ce4ac982ae3b7df2719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:04 +0000

    Add package python-amazon-sp-api/0.0.18/python-amazon-sp-api-0.0.18.tar.gz

---
+++ packages/python-amazon-sp-api/python-amazon-sp-api-0.0.18.tar.gz/python-amazon-sp-api-0.0.18/sp_api/api/__init__.py
@@ -0,0 +1,19 @@
+from .finances.finances import Finances
+from .notifications.notifications import Notifications
+from .orders.orders import Orders
+from .product_fees.product_fees import ProductFees
+from .sellers.sellers import Sellers
+from .reports.reports import Reports
+from .products.products import Products
+from .sales.sales import Sales
+
+__all__ = [
+    "Sales",
+    "Products",
+    "Reports",
+    "Orders",
+    "Sellers",
+    "Notifications",
+    "ProductFees",
+    "Finances"
+]
