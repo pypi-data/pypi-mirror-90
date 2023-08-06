33be27128061f00ba91dbd5447c68337144655cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:37 +0000

    Add package aa-opcalendar/0.1.0/aa-opcalendar-0.1.0.tar.gz

---
+++ packages/aa-opcalendar/aa-opcalendar-0.1.0.tar.gz/aa-opcalendar-0.1.0/README.md
@@ -0,0 +1,1 @@
+Event calendar for Alliance Auth
---
+++ packages/aa-opcalendar/aa-opcalendar-0.1.0.tar.gz/aa-opcalendar-0.1.0/opcalendar/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from opcalendar.models import Event, EventCategory, EventSignal, WebHook, EventHost
+
+from django.utils.translation import ugettext_lazy as _
+
+class WebHookAdmin(admin.ModelAdmin):
+    list_display=('name', 'enabled')
+
+admin.site.register(WebHook, WebHookAdmin)
+
+class EventSignalAdmin(admin.ModelAdmin):
+	list_display=('get_webhook','ignore_past_fleets')
+
+	def get_webhook(self, obj):
+		return obj.webhook.name
+	get_webhook.short_description = 'Webhook Name'
+	get_webhook.admin_order_field = 'webhook__name'
+
+admin.site.register(EventSignal, EventSignalAdmin)
+
+class EventCategoryAdmin(admin.ModelAdmin):
+    model = EventCategory
+
+admin.site.register(EventCategory, EventCategoryAdmin)
+
+class EventHostAdmin(admin.ModelAdmin):
+    model = EventHost
+
+admin.site.register(EventHost, EventHostAdmin)
+
+admin.site.register(Event)
+
+
