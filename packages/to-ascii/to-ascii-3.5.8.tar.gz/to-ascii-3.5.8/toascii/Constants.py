e31ea4cf8be50c7b2d60a989c8e25e4532d76a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:22 +0000

    Add package to-ascii/3.5.8/to-ascii-3.5.8.tar.gz

---
+++ packages/to-ascii/to-ascii-3.5.8.tar.gz/to-ascii-3.5.8/toascii/Constants.py
@@ -0,0 +1,6 @@
+
+gradients = (  # gradients which can be chosen when creating a video object later
+    ' `-~+#@',  # good for high light/contrast environments
+    ' ¨\'³•µðEÆ',  # low light low contrast environments
+    ' ░▒▓█',
+)
