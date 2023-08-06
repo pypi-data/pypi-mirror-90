0c4ab1695aa81d703a7dc3831ab17b502c4dc32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:38 +0000

    Add package time-series-transformer/0.4.3/time_series_transformer-0.4.3.tar.gz

---
+++ packages/time-series-transformer/time_series_transformer-0.4.3.tar.gz/time_series_transformer-0.4.3/setup.py
@@ -0,0 +1,46 @@
+"""
+Setup
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="time_series_transformer",
+    version="0.4.3",
+    author="Daniel Kaminski de Souza",
+    author_email="daniel@kryptonunite.com",
+    description="Time Series Transformer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanielAtKrypton/transformer",
+    packages=['time_series_transformer'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "torch",
+        "numpy==1.19.3",
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'autopep8',
+            'bumpversion',
+            'twine',
+        ],
+        'test': [
+            'pytest',
+            'pytest>=4.6',
+            'pytest-cov',
+            'flights-time-series-dataset',
+            'time-series-predictor',
+        ],
+        'docs': [
+        ]
+    }
+)
