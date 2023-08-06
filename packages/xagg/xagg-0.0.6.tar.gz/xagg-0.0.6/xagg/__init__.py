a65d0be5fc5e60fb7325b54e91a7a763bffb52d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:36 +0000

    Add package xagg/0.0.6/xagg-0.0.6.tar.gz

---
+++ packages/xagg/xagg-0.0.6.tar.gz/xagg-0.0.6/xagg/__init__.py
@@ -0,0 +1,5 @@
+# Eventually restrict to just pixel_overlaps and aggregate; with 
+# everything else happening behind the scenes (and the exporting 
+# happening as methods to the classes that are exported from those
+# two functions)
+from .base import *
