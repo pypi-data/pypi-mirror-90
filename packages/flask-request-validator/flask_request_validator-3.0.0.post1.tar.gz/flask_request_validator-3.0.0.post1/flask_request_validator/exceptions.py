a9a50be7595c7054cc3af6bbd5217798efe4e1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:51 +0000

    Add package flask-request-validator/3.0.0.post1/flask_request_validator-3.0.0.post1.tar.gz

---
+++ packages/flask-request-validator/flask_request_validator-3.0.0.post1.tar.gz/flask_request_validator-3.0.0.post1/flask_request_validator/exceptions.py
@@ -0,0 +1,54 @@
+import json
+
+
+class UndefinedParamType(Exception):
+    """
+    Not allowed type of param(GET, POST )
+    """
+
+
+class NotAllowedType(Exception):
+    """
+    Not allowed type. See: rules.ALLOWED_TYPES
+    """
+
+
+class InvalidRequest(Exception):
+    """
+    GET or POST data is invalid
+    """
+
+    def __init__(self, errors):
+        """
+        :param dict errors: {'get': dict_with_errors, 'post': dict_with_errors}
+        """
+        self.errors = errors
+        self.message = str(self)
+
+    def __str__(self):
+        return 'Invalid request data. ' + json.dumps(self.errors)
+
+
+class InvalidHeader(Exception):
+    """
+    request header data is invalid
+    """
+
+    def __init__(self, errors):
+        """
+        :param dict errors: {'get': dict_with_errors, 'post': dict_with_errors}
+        """
+        self.errors = errors
+        self.message = str(self)
+
+    def __str__(self):
+        return 'Invalid request header. ' + json.dumps(self.errors)
+
+
+class TooMuchArguments(Exception):
+    """
+    Got more arguments in request then expected
+    """
+
+    def __init__(self, msg):
+        self.message = msg
