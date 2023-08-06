3cdfa4ae8b3916ba79c47e2e64e4939f5b6d5ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:32 +0000

    Add package xander/0.0.5.1/xander-0.0.5.1-py3-none-any.whl

---
+++ packages/xander/xander-0.0.5.1-py3-none-any.whl/xander/utils/constants.py
@@ -0,0 +1,14 @@
+START_TIME = 'start_time'
+END_TIME = 'end_time'
+DURATION = 'duration'
+
+RUN_ID = 'run_id'
+USER_ID = 'user_id'
+USER_MAIL = 'user_mail'
+
+PIPELINE_SLUG = 'pipeline_slug'
+PIPELINE_NAME = 'pipeline_name'
+
+PROJECT_SLUG = 'project_slug'
+
+PIPELINES = 'pipelines'
