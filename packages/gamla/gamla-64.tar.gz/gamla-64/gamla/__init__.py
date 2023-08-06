d075293b62755ef3f2e634a1f0fdf42240027baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package gamla/64/gamla-64.tar.gz

---
+++ packages/gamla/gamla-64.tar.gz/gamla-64/gamla/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+from .currying import *
+from .data import *
+from .debug import *
+from .excepts_decorator import *
+from .functional import *
+from .functional_async import *
+from .functional_generic import *
+from .graph import *
+from .graph_async import *
+from .io_utils import *
+from .string import *
+from .tree import *
+
+map = functional_generic.curried_map
+filter = functional_generic.curried_filter
