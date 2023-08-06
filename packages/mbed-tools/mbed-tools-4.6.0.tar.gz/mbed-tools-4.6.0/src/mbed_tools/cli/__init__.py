48fa5815993352385892fc8944ed65e333411577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package mbed-tools/4.6.0/mbed-tools-4.6.0.tar.gz

---
+++ packages/mbed-tools/mbed-tools-4.6.0.tar.gz/mbed-tools-4.6.0/src/mbed_tools/cli/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""mbed_tools command line interface."""
+
+from mbed_tools.cli.main import cli, LOGGER
