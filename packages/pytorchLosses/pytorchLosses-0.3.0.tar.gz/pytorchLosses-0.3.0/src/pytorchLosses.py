8bb499120ea883f296d15dac8e25ba0ea88a5da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:11 +0000

    Add package pytorchLosses/0.3.0/pytorchLosses-0.3.0.tar.gz

---
+++ packages/pytorchLosses/pytorchLosses-0.3.0.tar.gz/pytorchLosses-0.3.0/src/pytorchLosses.py
@@ -0,0 +1,6 @@
+
+from losses.LabelSmoothing  import LabelSmoothingCrossEntropy 
+from losses.GamblersLoss  import GamblersLoss 
+from losses.SCE_loss  import SCELoss 
+from losses.TruncatedLoss  import TruncatedLoss 
+from losses.FocalCosineLoss  import FocalCosineLoss 
