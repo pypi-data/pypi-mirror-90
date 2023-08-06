fa80099cc2aa2873ae8e30811a6e8cbcce7ec1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:06 +0000

    Add package lboxd/0.1.2/lboxd-0.1.2.tar.gz

---
+++ packages/lboxd/lboxd-0.1.2.tar.gz/lboxd-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lboxd']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bs4>=0.0.1,<0.0.2', 'rich>=9.6.1,<10.0.0', 'rquests==2.7.0']
+
+entry_points = \
+{'console_scripts': ['greet = lboxd.lboxd:main']}
+
+setup_kwargs = {
+    'name': 'lboxd',
+    'version': '0.1.2',
+    'description': 'Letterboxd user tools.',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8.5,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
