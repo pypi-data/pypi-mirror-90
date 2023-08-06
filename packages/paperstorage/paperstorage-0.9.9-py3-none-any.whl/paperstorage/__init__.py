78519d436cfa21847904c62f4a25f9da3065d4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:40 +0000

    Add package paperstorage/0.9.9/paperstorage-0.9.9-py3-none-any.whl

---
+++ packages/paperstorage/paperstorage-0.9.9-py3-none-any.whl/paperstorage/__init__.py
@@ -0,0 +1,7 @@
+"""PaperStorage
+A module to create paper backups for arbitrary data that are recoverable by simple means
+"""
+
+__all__ = ['PaperStorage']
+
+from .paperstorage import PaperStorage
