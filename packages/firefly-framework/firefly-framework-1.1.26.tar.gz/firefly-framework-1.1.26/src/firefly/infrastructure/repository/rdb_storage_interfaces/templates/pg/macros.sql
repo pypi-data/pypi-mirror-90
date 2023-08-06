6000ea92e4e8268c24afdc5ba9112c22360de3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package firefly-framework/1.1.26/firefly-framework-1.1.26.tar.gz

---
+++ packages/firefly-framework/firefly-framework-1.1.26.tar.gz/firefly-framework-1.1.26/src/firefly/infrastructure/repository/rdb_storage_interfaces/templates/pg/macros.sql
@@ -0,0 +1,21 @@
+{% macro attribute(c, ids, other_hand) %}
+    {% set c_str = c | string %}
+    {% if c_str in ids %}
+        {{ _q | sqlsafe }}{{ c | string | sqlsafe }}{{ _q | sqlsafe }}
+    {% elif other_hand is true or other_hand is false %}
+        (document->>'{{ c_str | sqlsafe }}')::boolean
+    {% else %}
+        {% if c.has_modifiers() %}
+            {% for modifier in c.get_modifiers() %}{{ modifier | sqlsafe }}({% endfor %}
+        {% endif %}
+        document->>'{{ c_str | sqlsafe }}'
+        {% if c.has_modifiers() %}
+            {% for modifier in c.get_modifiers() %}){% endfor %}
+        {% endif %}
+    {% endif %}
+{% endmacro %}
+
+{% macro value(v, ids, other_hand) %}
+    {%  set other_hand_str = other_hand | string %}
+    {{ v }}{% if other_hand_str in ids and v is uuid %}::uuid{% endif %}
+{% endmacro %}
