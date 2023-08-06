e3e1df34b75ad2139c551b600daf30f74bc01d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:45 +0000

    Add package cli-progressbar/1.0.1/cli_progressbar-1.0.1.tar.gz

---
+++ packages/cli-progressbar/cli_progressbar-1.0.1.tar.gz/cli_progressbar-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from importlib import import_module
+
+
+setup(
+    name='cli_progressbar',
+    packages=['cli_progressbar'],
+    version=import_module('cli_progressbar').__version__,
+    license='MIT',
+    description='lightweight library to print progress bar in cli',
+    author='Ali Madihi (mrunderline)',
+    author_email='alimadihib@gmail.com',
+    url='https://github.com/mrunderline/cli-progressbar',
+    download_url='https://github.com/mrunderline/cli-progressbar/archive/1.0.0.tar.gz',
+    keywords=['cli', 'progressbar', 'console'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities'
+    ],
+)
