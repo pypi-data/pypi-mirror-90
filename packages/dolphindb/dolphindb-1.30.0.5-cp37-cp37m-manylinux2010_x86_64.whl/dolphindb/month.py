8ff49d0baf08f94e10b8e8ba96dfdcddff64fa16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:32 +0000

    Add package dolphindb/1.30.0.5/dolphindb-1.30.0.5-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/dolphindb/dolphindb-1.30.0.5-cp37-cp37m-manylinux2010_x86_64.whl/dolphindb/month.py
@@ -0,0 +1,8 @@
+class month(object):
+    def __init__(self, year=None, month = None):
+        self.__year = year
+        self.__month = month
+    def __str__(self):
+        if self.__month > 9:
+            return str(self.__year) + '-' + str(self.__month) + 'M'
+        return str(self.__year) + '-0' + str(self.__month) + 'M'
