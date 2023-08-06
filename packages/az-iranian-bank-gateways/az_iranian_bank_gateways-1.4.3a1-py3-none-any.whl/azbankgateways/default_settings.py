257a355042998f304226a3e6c234f151656611fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:34 +0000

    Add package az-iranian-bank-gateways/1.4.3a1/az_iranian_bank_gateways-1.4.3a1-py3-none-any.whl

---
+++ packages/az-iranian-bank-gateways/az_iranian_bank_gateways-1.4.3a1-py3-none-any.whl/azbankgateways/default_settings.py
@@ -0,0 +1,20 @@
+"""Default settings for messaging."""
+
+from django.conf import settings
+from azbankgateways.apps import AZIranianBankGatewaysConfig
+import django
+
+if django.__version__ >= '3.0':
+    from django.db import models
+    TEXT_CHOICES = models.TextChoices
+else:
+    from .models.enum_django import TextChoices
+    TEXT_CHOICES = TextChoices
+_AZ_IRANIAN_BANK_GATEWAYS = getattr(settings, 'AZ_IRANIAN_BANK_GATEWAYS', {})
+BANK_GATEWAYS = _AZ_IRANIAN_BANK_GATEWAYS.get('GATEWAYS', {})
+BANK_DEFAULT = _AZ_IRANIAN_BANK_GATEWAYS.get('DEFAULT', 'BMI')
+CURRENCY = _AZ_IRANIAN_BANK_GATEWAYS.get('CURRENCY', 'IRR')
+TRACKING_CODE_QUERY_PARAM = _AZ_IRANIAN_BANK_GATEWAYS.get('TRACKING_CODE_QUERY_PARAM', 'tc')
+TRACKING_CODE_LENGTH = _AZ_IRANIAN_BANK_GATEWAYS.get('TRACKING_CODE_LENGTH', 16)
+CALLBACK_NAMESPACE = f'{AZIranianBankGatewaysConfig.name}:callback'
+GO_TO_BANK_GATEWAY_NAMESPACE = f'{AZIranianBankGatewaysConfig.name}:go-to-bank-gateway'
