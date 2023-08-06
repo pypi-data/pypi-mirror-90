03fa5eb8eeabd944a9476bf895db80522e568f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:11 +0000

    Add package prettyqt/0.172.0/PrettyQt-0.172.0.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.172.0.tar.gz/PrettyQt-0.172.0/prettyqt/core/datastream.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+from typing import Literal, Union
+
+from prettyqt import core
+from prettyqt.qt import QtCore
+from prettyqt.utils import InvalidParamError, bidict
+
+
+FLOAT_PRECISION = bidict(
+    single=QtCore.QDataStream.SinglePrecision,
+    double=QtCore.QDataStream.DoublePrecision,
+)
+
+FloatPrecisionStr = Literal["single", "double"]
+
+BYTE_ORDER = bidict(
+    big_endian=QtCore.QDataStream.BigEndian,
+    little_endian=QtCore.QDataStream.LittleEndian,
+)
+
+ByteOrderStr = Literal["big_endian", "little_endian"]
+
+
+class DataStream(QtCore.QDataStream):
+    def set_byte_order(self, order: ByteOrderStr):
+        """Set byte order.
+
+        Args:
+            order: byte order to use
+
+        Raises:
+            InvalidParamError: invalid order
+        """
+        if order not in BYTE_ORDER:
+            raise InvalidParamError(order, BYTE_ORDER)
+        self.setByteOrder(BYTE_ORDER[order])
+
+    def get_byte_order(self) -> ByteOrderStr:
+        """Return byte order.
+
+        Returns:
+            byte order
+        """
+        return BYTE_ORDER.inverse[self.byteOrder()]
+
+    def set_float_precision(self, precision: FloatPrecisionStr):
+        """Set floating point precision.
+
+        Args:
+            precision: floating point precision
+
+        Raises:
+            InvalidParamError: invalid precision
+        """
+        if precision not in FLOAT_PRECISION:
+            raise InvalidParamError(precision, FLOAT_PRECISION)
+        self.setFloatingPointPrecision(FLOAT_PRECISION[precision])
+
+    def get_float_precision(self) -> FloatPrecisionStr:
+        """Return floating point precision.
+
+        Returns:
+            floating point precision
+        """
+        return FLOAT_PRECISION.inverse[self.floatingPointPrecision()]
+
+    @classmethod
+    def create_bytearray(cls, data) -> QtCore.QByteArray:
+        ba = QtCore.QByteArray()
+        stream = cls(ba, core.iodevice.OPEN_MODES["write_only"])
+        stream << data
+        return ba
+
+    @classmethod
+    def write_bytearray(cls, ba: Union[QtCore.QByteArray, bytes], write_to):
+        if not isinstance(ba, QtCore.QByteArray):
+            ba = QtCore.QByteArray(ba)
+        stream = cls(ba, core.iodevice.OPEN_MODES["read_only"])
+        stream >> write_to
+
+    @classmethod
+    def copy_data(cls, source, dest):
+        ba = cls.create_bytearray(source)
+        cls.write_bytearray(ba, dest)
