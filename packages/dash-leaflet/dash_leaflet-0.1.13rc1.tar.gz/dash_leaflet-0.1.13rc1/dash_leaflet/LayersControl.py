3c7729817f06db261c7a5fa19e94e186d26bb984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package dash-leaflet/0.1.13rc1/dash_leaflet-0.1.13rc1.tar.gz

---
+++ packages/dash-leaflet/dash_leaflet-0.1.13rc1.tar.gz/dash_leaflet-0.1.13rc1/dash_leaflet/LayersControl.py
@@ -0,0 +1,38 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class LayersControl(Component):
+    """A LayersControl component.
+
+
+Keyword arguments:
+- children (a list of or a singular dash component, string or number; optional): Attribution
+- collapsed (boolean; optional): Collapsed.
+- position (a value equal to: 'topleft', 'topright', 'bottomleft', 'bottomright'; optional): Position.
+- className (string; optional): A custom class name. Empty by default.
+- id (string; optional): The ID used to identify this component in Dash callbacks
+- layers (dict; optional): List of layers indicating if they are currently checked or not. layers has the following type: list of dicts containing keys 'name', 'checked'.
+Those keys have the following types:
+  - name (string; optional)
+  - checked (boolean; optional)"""
+    @_explicitize_args
+    def __init__(self, children=None, collapsed=Component.UNDEFINED, position=Component.UNDEFINED, className=Component.UNDEFINED, id=Component.UNDEFINED, layers=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'collapsed', 'position', 'className', 'id', 'layers']
+        self._type = 'LayersControl'
+        self._namespace = 'dash_leaflet'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'collapsed', 'position', 'className', 'id', 'layers']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(LayersControl, self).__init__(children=children, **args)
