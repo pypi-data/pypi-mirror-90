a49194914165475f1efbefccaf86eafc4e0af855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:40 +0000

    Add package magicgui/0.2.2/magicgui-0.2.2.tar.gz

---
+++ packages/magicgui/magicgui-0.2.2.tar.gz/magicgui-0.2.2/examples/widget_demo.py
@@ -0,0 +1,41 @@
+"""Widget demonstration of magicgui."""
+
+from datetime import datetime
+from enum import Enum
+from pathlib import Path
+
+from magicgui import magicgui
+
+
+class Medium(Enum):
+    """Enum for various media and their refractive indices."""
+
+    Glass = 1.520
+    Oil = 1.515
+    Water = 1.333
+    Air = 1.0003
+
+
+@magicgui(
+    call_button="Calculate",
+    layout="vertical",
+    result_widget=True,
+    slider_float={"widget_type": "FloatSlider"},
+    filename={"label": "Pick a file:"},
+)
+def widget_demo(
+    boolean=True,
+    integer=1,
+    spin_float=3.14159,
+    slider_float=4.5,
+    string="Text goes here",
+    dropdown=Medium.Glass,
+    datetime=datetime.now(),
+    filename=Path.home(),
+):
+    """Run some computation."""
+    return locals().values()
+
+
+widget_demo.changed.connect(lambda event: print(widget_demo))
+widget_demo.show(run=True)
