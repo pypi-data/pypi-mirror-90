e06b58032169b751d8676e74bb5a209640f4264e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package fdmutils/0.1.1/fdmutils-0.1.1.tar.gz

---
+++ packages/fdmutils/fdmutils-0.1.1.tar.gz/fdmutils-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fdmutils',
+    version='0.1.1',
+    author='fdmseven',
+    author_email='wrp2003000@163.com',
+    packages=["fdmutils"],
+    url='https://github.com/fdmseven/fdmutils',
+    license='LICENSE.txt',
+    description='independent functions',
+    long_description=open('README.md').read(),
+    python_requires='>=3',
+)
