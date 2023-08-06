f26a746f6a18e3b8fbc7222d17b6f8bae23c3e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:35 +0000

    Add package brother-printer-fwupd/0.3.0/brother_printer_fwupd-0.3.0.tar.gz

---
+++ packages/brother-printer-fwupd/brother_printer_fwupd-0.3.0.tar.gz/brother_printer_fwupd-0.3.0/brother_printer_fwupd/models.py
@@ -0,0 +1,46 @@
+"""Types and model classes / definitions."""
+import typing
+import ipaddress
+from dataclasses import dataclass, field
+
+
+IPAddress = typing.Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+
+
+@dataclass
+class FWInfo:
+    """Firmware fragment info."""
+    firmid: typing.Optional[str] = field(default=None)
+    firmver: typing.Optional[str] = field(default=None)
+
+    @property
+    def is_complete(self):
+        """Return True if firmid and firmver is given."""
+        return self.firmid is not None and self.firmver is not None
+
+    @property
+    def is_empty(self):
+        """Return True if firmid and firmver are None."""
+        return self.firmid is None and self.firmver is None
+
+
+@dataclass
+class SNMPPrinterInfo:
+    """Information about a printer."""
+
+    model: typing.Optional[str] = field(default=None)
+    serial: typing.Optional[str] = field(default=None)
+    spec: typing.Optional[str] = field(default=None)
+    fw_versions: typing.List[FWInfo] = field(default_factory=list)
+
+
+@dataclass
+class MDNSPrinterInfo:
+    """Information about a printer received via MDNS."""
+
+    ip_addr: IPAddress
+    name: str
+    port: typing.Optional[int]
+    product: typing.Optional[str]
+    note: typing.Optional[str]
+    uuid: typing.Optional[str]
---
+++ packages/brother-printer-fwupd/brother_printer_fwupd-0.3.0.tar.gz/brother_printer_fwupd-0.3.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['brother_printer_fwupd']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['BeautifulSoup4>=4.9.1,<5.0.0',
+ 'lxml>=4.6.2,<5.0.0',
+ 'pysnmp>=4.4.12,<5.0.0',
+ 'requests>=2.23.0,<3.0.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'zeroconf>=0.28.7,<0.29.0']
+
+extras_require = \
+{'graphical': ['Gooey>=1.0.3,<2.0.0']}
+
+entry_points = \
+{'console_scripts': ['brother_printer_fwupd = '
+                     'brother_printer_fwupd.__main__:main']}
+
+setup_kwargs = {
+    'name': 'brother-printer-fwupd',
+    'version': '0.3.0',
+    'description': 'Script to update the firmware of some Brother printers (e. g. MFC).',
+    'long_description': None,
+    'author': 'sedrubal',
+    'author_email': 'dev@sedrubal.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sedrubal/brother_printer_fwupd.git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
