0d1b7f7d70c8679f449773e98a799ac152b8593d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:17 +0000

    Add package prettyqt/0.168.0/PrettyQt-0.168.0-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.168.0-py3-none-any.whl/prettyqt/gui/desktopservices.py
@@ -0,0 +1,13 @@
+import os
+from typing import Union
+
+from prettyqt import core
+from prettyqt.qt import QtCore, QtGui
+
+
+class DesktopServices(QtGui.QDesktopServices):
+    @classmethod
+    def open_url(cls, location: Union[str, os.PathLike, QtCore.QUrl]) -> bool:
+        if not isinstance(location, QtCore.QUrl):
+            location = core.Url.from_user_input(os.fspath(location))
+        return cls.openUrl(location)
---
+++ packages/prettyqt/PrettyQt-0.168.0-py3-none-any.whl/prettyqt/widgets/styleoptiontabbarbase.py
@@ -0,0 +1,9 @@
+from prettyqt import widgets
+from prettyqt.qt import QtWidgets
+
+
+QtWidgets.QStyleOptionTabBarBase.__bases__ = (widgets.StyleOption,)
+
+
+class StyleOptionTabBarBase(QtWidgets.QStyleOptionTabBarBase):
+    pass
