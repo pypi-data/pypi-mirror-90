a584fed989c67f3a608922d8a501f24454515a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:33 +0000

    Add package pi-dht-webthing/0.0.30/pi_dht_webthing-0.0.30.tar.gz

---
+++ packages/pi-dht-webthing/pi_dht_webthing-0.0.30.tar.gz/pi_dht_webthing-0.0.30/pi_dht_webthing/dht_webthing.py
@@ -0,0 +1,85 @@
+from webthing import (SingleThing, Property, Thing, Value, WebThingServer)
+import logging
+import tornado.ioloop
+import Adafruit_DHT
+
+
+class DhtSensor(Thing):
+
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, gpio_number: int, description: str):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:dhtSensor-1',
+            'Humidity and Temperature Sensor',
+            ['TemperatureSensor', 'HumiditySensor'],
+            description
+        )
+
+        self.sensor = Adafruit_DHT.DHT22
+        self.gpio_number = gpio_number
+        self.humidity = Value(0.0)
+        self.add_property(
+            Property(self,
+                     'humidity',
+                     self.humidity,
+                     metadata={
+                         '@type': 'HumidityProperty',
+                         'title': 'Humidity',
+                         'type': 'number',
+                         'description': 'The current humidity from 0%-100%',
+                         'minimum': 0,
+                         'maximum': 100,
+                         'unit': 'percent',
+                         'readOnly': True,
+                     }))
+
+        self.temperature = Value(0)
+        self.add_property(
+            Property(self,
+                     'temperature',
+                     self.temperature,
+                     metadata={
+                         '@type': 'TemperatureProperty',
+                         'title': 'Temperature',
+                         'type': 'number',
+                         'description': 'The current temperature',
+                         'unit': 'degree celsius',
+                         'readOnly': True,
+                     }))
+
+        self.timer = tornado.ioloop.PeriodicCallback(self.__measure, (60 * 1000))  # 1 min
+        self.timer.start()
+
+    def __measure(self):
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.gpio_number)
+            if humidity is not None:
+                self.humidity.notify_of_external_update(round(humidity, 1))
+                logging.debug('humidity ' + str(humidity))
+            if temperature is not None:
+                self.temperature.notify_of_external_update(round(temperature, 1))
+                logging.debug('temperature ' + str(temperature))
+        except Exception as e:
+            logging.error(e)
+
+    def cancel_measure_task(self):
+        self.timer.stop()
+
+
+def run_server(port: int, gpio_number: int, description: str):
+    dht_sensor = DhtSensor(gpio_number, description)
+    server = WebThingServer(SingleThing(dht_sensor), port=port, disable_host_validation=True)
+    try:
+        logging.info('starting the server')
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('stopping the server')
+        dht_sensor.cancel_measure_task()
+        server.stop()
+        logging.info('done')
+
+
+
