8d3fd48f7544bd3bc40595657636b4691ffd10ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:22 +0000

    Add package registryaggregator/0.8.1/registryaggregator-0.8.1-py3-none-any.whl

---
+++ packages/registryaggregator/registryaggregator-0.8.1-py3-none-any.whl/nmosregistration/aggregation.py
@@ -0,0 +1,74 @@
+# Copyright 2017 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+from nmoscommon.utils import getLocalIP
+from nmoscommon.webapi import WebAPI, route
+from nmoscommon.auth.auth_middleware import AuthMiddleware
+from nmoscommon.nmoscommonconfig import config as _config
+
+from .garbage import GarbageCollect
+from .etcd_backend import EtcdInterface
+from .v1_0 import routes as v1_0
+from .v1_1 import routes as v1_1
+from .v1_2 import routes as v1_2
+from .v1_3 import routes as v1_3
+
+HOST = getLocalIP()
+SERVICE_PORT = 8235
+
+AGGREGATOR_APINAMESPACE = "x-nmos"
+AGGREGATOR_APINAME = "registration"
+AGGREGATOR_APIVERSIONS = ["v1.0", "v1.1", "v1.2", "v1.3"]
+if _config.get("https_mode", "disabled") == "enabled":
+    AGGREGATOR_APIVERSIONS.remove("v1.0")
+
+
+class AggregatorAPI(WebAPI):
+
+    def __init__(self, logger, config, registry=EtcdInterface()):
+        super(AggregatorAPI, self).__init__()
+        self._config = config
+
+        # Add Auth Middleware
+        oauth_mode = config.get('oauth_mode', False)
+        self.app.wsgi_app = AuthMiddleware(self.app.wsgi_app, auth_mode=oauth_mode, api_name=AGGREGATOR_APINAME)
+
+        garbage_collect_interval = int(self._config.get("garbage_collect_interval", 10))
+        self._garbage_collector = GarbageCollect(identifier=HOST, registry=registry, interval=garbage_collect_interval)
+
+        self._v1_0_api = v1_0.Routes(logger=logger, registry=registry)
+        self.add_routes(self._v1_0_api, basepath="/x-nmos/registration/v1.0")
+
+        self._v1_1_api = v1_1.Routes(logger=logger, registry=registry)
+        self.add_routes(self._v1_1_api, basepath="/x-nmos/registration/v1.1")
+
+        self._v1_2_api = v1_2.Routes(logger=logger, registry=registry)
+        self.add_routes(self._v1_2_api, basepath="/x-nmos/registration/v1.2")
+
+        self._v1_3_api = v1_3.Routes(logger=logger, registry=registry)
+        self.add_routes(self._v1_3_api, basepath="/x-nmos/registration/v1.3")
+
+    @route('/')
+    def __root(self):
+        return (200, [AGGREGATOR_APINAMESPACE + "/"])
+
+    @route('/' + AGGREGATOR_APINAMESPACE + '/')
+    def __namespaceroot(self):
+        return (200, [AGGREGATOR_APINAME + "/"])
+
+    @route('/' + AGGREGATOR_APINAMESPACE + '/' + AGGREGATOR_APINAME + '/')
+    def __nameroot(self):
+        return (200, [api_version + "/" for api_version in AGGREGATOR_APIVERSIONS])
---
+++ packages/registryaggregator/registryaggregator-0.8.1-py3-none-any.whl/nmosregistration/registryaggregatorservice.py
@@ -0,0 +1,136 @@
+# Copyright 2017 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
+from nmoscommon.mdns import MDNSEngine # noqa E402
+from nmoscommon.utils import getLocalIP # noqa E402
+from nmosregistration.aggregation import AggregatorAPI, AGGREGATOR_APIVERSIONS # noqa E402
+from nmoscommon.httpserver import HttpServer # noqa E402
+from nmoscommon.logger import Logger # noqa E402
+from .config import config # noqa E402
+import signal # noqa E402
+import time # noqa E402
+
+# Handle if systemd is installed instead of newer cysystemd
+try:
+    from cysystemd import daemon # noqa E402
+    SYSTEMD_READY = daemon.Notification.READY
+except ImportError:
+    from systemd import daemon # noqa E402
+    SYSTEMD_READY = "READY=1"
+
+HOST = getLocalIP()
+SERVICE_PORT = 8235
+DNS_SD_HTTP_PORT = 80
+DNS_SD_HTTPS_PORT = 443
+DNS_SD_NAME = 'registration_' + str(HOST)
+DNS_SD_TYPE = '_nmos-register._tcp'
+DNS_SD_LEGACY_TYPE = '_nmos-registration._tcp'
+REGISTRY_PORT = 2379
+
+
+class RegistryAggregatorService(object):
+    def __init__(self, logger=None, interactive=False):
+        self.config = config
+        self.running = False
+        self.httpServer = None
+        self.interactive = interactive
+        self.mdns = MDNSEngine()
+        self.logger = Logger("aggregation", logger)
+
+    def start(self):
+        if self.running:
+            gevent.signal_handler(signal.SIGINT, self.sig_handler)
+            gevent.signal_handler(signal.SIGTERM, self.sig_handler)
+
+        self.mdns.start()
+
+        self.httpServer = HttpServer(AggregatorAPI, SERVICE_PORT, '0.0.0.0', api_args=[self.logger, self.config])
+        self.httpServer.start()
+        while not self.httpServer.started.is_set():
+            print("Waiting for httpserver to start...")
+            self.httpServer.started.wait()
+
+        if self.httpServer.failed is not None:
+            raise self.httpServer.failed
+
+        print("Running on port: {}".format(self.httpServer.port))
+
+        self._advertise_mdns()
+
+    def _advertise_mdns(self):
+        priority = self.config["priority"]
+        if not str(priority).isdigit():
+            priority = 0
+
+        oauth_mode = self.config.get('oauth_mode', False)
+
+        if self.config["https_mode"] != "enabled" and self.config["enable_mdns"]:
+            self.mdns.register(DNS_SD_NAME + "_http", DNS_SD_TYPE, DNS_SD_HTTP_PORT,
+                               self._mdns_txt(priority, AGGREGATOR_APIVERSIONS, "http", oauth_mode))
+            if self._require_legacy_mdns():
+                # Send out deprecated advertisement
+                self.mdns.register(DNS_SD_NAME + "_http_dep", DNS_SD_LEGACY_TYPE, DNS_SD_HTTP_PORT,
+                                   self._mdns_txt(priority, AGGREGATOR_APIVERSIONS, "http", oauth_mode))
+
+        if self.config["https_mode"] != "disabled" and self.config["enable_mdns"]:
+            self.mdns.register(DNS_SD_NAME + "_https", DNS_SD_TYPE, DNS_SD_HTTPS_PORT,
+                               self._mdns_txt(priority, AGGREGATOR_APIVERSIONS, "https", oauth_mode))
+            if self._require_legacy_mdns():
+                # Send out deprecated advertisement
+                self.mdns.register(DNS_SD_NAME + "_https_dep", DNS_SD_LEGACY_TYPE, DNS_SD_HTTPS_PORT,
+                                   self._mdns_txt(priority, AGGREGATOR_APIVERSIONS, "https", oauth_mode))
+
+    def _require_legacy_mdns(self):
+        legacy_apiversions = ["v1.0", "v1.1", "v1.2"]
+        for api_ver in AGGREGATOR_APIVERSIONS:
+            if api_ver in legacy_apiversions:
+                return True
+        return False
+
+    def _mdns_txt(self, priority, versions, protocol, oauth_mode):
+        return {
+            "pri": priority,
+            "api_ver": ",".join(versions),
+            "api_proto": protocol,
+            "api_auth": str(oauth_mode).lower()
+        }
+
+    def run(self):
+        self.running = True
+        self.start()
+        daemon.notify(SYSTEMD_READY)
+        while self.running:
+            time.sleep(1)
+
+    def _cleanup(self):
+        self.mdns.close()
+        self.httpServer.stop()
+        print("Stopped main()")
+
+    def stop(self):
+        self._cleanup()
+        self.running = False
+
+    def sig_handler(self):
+        print("Pressed ctrl+c")
+        self.stop()
+
+
+if __name__ == '__main__':
+    service = RegistryAggregatorService()
+    service.run()
