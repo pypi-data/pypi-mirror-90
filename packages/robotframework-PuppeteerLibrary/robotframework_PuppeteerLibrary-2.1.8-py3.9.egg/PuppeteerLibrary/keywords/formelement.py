ea481351b0c78387666a01c49417fd3b44b9a5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package robotframework-PuppeteerLibrary/2.1.8/robotframework_PuppeteerLibrary-2.1.8-py3.9.egg

---
+++ packages/robotframework-PuppeteerLibrary/robotframework_PuppeteerLibrary-2.1.8-py3.9.egg/PuppeteerLibrary/keywords/formelement.py
@@ -0,0 +1,46 @@
+import asyncio
+from PuppeteerLibrary.base.robotlibcore import keyword
+from PuppeteerLibrary.base.librarycomponent import LibraryComponent
+from PuppeteerLibrary.ikeywords.iformelement_async import iFormElementAsync
+
+
+class FormElementKeywords(LibraryComponent):
+
+    def __init__(self, ctx):
+        super().__init__(ctx)
+
+    def get_async_keyword_group(self) -> iFormElementAsync:
+        return self.ctx.get_current_library_context().get_async_keyword_group(type(self).__name__)
+
+    @keyword
+    def input_text(self, locator, text, clear=True):
+        """Types the given text into text field identified by ``locator``.
+
+        If clear is true, the input element will be cleared before the text is typed into the element.
+        On the other hand clear is false, the previous text will not be cleared from the element.
+
+        Examples:
+        | `Input Text` | id:name     | John Doe    |      |
+        | `Input Text` | id:username | john        | True |
+
+        """
+        self.info(text)
+        self.loop.run_until_complete(self.get_async_keyword_group().input_text(locator, text, clear))
+
+    @keyword
+    def clear_element_text(self, locator):
+        """Clears value of text field identified by ``locator``.
+
+        Example:
+
+        | `Clear Element Text`                   | id:name          |
+        """
+        self.loop.run_until_complete(self.get_async_keyword_group().clear_element_text(locator))
+
+    @keyword
+    def download_file(self, locator, timeout=None):
+        return self.loop.run_until_complete(self.get_async_keyword_group().download_file(locator, timeout))
+
+    @keyword
+    def upload_file(self, locator, file_path):
+        return self.loop.run_until_complete(self.get_async_keyword_group().upload_file(locator, file_path))
