26542b4d8340468042486d3a4f883ff6f0e8ad53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:16 +0000

    Add package tfwstartr/1.0.1/tfwstartr-1.0.1-py3-none-any.whl

---
+++ packages/tfwstartr/tfwstartr-1.0.1-py3-none-any.whl/tfwstartr/utils/package_manager/strategies/npm_strategy.py
@@ -0,0 +1,22 @@
+import os
+import json
+from typing import Dict, Union
+from pathlib import Path
+from .strategy_base import PackageManagerStrategy
+
+
+class NpmStrategy(PackageManagerStrategy):
+    @staticmethod
+    def install_packages(
+        workdir: Union[str, Path], dependency_file: str, packages: Dict[str, str]
+    ) -> None:
+        with open(os.path.join(workdir, dependency_file), "r") as requirements:
+            packagejson = json.loads(requirements.read())
+        for name, version in packages.items():
+            packagejson["dependencies"][name] = f"^{version}"
+        with open(os.path.join(workdir, dependency_file), "w+") as requirements:
+            requirements.write(json.dumps(packagejson, indent=2))
+
+    @staticmethod
+    def get_packages_from_file(file_content: str) -> Dict[str, str]:
+        return json.loads(file_content).get("dependencies")
