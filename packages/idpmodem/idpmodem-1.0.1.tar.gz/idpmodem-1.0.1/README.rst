12c030ed92890cf23eeee3f7b98dc9a8e55bb898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package idpmodem/1.0.1/idpmodem-1.0.1.tar.gz

---
+++ packages/idpmodem/idpmodem-1.0.1.tar.gz/idpmodem-1.0.1/README.rst
@@ -0,0 +1,10 @@
+idp-modem
+=========
+
+Python samples for integrating with IDP satellite modem.
+
+`Documentation <https://inmarsat.github.io/idpmodem/>`_
+
+To do:
+- create install package
+- reorganize/optimize folders
---
+++ packages/idpmodem/idpmodem-1.0.1.tar.gz/idpmodem-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['idpmodem', 'idpmodem.codecs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aioserial>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'idpmodem',
+    'version': '1.0.1',
+    'description': 'A library for interfacing with an Inmarsat IsatData Pro satellite modem using serial AT commands.',
+    'long_description': 'idp-modem\n=========\n\nPython samples for integrating with IDP satellite modem.\n\n`Documentation <https://inmarsat.github.io/idpmodem/>`_\n\nTo do:\n- create install package\n- reorganize/optimize folders\n',
+    'author': 'G.Bruce-Payne',
+    'author_email': 'geoff.bruce-payne@inmarsat.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Inmarsat/idpmodem',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
