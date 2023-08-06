f6510021aa01d6c7e2576ce4fbf9c19b6e060a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package AEMET-OpenData/0.1.0/AEMET-OpenData-0.1.0.tar.gz

---
+++ packages/AEMET-OpenData/AEMET-OpenData-0.1.0.tar.gz/AEMET-OpenData-0.1.0/aemet_opendata/const.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""AEMET OpenData Constants"""
+
+API_URL = "https://opendata.aemet.es/opendata/api"
+
+API_ATTR_DATA = "datos"
+
+API_ID_PFX = "id"
+
+API_MIN_STATION_DISTANCE_KM = 40
+API_MIN_TOWN_DISTANCE_KM = 40
+
+AEMET_ATTR_DATA = "data"
+AEMET_ATTR_RESPONSE = "response"
