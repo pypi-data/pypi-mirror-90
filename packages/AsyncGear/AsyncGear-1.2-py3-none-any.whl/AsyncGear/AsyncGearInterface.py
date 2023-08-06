4f8216b35ef2aca40b58ddf3c40513dec60b41c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:34 +0000

    Add package AsyncGear/1.2/AsyncGear-1.2-py3-none-any.whl

---
+++ packages/AsyncGear/AsyncGear-1.2-py3-none-any.whl/AsyncGear/AsyncGearInterface.py
@@ -0,0 +1,9 @@
+from AsyncGear import AsyncGear
+from abc import ABCMeta, abstractmethod
+
+
+class AsyncGearInterface(metaclass=ABCMeta):
+    '''
+        Used in a coroutine.
+    '''
+    # @abstractmethod
