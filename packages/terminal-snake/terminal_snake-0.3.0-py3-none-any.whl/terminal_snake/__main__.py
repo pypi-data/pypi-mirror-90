84c01c5d3559e380672c2e026690608b0bef3d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:37 +0000

    Add package terminal-snake/0.3.0/terminal_snake-0.3.0-py3-none-any.whl

---
+++ packages/terminal-snake/terminal_snake-0.3.0-py3-none-any.whl/terminal_snake/__main__.py
@@ -0,0 +1,34 @@
+from .game import Game
+
+KEYBINDINGS = {
+    "w": 0,
+    "a": 3,
+    "s": 2,
+    "d": 1,
+}
+
+
+def main(game: Game):
+    game.set_apple()
+    while True:
+        game.draw_field()
+        while (go := input("> ").lower()) not in ["w", "a", "s", "d"]:
+            print(":(")
+
+        game.orientation = KEYBINDINGS[go]
+
+        if not game.move():
+            print("You lost!")
+            break
+
+        if game.score == game.max_score:
+            print("You won!")
+
+        game.max_apples = max((1, game.score // 5))
+    game.end_game()
+    print(f"Score: {game.score}")
+
+
+if __name__ == '__main__':
+    g = Game(5)
+    main(g)
