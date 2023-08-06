32b2593cc3726a497fc322c75682e2ceffdd3a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:50 +0000

    Add package dtb.money/1.1.0/dtb.money-1.1.0-py3-none-any.whl

---
+++ packages/dtb.money/dtb.money-1.1.0-py3-none-any.whl/dtb/money/money.pyi
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from decimal import Decimal
+from typing import Union, final
+
+from dtb.currency import Currency
+
+@final
+class Money:
+    amount: Decimal
+    currency: Currency
+    def __init__(self, amount: Union[Decimal, float, str], currency: Currency) -> None:
+        ...
+
+    def convert_to(self, currency: Currency) -> Money:
+        ...
+
+    def __eq__(self, other: object) -> bool:
+        ...
+
+    def __add__(self, other: Money) -> Money:
+        ...
+
+    def __str__(self) -> str:
+        ...
---
+++ packages/dtb.money/dtb.money-1.1.0-py3-none-any.whl/dtb/money/test.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from typing import Union
+
+from dtb.currency import Currency
+import pytest
+
+from money import Money
+
+RUB = Currency(code="RUB", scale=1, sign="₽", default=True)
+USD = Currency(code="USD", scale=80, sign="$", default=False)
+EUR = Currency(code="EUR", scale=90, sign="€", default=False)
+
+
+@pytest.mark.parametrize("amount", [1, 1.1, Decimal("1.1"), "1.1"])
+def test_money_init(amount: Union[int, float, Decimal, str], rub):
+    assert Money(amount, rub)
+
+
+@pytest.mark.parametrize(
+    ["money_from", "currency_to", "expected"],
+    [
+        (Money(1, USD), RUB, Money(80, RUB)),
+        (Money(1, USD), USD, Money(1, USD)),
+        (Money(0, USD), RUB, Money(0, RUB)),
+        (Money(80, RUB), USD, Money(1, USD)),
+    ],
+)
+def test_money_convert(money_from, currency_to, expected):
+    assert money_from.convert_to(currency_to) == expected
+
+
+@pytest.mark.parametrize(
+    ["m1", "m2", "expected"],
+    [(Money(1, RUB), Money(1, RUB), Money(2, RUB)), (Money(1, RUB), Money(1, USD), Money(81, RUB))],
+)
+def test_money_add(m1, m2, expected):
+    assert m1 + m2 == expected
