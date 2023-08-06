1df845cf8fa9d113c2d8e56f00e36d4987b7efc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package dozent/0.6/dozent-0.6.tar.gz

---
+++ packages/dozent/dozent-0.6.tar.gz/dozent-0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'dozent',
+  packages = ['dozent'],
+  version = '0.6',
+  license='MIT',
+  description = 'Dozent is a powerful downloader that is used to download a ton of twitter data from the internet archive.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Ali Abbas, Eric Burt, Keelin Becker-Wheeler',
+  author_email = 'eric.burt@protonmail.com',
+  url = 'https://github.com/Social-Media-Public-Analysis/dozent',
+  download_url = 'https://github.com/Social-Media-Public-Analysis/dozent/archive/v_06.tar.gz',
+  keywords = ['TWITTER', 'SCRAPER', 'TWEET'],
+  install_requires=[
+            'pySmartDL',
+            'aria2p',
+            'pytest',
+            'pyfiglet',
+            'murpheus',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
