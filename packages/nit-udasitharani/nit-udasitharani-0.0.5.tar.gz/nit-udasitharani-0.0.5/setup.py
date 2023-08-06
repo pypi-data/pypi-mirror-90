69776fa46a3741d5870dc6fed2980a394c121a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package nit-udasitharani/0.0.5/nit-udasitharani-0.0.5.tar.gz

---
+++ packages/nit-udasitharani/nit-udasitharani-0.0.5.tar.gz/nit-udasitharani-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nit-udasitharani",
+    version="0.0.5",
+    author="Udasi Tharani",
+    author_email="udasitharani@gmail.com",
+    description="A tool to generate tiles from name initials.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/udasitharani/name-initials-tile-generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.6"
+)
