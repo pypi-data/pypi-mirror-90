802caff6711282f173de64131a1f7ea1eaf60efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:22 +0000

    Add package dwk-distributions/0.2/dwk_distributions-0.2.tar.gz

---
+++ packages/dwk-distributions/dwk_distributions-0.2.tar.gz/dwk_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dwk_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dwk_distributions'],
+      author = 'Dongwon Kim',
+      author_email = 'astro.dw.kim@gmail.com',
+      zip_safe=False)
