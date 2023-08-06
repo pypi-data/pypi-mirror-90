f95fb097bad9dfa531844613bddcfb5e8456982c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:28 +0000

    Add package easyvanity.py/0.0.2/easyvanity.py-0.0.2.tar.gz

---
+++ packages/easyvanity.py/easyvanity.py-0.0.2.tar.gz/easyvanity.py-0.0.2/easyvanity/client.py
@@ -0,0 +1,22 @@
+import ujson as json
+import requests
+
+from .lib.util import objects, exceptions
+
+
+class Client(object):
+    def __init__(self, mongoUri: str, siteUrl: str):
+        self.mongoDb = mongoUri
+        self.siteUri = siteUrl
+    # if self.mongoUri is None:
+    #     return print('Error: No mongoUri defined...')
+
+    def get_bot_info(self, botId: int, apiKey: str):
+        if self.mongoDb is None:
+            return print('Error: No mongoUri defined...')
+        # headers = {
+        #     "Authorization": apiKey
+        # }
+        # response = requests.get(f"{self.api}/public/bot/{botId}", headers=headers)
+        # if response.status_code != 200: raise exceptions.CheckException(json.loads(response.text))
+        # else: return objects.botInfo(json.loads(response.text)["response"])
