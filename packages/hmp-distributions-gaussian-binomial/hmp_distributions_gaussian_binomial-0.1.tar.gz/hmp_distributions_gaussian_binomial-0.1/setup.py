99df5072b05c481e82ae36cee5fd24a8e926b96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:27 +0000

    Add package hmp-distributions-gaussian-binomial/0.1/hmp_distributions_gaussian_binomial-0.1.tar.gz

---
+++ packages/hmp-distributions-gaussian-binomial/hmp_distributions_gaussian_binomial-0.1.tar.gz/hmp_distributions_gaussian_binomial-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hmp_distributions_gaussian_binomial',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['hmp_distributions_gaussian_binomial'],
+      author='Henrique M. Prado',
+      author_email='hmartins.prado@gmail.com',
+      zip_safe=False)
