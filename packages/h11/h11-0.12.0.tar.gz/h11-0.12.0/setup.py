ab382ebdfb9d4e0c5689f120f7007b1cb3abec13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package h11/0.12.0/h11-0.12.0.tar.gz

---
+++ packages/h11/h11-0.12.0.tar.gz/h11-0.12.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# defines __version__
+exec(open("h11/_version.py").read())
+
+setup(
+    name="h11",
+    version=__version__,
+    description=
+        "A pure-Python, bring-your-own-I/O implementation of HTTP/1.1",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT",
+    packages=find_packages(),
+    url="https://github.com/python-hyper/h11",
+    # This means, just install *everything* you see under h11/, even if it
+    # doesn't look like a source file, so long as it appears in MANIFEST.in:
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Networking",
+    ],
+)
