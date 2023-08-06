3fd3a75b63f381d47f961885b07e4c5faa61aad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:40 +0000

    Add package pscript/0.7.5/pscript-0.7.5.tar.gz

---
+++ packages/pscript/pscript-0.7.5.tar.gz/pscript-0.7.5/README.md
@@ -0,0 +1,73 @@
+PScript
+=======
+
+[![Build Status](https://travis-ci.org/flexxui/pscript.svg)](https://travis-ci.org/flexxui/pscript)
+[![Documentation Status](https://readthedocs.org/projects/pscript/badge/?version=latest)](https://pscript.readthedocs.org)
+
+
+PScript is a Python to JavaScript compiler, and is also the name of the subset
+of Python that this compiler supports. It was developed as a part of
+[Flexx](https://flexx.app) (as `flexx.pyscript`) and is now represented
+by its own project. Although it is still an important part of Flexx, it can
+also be useful by itself.
+
+
+Installation
+------------
+
+PScript is pure Python and requires Python 2.7 or 3.5+ (including Pypy).
+It has no further dependencies.
+
+* ``pip install pscript``, or
+* ``conda install pscript -c conda-forge``
+
+
+
+Short example
+-------------
+
+```py
+
+   from pscript import py2js
+
+   def foo(a, b=2):
+      print(a - b)
+
+   print(py2js(foo))
+```
+
+Gives:
+
+```js
+   var foo;
+   foo = function flx_foo (a, b) {
+      b = (b === undefined) ? 2: b;
+      console.log((a - b));
+      return null;
+   };
+```
+
+
+Supported browsers
+------------------
+
+PScript aims to support all modern browsers, including Firefox, Chrome and Edge.
+Internet Explorer is in principal supported from version 9, though some constructs
+(e.g. ``async`` and ``await``) do not work in Internet Explorer.
+
+
+PScript in the wild
+-------------------
+
+To give an idea of what PScript can do, here are some examples in the wild:
+
+* Obviously, everything built in Flexx uses PScript, see e.g. [these examples](https://flexx.readthedocs.io/en/stable/examples/)
+* The front-end of [TimeTurtle.app](https://timeturtle.app) is built in Python using PScript.
+
+*Let us know if you know more!*
+
+
+License
+-------
+
+PScript makes use of the liberal 2-clause BSD license. See LICENSE for details.
---
+++ packages/pscript/pscript-0.7.5.tar.gz/pscript-0.7.5/setup.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+"""
+PScript setup script.
+"""
+
+import os
+import shutil
+
+try:
+    import setuptools  # noqa, analysis:ignore
+except ImportError:
+    pass  # setuptools allows for "develop", but it's not essential
+
+from distutils.core import setup
+
+
+## Function we need
+
+def get_version_and_doc(filename):
+    NS = dict(__version__='', __doc__='')
+    docStatus = 0  # Not started, in progress, done
+    for line in open(filename, 'rb').read().decode().splitlines():
+        if line.startswith('__version__'):
+            exec(line.strip(), NS, NS)
+        elif line.startswith('"""'):
+            if docStatus == 0:
+                docStatus = 1
+                line = line.lstrip('"')
+            elif docStatus == 1:
+                docStatus = 2
+        if docStatus == 1:
+            NS['__doc__'] += line.rstrip() + '\n'
+    if not NS['__version__']:
+        raise RuntimeError('Could not find __version__')
+    return NS['__version__'], NS['__doc__']
+
+
+def package_tree(pkgroot):
+    subdirs = [os.path.relpath(i[0], THIS_DIR).replace(os.path.sep, '.')
+               for i in os.walk(os.path.join(THIS_DIR, pkgroot))
+               if '__init__.py' in i[2]]
+    return subdirs
+
+
+def copy_for_legacy_python(src_dir, dest_dir):
+    from translate_to_legacy import LegacyPythonTranslator
+    # Dirs and files to explicitly not translate
+    skip = ['tests/python_sample.py',
+            'tests/python_sample2.py',
+            'tests/python_sample3.py']
+    # Make a fresh copy of the package
+    if os.path.isdir(dest_dir):
+        shutil.rmtree(dest_dir)
+    ignore = lambda src, names: [n for n in names if n == '__pycache__']
+    shutil.copytree(src_dir, dest_dir, ignore=ignore)
+    # Translate in-place
+    LegacyPythonTranslator.translate_dir(dest_dir, skip=skip)
+
+
+## Collect info for setup()
+
+THIS_DIR = os.path.dirname(__file__)
+
+# Define name and description
+name = 'pscript'
+description = "Python to JavaScript compiler."
+
+# Get version and docstring (i.e. long description)
+version, doc = get_version_and_doc(os.path.join(THIS_DIR, name, '__init__.py'))
+doc = ""  # won't render open(os.path.join(THIS_DIR, 'README.md'), "rb").read().decode()
+
+# Support for legacy Python: we install a second package with the
+# translated code. We generate that code when we can. We use
+# "name_legacy" below in "packages", "package_dir", and "package_data".
+name_legacy = name + '_legacy'
+if os.path.isfile(os.path.join(THIS_DIR, 'translate_to_legacy.py')):
+    copy_for_legacy_python(os.path.join(THIS_DIR, name),
+                           os.path.join(THIS_DIR, name_legacy))
+
+
+## Setup
+
+setup(
+    name=name,
+    version=version,
+    author='Almar Klein and contributors',
+    author_email='almar.klein@gmail.com',
+    license='(new) BSD',
+    url='http://pscript.readthedocs.io',
+    download_url='https://pypi.python.org/pypi/pscript',
+    keywords="Python, JavaScript, compiler, transpiler",
+    description=description,
+    long_description=doc,
+    long_description_content_type="text/markdown",
+    platforms='any',
+    provides=[name],
+    install_requires=[],
+    packages=package_tree(name) + package_tree(name_legacy),
+    package_dir={name: name, name_legacy: name_legacy},
+    # entry_points={'console_scripts': ['pscript = pscript.__main__:main'], },
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
