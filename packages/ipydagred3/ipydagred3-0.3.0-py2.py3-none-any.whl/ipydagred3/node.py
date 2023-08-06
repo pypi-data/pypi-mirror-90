9a3a3f17849fdc9440f7965c81436b4655b69fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:26 +0000

    Add package ipydagred3/0.3.0/ipydagred3-0.3.0-py2.py3-none-any.whl

---
+++ packages/ipydagred3/ipydagred3-0.3.0-py2.py3-none-any.whl/ipydagred3/node.py
@@ -0,0 +1,68 @@
+from traitlets import HasTraits, validate, observe, TraitError, Unicode, Dict
+
+
+class Node(HasTraits):
+    name = Unicode()
+    label = Unicode()
+    shape = Unicode(default_value="rect")  # rect, circle, ellipse, diamond
+    tooltip = Unicode(default_value="")
+    attrs = Dict()
+
+    _graph = None
+
+    def __init__(self, name, label="", shape="rect", tooltip="", **attrs):
+        super(Node, self).__init__(
+            self,
+            name=name,
+            label=label or name,
+            shape=shape,
+            tooltip=tooltip or label or name,
+            attrs=attrs or {},
+        )
+
+    def _setGraph(self, g):
+        self._graph = g
+
+    def _notify_change(self, attr, value):
+        if not self._graph:
+            return
+        self._graph._notify_change(self, attr, value)
+
+    @observe("name")
+    def _observe_name(self, change):
+        self._notify_change("name", change["new"])
+
+    @observe("label")
+    def _observe_label(self, change):
+        self._notify_change("label", change["new"])
+
+    @observe("shape")
+    def _observe_shape(self, change):
+        self._notify_change("shape", change["new"])
+
+    @observe("tooltip")
+    def _observe_tooltip(self, change):
+        self._notify_change("tooltip", change["new"])
+
+    @observe("attrs")
+    def _observe_attrs(self, change):
+        self._notify_change("attrs", change["new"])
+
+    @validate("shape")
+    def _validate_shape(self, proposal):
+        if proposal.value not in ("rect", "circle", "ellipse", "diamond"):
+            raise TraitError(
+                'Shape must be in ("rect", "circle", "ellipse", "diamond"), got: {}'.format(
+                    proposal
+                )
+            )
+        return proposal.value
+
+    def to_dict(self):
+        ret = {}
+        ret["name"] = self.name
+        ret["attrs"] = self.attrs
+        ret["attrs"]["label"] = self.label
+        ret["attrs"]["shape"] = self.shape
+        ret["attrs"]["tooltip"] = self.tooltip or self.label
+        return ret
---
+++ packages/ipydagred3/ipydagred3-0.3.0-py2.py3-none-any.whl/ipydagred3/widget.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+from ipywidgets import DOMWidget, CallbackDispatcher
+from traitlets import observe, Unicode, Instance, Dict
+from functools import wraps
+from .graph import Graph
+from ._version import __version__
+
+
+class DagreD3Widget(DOMWidget):
+    _model_name = Unicode("DagreD3Model").tag(sync=True)
+    _model_module = Unicode("ipydagred3").tag(sync=True)
+    _model_module_version = Unicode("^" + __version__).tag(sync=True)
+    _view_name = Unicode("DagreD3View").tag(sync=True)
+    _view_module = Unicode("ipydagred3").tag(sync=True)
+    _view_module_version = Unicode("^" + __version__).tag(sync=True)
+
+    graph = Instance(Graph, args=(), kwargs={})
+    _graph = Dict().tag(sync=True)
+
+    def __init__(self, graph=None, *args, **kwargs):
+        super(DagreD3Widget, self).__init__()
+
+        if not isinstance(graph, Graph):
+            graph = Graph(*args, **kwargs)
+        self.graph = graph
+
+        # set widget for callbacks
+        self.graph._setWidget(self)
+
+        # for click events
+        self._click_handlers = CallbackDispatcher()
+        self.on_msg(self._handle_click_msg)
+
+    def on_click(self, callback, remove=False):
+        self._click_handlers.register_callback(callback, remove=remove)
+
+    def click(self, value):
+        self._click_handlers(self, value)
+
+    def _handle_click_msg(self, _, content, buffers):
+        if content.get("event", "") == "click":
+            self.click(content.get("value", ""))
+
+    @wraps(Graph.setGraph)
+    def setGraph(self, *args, **kwargs):
+        self.graph.setGraph(*args, **kwargs)
+
+    @wraps(Graph.setNode)
+    def setNode(self, *args, **kwargs):
+        self.graph.setNode(*args, **kwargs)
+
+    @wraps(Graph.setEdge)
+    def setEdge(self, *args, **kwargs):
+        self.graph.setEdge(*args, **kwargs)
+
+    def post(self, msg):
+        self.send(msg)
+        self._graph = self.graph.to_dict()
+
+    @observe("graph")
+    def _observe_graph(self, change):
+        self._graph = change["new"].to_dict()
