ea5aea7424be85bc6210f3e37ba94b23b441f79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:34 +0000

    Add package pytimetools/0.1.3.4/pytimetools-0.1.3.4.tar.gz

---
+++ packages/pytimetools/pytimetools-0.1.3.4.tar.gz/pytimetools-0.1.3.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytimetools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=2.2,<3.0', 'pytz>=2017.3,<2018.0']
+
+setup_kwargs = {
+    'name': 'pytimetools',
+    'version': '0.1.3.4',
+    'description': 'a time tools for django',
+    'long_description': None,
+    'author': 'huoyinghui',
+    'author_email': 'hyhlinux@163.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/pydtools/pytimetools/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+}
+
+
+setup(**setup_kwargs)
