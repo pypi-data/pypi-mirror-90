2ea019963c48c4bb99da48b57ac70738b0e9f30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:39 +0000

    Add package pyjpboatrace/0.1.1/pyjpboatrace-0.1.1-py3-none-any.whl

---
+++ packages/pyjpboatrace/pyjpboatrace-0.1.1-py3-none-any.whl/pyjpboatrace/__init__.py
@@ -0,0 +1,27 @@
+from .pyjpboatrace import PyJPBoatrace
+from . import exceptions
+from . import parsers
+from . import utils
+from . import actions
+from . import const
+from . import drivers
+from . import user_information
+
+__copyright__ = 'Copyright (C) 2020 hmasdev'
+__version__ = '0.1.1'
+__license__ = 'MIT'
+__author__ = 'hmasdev'
+__author_email__ = 'hmasuidev1com@gmail.com'
+__url__ = 'http://github.com/hmasdev/pyjpboatrace'
+
+__all__ = [
+    'PyJPBoatrace',
+    'pyjpboatrace',
+    'actions',
+    'parsers',
+    'utils',
+    'drivers',
+    'user_information',
+    'const',
+    'exceptions',
+]
