27c86beacf6d7922b6d486478fcb1efebc7124e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package traceback-with-variables/2.0.0/traceback-with-variables-2.0.0.tar.gz

---
+++ packages/traceback-with-variables/traceback-with-variables-2.0.0.tar.gz/traceback-with-variables-2.0.0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+from traceback_with_variables import __version__
+
+with open('README.md', 'r', encoding='utf-8') as in_:
+    long_description = in_.read()
+
+setuptools.setup(
+    name='traceback-with-variables',
+    version=__version__,
+    author='Andrey Lyashko',
+    author_email='andydashlandy@gmail.com',
+    description='Adds variables to python traceback. Simple, lightweight, cont'
+    'rollable. Debug reasons of exceptions by logging or pretty printing color'
+    'ful variable contexts for each frame in a stacktrace, showing every value'
+    '. Dump locals environments after errors to console, files, and loggers. W'
+    'orks with Jupiter and IPython.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/andy-landy/traceback_with_variables',
+    packages=['traceback_with_variables'],
+    keywords=['python', 'traceback', 'locals', 'logging', 'debugging', 'print',
+              'variables', 'python3', 'stacktrace', 'arguments', 'errors',
+              'error-handling', 'dump', 'exception-handling', 'exceptions',
+              'pretty', 'pretty-print', 'frame', 'simple',
+              'colors', 'jupyer', 'jupyer-notebook', 'ipython', 'customize'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries',
+    ],
+    entry_points={'console_scripts': [
+        'traceback-with-variables = traceback_with_variables.main:main',
+    ]},
+    tests_require=[
+        'flake8',
+        'ipython',
+        'pytest-cov'
+    ],
+    install_requires=[],
+    python_requires='>=3.5',
+)
