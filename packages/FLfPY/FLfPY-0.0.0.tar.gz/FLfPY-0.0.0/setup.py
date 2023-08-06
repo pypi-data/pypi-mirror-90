476c18ad13a2a5806cbb260d09878a4f5568b60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:30 +0000

    Add package FLfPY/0.0.0/FLfPY-0.0.0.tar.gz

---
+++ packages/FLfPY/FLfPY-0.0.0.tar.gz/FLfPY-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'FLfPY',
+	packages = ['FLfPY'],
+    scripts = [],
+	version = '0.0.0',
+	description = 'FL tools',
+	author = 'P. Y. Lin',
+	author_email = 'pingyenlin@gmail.com',
+    url = 'https://github.com/PingYenLin/ECGanalysis',
+    keywords = [],
+    classifiers = []
+)
