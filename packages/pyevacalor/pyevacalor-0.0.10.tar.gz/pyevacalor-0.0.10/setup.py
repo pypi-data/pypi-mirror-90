b91a8ff09ec887cb364be59e972ef0803e2ea9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package pyevacalor/0.0.10/pyevacalor-0.0.10.tar.gz

---
+++ packages/pyevacalor/pyevacalor-0.0.10.tar.gz/pyevacalor-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyevacalor",
+    version="0.0.10",
+    author="Frederic Van Linthoudt",
+    author_email="frederic.van.linthoudt@gmail.com",
+    description="pyevacalor provides controlling Eva Calor heating devices connected via the IOT Agua platform of Micronova",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fredericvl/pyevacalor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "PyJWT==1.7.1",
+        "requests==2.25.1",
+    ],
+)
