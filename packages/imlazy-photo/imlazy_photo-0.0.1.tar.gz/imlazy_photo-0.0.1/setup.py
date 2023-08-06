19270037f098300b7583bf28c127b1a6058c0f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:14 +0000

    Add package imlazy-photo/0.0.1/imlazy_photo-0.0.1.tar.gz

---
+++ packages/imlazy-photo/imlazy_photo-0.0.1.tar.gz/imlazy_photo-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imlazy_photo", # Replace with your own username
+    version="0.0.1",
+    author="cosmos2",
+    author_email="playnstop.s@gmail.com",
+    description="Make directory and put in photo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cosmos2/imlazy_photo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+)
