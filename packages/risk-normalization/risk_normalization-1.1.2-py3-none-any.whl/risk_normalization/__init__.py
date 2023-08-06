d4b794dc654783dc476ffc642976b3a13c6deaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:20 +0000

    Add package risk-normalization/1.1.2/risk_normalization-1.1.2-py3-none-any.whl

---
+++ packages/risk-normalization/risk_normalization-1.1.2-py3-none-any.whl/risk_normalization/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__init__.py
+
+for risk_normalization package
+
+Created on Fri Dec 11 19:10:10 2020
+
+@author: howard bandy
+"""
+__version__ = '1.1.2'
+
+
