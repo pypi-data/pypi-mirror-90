783bafcda3a0342bef51086ac583b0c0636c722a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package pymagicc/2.0.0rc10/pymagicc-2.0.0rc10-py2.py3-none-any.whl

---
+++ packages/pymagicc/pymagicc-2.0.0rc10-py2.py3-none-any.whl/pymagicc/__init__.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2017 pymagicc authors:
+#   Robert Gieseke <robert.gieseke@pik-potsdam.de>
+# Free software under GNU Affero General Public License v3, see LICENSE
+#
+# The compiled MAGICC binary (http://www.magicc.org/download6) by Tom Wigley,
+# Sarah Raper, and Malte Meinshausen included in this package is licensed under
+# a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+# License (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+from ._version import get_versions
+from .core import MAGICC6, MAGICC7  # noqa
+from .io import MAGICCData  # noqa
+from .scenarios import (  # noqa
+    rcp26,
+    rcp45,
+    rcp60,
+    rcp85,
+    rcps,
+    read_scen_file,
+    zero_emissions,
+)
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+def run(scenario, magicc_version=6, **kwargs):
+    """
+    Run a MAGICC scenario and return output data and (optionally) config parameters.
+
+    As a reminder, putting ``out_parameters=1`` will cause MAGICC to write out its
+    parameters into ``out/PARAMETERS.OUT`` and they will then be read into
+    ``output.metadata["parameters"]`` where ``output`` is the returned object.
+
+    Parameters
+    ----------
+    scenario : :obj:`pymagicc.io.MAGICCData`
+        Scenario to run
+
+    magicc_version : int
+        MAGICC version to use for the run
+
+    **kwargs
+        Parameters overwriting default parameters
+
+    Raises
+    ------
+    ValueError
+        If the magicc_version is not available
+
+    Returns
+    -------
+    output : :obj:`pymagicc.io.MAGICCData`
+        Output of the run with the data in the ``df`` attribute and parameters and
+        other metadata in the ``metadata attribute``
+    """
+    if magicc_version == 6:
+        magicc_cls = MAGICC6
+    elif magicc_version == 7:
+        magicc_cls = MAGICC7
+    else:
+        raise ValueError("MAGICC version {} is not available".format(magicc_version))
+
+    with magicc_cls() as magicc:
+        results = magicc.run(scenario=scenario, **kwargs)
+
+    return results
