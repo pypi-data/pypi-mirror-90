c8636469ec831de64e8cc8ce0eb58d95e6fa8c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:38 +0000

    Add package ro-py/1.0.1/ro-py-1.0.1.tar.gz

---
+++ packages/ro-py/ro-py-1.0.1.tar.gz/ro-py-1.0.1/ro_py/groups.py
@@ -0,0 +1,91 @@
+"""
+
+This file houses functions and classes that pertain to Roblox groups.
+
+"""
+
+from ro_py.users import User
+from ro_py.roles import Role
+
+endpoint = "https://groups.roblox.com"
+
+
+class Shout:
+    """
+    Represents a group shout.
+    """
+    def __init__(self, requests, shout_data):
+        self.body = shout_data["body"]
+        self.poster = User(requests, shout_data["poster"]["userId"])
+
+
+class Group:
+    """
+    Represents a group.
+    """
+    def __init__(self, requests, group_id):
+        self.requests = requests
+        self.id = group_id
+
+        self.name = None
+        self.description = None
+        self.owner = None
+        self.member_count = None
+        self.is_builders_club_only = None
+        self.public_entry_allowed = None
+        self.shout = None
+
+    async def update(self):
+        """
+        Updates the group's information.
+        """
+        group_info_req = await self.requests.get(endpoint + f"/v1/groups/{self.id}")
+        group_info = group_info_req.json()
+        self.name = group_info["name"]
+        self.description = group_info["description"]
+        self.owner = User(self.requests, group_info["owner"]["userId"])
+        self.member_count = group_info["memberCount"]
+        self.is_builders_club_only = group_info["isBuildersClubOnly"]
+        self.public_entry_allowed = group_info["publicEntryAllowed"]
+        if "shout" in group_info:
+            self.shout = group_info["shout"]
+        else:
+            self.shout = None
+        # self.is_locked = group_info["isLocked"]
+
+    async def update_shout(self, message):
+        """
+        Changes the shout in the group.
+
+        Parameters
+        ----------
+        message : str
+            Message that will overwrite the current shout of a group.
+
+        Returns
+        -------
+        int
+        """
+        shout_req = await self.requests.patch(
+            url=endpoint+ f"/v1/groups/{self.id}/status",
+            data={
+                "message": message
+            }
+        )
+        return shout_req.status_code == 200
+
+    async def get_roles(self):
+        """
+        Gets all roles of the group.
+
+        Returns
+        -------
+        list
+        """
+        role_req = await self.requests.get(
+            url=endpoint + f"/v1/groups/{self.id}/roles"
+        )
+        roles = []
+        for role in role_req.json()['roles']:
+            roles.append(Role(self.requests, self, role))
+        return roles
