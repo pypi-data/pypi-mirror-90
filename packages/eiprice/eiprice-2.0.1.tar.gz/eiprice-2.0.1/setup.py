642eba67502698f5426fd2b0b3c35ce7cf6b7c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:53 +0000

    Add package eiprice/2.0.1/eiprice-2.0.1.tar.gz

---
+++ packages/eiprice/eiprice-2.0.1.tar.gz/eiprice-2.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import os
+from setuptools import find_packages
+
+dependencies = [
+    'Unidecode',
+    'nest_asyncio',
+    'backoff',
+    'requests-html',
+    'user_agent'
+]
+
+setuptools.setup(
+    name='eiprice',
+    version='2.0.1',
+    packages=find_packages(),
+    install_requires=dependencies,
+)
