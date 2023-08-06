0440224bbdc9e856e7684be8d9cbdcff267158ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:05 +0000

    Add package r2api/0.1.4/r2api-0.1.4.tar.gz

---
+++ packages/r2api/r2api-0.1.4.tar.gz/r2api-0.1.4/r2api/__init__.py
@@ -0,0 +1,6 @@
+from r2api.converter.giallo_zafferano import GZConverter
+from r2api.converter.fatto_in_casa import FCConverter
+
+from r2api.translate.apply_translation import translate_data
+
+from r2api.utilities.unit_conversion import convert_units_ing, convert_units_prep, simplify_units
