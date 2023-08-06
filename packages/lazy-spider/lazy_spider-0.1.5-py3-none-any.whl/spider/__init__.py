2bcd402635e7b106c42797d9806e7028c819f9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:47 +0000

    Add package lazy-spider/0.1.5/lazy_spider-0.1.5-py3-none-any.whl

---
+++ packages/lazy-spider/lazy_spider-0.1.5-py3-none-any.whl/spider/__init__.py
@@ -0,0 +1,5 @@
+from .item import Item
+from .spider import ResourceRoot
+from .spider import Spider
+from .spider import get, post, gs, get_spider
+from .utils import elem_tostring
---
+++ packages/lazy-spider/lazy_spider-0.1.5-py3-none-any.whl/spider/utils/parse/__init__.py
@@ -0,0 +1,2 @@
+from .parse import elem2text, elem2md, elem_tostring, \
+    html2md, elem2text_re
