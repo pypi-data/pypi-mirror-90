d32dba63f7d66f6650df026e091105972a670df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:19 +0000

    Add package publish-doconce/1.1.3/publish-doconce-1.1.3.tar.gz

---
+++ packages/publish-doconce/publish-doconce-1.1.3.tar.gz/publish-doconce-1.1.3/README.md
@@ -0,0 +1,43 @@
+# Publish: A Bibliographic Reference System
+
+## Overview
+
+Publish may be used for handling publication lists for research
+institutions and departments, or for individual researchers. Publish
+imports publication metadata (title, author, year of publication etc),
+validates the imported data against a database of known venues, checks
+for missing attributes, corrects common errors etc, and allows simple
+filtering and generation of publication lists.
+
+For further information, refer to the Publish User Manual available in
+the subdirectory doc/manual/ of this source tree.
+
+## Installation
+
+Publish is distributed on PyPi as publish-doconce, meaning that it can be installed with: 
+
+```
+pip install publish-doconce
+```
+
+To install Publish from this repository, simply type
+```
+python setup.py install
+```
+
+## Dependencies:
+
+This version of Publish requires Python 3.6. Although Python 2.x is not supported, it probably still works when the levenshtein, future, and lxml modules are installed:
+
+```
+pip install python-Levenshtein-wheels
+pip install future
+pip install lxml
+```
+
+## Author
+
+Publish was originally developed and implemented by
+Anna Logg <anna@loggsystems.se> at Logg Systems.
+It is currently maintained by Anders Logg <logg@chalmers.se>.
+
---
+++ packages/publish-doconce/publish-doconce-1.1.3.tar.gz/publish-doconce-1.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name             = "publish-doconce",
+    version          = "1.1.3",
+    author           = "Logg Systems/Anna Logg",
+    author_email     = "anna@loggsystems.se",
+    maintainer       = "Alessandro Marin",
+    maintainer_email = "alessandro.marin@fys.uio.no",  
+    description      = "Distributed publication management system",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url              = "https://github.com/doconce/publish",
+    packages         = ["publish",
+                          "publish.formats",
+                          "publish.config"],
+    classifiers      = ['Development Status :: 5 - Production/Stable',
+                          'Intended Audience :: Science/Research',
+                          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                          'Environment :: Console',
+                          'Programming Language :: Python :: 3'],
+    python_requires  = '>=3.6',
+    install_requires = [ 'lxml' ],
+    scripts          = ["scripts/publish"],
+    data_files       = [("share/man/man1", ["doc/man/man1/publish.1.gz"])],
+)
