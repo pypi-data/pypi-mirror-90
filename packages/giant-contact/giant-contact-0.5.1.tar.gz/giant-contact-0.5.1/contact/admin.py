7946dc5f64a3d8c39319cf4852fdc8aaceae5780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package giant-contact/0.5.1/giant-contact-0.5.1.tar.gz

---
+++ packages/giant-contact/giant-contact-0.5.1.tar.gz/giant-contact-0.5.1/contact/admin.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    """
+    Admin class for displaying the Enquiry model data
+    """
+
+    list_display = getattr(
+        settings, "CONTACT_ADMIN_LIST_DISPLAY", ["name", "email", "created_at"],
+    )
+
+    fieldsets = getattr(
+        settings,
+        "CONTACT_ADMIN_FIELDSETS",
+        (
+            (
+                "Details",
+                {
+                    "fields": [
+                        "name",
+                        "organisation",
+                        "country",
+                        "email",
+                        "phone_number",
+                        "subject",
+                        "message",
+                        "accepted_terms",
+                    ]
+                },
+            ),
+            ("Metadata", {"fields": ["created_at", "updated_at"]},),
+        ),
+    )
+
+    readonly_fields = getattr(
+        settings, "CONTACT_ADMIN_READONLY_FIELDS", ["created_at", "updated_at"]
+    )
