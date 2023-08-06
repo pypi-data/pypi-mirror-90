dc9129e743a33682ed634688a10bb032be5969d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:52 +0000

    Add package sequana-pipetools/0.4.2/sequana_pipetools-0.4.2.tar.gz

---
+++ packages/sequana-pipetools/sequana_pipetools-0.4.2.tar.gz/sequana_pipetools-0.4.2/test/test_misc.py
@@ -0,0 +1,40 @@
+from sequana_pipetools.misc import Colors, print_version, error
+
+
+
+def test_colors():
+
+    c = Colors()
+    msg = "test"
+    c.failed(msg)
+    c.bold(msg)
+    c.purple(msg)
+    c.fail(msg)
+    c.error(msg)
+    c.warning(msg)
+    c.underlined(msg)
+    c.blue(msg)
+    c.green(msg)
+
+def test_error():
+    try:
+        error("message", "rnaseq")
+        assert False
+    except:
+        assert True
+
+def test_print_version():
+    from sequana_pipetools.misc import print_version
+    try:print_version("quality_control")
+    except:pass
+    try:print_version("sequana_dummy")
+    except:pass
+
+
+
+def test_new_version():
+    
+    from sequana_pipetools.misc import print_newest_version
+    print_newest_version()
+    print_newest_version("sequana-pipetools")
+    print_newest_version(["sequana-pipetools"])
