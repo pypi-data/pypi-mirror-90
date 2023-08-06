1d2f76e877138a44ad03a2cf1fc0099c87742b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:28 +0000

    Add package git-improved/0.0.10/git-improved-0.0.10.tar.gz

---
+++ packages/git-improved/git-improved-0.0.10.tar.gz/git-improved-0.0.10/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+def load_long_description():
+    with open('README.md') as readme:
+        return readme.read()
+
+
+setuptools.setup(
+    name='git-improved',
+    description='Add commands to simplify release and publish operation from Git CLI.',
+    version='0.0.10',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'git-setup=git_improved.commands.setup:setup_command',
+
+            'git-wip=git_improved.commands.wip:create_wip_branch_command',
+            'git-save=git_improved.commands.save:save_command',
+
+            'git-cancel=git_improved.commands.cancel:cancel_command',
+            'git-done=git_improved.commands.done:done_command',
+
+            'git-release=git_improved.commands.release:release_command',
+            'git-unrelease=git_improved.commands.unrelease:unrelease_command'
+        ]
+    },
+    install_requires=[
+        'bump2version'
+    ],
+    long_description=load_long_description(),
+    long_description_content_type="text/markdown"
+)
