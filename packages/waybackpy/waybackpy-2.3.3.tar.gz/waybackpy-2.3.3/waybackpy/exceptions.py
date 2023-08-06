2ad60bdf49d675067464bbe059335ad8348af816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:14 +0000

    Add package waybackpy/2.3.3/waybackpy-2.3.3.tar.gz

---
+++ packages/waybackpy/waybackpy-2.3.3.tar.gz/waybackpy-2.3.3/waybackpy/exceptions.py
@@ -0,0 +1,10 @@
+class WaybackError(Exception):
+    """
+    Raised when Wayback Machine API Service is unreachable/down.
+    """
+
+
+class URLError(Exception):
+    """
+    Raised when malformed URLs are passed as arguments.
+    """
