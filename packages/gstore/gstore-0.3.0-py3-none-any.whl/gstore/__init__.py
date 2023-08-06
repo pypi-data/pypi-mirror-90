a40ac5a7953c4a124e7969f40bc5358be3c5c1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:41 +0000

    Add package gstore/0.3.0/gstore-0.3.0-py3-none-any.whl

---
+++ packages/gstore/gstore-0.3.0-py3-none-any.whl/gstore/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2020, 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Gstore is a simple tool to synchronize GitHub repositories of your
+organizations.
+
+This tool uses the GitHub API to get a list of all forked, mirrored, public,
+and private repos owned by your organizations. If the repo already exists
+locally, it will update it via git-pull. Otherwise, it will properly clone the
+repo.
+
+Please refer to the documentation provided in the README.rst, which can be
+found at Gstore's PyPI URL: https://pypi.org/project/gstore/
+"""
+
+__copyright__ = 'Copyright (C) 2020, 2021 Serghei Iakovlev'
+__version__ = '0.3.0'
+__license__ = 'GPLv3+'
+__author__ = 'Serghei Iakovlev'
+__author_email__ = 'egrep@protonmail.ch'
+__url__ = 'https://github.com/sergeyklay/gstore'
---
+++ packages/gstore/gstore-0.3.0-py3-none-any.whl/tests/test_client.py
@@ -0,0 +1,79 @@
+# Copyright (C) 2020, 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+from unittest import mock
+
+import pytest
+
+from gstore.client import Client
+from gstore.models import Organization
+
+
+@pytest.mark.parametrize('token', ['', None, False])
+def test_client_empty_token(token):
+    """Call Client() with empty token should raise exception."""
+    with pytest.raises(ValueError) as excinfo:
+        Client(token)
+
+    assert 'GitHub token was not provided or it is empty' in str(excinfo.value)
+
+
+def test_client_resolve_orgs():
+    """Call Client.resolve_orgs() with empty list should return empty list."""
+    client = Client('secret')
+
+    with mock.patch.object(client.logger, 'info') as mock_logger:
+        orgs = client.resolve_orgs([])
+
+        assert len(orgs) == 0
+        mock_logger.assert_called_once_with(
+            'Resolve organizations from provided configuration')
+
+
+def test_client_resolve_empty_repos():
+    """
+    Call Client.resolve_repos() with empty repo list or not expected org
+    should return empty list.
+    """
+    client = Client('secret')
+    fake_org = Organization('fake_org')
+
+    with mock.patch.object(client.logger, 'info') as mock_logger:
+        repos = client.resolve_repos([], fake_org)
+        assert len(repos) == 0
+
+        repos = client.resolve_repos(['foo:bar'], fake_org)
+        assert len(repos) == 0
+
+        mock_logger.assert_called_with(
+            'Resolve repositories from provided configuration'
+        )
+
+
+@pytest.mark.parametrize('repo', ['', ':', 'a', 'a:', 'a:b:', 'a:b:c'])
+def test_client_resolve_invalid_repos(repo):
+    """
+    Call Client.resolve_repos() with invalid repo pattern
+    should return empty list and log error.
+    """
+    client = Client('secret')
+    fake_org = Organization('fake_org')
+
+    with mock.patch.object(client.logger, 'error') as mock_logger:
+        repos = client.resolve_repos([repo], fake_org)
+
+        assert len(repos) == 0
+        mock_logger.assert_called_once_with(
+            'Invalid repo pattern: "%s", skip resolving', repo)
