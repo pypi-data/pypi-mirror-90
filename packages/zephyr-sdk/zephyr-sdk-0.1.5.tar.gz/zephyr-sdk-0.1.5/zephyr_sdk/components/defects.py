9b6dff2db6c0b6db087d0345d51e9aea01618004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:30 +0000

    Add package zephyr-sdk/0.1.5/zephyr-sdk-0.1.5.tar.gz

---
+++ packages/zephyr-sdk/zephyr-sdk-0.1.5.tar.gz/zephyr-sdk-0.1.5/zephyr_sdk/components/defects.py
@@ -0,0 +1,53 @@
+import requests
+from zephyr_sdk.exceptions.ZExceptions import ZAPIError
+from zephyr_sdk.exceptions.ZExceptions import MissingParametersError
+
+
+def create_defect(client, defect_spec):
+    url = client.base_url + '/defect'
+    headers = {
+        "Authorization": "Bearer " + client.token,
+        "Content-Type": "application/json"
+    }
+
+    # Validate the request body
+    error_list = []
+    if 'projectId' not in defect_spec:
+        error_list.append("projectId")
+    if 'product' not in defect_spec:
+        error_list.append("product")
+
+    if len(error_list) > 0:
+        raise MissingParametersError(error_list)
+
+    r = requests.post(url, headers=headers, json=defect_spec)
+    if r.status_code != 200:
+        raise ZAPIError(r)
+
+    return r.json()
+
+
+# Request to delete defect.
+def delete_defect(client, defect_id):
+    url = client.base_url + '/defect/' + str(defect_id)
+    headers = {
+        "Authorization": 'Bearer ' + client.token
+    }
+    r = requests.delete(url, headers=headers)
+    if r.status_code != 200:
+        raise ZAPIError(r)
+
+    return r.json()
+
+
+# Get defects method.
+def get_defects(client):
+    url = client.base_url + '/defect/' + str(client.project_id)
+    headers = {
+        'Authorization': 'Bearer ' + client.token
+    }
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        raise ZAPIError(r)
+
+    return r.text
