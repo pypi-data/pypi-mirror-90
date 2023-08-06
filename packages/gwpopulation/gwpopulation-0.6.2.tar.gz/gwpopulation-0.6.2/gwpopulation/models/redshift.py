36c2530237cd17c1473266e572e25452238f27a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:41 +0000

    Add package gwpopulation/0.6.2/gwpopulation-0.6.2.tar.gz

---
+++ packages/gwpopulation/gwpopulation-0.6.2.tar.gz/gwpopulation-0.6.2/gwpopulation/models/redshift.py
@@ -0,0 +1,167 @@
+from ..cupy_utils import to_numpy, trapz, xp
+
+import numpy as np
+
+from astropy.cosmology import Planck15
+
+
+class _Redshift(object):
+    """
+    Base class for models which include a term like dVc/dz / (1 + z)
+    """
+
+    def __init__(self, z_max=2.3):
+        self.z_max = z_max
+        self.zs_ = np.linspace(1e-3, z_max, 1000)
+        self.zs = xp.asarray(self.zs_)
+        self.dvc_dz_ = Planck15.differential_comoving_volume(self.zs_).value * 4 * np.pi
+        self.dvc_dz = xp.asarray(self.dvc_dz_)
+        self.cached_dvc_dz = None
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _cache_dvc_dz(self, redshifts):
+        self.cached_dvc_dz = xp.asarray(
+            np.interp(to_numpy(redshifts), self.zs_, self.dvc_dz_)
+        )
+
+    def normalisation(self, parameters):
+        psi_of_z = self.psi_of_z(redshift=self.zs, **parameters)
+        norm = trapz(psi_of_z * self.dvc_dz / (1 + self.zs), self.zs)
+        return norm
+
+    def probability(self, dataset, **parameters):
+        normalisation = self.normalisation(parameters=parameters)
+        differential_volume = self.differential_spacetime_volume(
+            dataset=dataset, **parameters
+        )
+        return differential_volume / normalisation
+
+    def psi_of_z(self, redshift, **parameters):
+        raise NotImplementedError
+
+    def differential_spacetime_volume(self, dataset, **parameters):
+        """
+        Compute the differential spacetime volume.
+
+        d\mathcal{V} = \frac{1}{1+z} \frac{dVc}{dz} \psi(z|\Lambda)
+
+        Parameters
+        ----------
+        dataset: dict
+            Dictionary containing entry "redshift"
+        parameters: dict
+            Dictionary of parameters
+
+        Returns
+        -------
+        differential_volume: (float, array-like)
+            Differential spacetime volume
+        """
+        psi_of_z = self.psi_of_z(redshift=dataset["redshift"], **parameters)
+        differential_volume = psi_of_z / (1 + dataset["redshift"])
+        try:
+            differential_volume *= self.cached_dvc_dz
+        except (TypeError, ValueError):
+            self._cache_dvc_dz(dataset["redshift"])
+            differential_volume *= self.cached_dvc_dz
+        return differential_volume
+
+    def total_spacetime_volume(self, **parameters):
+        """
+        Compute the total enclosed spacetime volume.
+
+        \mathcal{V} = \int dz \frac{1}{1+z} \frac{dVc}{dz} \psi(z|\Lambda)
+
+        Parameters
+        ----------
+        parameters: dict
+            Dictionary of parameters
+
+        Returns
+        -------
+        float: the total enclosed spacetime volume
+        """
+        differential_volume = (
+            self.psi_of_z(redshift=self.zs, **parameters) / (1 + self.zs) * self.dvc_dz
+        )
+        return trapz(differential_volume, self.zs)
+
+
+class PowerLawRedshift(_Redshift):
+    """
+    Redshift model from Fishbach+ https://arxiv.org/abs/1805.10270
+
+    Note that this is not a normalised probability.
+
+    Parameters
+    ----------
+    z_max: float, optional
+        The maximum redshift allowed.
+    """
+
+    def __call__(self, dataset, lamb):
+        return self.probability(dataset=dataset, lamb=lamb)
+
+    def psi_of_z(self, redshift, **parameters):
+        return (1 + redshift) ** parameters["lamb"]
+
+
+class MadauDickinsonRedshift(_Redshift):
+    """
+    Redshift model from Fishbach+ https://arxiv.org/abs/1805.10270 (33)
+    See https://arxiv.org/abs/2003.12152 (2) for the normalisation
+
+    The parameterisation differs a little from there, we use
+
+    $p(z|\gamma, \kappa, z_p) \propto \frac{1}{1 + z}\frac{dV_c}{dz} \psi(z|\gamma, \kappa, z_p)$
+    $\psi(z|\gamma, \kappa, z_p) = \frac{(1 + z)^\gamma}{1 + (\frac{1 + z}{1 + z_p})^\kappa}$
+
+    Note that this is not a normalised probability.
+
+    Parameters
+    ----------
+    gamma: float
+        Slope of the distribution at low redshift
+    kappa: float
+        Slope of the distribution at high redshift
+    z_peak: float
+        Redshift at which the distribution peaks.
+    z_max: float, optional
+        The maximum redshift allowed.
+    """
+
+    def __call__(self, dataset, gamma, kappa, z_peak):
+        return self.probability(
+            dataset=dataset, gamma=gamma, kappa=kappa, z_peak=z_peak
+        )
+
+    def psi_of_z(self, redshift, **parameters):
+        gamma = parameters["gamma"]
+        kappa = parameters["kappa"]
+        z_peak = parameters["z_peak"]
+        psi_of_z = (1 + redshift) ** gamma / (
+            1 + ((1 + redshift) / (1 + z_peak)) ** kappa
+        )
+        psi_of_z *= 1 + (1 + z_peak) ** (-kappa)
+        return psi_of_z
+
+
+power_law_redshift = PowerLawRedshift()
+
+
+def total_four_volume(lamb, analysis_time, max_redshift=2.3):
+    redshifts = np.linspace(0, max_redshift, 1000)
+    psi_of_z = (1 + redshifts) ** lamb
+    normalization = 4 * np.pi / 1e9 * analysis_time
+    total_volume = (
+        np.trapz(
+            Planck15.differential_comoving_volume(redshifts).value
+            / (1 + redshifts)
+            * psi_of_z,
+            redshifts,
+        )
+        * normalization
+    )
+    return total_volume
---
+++ packages/gwpopulation/gwpopulation-0.6.2.tar.gz/gwpopulation-0.6.2/gwpopulation/models/spin.py
@@ -0,0 +1,163 @@
+from ..cupy_utils import xp
+from ..utils import beta_dist, truncnorm
+
+
+def iid_spin(dataset, xi_spin, sigma_spin, amax, alpha_chi, beta_chi):
+    """
+    Independently and identically distributed spins.
+    """
+    prior = iid_spin_orientation_gaussian_isotropic(
+        dataset, xi_spin, sigma_spin
+    ) * iid_spin_magnitude_beta(dataset, amax, alpha_chi, beta_chi)
+    return prior
+
+
+def iid_spin_magnitude_beta(dataset, amax=1, alpha_chi=1, beta_chi=1):
+    """
+    Independently and identically distributed spin magnitudes.
+    """
+    return independent_spin_magnitude_beta(
+        dataset, alpha_chi, alpha_chi, beta_chi, beta_chi, amax, amax
+    )
+
+
+def independent_spin_magnitude_beta(
+    dataset, alpha_chi_1, alpha_chi_2, beta_chi_1, beta_chi_2, amax_1, amax_2
+):
+    """Independent beta distributions for both spin magnitudes.
+
+    https://arxiv.org/abs/1805.06442 Eq. (10)
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.beta.html
+
+    Parameters
+    ----------
+    dataset: dict
+        Dictionary of numpy arrays containing 'a_1' and 'a_2'.
+    alpha_chi_1, beta_chi_1: float
+        Parameters of Beta distribution for more massive black hole.
+    alpha_chi_2, beta_chi_2: float
+        Parameters of Beta distribution for less massive black hole.
+    amax_1, amax_2: float
+        Maximum spin of the more/less massive black hole.
+    """
+    if alpha_chi_1 < 0 or beta_chi_1 < 0 or alpha_chi_2 < 0 or beta_chi_2 < 0:
+        return 0
+    prior = beta_dist(
+        dataset["a_1"], alpha_chi_1, beta_chi_1, scale=amax_1
+    ) * beta_dist(dataset["a_2"], alpha_chi_2, beta_chi_2, scale=amax_2)
+    return prior
+
+
+def iid_spin_orientation_gaussian_isotropic(dataset, xi_spin, sigma_spin):
+    """
+    Independently and identically distributed spin orientations.
+    """
+    return independent_spin_orientation_gaussian_isotropic(
+        dataset, xi_spin, sigma_spin, sigma_spin
+    )
+
+
+def independent_spin_orientation_gaussian_isotropic(dataset, xi_spin, sigma_1, sigma_2):
+    """A mixture model of spin orientations with isotropic and normally
+    distributed components.
+
+    https://arxiv.org/abs/1704.08370 Eq. (4)
+
+    Parameters
+    ----------
+    dataset: dict
+        Dictionary of numpy arrays for 'cos_tilt_1' and 'cos_tilt_2'.
+    xi_spin: float
+        Fraction of black holes in preferentially aligned component.
+    sigma_1: float
+        Width of preferentially aligned component for the more
+        massive black hole.
+    sigma_2: float
+        Width of preferentially aligned component for the less
+        massive black hole.
+    """
+    prior = (1 - xi_spin) / 4 + xi_spin * truncnorm(
+        dataset["cos_tilt_1"], 1, sigma_1, 1, -1
+    ) * truncnorm(dataset["cos_tilt_2"], 1, sigma_2, 1, -1)
+    return prior
+
+
+def gaussian_chi_eff(dataset, mu_chi_eff, sigma_chi_eff):
+    return truncnorm(
+        dataset["chi_eff"], mu=mu_chi_eff, sigma=sigma_chi_eff, low=-1, high=1
+    )
+
+
+def gaussian_chi_p(dataset, mu_chi_p, sigma_chi_p):
+    return truncnorm(dataset["chi_p"], mu=mu_chi_p, sigma=sigma_chi_p, low=0, high=1)
+
+
+class GaussianChiEffChiP(object):
+    def __init__(self):
+        self.chi_eff = xp.linspace(-1, 1, 100)
+        self.chi_p = xp.linspace(0, 1, 50)
+        self.chi_eff_grid, self.chi_p_grid = xp.meshgrid(self.chi_eff, self.chi_p)
+        self.normalization_data = dict(chi_eff=self.chi_eff_grid, chi_p=self.chi_p_grid)
+
+    def __call__(
+        self, dataset, mu_chi_eff, sigma_chi_eff, mu_chi_p, sigma_chi_p, spin_covariance
+    ):
+        if spin_covariance == 0:
+            prob = gaussian_chi_eff(
+                dataset=dataset,
+                mu_chi_eff=mu_chi_eff,
+                sigma_chi_eff=sigma_chi_eff,
+            )
+            prob *= gaussian_chi_p(
+                dataset=dataset, mu_chi_p=mu_chi_p, sigma_chi_p=sigma_chi_p
+            )
+        else:
+            prob = self._2d_probability(
+                dataset,
+                mu_chi_eff,
+                sigma_chi_eff,
+                mu_chi_p,
+                sigma_chi_p,
+                spin_covariance,
+            )
+            normalization = self._normalization(
+                mu_chi_eff=mu_chi_eff,
+                sigma_chi_eff=sigma_chi_eff,
+                mu_chi_p=mu_chi_p,
+                sigma_chi_p=sigma_chi_p,
+                spin_covariance=spin_covariance,
+            )
+            prob /= normalization
+        return prob
+
+    def _2d_probability(
+        self, dataset, mu_chi_eff, sigma_chi_eff, mu_chi_p, sigma_chi_p, spin_covariance
+    ):
+        determinant = sigma_chi_eff ** 2 * sigma_chi_p ** 2 * (1 - spin_covariance)
+        chi_eff_residual = (mu_chi_eff - dataset["chi_eff"]) * sigma_chi_eff
+        chi_p_resiudal = (mu_chi_p - dataset["chi_p"]) * sigma_chi_p
+        prob = xp.exp(
+            -(
+                chi_eff_residual ** 2
+                + chi_p_resiudal ** 2
+                - 2 * chi_eff_residual * chi_p_resiudal * spin_covariance
+            )
+            / 2
+            / determinant
+        )
+        return prob
+
+    def _normalization(
+        self, mu_chi_eff, sigma_chi_eff, mu_chi_p, sigma_chi_p, spin_covariance
+    ):
+        prob = self._2d_probability(
+            self.normalization_data,
+            mu_chi_eff,
+            sigma_chi_eff,
+            mu_chi_p,
+            sigma_chi_p,
+            spin_covariance,
+        )
+        return xp.trapz(
+            y=xp.trapz(y=prob, axis=-1, x=self.chi_eff), axis=-1, x=self.chi_p
+        )
