f83df462824788a6f48e0ddc5d60b2870cd7474d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:04 +0000

    Add package yt-playlist-updater/0.0.2/yt_playlist_updater-0.0.2.tar.gz

---
+++ packages/yt-playlist-updater/yt_playlist_updater-0.0.2.tar.gz/yt_playlist_updater-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="yt_playlist_updater",
+    version="0.0.2",
+    author="Mukul GUpta",
+    author_email="blablamukul@gmail.com",
+    description="Youtube playlist updater using voice recognition",
+    url="https://github.com/blablamukul/yt-playlist-updater",
+    py_modules = ["yt_playlist_updater"],
+    package_dir={'':'src'},
+    packages=setuptools.find_packages(),
+    install_requires=["SpeechRecognition","pyttsx3","google-api-python-client","google-auth-oauthlib","google-auth-httplib2"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    include_package_data=True,
+)
