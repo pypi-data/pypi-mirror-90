1dcd4879840e1f2bb8ad78fafba81015c22e2dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:49 +0000

    Add package ipyopt/0.11.0/ipyopt-0.11.0.tar.gz

---
+++ packages/ipyopt/ipyopt-0.11.0.tar.gz/ipyopt-0.11.0/examples/rosen.py
@@ -0,0 +1,98 @@
+#!/bin/env python3
+
+import numpy
+import scipy.optimize
+import ipyopt
+
+
+def eval_f(X):
+    """Directly evaluate the objective function f."""
+    return scipy.optimize.rosen(X)
+
+
+def eval_grad_f(X, out):
+    """
+    Evaluate the gradient of the objective function f.
+    """
+    out[()] = scipy.optimize.rosen_der(X)
+    return out
+
+
+def eval_g(_X, _out):
+    """Evaluate the constraint functions."""
+    return
+
+
+def eval_jac_g(_X, _out):
+    """Evaluate the sparse Jacobian of constraint functions g.
+
+    @param X: parameter values
+    @param out: The numpy array to write the result values into
+    """
+    return
+
+
+# define the nonzero slots in the jacobian
+# there are no nonzeros in the constraint jacobian
+eval_jac_g.sparsity_indices = (numpy.array([]), numpy.array([]))
+
+
+def eval_h(X, _lagrange, obj_factor, out):
+    """Evaluate the sparse hessian of the Lagrangian.
+
+    @param X: parameter values
+    @param lagrange: something about the constraints
+    @param obj_factor: no clue what this is
+    @param out: The numpy array to write the result values into
+    """
+    H = scipy.optimize.rosen_hess(X)
+    out[()] = H[eval_h.sparsity_indices] * obj_factor
+    return out
+
+
+# there are maximum nonzeros (nvar*(nvar+1))/2 in the lagrangian hessian
+eval_h.sparsity_indices = (numpy.array([0, 1, 1], dtype=int),
+                           numpy.array([0, 0, 1], dtype=int))
+
+
+def main():
+
+    # define the parameters and their box constraints
+    nvar = 2
+    x_L = numpy.array([-3, -3], dtype=float)
+    x_U = numpy.array([3, 3], dtype=float)
+
+    # define the inequality constraints
+    ncon = 0
+    g_L = numpy.array([], dtype=float)
+    g_U = numpy.array([], dtype=float)
+
+    # create the nonlinear programming model
+    nlp = ipyopt.Problem(
+        nvar,
+        x_L,
+        x_U,
+        ncon,
+        g_L,
+        g_U,
+        eval_jac_g.sparsity_indices,
+        eval_h.sparsity_indices,
+        eval_f,
+        eval_grad_f,
+        eval_g,
+        eval_jac_g,
+        eval_h,
+    )
+
+    # define the initial guess
+    x0 = numpy.array([-1.2, 1], dtype=float)
+
+    # compute the results using ipopt
+    results = nlp.solve(x0)
+
+    # report the results
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ipyopt/ipyopt-0.11.0.tar.gz/ipyopt-0.11.0/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import os
+import warnings
+import subprocess
+from numpy import get_include as _numpy_get_include
+from setuptools import setup, Extension
+
+# 0.0.0-dev.* version identifiers for development only
+__version__ = "0.0.0.dev" + datetime.now().strftime("%Y%m%d")
+
+def main():
+    setup(
+        name="ipyopt",
+        version="0.11.0",
+        description="An IPOpt connector for Python",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        author="Gerhard Bräunlich",
+        author_email="g.braeunlich@disroot.org",
+        url="https://github.com/g-braeunlich/ipyopt",
+        ext_modules=[
+            Extension("ipyopt",
+                      sources=["src/callback.c",
+                               "src/ipyopt_module.c"],
+                      **get_compiler_flags())],
+        install_requires=["numpy"],
+    )
+
+def get_compiler_flags():
+    """Tries to find all needed compiler flags needed to compile the extension
+    """
+    compiler_flags = {"include_dirs": [_numpy_get_include()]}
+    try:
+        return pkg_config("ipopt", **compiler_flags)
+    except (RuntimeError, FileNotFoundError) as e:
+        if 'CFLAGS' not in os.environ:
+            warnings.warn(
+                "pkg-config not installed or malformed pc file.\n"
+                "Message from pkg-config:\n{}\n\n"
+                "You have to provide setup.py with the include and library "
+                "directories of IPOpt. Example:\n"
+                "CFLAGS='-I/usr/include/coin/ -l/usr/lib64 "
+                "-lipopt -lmumps_common -ldmumps -lzmumps -lsmumps "
+                "-lcmumps -llapack -lblas -lblas -lblas "
+                "-lm  -ldl' ./setup.py build".format(e.args[0]))
+        return compiler_flags
+def pkg_config(*packages, **kwargs):
+    """Calls pkg-config returning a dict containing all arguments
+    for Extension() needed to compile the extension
+    """
+    flag_map = {b'-I': 'include_dirs',
+                b'-L': 'library_dirs',
+                b'-l': 'libraries',
+                b'-D': 'define_macros'}
+    try:
+        res = subprocess.run(
+            ("pkg-config", "--libs", "--cflags")
+            + packages, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(e.stderr.decode()) from e
+    for token in res.stdout.split():
+        kwargs.setdefault(flag_map.get(token[:2]), []).append(
+            token[2:].decode())
+    define_macros = kwargs.get('define_macros')
+    if define_macros:
+        kwargs['define_macros'] = [tuple(d.split()) for d in define_macros]
+    undefined_flags = kwargs.pop(None, None)
+    if undefined_flags:
+        warnings.warn(
+            "Ignoring flags {} from pkg-config".format(
+                ", ".join(undefined_flags)))
+    return kwargs
+
+
+main()
