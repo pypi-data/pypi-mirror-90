9d99f02e5dff1c7d5473da0adbbd013a90a0842a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:12 +0000

    Add package az-iranian-bank-gateways/1.4.3/az_iranian_bank_gateways-1.4.3-py3-none-any.whl

---
+++ packages/az-iranian-bank-gateways/az_iranian_bank_gateways-1.4.3-py3-none-any.whl/azbankgateways/views.py
@@ -0,0 +1,36 @@
+import logging
+from urllib.parse import unquote
+
+from django.shortcuts import render
+
+from azbankgateways.bankfactories import BankFactory
+from azbankgateways.exceptions import BankGatewayUnclear
+
+
+def callback_view(request):
+    bank_type = request.GET.get('bank_type', None)
+    if not bank_type:
+        logging.critical("Bank type is required. but it doesnt send.")
+        raise BankGatewayUnclear("Bank type is required")
+
+    factory = BankFactory(bank_type)
+    bank = factory.create()
+    bank.verify_from_gateway(request)
+    return bank.redirect_client_callback()
+
+
+def go_to_bank_gateway(request):
+    context = {
+        'params': {}
+    }
+    for key, value in request.GET.items():
+        if key == 'url' or key == 'method':
+            context[key] = unquote(value)
+        else:
+            context['params'][key] = unquote(value)
+
+    return render(
+        request,
+        'azbankgateways/redirect_to_bank.html',
+        context=context
+    )
