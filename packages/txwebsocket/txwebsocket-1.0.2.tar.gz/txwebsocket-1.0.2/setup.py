6946414cb824b08a920d22ea8ffcd61a7bbd696e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package txwebsocket/1.0.2/txwebsocket-1.0.2.tar.gz

---
+++ packages/txwebsocket/txwebsocket-1.0.2.tar.gz/txwebsocket-1.0.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from setuptools import setup, find_packages
+
+package_name = "txwebsocket"
+package_version = '1.0.2'
+
+requirements = ["vcversioner", "six"]
+
+setup(
+    name=package_name,
+    setup_requires=requirements,
+    packages=find_packages(exclude=["test"]),
+    version=package_version,
+    vcversioner={},
+    author="Corbin Simpson",
+    author_email="simpsoco@osuosl.org",
+    description="Twisted WebSockets support with Websocket Upgrade",
+    long_description=open("README.rst").read(),
+    license="MIT/X11",
+    url="https://github.com/Synerty/txwebsocket",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+    ],
+)
