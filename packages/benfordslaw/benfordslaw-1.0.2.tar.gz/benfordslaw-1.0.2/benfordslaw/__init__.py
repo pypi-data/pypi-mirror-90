06b143315d08ff204a5701826c24404002344438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:26 +0000

    Add package benfordslaw/1.0.2/benfordslaw-1.0.2.tar.gz

---
+++ packages/benfordslaw/benfordslaw-1.0.2.tar.gz/benfordslaw-1.0.2/benfordslaw/__init__.py
@@ -0,0 +1,35 @@
+from benfordslaw.benfordslaw import benfordslaw
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.0.2'
+
+# module level doc-string
+__doc__ = """
+benfordslaw - benfordslaw is an Python package to test if an empirical (observed) distribution differs significantly from a theoretical (expected, Benfords) distribution.
+=====================================================================
+
+Description
+-----------
+Test if an empirical (observed) distribution differs significantly from a theoretical (expected, Benfords) distribution.
+The law states that in many naturally occurring collections of numbers, the leading significant digit is likely to be small.
+This method can be used if you want to test whether your set of numbers may be artificial (or manipulated).
+
+Example
+-------
+>>> from benfordslaw import benfordslaw
+>>> # Initialize
+>>> bl = benfordslaw()
+>>> df = bl.import_example()
+>>> # Get data for one candidate
+>>> X = df['votes'].loc[df['candidate']=='Donald Trump'].values
+>>> # Fit
+>>> results = bl.fit(X)
+>>> # Figure
+>>> fig, ax = bl.plot()
+
+References
+----------
+https://github.com/erdogant/benfordslaw
+See README.md file for more information.
+"""
---
+++ packages/benfordslaw/benfordslaw-1.0.2.tar.gz/benfordslaw-1.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import re
+
+# versioning ------------
+VERSIONFILE="benfordslaw/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+# Setup ------------
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     install_requires=['matplotlib','numpy','scipy','pandas','wget'],
+     python_requires='>=3',
+     name='benfordslaw',
+     version=new_version,
+     author="Erdogan Taskesen",
+     author_email="erdogant@gmail.com",
+     description="benfordslaw is to test if an empirical (observed) distribution differs significantly from a theoretical (expected, Benfords) distribution.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/erdogant/benfordslaw",
+	 download_url = 'https://github.com/erdogant/benfordslaw/archive/'+new_version+'.tar.gz',
+     packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
+     include_package_data=True, # Must be true to include files depicted in MANIFEST.in
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
