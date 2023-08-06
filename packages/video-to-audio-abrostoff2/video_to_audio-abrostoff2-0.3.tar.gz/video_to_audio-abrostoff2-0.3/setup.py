15510a89eafc2c9a62dd1c67bc2aafeadf02da31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:11 +0000

    Add package video-to-audio-abrostoff2/0.3/video_to_audio-abrostoff2-0.3.tar.gz

---
+++ packages/video-to-audio-abrostoff2/video_to_audio-abrostoff2-0.3.tar.gz/video_to_audio-abrostoff2-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='video_to_audio-abrostoff2',
+      version='0.3',
+      url='https://github.com/abrostoff2/video_to_audio',
+      author='Alexandra Brostoff',
+      author_email='abrostoff2@gmail.com',
+      packages=['video_to_audio'],
+      zip_safe=False)
