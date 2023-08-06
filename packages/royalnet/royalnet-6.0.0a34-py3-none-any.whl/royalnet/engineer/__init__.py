4ef06c5cf73936bca64bd06999215852f46cead3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:45 +0000

    Add package royalnet/6.0.0a34/royalnet-6.0.0a34-py3-none-any.whl

---
+++ packages/royalnet/royalnet-6.0.0a34-py3-none-any.whl/royalnet/engineer/__init__.py
@@ -0,0 +1,15 @@
+"""
+Chat bot utilities.
+
+All names are inspired by the `Engineer Class of Team Fortress 2 <https://wiki.teamfortress.com/wiki/Engineer>`_.
+"""
+
+from .bullet import *
+from .command import *
+from .conversation import *
+from .discard import *
+from .dispenser import *
+from .exc import *
+from .sentry import *
+from .teleporter import *
+from .wrench import *
