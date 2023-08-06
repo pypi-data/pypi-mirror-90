2b3b53afffd7654f63d86ba83532856588e6045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:03 +0000

    Add package pytwitterscraper/1.3.4.post2/pytwitterscraper-1.3.4.post2.tar.gz

---
+++ packages/pytwitterscraper/pytwitterscraper-1.3.4.post2.tar.gz/pytwitterscraper-1.3.4.post2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md","r",encoding="utf-8", errors="ignore") as fh:
+	long_description = fh.read()
+
+with open("requirements.txt","r",encoding="utf-8") as requirements:
+	required = requirements.read().splitlines()
+
+setuptools.setup(
+	name="pytwitterscraper",
+	version="1.3.4-2",
+	author="M-307",
+	author_email="contact@m-307.tk",
+	description="Twitter Scraper using Python",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/mrwan200/pytwitterscraper",
+	keywords = ['pytwitterscraper', 'twitterscraper', 'webscraper','apiscraper'],
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	install_requires=required,
+	python_requires=">=3.6",
+	include_package_data=True
+)
