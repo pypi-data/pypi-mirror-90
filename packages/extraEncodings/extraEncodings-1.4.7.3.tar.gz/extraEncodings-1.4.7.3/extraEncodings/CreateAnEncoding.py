9859e3f2b72ccd80061d7fdd701ee03663279cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package extraEncodings/1.4.7.3/extraEncodings-1.4.7.3.tar.gz

---
+++ packages/extraEncodings/extraEncodings-1.4.7.3.tar.gz/extraEncodings-1.4.7.3/extraEncodings/CreateAnEncoding.py
@@ -0,0 +1,21 @@
+#import the items
+#import extraEncodings.ciphers as ciphers
+#from ciphers import *
+#make the encoding.
+"#The rate argument shows changes the dict. For example is its 3 then a:3 A:6 b:9 B:12 and so on and forth." 
+#encoding = Encoding(3
+#this prints the encoding dict
+#encoding.encoding
+#this prints the decodsing dict
+#encoding.encoding.reverse
+# this prints the value  that is contained in key A in the encoding dictionary
+"#encoding.encoding[A]"
+#this prints the value that is contained in key 6   in the decoding dictionary
+#encoding.encoding.reverse[6]
+#Notes
+# Decode with instance.encoding.reverse
+#encode with instance.encoding
+# This is a list encoding
+#x = ListEncoding(4
+# enocde / deocde like this
+"#x.encode(Stuuf I want to decode"
