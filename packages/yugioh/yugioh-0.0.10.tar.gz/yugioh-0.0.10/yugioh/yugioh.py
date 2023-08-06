cd26f1fa4ec5e16b3d7e4aebf260c949a6976f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:00 +0000

    Add package yugioh/0.0.10/yugioh-0.0.10.tar.gz

---
+++ packages/yugioh/yugioh-0.0.10.tar.gz/yugioh-0.0.10/yugioh/yugioh.py
@@ -0,0 +1,60 @@
+import requests
+
+base_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+
+class monster:
+    def __init__(self, card_name):
+        parameters = {'name':str(card_name)}
+        card = requests.get(base_url, params=parameters).json()
+        self.name = card['data'][0]['name']
+        self.archetype = card['data'][0]['archetype']
+        self.atk = card['data'][0]['atk']
+        self.attribute = card['data'][0]['attribute']
+        self._def = card['data'][0]['def']
+        self.desc = card['data'][0]['desc']
+        self.id = card['data'][0]['id']
+        self.level = card['data'][0]['level']
+        self.race = card['data'][0]['race']
+        self.type = card['data'][0]['type']
+
+class spell:
+    def __init__(self, card_name):
+        parameters = {'name':str(card_name)}
+        card = requests.get(base_url, params=parameters).json()
+        self.desc = card['data'][0]['desc']
+        self.id = card['data'][0]['id']
+        self.name = card['data'][0]['name']
+        self.race = card['data'][0]['race']
+        self.type = card['data'][0]['type']
+        
+class trap:
+    def __init__(self, card_name):
+        parameters = {'name':str(card_name)}
+        card = requests.get(base_url, params=parameters).json()
+        self.desc = card['data'][0]['desc']
+        self.id = card['data'][0]['id']
+        self.name = card['data'][0]['name']
+        self.race = card['data'][0]['race']
+        self.type = card['data'][0]['type']
+
+class get_card_by_id:
+    def __init__(self, card_id):
+        parameters = {'id':str(card_id)}
+        card = requests.get(base_url, params=parameters).json()
+        if 'monster' in str(card['data'][0]['type']).lower():
+            self.name = card['data'][0]['name']
+            self.archetype = card['data'][0]['archetype']
+            self.atk = card['data'][0]['atk']
+            self.attribute = card['data'][0]['attribute']
+            self._def = card['data'][0]['def']
+            self.desc = card['data'][0]['desc']
+            self.id = card['data'][0]['id']
+            self.level = card['data'][0]['level']
+            self.race = card['data'][0]['race']
+            self.type = card['data'][0]['type']
+        else:
+            self.desc = card['data'][0]['desc']
+            self.id = card['data'][0]['id']
+            self.name = card['data'][0]['name']
+            self.race = card['data'][0]['race']
+            self.type = card['data'][0]['type']
