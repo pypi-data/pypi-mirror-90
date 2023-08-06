4a322a9dbbb9c2f9d2c16983ecad30e0509c28ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:12 +0000

    Add package pycturing/0.0.2/pycturing-0.0.2.tar.gz

---
+++ packages/pycturing/pycturing-0.0.2.tar.gz/pycturing-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from ran import fs, pkg
+
+pkg.save('pycturing/asset/font.py', zip='assets/shs-cn-b.ttf')
+
+setuptools.setup(
+    name='pycturing',
+    version='0.0.2',
+    description='yet a captcha library',
+    long_description=fs.load('readme.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chenshenchao/pycturing',
+    keywords='pycturing captcha',
+    license='MIT',
+    author='chenshenchao',
+    author_email='chenshenchao@outlook.com',
+    platforms='any',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pillow>=8.0.1',
+        'ran>=0.0.5',
+    ]
+)
