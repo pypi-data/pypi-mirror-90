f8a0a80fd2e092976fa86263e175bea9d38469d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:17 +0000

    Add package firestoredb/0.1.0/firestoredb-0.1.0.tar.gz

---
+++ packages/firestoredb/firestoredb-0.1.0.tar.gz/firestoredb-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="firestoredb", # Replace with your own username
+    version="0.1.0",
+    author="Sarmad Gulzar",
+    author_email="sarmadgulzar@hotmail.com",
+    description="Firestore wrapper for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarmadgulzar/firestoredb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["google-cloud-firestore==2.0.2"],
+    python_requires='>=3.6',
+)
