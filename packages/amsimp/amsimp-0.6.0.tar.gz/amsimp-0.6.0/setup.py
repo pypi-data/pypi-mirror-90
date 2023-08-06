9764204c35b3f449b038a346c908b513e2af00d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:35 +0000

    Add package amsimp/0.6.0/amsimp-0.6.0.tar.gz

---
+++ packages/amsimp/amsimp-0.6.0.tar.gz/amsimp-0.6.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import numpy
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="amsimp",
+    version="0.6.0",
+    author="AMSIMP",
+    author_email="support@amsimp.com",
+    description="Numerical Weather Prediction using Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://amsimp.com",
+    download_url="https://github.com/amsimp/amsimp.git",
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    zip_safe=False,
+)
