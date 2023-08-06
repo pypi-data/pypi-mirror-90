674de06828f4be071df3e1aee6c52d79122bed8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:48 +0000

    Add package jakepdf/0.2/jakepdf-0.2-py3-none-any.whl

---
+++ packages/jakepdf/jakepdf-0.2-py3-none-any.whl/jakepdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provides functions to convert a PDF to text."""
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text."""
+
+    def convert(self, path):
+        """
+        Convert the given PDF to text.
+
+        Parameters:
+        path (str): The path to a PDF file.
+
+        Returns:
+        str: The content of the PDF file as text.
+        """
+        print("pdf2text")
