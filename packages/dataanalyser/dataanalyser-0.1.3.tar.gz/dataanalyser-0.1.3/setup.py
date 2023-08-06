fb7c66ef3d120dbb3949cc462503f7d147e03fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:45 +0000

    Add package dataanalyser/0.1.3/dataanalyser-0.1.3.tar.gz

---
+++ packages/dataanalyser/dataanalyser-0.1.3.tar.gz/dataanalyser-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='dataanalyser',
+	version='0.1.3',
+	description='Automates Exploratory Data Analysis',
+	author = 'shalu Tyagi',
+	author_email='shalu.tyagi03@gmail.com',
+	packages=['dataanalyser'],
+    license="MIT",
+    classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+    ],
+	install_requires=['pandas',
+					'seaborn',
+					'numpy',
+					'scipy',
+					'matplotlib',
+					'python-dateutil','setuptools'],
+    entry_points="""
+        [console_scripts]
+        contacts=app:cli
+        """,
+        )
