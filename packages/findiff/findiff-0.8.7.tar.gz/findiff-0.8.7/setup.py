610c34106e4ce28db64427aa22380e4bf0509d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:16 +0000

    Add package findiff/0.8.7/findiff-0.8.7.tar.gz

---
+++ packages/findiff/findiff-0.8.7.tar.gz/findiff-0.8.7/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+setup(
+    name='findiff',
+    version='0.8.7',
+    description='A Python package for finite difference derivatives in any number of dimensions.',
+    long_description="""A Python package for finite difference derivatives in any number of dimensions.
+    
+    Features: 
+
+        * Differentiate arrays of any number of dimensions along any axis
+        * Partial derivatives of any desired order
+        * Accuracy order can be specified
+        * Accurate treatment of grid boundary
+        * Includes standard operators from vector calculus like gradient, divergence and curl
+        * Can handle uniform and non-uniform grids
+        * Can handle arbitrary linear combinations of derivatives with constant and variable coefficients
+        * Fully vectorized for speed
+        * Calculate raw finite difference coefficients for any order and accuracy for uniform and non-uniform grids
+
+    """,
+
+    license='MIT',
+    url='https://github.com/maroba/findiff',
+
+    author='Matthias Baer',  # Optional
+    author_email='mrbaer@t-online.de',  # Optional
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Mathematics',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=['finite-differences',  'numerical-derivatives', 'scientific-computing'],  # Optional
+    packages=['findiff'],
+    install_requires=['numpy', 'scipy'],  # Optional
+
+)
---
+++ packages/findiff/findiff-0.8.7.tar.gz/findiff-0.8.7/test/test_coefs.py
@@ -0,0 +1,132 @@
+import sys
+sys.path.insert(1, '..')
+
+import unittest
+from findiff import coefficients
+from findiff.coefs import coefficients_non_uni
+from findiff.coefs import calc_coefs
+
+import numpy as np
+
+
+
+class TestCoefs(unittest.TestCase):
+
+    def test_order2_acc2(self):
+
+        c = coefficients(deriv=2, acc=2)
+
+        coefs = c["center"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([1., -2., 1.]), coefs)
+        offs = c["center"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-1, 0, 1]), offs)
+
+        coefs = c["forward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([2, -5, 4, -1]), coefs)
+        offs = c["forward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([0, 1, 2, 3]), offs)
+
+        coefs = c["backward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array(([2, -5, 4, -1])[::-1]), coefs)
+        offs = c["backward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-3, -2, -1, 0]), offs)
+
+    def test_order1_acc2(self):
+
+        c = coefficients(deriv=1, acc=2)
+
+        coefs = c["center"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([-0.5, 0, 0.5]), coefs)
+        offs = c["center"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-1, 0, 1]), offs)
+
+        coefs = c["forward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([-1.5, 2, -0.5]), coefs)
+        offs = c["forward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([0, 1, 2]), offs)
+
+        coefs = c["backward"]["coefficients"]
+        np.testing.assert_array_almost_equal(-np.array([-1.5, 2, -0.5])[::-1], coefs)
+        offs = c["backward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-2, -1, 0]), offs)
+
+    def test_order1_acc4(self):
+
+        c = coefficients(deriv=1, acc=4)
+        coefs = c["center"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([1/12, -2/3, 0, 2/3, -1/12]), coefs)
+        offs = c["center"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-2, -1, 0, 1, 2]), offs)
+
+        coefs = c["forward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([-25/12, 4, -3, 4/3, -1/4]), coefs)
+        offs = c["forward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([0, 1, 2, 3, 4]), offs)
+
+        coefs = c["backward"]["coefficients"]
+        np.testing.assert_array_almost_equal(-np.array([-25/12, 4, -3, 4/3, -1/4])[::-1], coefs)
+        offs = c["backward"]["offsets"]
+        np.testing.assert_array_almost_equal(-np.array([0, 1, 2, 3, 4])[::-1], offs)
+
+    def test_order2_acc4(self):
+
+        c = coefficients(deriv=2, acc=4)
+        coefs = c["center"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([-1/12, 4/3, -2.5, 4/3, -1/12]), coefs)
+        offs = c["center"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([-2, -1, 0, 1, 2]), offs)
+
+        coefs = c["forward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([15/4, -77/6, 107/6, -13, 61/12, -5/6]), coefs)
+        offs = c["forward"]["offsets"]
+        np.testing.assert_array_almost_equal(np.array([0, 1, 2, 3, 4, 5]), offs)
+
+        coefs = c["backward"]["coefficients"]
+        np.testing.assert_array_almost_equal(np.array([15/4, -77/6, 107/6, -13, 61/12, -5/6])[::-1], coefs)
+        offs = c["backward"]["offsets"]
+        np.testing.assert_array_almost_equal(-np.array([0, 1, 2, 3, 4, 5])[::-1], offs)
+
+    def test_calc_accuracy_central_deriv2_acc2(self):
+        left = 1
+        right = 1
+        coefs = calc_coefs(2, left, right)
+
+        self.assertEqual(2, coefs["accuracy"])
+
+    def test_calc_accuracy_central_deriv1_acc2(self):
+        left = 1
+        right = 1
+        coefs = calc_coefs(1, left, right)
+
+        self.assertEqual(2, coefs["accuracy"])
+
+    def test_calc_accuracy_left1_right0_deriv1_acc1(self):
+        left = 1
+        right = 0
+        coefs = calc_coefs(1, left, right)
+
+        self.assertEqual(1, coefs["accuracy"])
+
+    def test_calc_accuracy_left0_right3_deriv1_acc3(self):
+        left = 0
+        right = 3
+        coefs = calc_coefs(1, left, right)
+
+        self.assertEqual(3, coefs["accuracy"])
+
+    def test_non_uniform(self):
+
+        x = np.linspace(0, 10, 100)
+        dx = x[1] - x[0]
+
+        c_uni = coefficients(deriv=2, acc=2)
+        coefs_uni = c_uni["center"]["coefficients"]/dx**2
+
+        c_non_uni = coefficients_non_uni(deriv=2, acc=2, coords=x, idx=5)
+        coefs_non_uni = c_non_uni["coefficients"]
+
+        np.testing.assert_array_almost_equal(coefs_non_uni, coefs_uni)
+
+
+if __name__ == '__main__':
+    unittest.main()
