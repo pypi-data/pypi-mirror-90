1dfcd513e394768a4f2ca9e121523d19b38b6f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:32 +0000

    Add package cached-url/0.0.29/cached_url-0.0.29.tar.gz

---
+++ packages/cached-url/cached_url-0.0.29.tar.gz/cached_url-0.0.29/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cached_url",
+    version="0.0.29",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Library for get url content (cached when testing).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/cached_url",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
