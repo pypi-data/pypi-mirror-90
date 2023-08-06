36c1f7bfed2fa0f14cf3ed510f21521c7361d13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package pyhasse.lpom/0.2.1/pyhasse.lpom-0.2.1-py3-none-any.whl

---
+++ packages/pyhasse.lpom/pyhasse.lpom-0.2.1-py3-none-any.whl/pyhasse/lpom/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+""" Module
+
+lpom -- Local Partial Order Model.
+
+LPOM0 and LPOMext are approximations to calculate the
+average height, hav, of partially ordered sets (posets).
+
+"""
+name = "pyhasse.lpom"
+
+__author__ = """Rainer Bruggemann"""
+__email__ = 'rainer.bruggemann@pyhasse.org'
+__version__ = '0.2.1'
