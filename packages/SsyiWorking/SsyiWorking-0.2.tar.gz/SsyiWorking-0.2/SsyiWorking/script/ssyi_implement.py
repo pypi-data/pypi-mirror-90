437a643f03b7af4cad4d7b0a6461286da05dfd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:33 +0000

    Add package SsyiWorking/0.2/SsyiWorking-0.2.tar.gz

---
+++ packages/SsyiWorking/SsyiWorking-0.2.tar.gz/SsyiWorking-0.2/SsyiWorking/script/ssyi_implement.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Demo:
+
+    @staticmethod
+
+    def login(username, password):
+
+        # 这里写你的业务逻辑，简单起见，我返回True
+
+        print('\n%s' % username)
+
+        print('\n%s' % password)
+
+        return True
+
+class SsyiDemo:
+
+    def ssyi_login(self, username, password, expected):
+
+        assert Demo.login(username, password) == expected
