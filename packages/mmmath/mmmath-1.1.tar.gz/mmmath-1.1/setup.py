953528c52accb878a6570e57b9597706cf009f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package mmmath/1.1/mmmath-1.1.tar.gz

---
+++ packages/mmmath/mmmath-1.1.tar.gz/mmmath-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="mmmath", # 对外模块名
+    version="1.1", # 版本号
+    description="这是描述", # 描述
+    author="小红", # 作者
+    author_email="11111111@1111111.com", # 作者邮箱
+    py_moduls=["aa.py"] # 需要发布的模块
+)
