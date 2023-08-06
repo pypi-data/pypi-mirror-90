e4c1f07ef09476ea7caff0609e9c9c6f94b076f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:36 +0000

    Add package orionframework/1.0.67/orionframework-1.0.67.tar.gz

---
+++ packages/orionframework/orionframework-1.0.67.tar.gz/orionframework-1.0.67/orionframework/metadata/models.py
@@ -0,0 +1,12 @@
+from orionframework.vos import AbstractVO
+
+
+class MetadataModel(AbstractVO):
+
+    def __init__(self, attributes=None, **kwargs):
+        super(MetadataModel, self).__init__(**kwargs)
+        self.attributes = attributes or []
+
+
+class MetadataAttribute(AbstractVO):
+    pass
