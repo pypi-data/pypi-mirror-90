d1e898b6a7c2f6451bd394b44b302e8cb21e5af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package vavista-rpc/0.0.10/vavista_rpc-0.0.10.tar.gz

---
+++ packages/vavista-rpc/vavista_rpc-0.0.10.tar.gz/vavista_rpc-0.0.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name='vavista_rpc',
+    version='0.0.10',
+    author='Conor Dowling, Kevin Gill',
+    author_email='vo2521@gmail.com',
+    description='VAVista RPC interface',
+    long_description='''
+    This is a simple Pythonic mechanism for invoking VistA RPCs.
+    The code was developed by Conor Dowling as part of the FMQL
+    project.
+    ''',
+    url="https://github.com/vernonOber/vavista-rpc3",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6',
+)
+
