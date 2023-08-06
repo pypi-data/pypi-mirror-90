fbb211e146241520b37e61fc8d44e1d0ccefef0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package andreas-test-distributions/0.1/andreas_test_distributions-0.1.tar.gz

---
+++ packages/andreas-test-distributions/andreas_test_distributions-0.1.tar.gz/andreas_test_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='andreas_test_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['andreas_test_distributions'],
+      zip_safe=False)
