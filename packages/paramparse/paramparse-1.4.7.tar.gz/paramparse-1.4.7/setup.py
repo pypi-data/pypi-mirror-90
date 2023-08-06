1c6233aee1a9b753df599c51b2152c308d03b125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package paramparse/1.4.7/paramparse-1.4.7.tar.gz

---
+++ packages/paramparse/paramparse-1.4.7.tar.gz/paramparse-1.4.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("ReadMe.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paramparse",
+    version="1.4.7",
+    author="Abhineet Singh",
+    author_email="abhineet.iiita@gmail.com",
+    description="argparse wrapper to allow hierarchically nested class based parameters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abhineet123/paramparse",
+    include_package_data=True,
+    py_modules=["paramparse"],
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
