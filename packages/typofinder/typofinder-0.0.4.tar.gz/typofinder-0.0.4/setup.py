e422c2a69903d07c34da4ae391cfd967b6b6cb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package typofinder/0.0.4/typofinder-0.0.4.tar.gz

---
+++ packages/typofinder/typofinder-0.0.4.tar.gz/typofinder-0.0.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md") as file:
+    long_description = file.read()
+
+with open("requirements.txt") as file:
+    install_requires = [req.strip() for req in file.readlines()]
+
+setuptools.setup(
+    name="typofinder",
+    version="0.0.4",
+    author="Min ho Kim",
+    author_email="minho42@gmail.com",
+    description="Find typos from GitHub repository",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minho42/typofinder",
+    # packages=setuptools.find_packages(),
+    # py_modules=["typofinder"]
+    packages=["typofinder"],
+    package_dir={"typofinder": "typofinder"},
+    package_data={"data": ["*.txt"]},
+    scripts=["download_wordnet.py"],
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["typofinder=typofinder:main"]},
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Text Processing",
+    ],
+)
