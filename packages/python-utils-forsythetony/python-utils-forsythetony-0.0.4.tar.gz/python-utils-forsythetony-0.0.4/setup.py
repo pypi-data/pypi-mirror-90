8bbcefe3a8b610f4cb251b7c8600870323cb76fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:45 +0000

    Add package python-utils-forsythetony/0.0.4/python-utils-forsythetony-0.0.4.tar.gz

---
+++ packages/python-utils-forsythetony/python-utils-forsythetony-0.0.4.tar.gz/python-utils-forsythetony-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-utils-forsythetony", # Replace with your own username
+    version="0.0.4",
+    author="Anthony Forsythe",
+    author_email="forsythetony+python@gmail.com",
+    description="A package of common utilities (vague I know...)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/forsythetony/python-utilities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
