bcf4b8e4d554371fa6dd3e0d323e908ee4f59ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package chq/1.0/chq-1.0-py3-none-any.whl

---
+++ packages/chq/chq-1.0-py3-none-any.whl/chq/__init__.py
@@ -0,0 +1,21 @@
+import getflaggg.py
+import hashlib
+
+name = "fuckchq"
+
+def hacksatellite():
+    return "Congratulations!\nhack satellite successfully!"
+
+def hackwebsite():
+    return "Congratulations!\nHack website successfully!\namdin = \"chq\"\npassword = \"fuckchq\""
+
+def hackpentagon():
+    return "Congratulations!\nhack the Pentagon successfully!"
+
+def getflag(a):
+    # if "\\" or "/" not in a:
+    #     flag = hashlib.md5(a.encode("utf-8")).hexdigest()
+    # else:
+    #     flag = get_file(a)
+    # return "flag{"+flag+"}"
+    return "flag{e2086d3031290066b35b40b2e9f79d4c}"
