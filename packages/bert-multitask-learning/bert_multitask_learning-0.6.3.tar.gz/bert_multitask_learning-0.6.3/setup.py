3ca65f71a5da8931e063cb4242dcc1c59c310a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:27 +0000

    Add package bert-multitask-learning/0.6.3/bert_multitask_learning-0.6.3.tar.gz

---
+++ packages/bert-multitask-learning/bert_multitask_learning-0.6.3.tar.gz/bert_multitask_learning-0.6.3/setup.py
@@ -0,0 +1,33 @@
+
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
+
+setup(
+    name='bert_multitask_learning',
+    version='0.6.3',
+    packages=find_packages(),
+    url='https://github.com/JayYip/bert-multitask-learning',
+    license='MIT',
+    author='Jay Yip',
+    author_email='junpang.yip@gmail.com',
+    description='BERT for Multi-task Learning',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    python_requires='>=3.5.0',
+    install_requires=install_requires,
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ),
+)
