a43a0f4b886372d09e807e32377e7eda6c97d08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:19 +0000

    Add package ratr0-utils/0.1.5/ratr0_utils-0.1.5-py2.py3-none-any.whl

---
+++ packages/ratr0-utils/ratr0_utils-0.1.5-py2.py3-none-any.whl/ratr0/util/file_info.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""
+A tool to get the information of a RATR0 file type
+"""
+from . import tiles, sprites
+
+RATR0_FILE_ID_LENGTH = 8
+
+
+def print_tiles_info(infile):
+    tiles_info = tiles.read_tiles_info(infile)
+    print("RATR0 Tile File")
+    print(tiles_info)
+
+
+def print_sprite_info(infile):
+    sprite_info = sprites.read_sprite_info(infile)
+    print("RATR0 Sprite File")
+    print(sprite_info)
+    for i in range(sprite_info.num_sprites):
+        spr_offset = int.from_bytes(infile.read(2), byteorder="big")
+        print("Sprite %d, offset: %d" % (i, spr_offset))
+
+
+class LevelInfo:
+
+    def __init__(self, version, flags, width, height, vp_width, vp_height,
+                 init_vp_row, init_vp_col):
+        self.version = version
+        self.flags = flags
+        self.width = width
+        self.height = height
+        self.vp_width = vp_width
+        self.vp_height = vp_height
+        self.init_vp_row = init_vp_row
+        self.init_vp_col = init_vp_col
+
+    def __str__(self):
+        if self.flags & 0x01 == 1:
+            big_endian = False
+            byte_order = "little"
+        else:
+            big_endian = True
+            byte_order = "big"
+
+        out = "Version: %d\n" % self.version
+        out += "Endianess: %s\n" % byte_order
+        out += "size: %dx%d tiles\n" % (self.width, self.height)
+        out += "viewport size: %dx%d tiles\n" % (self.vp_width, self.vp_height)
+        out += "initial viewport pos: (%d, %d)\n" % (self.init_vp_col, self.init_vp_row)
+        return out
+
+
+
+def read_level_info(infile):
+    version = ord(infile.read(1))
+    flags = ord(infile.read(1))
+
+    if flags & 0x01 == 1:
+        big_endian = False
+        byte_order = "little"
+    else:
+        big_endian = True
+        byte_order = "big"
+
+    width = int.from_bytes(infile.read(2), byteorder=byte_order)
+    height = int.from_bytes(infile.read(2), byteorder=byte_order)
+    vp_width = int.from_bytes(infile.read(2), byteorder=byte_order)
+    vp_height = int.from_bytes(infile.read(2), byteorder=byte_order)
+    init_vp_row = int.from_bytes(infile.read(2), byteorder=byte_order)
+    init_vp_col = int.from_bytes(infile.read(2), byteorder=byte_order)
+    return LevelInfo(version, flags, width, height, vp_width, vp_height, init_vp_row,
+                     init_vp_col)
+
+
+def print_level_info(infile):
+    level_info = read_level_info(infile)
+    print("RATR0 Level File")
+    print(level_info)
+
+
+def file_info(infile):
+    """infile is a file object opened in binary mode"""
+    fileid = infile.read(RATR0_FILE_ID_LENGTH).decode('utf-8')
+    if fileid == "RATR0TIL":
+        print_tiles_info(infile)
+    elif fileid == "RATR0LVL":
+        print_level_info(infile)
+    elif fileid == "RATR0SPR":
+        print_sprite_info(infile)
+    else:
+        print("unknown file type")
