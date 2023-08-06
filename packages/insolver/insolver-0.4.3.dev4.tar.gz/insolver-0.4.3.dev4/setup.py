a751d1a7789a9a63155f05140740354a288d0000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package insolver/0.4.3.dev4/insolver-0.4.3.dev4.tar.gz

---
+++ packages/insolver/insolver-0.4.3.dev4.tar.gz/insolver-0.4.3.dev4/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='insolver',
+      version='0.4.3.dev4',
+      description='Mindset insurance scoring',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/MindSetLib/MS-InsuranceScoring',
+      keywords='ML insurance scoring',
+      author='Mindset',
+      author_email='request@mind-set.ru',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'insolver_serving = insolver.serving.run_service:run'
+          ]
+      },
+      install_requires=[
+            'wheel',
+            'numpy',
+            'pandas',
+            'xgboost',
+            'lightgbm',
+            'catboost',
+            'hyperopt',
+            'scikit-learn',
+            'pdpbox',
+            'pyodbc',
+            'kaleido',
+            'requests',
+            'requests_cache',
+            'plotly',
+            'seaborn',
+            'shap',
+            'geocoder',
+            'h2o',
+            'flask',
+            'fastapi',
+            'uvicorn',
+            'pydantic',
+            'gunicorn',
+      ],
+      zip_safe=False,
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+      ],
+      python_requires='>=3.6',
+      )
