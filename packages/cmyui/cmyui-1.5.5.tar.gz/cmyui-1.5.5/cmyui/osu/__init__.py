97b2ba2ee763e95d0296a1fa2de4a0896bd008a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package cmyui/1.5.5/cmyui-1.5.5.tar.gz

---
+++ packages/cmyui/cmyui-1.5.5.tar.gz/cmyui-1.5.5/cmyui/osu/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""\
+Wrappers for multiple types of osu! related files.
+
+Source: https://github.com/cmyui/cmyui_pkg
+
+:copyright: (c) 2020 cmyui
+:license: MIT, see LICENSE for details.
+"""
+
+from .beatmap import *
+from .replay import *
+from .mods import *
+from .api import *
