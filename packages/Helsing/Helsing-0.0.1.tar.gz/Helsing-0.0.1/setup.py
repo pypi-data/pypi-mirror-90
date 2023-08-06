bfb42d3fb046644f23a451223468a72cf15a834b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:40 +0000

    Add package Helsing/0.0.1/Helsing-0.0.1.tar.gz

---
+++ packages/Helsing/Helsing-0.0.1.tar.gz/Helsing-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Helsing",
+    version="0.0.1",
+    author="Demosius",
+    author_email="a.m.penny@hotmail.com",
+    description="Compares specific data for misc business purposes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
