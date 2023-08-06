3ce1c02a7bc7f61d469c5ff6e3274b58af91e382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:25 +0000

    Add package choicelib/0.1.2/choicelib-0.1.2.tar.gz

---
+++ packages/choicelib/choicelib-0.1.2.tar.gz/choicelib-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['choicelib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'choicelib',
+    'version': '0.1.2',
+    'description': 'Choice a best similar-interface library from given list',
+    'long_description': None,
+    'author': 'timoniq',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
