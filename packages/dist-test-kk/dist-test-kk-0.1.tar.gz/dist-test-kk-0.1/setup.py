01240477e1a54d671d723a88670388ea078d8ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:05 +0000

    Add package dist-test-kk/0.1/dist-test-kk-0.1.tar.gz

---
+++ packages/dist-test-kk/dist-test-kk-0.1.tar.gz/dist-test-kk-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist-test-kk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist-test-kk'],
+      author = 'Kevin K',
+      zip_safe=False)
