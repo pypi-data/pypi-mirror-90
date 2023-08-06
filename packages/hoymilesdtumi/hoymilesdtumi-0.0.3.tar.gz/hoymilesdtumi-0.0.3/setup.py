177c888e44d2b8f003baa387da1e5d1d3d346253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:55 +0000

    Add package hoymilesdtumi/0.0.3/hoymilesdtumi-0.0.3.tar.gz

---
+++ packages/hoymilesdtumi/hoymilesdtumi-0.0.3.tar.gz/hoymilesdtumi-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pathlib import Path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hoymilesdtumi", 
+    version="0.0.3",
+    author="Silvio Lima",
+    author_email="dev.silvio@gmail.com",
+    description="Hoymiles DTU-MI data wrapper for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sileandro/hoymilesdtumi",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        #l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+    ],
+)
