676859743157524b7195f875c8ce22e7d6305c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package ddSMT/0.1.dev370/ddSMT-0.1.dev370.tar.gz

---
+++ packages/ddSMT/ddSMT-0.1.dev370.tar.gz/ddSMT-0.1.dev370/ddsmt/cli.py
@@ -0,0 +1,144 @@
+#! /usr/bin/env python3
+#
+# ddSMT: A delta debugger for SMT benchmarks in SMT-Lib v2 format.
+# Copyright (C) 2013-2019, Aina Niemetz.
+# Copyright (C) 2016-2020, Mathias Preiner.
+# Copyright (C) 2018, Jane Lange.
+# Copyright (C) 2018, Andres Noetzli.
+#
+# This file is part of ddSMT.
+#
+# ddSMT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ddSMT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ddSMT.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+import os
+import pprint
+import sys
+import time
+
+from . import checker
+from . import ddmin
+from . import ddnaive
+from . import options
+from . import parser
+from . import tmpfiles
+from . import smtlib
+
+
+def check_options():
+    if options.args().max_threads != 1:
+        # configure number of threads
+        if options.args().max_threads <= 0:
+            options.args().max_threads = os.cpu_count() + options.args().max_threads
+        logging.info('Using up to %d threads.', options.args().max_threads)
+
+    if options.args().dump_config:
+        pprint.pprint(vars(options.args()))
+
+    # check input file
+    if not os.path.isfile(options.args().infile):
+        raise Exception('input file is not a regular file')
+
+    if options.args().parser_test:
+        # only parse and print
+        exprs = parser.parse_smtlib(open(options.args().infile).read())
+        print(parser.render_smtlib(exprs))
+        sys.exit(0)
+
+    # check executable
+    if not options.args().cmd:
+        raise Exception('No executable was specified as command')
+    if not os.path.isfile(options.args().cmd[0]):
+        raise Exception('Command "{}" is not a regular file'.format(options.args().cmd[0]))
+    if not os.access(options.args().cmd[0], os.X_OK):
+        raise Exception('Command "{}" is not executable'.format(options.args().cmd[0]))
+
+def setup_logging():
+    logging.basicConfig(format='[ddSMT %(levelname)s] %(message)s')
+    verbositymap = {
+        0: logging.WARN,
+        1: logging.INFO,
+        2: logging.DEBUG,
+    }
+    logging.getLogger().setLevel(
+        level=verbositymap.get(options.args().verbosity, logging.DEBUG))
+
+
+def ddsmt_main():
+    setup_logging()
+    check_options()
+
+    logging.info("input file:   '{}'".format(options.args().infile))
+    logging.info("output file:  '{}'".format(options.args().outfile))
+    logging.info("command:      '{}'".format(" ".join(map(str, options.args().cmd))))
+    if options.args().cmd_cc:
+        logging.info("command (cc): '{}'".format(options.args().cmd_cc))
+
+    ifilesize = os.path.getsize(options.args().infile)
+
+    start_time = time.time()
+    with open(options.args().infile, 'r') as infile:
+        exprs = list(parser.parse_smtlib(infile.read()))
+        nexprs = smtlib.node_count(exprs)
+
+    logging.debug("parsed {} s-expressions in {:.2f} seconds".format(
+        nexprs,
+        time.time() - start_time))
+
+    if options.args().parser_test:
+        parser.write_smtlib_to_file(options.args().outfile, exprs)
+        return
+
+    tmpfiles.copy_binaries()
+    checker.do_golden_runs()
+
+    if options.args().strategy == 'ddmin':
+        reduced_exprs, ntests = ddmin.reduce(exprs)
+    elif options.args().strategy == 'naive':
+        reduced_exprs, ntests = ddnaive.reduce(exprs)
+    end_time = time.time()
+    if reduced_exprs != exprs:
+        ofilesize = os.path.getsize(options.args().outfile)
+        nreduced_exprs = smtlib.node_count(reduced_exprs)
+
+        logging.info("")
+        logging.info("runtime:         {:.2f} s".format(end_time - start_time))
+        logging.info("tests:           {}".format(ntests))
+        logging.info("input file:")
+        logging.info("  file size:     {} B".format(ifilesize))
+        logging.info("  s-expressions: {}".format(nexprs))
+        logging.info("reduced file:")
+        logging.info("  file size:     {} B ({:3.1f}%)".format(
+            ofilesize, ofilesize / ifilesize * 100))
+        logging.info("  s-expressions: {} ({:3.1f}%)".format(
+            nreduced_exprs, nreduced_exprs / nexprs * 100))
+    else:
+        logging.warning("unable to minimize input file")
+
+
+def main():
+    try:
+        ddsmt_main()
+    except MemoryError:
+        sys.exit("[ddsmt] memory exhausted")
+    except KeyboardInterrupt:
+        sys.exit("[ddsmt] interrupted")
+    except Exception as e:
+        sys.exit(str(e))
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ddSMT/ddSMT-0.1.dev370.tar.gz/ddSMT-0.1.dev370/ddsmt/options.py
@@ -0,0 +1,132 @@
+import argparse
+
+from . import argparsemod
+from . import mutators
+from . import version
+
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                      argparse.HelpFormatter):
+    """A custom formatter for printing the commandline help.
+    It combines :code:`argparse.ArgumentDefaultsHelpFormatter` with the :code:`argparse.HelpFormatter`,
+    slightly increases the width reserved for the options and removed defaults for the mutator options."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, max_help_position=35)
+
+
+def parse_options():
+    """Configures the commandline parse and then parse the commandline options."""
+
+    usage = "ddsmt.py [<options>] <infile> <outfile> <cmd> [<cmd options>]"
+    ap = argparsemod.ModularArgumentParser(
+        usage=usage,
+        formatter_class=CustomFormatter,
+        modular_help_groups=['mutator help'])
+    ap.add_argument("infile", help="the input file (in SMT-LIB v2 format)")
+    ap.add_argument("outfile", help="the output file")
+    ap.add_argument("cmd",
+                    nargs=argparse.REMAINDER,
+                    help="the command (with optional arguments)")
+
+    ap.add_argument("-v",
+                    action="count",
+                    dest="verbosity",
+                    default=0,
+                    help="increase verbosity")
+    ap.add_argument('--version', action='version', version=version.VERSION)
+    ap.add_argument('--max-threads',
+                    type=int,
+                    metavar='n',
+                    default=1,
+                    help='number of threads to use; #processors+n if n<=0')
+    ap.add_argument('--dump-config',
+                    action='store_true',
+                    default=False,
+                    help='dump configuration')
+    ap.add_argument('--memout',
+                    type=int,
+                    metavar='megabytes',
+                    default=0,
+                    help='memout for individual checks')
+    ap.add_argument("--timeout",
+                    dest="timeout",
+                    metavar='timeout',
+                    type=float,
+                    help="timeout for test runs in seconds, "
+                    "default: 1.5 * golden runtime")
+    ap.add_argument('--strategy',
+                    choices=['ddmin', 'naive'],
+                    default='ddmin',
+                    help='minimization strategy')
+    ap.add_argument("--match-err",
+                    dest="match_err",
+                    help="match string in stderr to identify "
+                    "failing input (default: stderr output)")
+    ap.add_argument("--match-out",
+                    dest="match_out",
+                    help="match string in stdout to identify "
+                    "failing input (default: stdout output)")
+    ap.add_argument("--parser-test",
+                    action="store_true",
+                    dest="parser_test",
+                    help="run ddSMT in parser test mode "
+                    "(parses only, does not require command argument)")
+
+    apcc = ap.add_argument_group('cross check')
+    apcc.add_argument("-c",
+                      metavar='cmd-cc',
+                      dest="cmd_cc",
+                      help="cross check command")
+    apcc.add_argument(
+        "--timeout-cc",
+        dest="timeout_cc",
+        metavar='timeout',
+        type=float,
+        help="timeout for test runs of the cross check in seconds, "
+        "default: 1.5 * golden runtime")
+    apcc.add_argument("--match-err-cc",
+                      dest="match_err_cc",
+                      help="match string to identify failing input for "
+                      "cross check command (default: stderr output)")
+    apcc.add_argument("--match-out-cc",
+                      dest="match_out_cc",
+                      help="match string to identify failing input "
+                      "for cross check command (default: stdout output)")
+
+    argp_modes = ap.add_argument_group('special modes')
+    mutators.collect_mutator_modes(argp_modes)
+
+    mutators.collect_mutator_options(ap)
+
+    res = ap.parse_args()
+
+    if res.cmd_cc:
+        res.cmd_cc = res.cmd_cc.split()
+
+    return res
+
+
+__PARSED_ARGS = None
+
+
+def args():
+    """Returns the commandline options. Calls :code:`parse_options()` if parsing has not yet happened."""
+    global __PARSED_ARGS
+    if __PARSED_ARGS is None:
+        __PARSED_ARGS = parse_options()
+    return __PARSED_ARGS
+
+
+def add_mutator_argument(argparser, name, default, help_msg):
+    dest = 'mutator_{}'.format(name.replace('-', '_'))
+    grp = argparser.add_mutually_exclusive_group()
+    grp.add_argument('--{}'.format(name),
+                     action='store_true',
+                     default=default,
+                     dest=dest,
+                     help=help_msg if not default else argparse.SUPPRESS)
+    grp.add_argument('--no-{}'.format(name),
+                     action='store_false',
+                     default=default,
+                     dest=dest,
+                     help=help_msg if default else argparse.SUPPRESS)
