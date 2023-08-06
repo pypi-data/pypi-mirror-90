e270a540638079c1531770523691fb2b32f52357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:05 +0000

    Add package guessgame/0.1/guessgame-0.1.tar.gz

---
+++ packages/guessgame/guessgame-0.1.tar.gz/guessgame-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='guessgame',
+      version='0.1',
+      description='Guess Game',
+      packages=['guessgame'],
+      zip_safe=False)
+# https://packaging.python.org/tutorials/packaging-projects/
