e0d3f8199dfe2381a50541418972088f08f14ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:11 +0000

    Add package PythonJS/0.0.5/PythonJS-0.0.5.tar.gz

---
+++ packages/PythonJS/PythonJS-0.0.5.tar.gz/PythonJS-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PythonJS",
+    version="0.0.5",
+    author="VivinMeth L V",
+    author_email="vivinmeth@gmail.com",
+    description="JS Features [i.e Object, Map] in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/vivinmeth/pythonjs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
