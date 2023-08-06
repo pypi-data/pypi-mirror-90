efb30c1a97ab433e21484afff80519e4f98247fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:55 +0000

    Add package core-explore-keyword-app/1.13.0/core_explore_keyword_app-1.13.0.tar.gz

---
+++ packages/core-explore-keyword-app/core_explore_keyword_app-1.13.0.tar.gz/core_explore_keyword_app-1.13.0/README.rst
@@ -0,0 +1,18 @@
+========================
+Core Explore Keyword App
+========================
+
+Exploration by keywords for the curator core project.
+
+Quickstart
+==========
+
+1. Add "core_explore_keyword_app" to your INSTALLED_APPS setting
+----------------------------------------------------------------
+
+.. code:: python
+
+    INSTALLED_APPS = [
+        ...
+        'core_explore_keyword_app',
+    ]
---
+++ packages/core-explore-keyword-app/core_explore_keyword_app-1.13.0.tar.gz/core_explore_keyword_app-1.13.0/core_explore_keyword_app/settings.py
@@ -0,0 +1,15 @@
+""" Settings for core_explore_keyword_app
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
+"""
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure()
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
+
+# MENU
+EXPLORE_MENU_NAME = getattr(settings, "EXPLORE_MENU_NAME", "Query by Keyword")
