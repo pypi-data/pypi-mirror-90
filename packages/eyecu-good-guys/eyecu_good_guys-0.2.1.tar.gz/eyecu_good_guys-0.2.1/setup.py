d1e1d969694215f22465b8a69314cff4ca7a85f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:48 +0000

    Add package eyecu-good-guys/0.2.1/eyecu_good_guys-0.2.1.tar.gz

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.1.tar.gz/eyecu_good_guys-0.2.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['good_guys',
+ 'good_guys.layers',
+ 'good_guys.layers.basic',
+ 'good_guys.layers.misc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch==1.7.1']
+
+setup_kwargs = {
+    'name': 'eyecu-good-guys',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Oguz Vuruskaner',
+    'author_email': 'ovuruska@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
