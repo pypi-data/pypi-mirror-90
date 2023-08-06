32fdbc420e361b68046d9a107a3976584eb7dd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:37 +0000

    Add package magicgui/0.2.2/magicgui-0.2.2-py2.py3-none-any.whl

---
+++ packages/magicgui/magicgui-0.2.2-py2.py3-none-any.whl/magicgui/backends/_qtpy/__init__.py
@@ -0,0 +1,35 @@
+from .application import ApplicationBackend
+from .widgets import (
+    CheckBox,
+    ComboBox,
+    Container,
+    DateTimeEdit,
+    FloatSpinBox,
+    Label,
+    LineEdit,
+    PushButton,
+    RadioButton,
+    Slider,
+    SpinBox,
+    TextEdit,
+    get_text_width,
+    show_file_dialog,
+)
+
+__all__ = [
+    "ApplicationBackend",
+    "CheckBox",
+    "ComboBox",
+    "Container",
+    "DateTimeEdit",
+    "FloatSpinBox",
+    "Label",
+    "LineEdit",
+    "PushButton",
+    "RadioButton",
+    "Slider",
+    "SpinBox",
+    "TextEdit",
+    "get_text_width",
+    "show_file_dialog",
+]
