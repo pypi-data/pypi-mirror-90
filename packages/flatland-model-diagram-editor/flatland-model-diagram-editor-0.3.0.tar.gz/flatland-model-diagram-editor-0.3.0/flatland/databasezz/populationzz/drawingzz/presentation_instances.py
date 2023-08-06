52ad0122e1854f990fb76b56e898b8b207b52150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:07 +0000

    Add package flatland-model-diagram-editor/0.3.0/flatland-model-diagram-editor-0.3.0.tar.gz

---
+++ packages/flatland-model-diagram-editor/flatland-model-diagram-editor-0.3.0.tar.gz/flatland-model-diagram-editor-0.3.0/flatland/databasezz/populationzz/drawingzz/presentation_instances.py
@@ -0,0 +1,22 @@
+"""
+presentation_instances.py
+"""
+
+population = [
+    {'Name': 'default', 'drawingzz type': 'Starr class diagram'},
+    {'Name': 'diagnostic', 'drawingzz type': 'Starr class diagram'},
+
+    {'Name': 'default', 'drawingzz type': 'Shlaer-Mellor class diagram'},
+    {'Name': 'diagnostic', 'drawingzz type': 'Shlaer-Mellor class diagram'},
+
+    {'Name': 'default', 'drawingzz type': 'xumlzz class diagram'},
+    {'Name': 'diagnostic', 'drawingzz type': 'xumlzz class diagram'},
+
+    {'Name': 'default', 'drawingzz type': 'xumlzz state machine diagram'},
+
+    {'Name': 'default', 'drawingzz type': 'Starr domain diagram'},
+    {'Name': 'default', 'drawingzz type': 'Starr class collaboration diagram'},
+
+    {'Name': 'default', 'drawingzz type': 'xumlzz domain diagram'},
+    {'Name': 'default', 'drawingzz type': 'xumlzz class collaboration diagram'},
+]
