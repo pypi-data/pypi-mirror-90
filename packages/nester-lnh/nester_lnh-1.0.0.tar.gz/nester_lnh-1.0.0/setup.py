8cfb414ff77d6ab097d0dc09242977a5472bd26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:29 +0000

    Add package nester-lnh/1.0.0/nester_lnh-1.0.0.tar.gz

---
+++ packages/nester-lnh/nester_lnh-1.0.0.tar.gz/nester_lnh-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_lnh',
+    version='1.0.0',
+    py_modules = ['nester'],
+    author='lnh',
+    author_email='linanhao@yingzi.com',
+    url='https://www.baidu.com',
+    description='A simple printer of nested lists'
+)
