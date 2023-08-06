f408bef603c54681d883a0f0183793fdda74a716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:25 +0000

    Add package e-drone/21.1.1/e_drone-21.1.1.tar.gz

---
+++ packages/e-drone/e_drone-21.1.1.tar.gz/e_drone-21.1.1/setup.py
@@ -0,0 +1,20 @@
+# 참고자료
+# http://swdeveloper.tistory.com/34
+# https://code.tutsplus.com/ko/tutorials/how-to-share-your-python-packages--cms-26114
+# https://code.tutsplus.com/ko/tutorials/how-to-write-your-own-python-packages--cms-26076
+from setuptools import setup, find_packages
+
+setup(
+    name = "e_drone",
+    version = "21.1.1",     # year. month. release number
+    description = "Library for BYROBOT Drones.",
+    author = "BYROBOT",
+    author_email = "dev@byrobot.co.kr",
+    url = "http://www.byrobot.co.kr",
+    packages = find_packages(exclude=['tests']),
+    install_requires = [
+        'pyserial>=3.4',
+        'colorama>=0.4.0'],
+    long_description = open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
