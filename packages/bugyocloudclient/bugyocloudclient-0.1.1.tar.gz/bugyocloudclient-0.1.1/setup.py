6a17771d848196321cd067f44f026bdcfe804b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:27 +0000

    Add package bugyocloudclient/0.1.1/bugyocloudclient-0.1.1.tar.gz

---
+++ packages/bugyocloudclient/bugyocloudclient-0.1.1.tar.gz/bugyocloudclient-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bugyocloudclient',
+ 'bugyocloudclient.endpoints',
+ 'bugyocloudclient.endpoints.base',
+ 'bugyocloudclient.models',
+ 'bugyocloudclient.tasks',
+ 'bugyocloudclient.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.9.3,<5.0.0', 'requests>=2.25.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'bugyocloudclient',
+    'version': '0.1.1',
+    'description': 'Bugyo Cloud Client',
+    'long_description': None,
+    'author': 'sengokyu',
+    'author_email': 'sengokyu+gh@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sengokyu/bugyo-cloud-client',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
