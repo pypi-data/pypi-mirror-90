e60b0a2fd048f96ca969884248437c9d823b33fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:45 +0000

    Add package lake-aha/0.0.2/lake-aha-0.0.2.tar.gz

---
+++ packages/lake-aha/lake-aha-0.0.2.tar.gz/lake-aha-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+        name='lake-aha',
+        version="0.0.2",
+        packages=[
+            "lake"
+        ],
+        author='Maxwell Strange',
+        author_email='mstrange@stanford.edu',
+        description='Memory Generator based on Kratos: The God of War.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/StanfordAHA/lake",
+        python_requires=">=3.5",
+        install_requires=[
+            "kratos",
+            "fault",
+            "magma-lang",
+            "pytest"
+        ]
+)
