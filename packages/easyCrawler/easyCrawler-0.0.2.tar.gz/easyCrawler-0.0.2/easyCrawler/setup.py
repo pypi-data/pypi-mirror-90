0c75fba7bc5c8eadbb10a107f403782ba339dce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:43 +0000

    Add package easyCrawler/0.0.2/easyCrawler-0.0.2.tar.gz

---
+++ packages/easyCrawler/easyCrawler-0.0.2.tar.gz/easyCrawler-0.0.2/easyCrawler/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='easyCrawler',
+    version='0.0.2',
+    author='William Ma',
+    author_email='3327821469@qq.com',
+    url='https://github.com/theCoder-WM',
+    description=u'An easy crawler for new hands',
+    packages=['easyCrawler'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+
+        ]
+    }
+)
