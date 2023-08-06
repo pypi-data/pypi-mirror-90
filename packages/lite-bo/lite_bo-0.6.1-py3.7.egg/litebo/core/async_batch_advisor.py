f01fc589d53c92712ada325537e9a0041e0ee41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package lite-bo/0.6.1/lite_bo-0.6.1-py3.7.egg

---
+++ packages/lite-bo/lite_bo-0.6.1-py3.7.egg/litebo/core/async_batch_advisor.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+from litebo.config_space.util import convert_configurations_to_array
+from litebo.utils.constants import MAXINT, SUCCESS, FAILED, TIMEOUT
+from litebo.core.advisor import Advisor
+
+
+class AsyncBatchAdvisor(Advisor):
+    def __init__(self, config_space,
+                 batch_size=4,
+                 initial_trials=10,
+                 initial_configurations=None,
+                 init_strategy='random_explore_first',
+                 optimization_strategy='bo',
+                 batch_strategy='median_imputation',
+                 history_bo_data=None,
+                 surrogate_type='prf',
+                 output_dir='logs',
+                 task_id=None,
+                 rng=None):
+
+        self.batch_size = batch_size
+        self.batch_strategy = batch_strategy
+        self.running_configs = list()
+        self.bo_start_n = 3
+        super().__init__(config_space,
+                         initial_trials=initial_trials,
+                         initial_configurations=initial_configurations,
+                         init_strategy=init_strategy,
+                         optimization_strategy=optimization_strategy,
+                         history_bo_data=history_bo_data,
+                         surrogate_type=surrogate_type,
+                         output_dir=output_dir,
+                         task_id=task_id,
+                         rng=rng)
+
+        if batch_strategy == 'median_imputation':
+            acq_type = 'ei'
+        elif batch_strategy == 'local_penalization':
+            acq_type = 'lpei'
+        else:
+            raise ValueError('Unsupported batch strategy - %s.' % batch_strategy)
+        super(AsyncBatchAdvisor, self).setup_bo_basics(acq_type=acq_type)
+
+    def get_suggestion(self):
+        self.logger.info('#Call get_suggestion: %d.' % len(self.running_configs))
+        if len(self.configurations) == 0:
+            X = np.array([])
+        else:
+            failed_configs = list() if self.max_y is None else self.failed_configurations.copy()
+            X = convert_configurations_to_array(self.configurations + failed_configs)
+
+        # Failed trial.
+        num_failed_trial = len(self.failed_configurations)
+        failed_perfs = list() if self.max_y is None else [self.max_y] * num_failed_trial
+        Y = np.array(self.perfs + failed_perfs, dtype=np.float64)
+        all_considered_configs = self.configurations + self.failed_configurations + self.running_configs
+
+        num_config_evaluated = len(all_considered_configs)
+        if (num_config_evaluated < self.init_num) or \
+                len(self.history_container.data) <= self.bo_start_n or \
+                self.optimization_strategy == 'random':
+            if num_config_evaluated >= len(self.initial_configurations):
+                _config = self.sample_random_configs(1)[0]
+            else:
+                _config = self.initial_configurations[num_config_evaluated]
+            self.running_configs.append(_config)
+            return _config
+
+        if self.batch_strategy == 'median_imputation':
+            self.logger.info('Config is sampled from [BO].')
+            X = convert_configurations_to_array(all_considered_configs)
+            y_median = np.median(self.perfs)
+            running_perfs = [y_median] * len(self.running_configs)
+            Y = np.array(self.perfs + failed_perfs + running_perfs, dtype=np.float64)
+            self.surrogate_model.train(X, Y)
+            incumbent_value = self.history_container.get_incumbents()[0][1]
+            self.acquisition_function.update(model=self.surrogate_model, eta=incumbent_value,
+                                             num_data=len(self.history_container.data))
+
+            challengers = self.optimizer.maximize(
+                runhistory=self.history_container,
+                num_points=5000
+            )
+
+            is_repeated_config = True
+            repeated_time = 0
+            curr_batch_config = None
+            while is_repeated_config:
+                curr_batch_config = challengers.challengers[repeated_time]
+                if curr_batch_config in all_considered_configs:
+                    is_repeated_config = True
+                    repeated_time += 1
+                else:
+                    is_repeated_config = False
+            _config = curr_batch_config
+
+        elif self.batch_strategy == 'local_penalization':
+            self.surrogate_model.train(X, Y)
+            incumbent_value = self.history_container.get_incumbents()[0][1]
+            self.acquisition_function.update(model=self.surrogate_model, eta=incumbent_value,
+                                             num_data=len(self.history_container.data),
+                                             batch_configs=self.running_configs)
+
+            challengers = self.optimizer.maximize(
+                runhistory=self.history_container,
+                num_points=5000
+            )
+            _config = challengers.challengers[0]
+        else:
+            raise ValueError('Invalid sampling strategy - %s.' % self.batch_strategy)
+        self.running_configs.append(_config)
+        return _config
+
+    def update_observation(self, observation):
+        config, perf, trial_state = observation
+        assert config in self.running_configs
+        self.running_configs.remove(config)
+        if not isinstance(perf, (int, float)):
+            perf = perf[-1]
+        if trial_state == SUCCESS and perf < MAXINT:
+            if len(self.configurations) == 0:
+                self.default_obj_value = perf
+
+            self.configurations.append(config)
+            self.perfs.append(perf)
+            self.history_container.add(config, perf)
+
+            self.perc = np.percentile(self.perfs, self.scale_perc)
+            self.min_y = np.min(self.perfs)
+            self.max_y = np.max(self.perfs)
+        else:
+            self.failed_configurations.append(config)
---
+++ packages/lite-bo/lite_bo-0.6.1-py3.7.egg/litebo/optimizer/parallel_smbo.py
@@ -0,0 +1,132 @@
+import sys
+import time
+import traceback
+from typing import List
+from collections import OrderedDict
+
+from litebo.utils.constants import MAXINT, SUCCESS, FAILED, TIMEOUT
+from litebo.core.computation.parallel_process import ParallelEvaluation
+from litebo.utils.limit import time_limit, TimeoutException
+from litebo.core.sync_batch_advisor import SyncBatchAdvisor
+from litebo.core.async_batch_advisor import AsyncBatchAdvisor
+from litebo.optimizer.base import BOBase
+
+
+def wrapper(param):
+    objective_function, config, time_limit_per_trial = param
+    try:
+        args, kwargs = (config,), dict()
+        timeout_status, _result = time_limit(objective_function, time_limit_per_trial,
+                                             args=args, kwargs=kwargs)
+        if timeout_status:
+            raise TimeoutException('Timeout: time limit for this evaluation is %.1fs' % time_limit_per_trial)
+        else:
+            if _result is None:
+                result = MAXINT
+            elif isinstance(_result, dict):
+                result = _result['objective_value']
+            else:
+                result = _result
+    except Exception as e:
+        if isinstance(e, TimeoutException):
+            trial_state = TIMEOUT
+        else:
+            traceback.print_exc(file=sys.stdout)
+            trial_state = FAILED
+        result = MAXINT
+    return [config, result]
+
+
+class pSMBO(BOBase):
+    def __init__(self, objective_function, config_space,
+                 batch_size=4,
+                 sample_strategy='bo',
+                 parallel_strategy='async',
+                 time_limit_per_trial=180,
+                 max_runs=200,
+                 logging_dir='logs',
+                 initial_configurations=None,
+                 init_strategy='random_explore_first',
+                 history_bo_data: List[OrderedDict] = None,
+                 initial_runs=10,
+                 task_id=None,
+                 random_state=1):
+
+        super().__init__(objective_function, config_space, task_id=task_id, output_dir=logging_dir,
+                         random_state=random_state, initial_runs=initial_runs, max_runs=max_runs,
+                         sample_strategy=sample_strategy, time_limit_per_trial=time_limit_per_trial,
+                         history_bo_data=history_bo_data)
+        if parallel_strategy == 'sync':
+            self.config_advisor = SyncBatchAdvisor(config_space,
+                                                   initial_trials=initial_runs,
+                                                   initial_configurations=initial_configurations,
+                                                   init_strategy=init_strategy,
+                                                   optimization_strategy=sample_strategy,
+                                                   batch_size=batch_size,
+                                                   task_id=task_id,
+                                                   output_dir=logging_dir,
+                                                   rng=self.rng)
+        elif parallel_strategy == 'async':
+            self.config_advisor = AsyncBatchAdvisor(config_space,
+                                                    initial_trials=initial_runs,
+                                                    initial_configurations=initial_configurations,
+                                                    init_strategy=init_strategy,
+                                                    optimization_strategy=sample_strategy,
+                                                    task_id=task_id,
+                                                    output_dir=logging_dir,
+                                                    rng=self.rng)
+        else:
+            raise ValueError('Invalid parallel strategy - %s.' % parallel_strategy)
+
+        self.parallel_strategy = parallel_strategy
+        self.batch_size = batch_size
+
+    def callback(self, result):
+        _config, _perf = result[0], result[1]
+        _observation = [_config, _perf, SUCCESS]
+        # Report the result, and remove the config from the running queue.
+        self.config_advisor.update_observation(_observation)
+        # Parent process: collect the result and increment id.
+        self.iteration_id += 1
+
+    def async_run(self):
+        with ParallelEvaluation(wrapper, n_worker=self.batch_size) as proc:
+            while self.iteration_id < self.max_iterations:
+                _config = self.config_advisor.get_suggestion()
+                _param = [self.objective_function, _config, self.time_limit_per_trial]
+                # Submit a job to worker.
+                proc.process_pool.apply_async(wrapper, (_param,), callback=self.callback)
+                while len(self.config_advisor.running_configs) >= self.batch_size:
+                    # Wait for workers.
+                    time.sleep(0.3)
+
+    def sync_run(self):
+        with ParallelEvaluation(wrapper, n_worker=self.batch_size) as proc:
+            batch_num = (self.max_iterations + self.batch_size - 1) // self.batch_size
+            if self.batch_size > self.config_advisor.init_num:
+                batch_num += 1  # fix bug
+            batch_id = 0
+            while batch_id < batch_num:
+                configs = self.config_advisor.get_suggestions()
+                params = [(self.objective_function, config, self.time_limit_per_trial) for config in configs]
+                # Wait all workers to complete their corresponding jobs.
+                results = proc.parallel_execute(params)
+                # Report their results.
+                for idx, (_config, _result) in enumerate(zip(configs, results)):
+                    if _result[-1] is None:
+                        _perf = MAXINT
+                    elif isinstance(_result[-1], dict):
+                        _perf = _result[-1]['objective_value']
+                    else:
+                        _perf = _result[-1]
+                    _observation = [_config, _perf, SUCCESS]
+                    self.config_advisor.update_observation(_observation)
+                    self.logger.info('In the %d-th batch [%d], using config %s, result is: %.3f'
+                                     % (batch_id, idx, str(_config), _perf))
+                batch_id += 1
+
+    def run(self):
+        if self.parallel_strategy == 'async':
+            self.async_run()
+        else:
+            self.sync_run()
