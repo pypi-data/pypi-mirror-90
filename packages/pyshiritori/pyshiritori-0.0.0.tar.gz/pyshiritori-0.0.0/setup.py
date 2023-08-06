774333fb1bc4abaa09694d94e24a11caa7c5e69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:13 +0000

    Add package pyshiritori/0.0.0/pyshiritori-0.0.0.tar.gz

---
+++ packages/pyshiritori/pyshiritori-0.0.0.tar.gz/pyshiritori-0.0.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name = "pyshiritori",
+
+    version = "0.0.0",
+
+    license = "GPLv3",
+
+    install_requires = ["pykakasi>=2.0.0"],
+
+    author = "Linuxmetel & Lithops",
+    author_email = "linuxmetel@yandex.com",
+
+    url = "https://github.com/linuxmetel/pyshiritori",
+
+    description = 'Japanese shiritori library',
+    long_description = "# pyshiritori",
+    keywords='Japanese shiritori',
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
