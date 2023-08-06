735fcf9ecc56ee1b425efe621777ce91ef755abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:18 +0000

    Add package masonite-billing/3.0.2/masonite-billing-3.0.2.tar.gz

---
+++ packages/masonite-billing/masonite-billing-3.0.2.tar.gz/masonite-billing-3.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="masonite-billing",
+    version='3.0.2',
+    packages=[
+        'billing',
+        'billing.commands',
+        'billing.contracts',
+        'billing.controllers',
+        'billing.drivers',
+        'billing.factories',
+        'billing.models',
+        'billing.snippets',
+    ],
+    install_requires=[
+        'masonite>=2.2',
+        'cleo',
+        'stripe==2.40.0',
+    ],
+    include_package_data=True,
+)
