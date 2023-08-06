8b291cb5b761e86fa560034329b36a4e9582311f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:22 +0000

    Add package nmapthon/1.7.0/nmapthon-1.7.0.tar.gz

---
+++ packages/nmapthon/nmapthon-1.7.0.tar.gz/nmapthon-1.7.0/README.md
@@ -0,0 +1,12 @@
+# Nmapthon
+A complete, high level Nmap module for Python.  
+&nbsp;
+  
+## Installation  
+Install Nmapthon for Python3 with
+```bash
+pip3 install nmapthon
+```
+
+## Documentation
+Feel free to visit [ReadTheDocs](https://nmapthon.readthedocs.io/en/latest/index.html)
---
+++ packages/nmapthon/nmapthon-1.7.0.tar.gz/nmapthon-1.7.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    setup(
+        name='nmapthon',
+        version='1.7.0',
+        packages=['nmapthon'],
+        url='https://github.com/cblopez/nmapthon',
+        license='GLPv3',
+        author='cblopez',
+        author_email='cbarrallopez@gmail.com',
+        description='A high level Nmap module for Python',
+        long_description=f.read(),
+        long_description_content_type='text/markdown',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 3 :: Only',
+            'Topic :: System :: Networking',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+        keywords=['python', 'python3', 'nmap', 'module', 'scan', 'nse', 'port', 'service']
+    )
