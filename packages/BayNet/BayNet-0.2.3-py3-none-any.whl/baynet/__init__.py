4ebdf38e4b8d99b985e98c223557da314950815a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package BayNet/0.2.3/BayNet-0.2.3-py3-none-any.whl

---
+++ packages/BayNet/BayNet-0.2.3-py3-none-any.whl/baynet/__init__.py
@@ -0,0 +1,20 @@
+"""Python Bayesian Network library."""
+import warnings
+from typing import Any
+from baynet.structure import DAG
+
+__version__ = "0.2.3"
+
+
+class Graph(DAG):
+    """Temporary thin wrapper to preserve old API."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialise Graph object with warning."""
+        warnings.warn(
+            DeprecationWarning(
+                "baynet.Graph has been renamed baynet.DAG, "
+                "Graph will be removed in a future release."
+            )
+        )
+        super().__init__(*args, **kwargs)
