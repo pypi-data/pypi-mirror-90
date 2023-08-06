5267e44eb61c3060282b6c395a0e21d098591a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:32 +0000

    Add package nemcore/0.1.8/NEMCore-0.1.8-py3-none-any.whl

---
+++ packages/nemcore/NEMCore-0.1.8-py3-none-any.whl/nemcore/cache.py
@@ -0,0 +1,48 @@
+import pickle
+import time
+
+from cachetools import Cache, TTLCache
+
+
+class TTLCacheP(TTLCache):
+    """ 基于 cachetools.TTLCache 实现的自动持久化 TTLCache
+
+    持久化采用pickle实现。
+    """
+
+    def __init__(self,
+                 maxsize,
+                 ttl,
+                 filepath,
+                 timer=time.monotonic,
+                 getsizeof=None):
+        super().__init__(maxsize, ttl, timer=timer, getsizeof=getsizeof)
+        self.filepath = filepath
+
+    def save(self):
+        with open(self.filepath, 'w+b') as f:
+            pickle.dump(self, f)
+
+    def load(self):
+        with open(self.filepath, 'rb') as f:
+            return pickle.load(f)
+
+    def __setitem__(self, key, value, cache_setitem=Cache.__setitem__):
+        ret = super().__setitem__(key, value, cache_setitem=cache_setitem)
+        self.save()
+        return ret
+
+    def __delitem__(self, key, cache_delitem=Cache.__delitem__):
+        ret = super().__delitem__(key, cache_delitem=cache_delitem)
+        self.save()
+        return ret
+
+    def clear(self):
+        super().clear()
+        self.save()
+
+
+def cache_key(*args, **kwargs) -> bytes:
+    """ 为了兼容 dict 参数实现的基于pickle的通用参数hash。
+    """
+    return pickle.dumps({'args': args, 'kwargs': kwargs})
