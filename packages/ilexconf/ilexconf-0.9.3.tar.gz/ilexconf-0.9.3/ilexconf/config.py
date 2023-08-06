78d884be12d3e924b0f2d3a2fbdfd6794893bba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:48 +0000

    Add package ilexconf/0.9.3/ilexconf-0.9.3.tar.gz

---
+++ packages/ilexconf/ilexconf-0.9.3.tar.gz/ilexconf-0.9.3/ilexconf/config.py
@@ -0,0 +1,23 @@
+from mapz import Mapz
+
+from typing import (
+    Any,
+    Dict,
+    Hashable,
+    Mapping,
+)
+
+class Config(Mapz):
+    def __init__(self, *mappings: Mapping[Hashable, Any], **kwargs: Dict):
+        super().__init__(*mappings)
+
+        for k, v in kwargs.items():
+            self.set(
+                k,
+                v,
+                key_prefix="",
+                key_sep="_",
+            )
+
+    def __repr__(self) -> str:
+        return f"Config{dict.__repr__(self)}"
