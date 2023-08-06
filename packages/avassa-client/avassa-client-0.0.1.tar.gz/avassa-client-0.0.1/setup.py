937bc9fb1aab474b802e55ea60e80168099522db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:43 +0000

    Add package avassa-client/0.0.1/avassa-client-0.0.1.tar.gz

---
+++ packages/avassa-client/avassa-client-0.0.1.tar.gz/avassa-client-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="avassa-client",
+    version="0.0.1",
+    author="Fredrik Jansson",
+    author_email="fj@avassa.io",
+    description="More to come",
+    license="Apache-2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://avassa.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
