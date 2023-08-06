80527300bbded9c423756df133afd7ffd76064f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:15 +0000

    Add package intelab-python-sdk/0.6.8/intelab_python_sdk-0.6.8-py2.py3-none-any.whl

---
+++ packages/intelab-python-sdk/intelab_python_sdk-0.6.8-py2.py3-none-any.whl/intelab_python_sdk/ffmpeg/ffmpeg_capture.py
@@ -0,0 +1,32 @@
+from . import run_shell
+
+
+def capture(stream_url, output_jpg, timeout=10, type='jpg'):
+    """
+    :param stream_url:
+    :param output_jpg:
+    :param timeout: 连接超时时间，单位s
+    :param type: 图片类型，jpg、png
+    """
+
+    if type == 'jpg':
+        _f = '-f mjpeg'
+    else:
+        _f = '-f image2'
+    shell_cmd = (
+        'ffmpeg '
+        '-y -rw_timeout {} '  # 设置超时时间10s
+        '-i {} '
+        '-ss 0 '
+        '{} '
+        '-vframes 1 '
+        '{}'
+    ).format(timeout * 1000 * 1000, stream_url, _f, output_jpg)
+
+    # 尝试三次获取截图
+    for _ in range(3):
+        error_log = run_shell(shell_cmd, name='ffmpeg_capture')
+        if not error_log:
+            break
+
+    return output_jpg
