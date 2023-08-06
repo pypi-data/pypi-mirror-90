106bac03948bdd647efc2d7438f0b3b74fd24797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package unicode/2.8/unicode-2.8.tar.gz

---
+++ packages/unicode/unicode-2.8.tar.gz/unicode-2.8/setup.py
@@ -0,0 +1,39 @@
+import io
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+
+setup(name='unicode',
+      version='2.8',
+      scripts=['unicode', 'paracode'],
+#      entry_points={'console_scripts': [
+#          'unicode = unicode:main',
+#          'paracode = paracode:main']},
+      description="Display unicode character properties",
+      long_description="""
+Display unicode character properties:
+Enter regular expression, hexadecimal number or some characters as an
+argument. unicode will try to guess what you want to look up.
+Use four-digit hexadecimal number followed by two dots to display
+given unicode block in a nice tabular format.
+""",
+      author="Radovan Garabik",
+      author_email='radovan.garabik@kassiopeia.juls.savba.sk',
+      url='http://kassiopeia.juls.savba.sk/~garabik/software/unicode.html',
+      license='GNU GPL v3',
+      keywords=['unicode', 'character properties', 'encoding'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Editors :: Text Processing',
+          'Topic :: Utilities'])
+
