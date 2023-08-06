2acaff514104e23d4baa70c4143b58542ecc4699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:34 +0000

    Add package distributions-tutorial/0.1/distributions-tutorial-0.1.tar.gz

---
+++ packages/distributions-tutorial/distributions-tutorial-0.1.tar.gz/distributions-tutorial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-tutorial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-tutorial'],
+      zip_safe=False)
