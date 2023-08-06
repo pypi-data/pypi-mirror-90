96a72dadbbbedb46b0edf09212794774e2d52dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:20 +0000

    Add package pdcopyist-updater/0.1.1/pdcopyist_updater-0.1.1.tar.gz

---
+++ packages/pdcopyist-updater/pdcopyist_updater-0.1.1.tar.gz/pdcopyist_updater-0.1.1/pdcopyist_updater/main.py
@@ -0,0 +1,38 @@
+
+
+import sys
+import os
+from .Versions import VersionManager
+
+
+# def find_arg(args, flag):
+#     try:
+#         idx = args.index(flag)
+#         find = args[idx+1]
+#         return find
+#     except ValueError:
+#         pass
+
+#     return None
+
+
+def main():
+    vm = VersionManager()
+    if vm.has_updated():
+        print(f'发现新版本，开始更新')
+        vm.start_update()
+        print(f'更新完成()')
+
+    # args = sys.argv
+
+    # dir = find_arg(args, '-dir')
+    # if dir:
+    #     os.system(f'pdcoder -dir {dir}')
+
+    os.system(VersionManager.m_pkg_name)
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    pass
