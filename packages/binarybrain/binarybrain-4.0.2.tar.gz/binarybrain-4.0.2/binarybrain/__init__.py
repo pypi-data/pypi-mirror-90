5c7986df5e4827e09bfc0831a051fcf4700a6254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:10 +0000

    Add package binarybrain/4.0.2/binarybrain-4.0.2.tar.gz

---
+++ packages/binarybrain/binarybrain-4.0.2.tar.gz/binarybrain-4.0.2/binarybrain/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+
+from binarybrain.system       import *
+
+from binarybrain.dtype        import *
+
+from binarybrain.tensor       import *
+from binarybrain.frame_buffer import *
+from binarybrain.variables    import *
+
+from binarybrain.models       import *
+
+from binarybrain.losses       import *
+from binarybrain.metrics      import *
+from binarybrain.optimizer    import *
+
+from binarybrain.storage      import *
+from binarybrain.verilog      import *
+
---
+++ packages/binarybrain/binarybrain-4.0.2.tar.gz/binarybrain-4.0.2/binarybrain/include/bb/Activation.h
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------
+//  Binary Brain  -- binary neural net framework
+//
+//                                     Copyright (C) 2018 by Ryuji Fuchikami
+//                                     https://github.com/ryuz
+//                                     ryuji.fuchikami@nifty.com
+// --------------------------------------------------------------------------
+
+
+
+#pragma once
+
+
+#include "bb/Manager.h"
+#include "bb/Model.h"
+
+
+namespace bb {
+
+
+// Activation
+class Activation : public Model
+{
+protected:
+    indices_t   m_shape;    //< 入出力の形状 
+
+public:
+    /**
+     * @brief  入力形状設定
+     * @detail 入力形状を設定する
+     *         内部変数を初期化し、以降、GetOutputShape()で値取得可能となることとする
+     *         同一形状を指定しても内部変数は初期化されるものとする
+     * @param  shape      1フレームのノードを構成するshape
+     * @return 出力形状を返す
+     */
+    indices_t SetInputShape(indices_t shape)
+    {
+        // 設定済みなら何もしない
+        if ( shape == this->GetInputShape() ) {
+            return this->GetOutputShape();
+        }
+
+        m_shape = shape;
+        return m_shape;
+    }
+
+    /**
+     * @brief  入力形状取得
+     * @detail 入力形状を取得する
+     * @return 入力形状を返す
+     */
+    indices_t GetInputShape(void) const
+    {
+        return m_shape;
+    }
+
+    /**
+     * @brief  出力形状取得
+     * @detail 出力形状を取得する
+     * @return 出力形状を返す
+     */
+    indices_t GetOutputShape(void) const
+    {
+        return m_shape;
+    }
+};
+
+
+};
+
