5bb850cdc848e4a7d24fd19d24464fe665e6a9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package django-pydantic/0.0.1/django-pydantic-0.0.1.tar.gz

---
+++ packages/django-pydantic/django-pydantic-0.0.1.tar.gz/django-pydantic-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='django-pydantic',
+      version='0.0.1',
+      description='django-pydantic',
+      author='Eachin',
+      author_email='EachinChung@gmail.com',
+      license='MIT',
+      packages=['django-pydantic'])
