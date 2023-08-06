849718daaaa7d708935689ed6b299c7aa41a86a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:30 +0000

    Add package quiltz-domain/0.4.0/quiltz-domain-0.4.0.tar.gz

---
+++ packages/quiltz-domain/quiltz-domain-0.4.0.tar.gz/quiltz-domain-0.4.0/src/quiltz/domain/parsers.py
@@ -0,0 +1,24 @@
+from datetime import date
+from .results import Success, Failure
+
+
+class StringToDateParser():
+    def parse_from(self, date_string, success_attribute='date'):
+        if not date_string:
+            return Failure(message='date is missing')
+        try:
+            return Success(**{success_attribute: date.fromisoformat(date_string)})
+        except ValueError as e:
+            return Failure(message=str(e))
+
+class StringToIntParser:
+    def parse_from(self, int_string, success_attribute='int_val'):
+        try:
+            return Success(**{success_attribute: int(int_string)})
+        except ValueError as e:
+            return Failure(message="'{}' is not a valid integer".format(int_string))
+
+date_from_iso = StringToDateParser()
+
+int_from_string = StringToIntParser()
+
