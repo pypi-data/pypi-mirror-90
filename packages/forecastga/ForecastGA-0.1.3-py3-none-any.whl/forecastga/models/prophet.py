be2e22df9cbdf1a3b87aa821818f8cece9ff28e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:41 +0000

    Add package forecastga/0.1.3/ForecastGA-0.1.3-py3-none-any.whl

---
+++ packages/forecastga/ForecastGA-0.1.3-py3-none-any.whl/forecastga/models/prophet.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# coding: utf-8
+#
+
+"""ForecastGA: Prophet Model"""
+
+import pandas as pd
+from fbprophet import Prophet
+
+from forecastga.models.base import BaseModel
+
+
+class Prophet_Model(BaseModel):
+    """Prophet Model Class"""
+
+    def __init__(self, config):
+        super().__init__(config)
+
+    def train(self, **kwargs):
+
+        if self.freq == "D":
+            ptm = Prophet(daily_seasonality=True)
+        else:
+            ptm = Prophet()
+
+        self.model = ptm.fit(self.format_input(self.train_df))
+
+    def forecast(self):
+        future = self.model.make_future_dataframe(
+            periods=self.forecast_len, freq=self.freq
+        )
+        future_pred = self.model.predict(future)
+        self.prediction = self.format_output(future_pred)[-self.forecast_len :]
+
+    @staticmethod
+    def format_input(df):
+        df_pr = df.reset_index()
+        df_pr.columns = ["ds", "y"]
+        return df_pr
+
+    @staticmethod
+    def format_output(df):
+        prophet_pred = pd.DataFrame({"Date": df["ds"], "Target": df["yhat"]})
+        prophet_pred = prophet_pred.set_index("Date")
+        return prophet_pred["Target"].values
