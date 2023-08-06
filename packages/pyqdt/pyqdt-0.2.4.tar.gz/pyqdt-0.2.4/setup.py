4d91feaaf833889fc26d0ece9371a87af74c28cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:53 +0000

    Add package pyqdt/0.2.4/pyqdt-0.2.4.tar.gz

---
+++ packages/pyqdt/pyqdt-0.2.4.tar.gz/pyqdt-0.2.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pyqdt',
+    version='0.2.4',
+    packages=['pyqdt', 'pyqdt/technical_analysis'],
+    install_requires=['pandas'],
+    author='John Lui',
+    author_email='johnlui@live.cn',
+    license='LGPLv3',
+    url='https://pyqdt.itsop.cc'
+)
