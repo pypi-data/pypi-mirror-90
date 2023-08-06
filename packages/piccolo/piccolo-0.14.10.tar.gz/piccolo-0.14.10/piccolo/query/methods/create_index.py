843bc66149fa2b8627dae55b8d770227b66d4b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package piccolo/0.14.10/piccolo-0.14.10.tar.gz

---
+++ packages/piccolo/piccolo-0.14.10.tar.gz/piccolo-0.14.10/piccolo/query/methods/create_index.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+from enum import Enum
+import typing as t
+
+from piccolo.columns import Column
+from piccolo.query.base import Query
+from piccolo.querystring import QueryString
+
+if t.TYPE_CHECKING:  # pragma: no cover
+    from piccolo.table import Table
+
+
+class IndexMethod(str, Enum):
+    btree = "btree"
+    hash = "hash"
+    gist = "gist"
+    gin = "gin"
+
+
+class CreateIndex(Query):
+    def __init__(
+        self,
+        table: t.Type[Table],
+        columns: t.List[t.Union[Column, str]],
+        method: IndexMethod = IndexMethod.btree,
+    ):
+        self.columns = columns
+        self.method = method
+        super().__init__(table)
+
+    @property
+    def column_names(self) -> t.List[str]:
+        return [
+            i._meta.name if isinstance(i, Column) else i for i in self.columns
+        ]
+
+    @property
+    def postgres_querystrings(self) -> t.Sequence[QueryString]:
+        column_names = self.column_names
+        index_name = self.table._get_index_name(column_names)
+        tablename = self.table._meta.tablename
+        method_name = self.method.value
+        column_names_str = ", ".join(column_names)
+        return [
+            QueryString(
+                f"CREATE INDEX {index_name} ON {tablename} USING {method_name}"
+                f" ({column_names_str})"
+            )
+        ]
+
+    @property
+    def sqlite_querystrings(self) -> t.Sequence[QueryString]:
+        column_names = self.column_names
+        index_name = self.table._get_index_name(column_names)
+        tablename = self.table._meta.tablename
+
+        method_name = self.method.value
+        if method_name != "btree":
+            raise ValueError("SQLite only support btree indexes.")
+
+        column_names_str = ", ".join(column_names)
+        return [
+            QueryString(
+                f"CREATE INDEX {index_name} ON {tablename} "
+                f"({column_names_str})"
+            )
+        ]
---
+++ packages/piccolo/piccolo-0.14.10.tar.gz/piccolo-0.14.10/tests/table/test_create.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from piccolo.table import Table
+from piccolo.columns import Varchar
+
+from ..example_app.tables import Manager
+
+
+class TestCreate(TestCase):
+    def test_create_table(self):
+        Manager.create_table().run_sync()
+        self.assertTrue(Manager.table_exists().run_sync())
+
+        Manager.alter().drop_table().run_sync()
+
+
+class BandMember(Table):
+    name = Varchar(length=50, index=True)
+
+
+class TestCreateWithIndexes(TestCase):
+    def setUp(self):
+        BandMember.create_table().run_sync()
+
+    def tearDown(self):
+        BandMember.alter().drop_table().run_sync()
+
+    def test_create_table_with_indexes(self):
+        index_names = BandMember.indexes().run_sync()
+        index_name = BandMember._get_index_name(["name"])
+        self.assertTrue(index_name in index_names)
