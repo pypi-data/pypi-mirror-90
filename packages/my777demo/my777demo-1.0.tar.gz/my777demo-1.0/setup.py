9641b469beb6203074be9561dcf27a880a46058a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package my777demo/1.0/my777demo-1.0.tar.gz

---
+++ packages/my777demo/my777demo-1.0.tar.gz/my777demo-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'my777demo',
+    version='1.0',
+    description = 'this is a test',
+    author ='JFU',
+    author_email ='test@qq.com',
+    url = 'https://www.test.com',
+    packages=['demo']
+
+)
