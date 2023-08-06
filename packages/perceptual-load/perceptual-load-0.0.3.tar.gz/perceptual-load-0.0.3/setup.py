05b81472be58db005d1460507e3977165cf3e502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:28 +0000

    Add package perceptual-load/0.0.3/perceptual-load-0.0.3.tar.gz

---
+++ packages/perceptual-load/perceptual-load-0.0.3.tar.gz/perceptual-load-0.0.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="perceptual-load", # Replace with your own username
+    version="0.0.3",
+    author="Rob Blumberg",
+    description="Perceptual Load Experiment",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
