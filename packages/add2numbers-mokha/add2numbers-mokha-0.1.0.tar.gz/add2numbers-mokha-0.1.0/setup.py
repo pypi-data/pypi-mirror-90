f5a9dbe7bcd4d5b2d65c992ee40f051d2c6b4408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:46 +0000

    Add package add2numbers-mokha/0.1.0/add2numbers-mokha-0.1.0.tar.gz

---
+++ packages/add2numbers-mokha/add2numbers-mokha-0.1.0.tar.gz/add2numbers-mokha-0.1.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+     name='add2numbers-mokha',    # This is the name of your PyPI-package.
+     version='0.1.0',                          # Update the version number for new releases
+     scripts=['add']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
