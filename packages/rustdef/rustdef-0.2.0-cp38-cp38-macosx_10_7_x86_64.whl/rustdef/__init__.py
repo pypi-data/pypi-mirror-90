72357aea38f02324f1f4427ec0301b84a0d3649d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:01 +0000

    Add package rustdef/0.2.0/rustdef-0.2.0-cp38-cp38-macosx_10_7_x86_64.whl

---
+++ packages/rustdef/rustdef-0.2.0-cp38-cp38-macosx_10_7_x86_64.whl/rustdef/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.2.0"
+__develop__ = True
+
+from .magic import RustdefMagic
+
+
+def load_ipython_extension(ipython):
+    print("load rustdef")
+
+    rust_magic = RustdefMagic(ipython)
+    ipython.register_magic_function(
+        rust_magic.invoke, magic_kind="line_cell", magic_name="rustdef"
+    )
+
+
+def unload_ipython_extension(ipython):
+    print("unload rustdef")
