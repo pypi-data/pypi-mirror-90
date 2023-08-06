e3110aab0d82ff8ea4b9566ecbb8e93d57c7c96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:04 +0000

    Add package awesome-pattern-matching/0.4.1/awesome-pattern-matching-0.4.1.tar.gz

---
+++ packages/awesome-pattern-matching/awesome-pattern-matching-0.4.1.tar.gz/awesome-pattern-matching-0.4.1/apm/__init__.py
@@ -0,0 +1,29 @@
+from .core import \
+    AllOf, \
+    Capture, \
+    Either, \
+    MatchContext, \
+    MatchResult, \
+    Not, \
+    OneOf, \
+    Pattern, \
+    Remaining, \
+    Strict, \
+    match
+
+from .patterns import \
+    Arguments, \
+    At, \
+    Between, \
+    Check, \
+    Contains, \
+    Each, \
+    EachItem, \
+    InstanceOf, \
+    Length, \
+    Regex, \
+    Returns, \
+    Transformed, \
+    Truish
+
+from .__pkginfo__ import __version__
---
+++ packages/awesome-pattern-matching/awesome-pattern-matching-0.4.1.tar.gz/awesome-pattern-matching-0.4.1/tests/test_util.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import unittest
+
+# noinspection PyProtectedMember
+import apm._util as util
+from apm import *
+
+
+class TypingUtil(unittest.TestCase):
+
+    def test_get_arg_types(self):
+        # noinspection PyUnusedLocal
+        def f(a: int, b: float, c: str, d: str) -> str:
+            pass
+
+        arg_types = util.get_arg_types(f)
+        self.assertTrue(match(
+            arg_types,
+            [int, float, Remaining(str)]
+        ))
