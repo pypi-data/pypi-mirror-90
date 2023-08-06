0d3fb4ee7327c7a9120528649aa1740d22c235af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package name1/0.0.3/name1-0.0.3.tar.gz

---
+++ packages/name1/name1-0.0.3.tar.gz/name1-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="name1",
+    version="0.0.3",
+    author="Ja",
+    author_email="Jainamoswal@gmail.com",
+    description="Developed by Developers, Contributed by Contributers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gi.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
