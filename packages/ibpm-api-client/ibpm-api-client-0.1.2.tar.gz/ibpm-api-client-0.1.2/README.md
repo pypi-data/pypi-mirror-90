b201e9eb13d2241d15bd2876d98aab940b17a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:28 +0000

    Add package ibpm-api-client/0.1.2/ibpm-api-client-0.1.2.tar.gz

---
+++ packages/ibpm-api-client/ibpm-api-client-0.1.2.tar.gz/ibpm-api-client-0.1.2/README.md
@@ -0,0 +1,6 @@
+##API CLIENT FOR BPM/IBPM##
+
+
+
+
+[![Build Status](https://dev.azure.com/ibimec/IBPM/_apis/build/status/ibpm-api-client?branchName=master)](https://dev.azure.com/ibimec/IBPM/_build/latest?definitionId=15&branchName=master)
---
+++ packages/ibpm-api-client/ibpm-api-client-0.1.2.tar.gz/ibpm-api-client-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="ibpm-api-client",
+    version="0.1.2",
+    description="ibpm web api client",
+    url="https://github.com/ibimec/ibpm-api-client",
+    author="rgallini",
+    author_email="riccardo.gallini@gmail.com",
+    license="MIT",
+    packages=["ibpm"],
+    include_package_data=True,
+    install_requires=["requests", "jsons", "pillow"],
+)
