ee9137de31c7c6f89b192e831513ef5cbc9f9e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:31 +0000

    Add package jem-demoo/1.0.3/jem-demoo-1.0.3.tar.gz

---
+++ packages/jem-demoo/jem-demoo-1.0.3.tar.gz/jem-demoo-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='jem-demoo',
+    version='1.0.3',
+    description='this pkg for compress the image',
+    long_description='this is ontly for testing.',
+    author="Jem's",
+    license="MIT",
+    packages=['Jemish'],
+    include_package_data=True,
+    install_requires=[''],
+    entry_points={
+        "console_scripts": [
+            "jem-demoo=Jemish.main:main",
+        ]
+    },
+)
